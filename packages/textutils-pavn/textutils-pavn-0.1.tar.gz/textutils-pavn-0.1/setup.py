ddafc5f2cff41bf3e789367dd83055258c1b5809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:07:13 +0000

    Add package textutils-pavn/0.1/textutils-pavn-0.1.tar.gz

---
+++ packages/textutils-pavn/textutils-pavn-0.1.tar.gz/textutils-pavn-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='textutils-pavn',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Pavan Aditya Kumar Gorrela',
+    author_email='kumarpavanaditya@gmail.com',
+    description='A simple text utility package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Pavan-Aditya-Kumar-Gorrela/textutils',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
