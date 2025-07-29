cedf8ee09be0c1431413c733aae183e9974d3e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:32 +0000

    Add package GoodByeSyntax/1.6/goodbyesyntax-1.6.tar.gz

---
+++ packages/GoodByeSyntax/goodbyesyntax-1.6.tar.gz/goodbyesyntax-1.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GoodByeSyntax',
+    version='1.6',
+    packages=find_packages(),
+    install_requires=[],
+    author='GoodByeSyntax',
+    author_email='goodbyesyntax.official@gmail.com',
+    description='GoodByeSyntax Python Module',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/GoodByeSyntax/GBS',
+    classifiers=[  
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+    ],
+)
