d97cced06384423edcf64db3492f1e8be3adcd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:35 +0000

    Add package flexprice/1.0.17/flexprice-1.0.17.tar.gz

---
+++ packages/flexprice/flexprice-1.0.17.tar.gz/flexprice-1.0.17/flexprice/api/__init__.py
@@ -0,0 +1,21 @@
+# flake8: noqa
+
+# import apis into api package
+from flexprice.api.auth_api import AuthApi
+from flexprice.api.customers_api import CustomersApi
+from flexprice.api.entitlements_api import EntitlementsApi
+from flexprice.api.environments_api import EnvironmentsApi
+from flexprice.api.events_api import EventsApi
+from flexprice.api.features_api import FeaturesApi
+from flexprice.api.integrations_api import IntegrationsApi
+from flexprice.api.invoices_api import InvoicesApi
+from flexprice.api.payments_api import PaymentsApi
+from flexprice.api.plans_api import PlansApi
+from flexprice.api.prices_api import PricesApi
+from flexprice.api.subscriptions_api import SubscriptionsApi
+from flexprice.api.tasks_api import TasksApi
+from flexprice.api.tenants_api import TenantsApi
+from flexprice.api.users_api import UsersApi
+from flexprice.api.wallets_api import WalletsApi
+from flexprice.api.secrets_api import SecretsApi
+
