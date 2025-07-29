f475b5ef2d74efcc3456ee229103ad7b35a98258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:46 +0000

    Add package doover-cli/0.1.0/doover_cli-0.1.0-py3-none-any.whl

---
+++ packages/doover-cli/doover_cli-0.1.0-py3-none-any.whl/doover_cli/utils/context.py
@@ -0,0 +1,15 @@
+from typing import TYPE_CHECKING
+
+import typer
+
+if TYPE_CHECKING:
+    from pydoover.cloud.api import Client
+    from .config import ConfigManager
+
+
+class Context(typer.Context):
+    api: "Client"
+    config_manager: "ConfigManager"
+    agent_query: str
+    agent_id: str
+    agent: str
