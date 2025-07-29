f633bd40e7ed94eeb4e1aa8cb50dc1ca4f26ecbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:16 +0000

    Add package sportsfeatures/0.0.64/sportsfeatures-0.0.64.tar.gz

---
+++ packages/sportsfeatures/sportsfeatures-0.0.64.tar.gz/sportsfeatures-0.0.64/sportsfeatures/remove_process.py
@@ -0,0 +1,21 @@
+"""The process for removing lookahead features."""
+
+import pandas as pd
+
+from .identifier import Identifier
+
+
+def remove_process(df: pd.DataFrame, identifiers: list[Identifier]) -> pd.DataFrame:
+    """Remove the features from the dataframe."""
+    drop_columns: set[str] = set()
+    for identifier in identifiers:
+        for feature_col in identifier.feature_columns:
+            drop_columns.add(feature_col)
+        for bet in identifier.bets:
+            drop_columns.add(bet.odds_column)
+            drop_columns.add(bet.bookie_id_column)
+            if bet.dt_column is not None:
+                drop_columns.add(bet.dt_column)
+            drop_columns.add(bet.bookie_name_column)
+            drop_columns.add(bet.canonical_column)
+    return df.drop(columns=list(drop_columns), errors="ignore").copy()
