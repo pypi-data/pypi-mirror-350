1b200d3cd63e801820deda1fcff7071bd2fa6cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:53 +0000

    Add package ms9/0.14.5/ms9-0.14.5-cp313-none-manylinux2014_x86_64.whl

---
+++ packages/ms9/ms9-0.14.5-cp313-none-manylinux2014_x86_64.whl/ms9/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Author: MaJian
+@Time: 2025/5/23 14:33
+@SoftWare: PyCharm
+@Project: mortal
+@File: __init__.py.py
+"""
+from .main import auth_code
+from .main import auth_build
