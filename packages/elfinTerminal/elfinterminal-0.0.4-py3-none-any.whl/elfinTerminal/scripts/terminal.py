8aff6d6a9bb055c50f656a16b48c26372c2244c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:26 +0000

    Add package elfinTerminal/0.0.4/elfinterminal-0.0.4-py3-none-any.whl

---
+++ packages/elfinTerminal/elfinterminal-0.0.4-py3-none-any.whl/elfinTerminal/scripts/terminal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @author: firstelfin
+# @time: 2025/05/22 21:51:33
+
+from elfinTerminal.scripts.config import set_args
+from elfinTerminal.setUp.pipSetUp import set_pip_source
+from elfinTerminal.setUp.pypiSetUp import set_pypi_token
+from elfinTerminal.setUp.vimSetUp import set_vim_config
+
+
+def elfin_terminal():
+    args = set_args()
+    if args.mode == "pip":
+        set_pip_source()
+    elif args.mode == "pypi":
+        if args.config == "token":
+            set_pypi_token()
+    elif args.mode == "vim":
+        set_vim_config()
+    else:
+        print("Invalid sub-command mode")
+
+
+if __name__ == '__main__':
+    elfin_terminal()
