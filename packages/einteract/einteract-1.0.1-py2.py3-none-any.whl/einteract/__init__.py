a6d1f0eb8cb145cc9326696b84c6434386d5ceac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:57 +0000

    Add package einteract/1.0.1/einteract-1.0.1-py2.py3-none-any.whl

---
+++ packages/einteract/einteract-1.0.1-py2.py3-none-any.whl/einteract/__init__.py
@@ -0,0 +1,19 @@
+"""
+An enhance version of ipywidgets' interactive. 
+It provides additional widgets like ListWidget and AnimationSlider
+"""
+
+__all__ = [
+    'InteractBase','callback', 'interactive','interact', 'patched_plotly', 'plt2html',
+    'ListWidget', 'AnimationSlider', 'Output'
+]
+
+from ipyslides.interaction import (
+    InteractBase, 
+    callback, 
+    interact, 
+    interactive,
+    patched_plotly)
+from ipyslides._base._widgets import ListWidget, AnimationSlider
+from ipyslides._base.widgets import Output # patched one
+from ipyslides.formatters import plt2html
