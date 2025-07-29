bbfc5f98769339acc685e133d33d1344643a3bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:42 +0000

    Add package icclogger/1.0/icclogger-1.0-py3-none-any.whl

---
+++ packages/icclogger/icclogger-1.0-py3-none-any.whl/icclogger/colors.py
@@ -0,0 +1,7 @@
+class ConsoleColors:
+    R = '\033[31m'  # red
+    G = '\033[32m'  # green
+    C = '\033[36m'  # cyan
+    W = '\033[0m'   # white
+    Y = '\033[33m'  # yellow
+    P = '\033[35m'  # purple
