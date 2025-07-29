9f6abfc79626d81c7eeb02013f943371d2ae95a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:28 +0000

    Add package TISControlProtocol/0.7.26/tiscontrolprotocol-0.7.26.tar.gz

---
+++ packages/TISControlProtocol/tiscontrolprotocol-0.7.26.tar.gz/tiscontrolprotocol-0.7.26/src/TISControlProtocol/Protocols/udp/PacketHandlers/EnergyFeedbackHandler.py
@@ -0,0 +1,29 @@
+from homeassistant.core import HomeAssistant
+import logging
+
+#TODO get a way to set 4 sensors together
+async def handle_energy_feedback(hass: HomeAssistant, info: dict):
+    """
+    Handle the feedback from an energy sensor.
+    """
+    device_id = info["device_id"]
+    channel_num = int(info["additional_bytes"][0]) + 1
+    sub_operation = int(info["additional_bytes"][1])
+
+    if sub_operation == 0xDA:
+
+        energy = [int((info["additional_bytes"][12]<<8)|(info["additional_bytes"][13])),
+                  int((info["additional_bytes"][6]<<8)|(info["additional_bytes"][7]))]
+
+        event_data = {
+            "device_id": device_id,
+            "channel_num": channel_num,
+            "feedback_type": "energy_feedback",
+            "energy": energy,
+            "additional_bytes": info["additional_bytes"],
+        }
+
+        try:
+            hass.bus.async_fire(str(info["device_id"]), event_data)
+        except Exception as e:
+            logging.error(f"error in firing event for feedback: {e}")
