0181b55f10d363d7066ef0d903b923b68629fe70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:02 +0000

    Add package ArtusAPI/1.3.6/ArtusAPI-1.3.6.tar.gz

---
+++ packages/ArtusAPI/ArtusAPI-1.3.6.tar.gz/ArtusAPI-1.3.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ArtusAPI',
+    version='1.3.6',
+    description='API to control Artus Family of Robots by Sarcomere Dynamics Inc',
+    project_description='Python API to control Artus Family of Robots by Sarcomere Dynamics Inc',
+    # package_dir={'': '/'},  # tells setuptools that your packages are under src
+    packages=find_packages(exclude=['data*', 'examples*', 'tests*','venv*', 'urdf*', 'ros2*']),
+    author='Sarcomere Dynamics Inc.',
+    url='https://sarcomeredynamics.com/home',
+    install_requires=[
+        'psutil',
+        'pyserial',
+        'esptool',
+        'pyyaml',
+        'tqdm',
+        'requests'
+    ],
+    python_requires='>=3.10',
+    entry_points={
+        'console_scripts': [
+            'artusapiflash = ArtusAPI.artus_api:main_flash',
+            'artusapimain = ArtusAPI.artus_api:main'
+        ]
+    }
+    # packages=find_packages(where='/'),
+    # other setup configurations
+)   
