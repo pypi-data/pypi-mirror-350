6fcf133f5a45a81279007797c647a96a33b979c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:54 +0000

    Add package i3tools/0.2.0/i3tools-0.2.0-py3-none-any.whl

---
+++ packages/i3tools/i3tools-0.2.0-py3-none-any.whl/i3tools/_message.py
@@ -0,0 +1,19 @@
+import subprocess
+import json
+
+
+def i3msg(itype, imsg):
+    p = subprocess.Popen(
+        [
+            "/bin/sh",
+            "-c",
+            "i3-msg {0} {1}".format(
+                "" if not itype else '-t "{0}"'.format(itype),
+                "" if not imsg else '"{0}"'.format(imsg),
+            ),
+        ],
+        stdout=subprocess.PIPE,
+    )
+
+    output, error = p.communicate()
+    return json.loads(output)
