568e1fe88b243e7471d324745fbe3ac7ba837002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:59 +0000

    Add package options-library/0.1.0/options_library-0.1.0.tar.gz

---
+++ packages/options-library/options_library-0.1.0.tar.gz/options_library-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="options_library",
+    version="0.1.0",
+    author="Arka Roychowdhury",
+    description="A Python package for financial options strategies",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
