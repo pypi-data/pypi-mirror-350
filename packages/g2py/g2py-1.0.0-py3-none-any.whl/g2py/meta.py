f62d0287f899e7ba906d2aee579aa208c562ac61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:35 +0000

    Add package g2py/1.0.0/g2py-1.0.0-py3-none-any.whl

---
+++ packages/g2py/g2py-1.0.0-py3-none-any.whl/g2py/meta.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''
+> pkg meta information
+'''
+
+__version__ = "1.0.0"
+__author__ = "G2PY Development Team"
+__description__ = "G2PY is a Python wrapper for the G2 visualization library from Ant Group."
+__url__ = "https://github.com/g2py/g2py"   
