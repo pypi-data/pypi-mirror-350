2222bd2234019caa878b4805dd024ad7d1b34d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:02 +0000

    Add package uv/0.7.8/uv-0.7.8.tar.gz

---
+++ packages/uv/uv-0.7.8.tar.gz/uv-0.7.8/crates/uv-auth/src/lib.rs
@@ -0,0 +1,47 @@
+use std::sync::{Arc, LazyLock};
+
+use tracing::trace;
+use url::Url;
+
+use cache::CredentialsCache;
+pub use credentials::Credentials;
+pub use index::{AuthPolicy, Index, Indexes};
+pub use keyring::KeyringProvider;
+pub use middleware::AuthMiddleware;
+use realm::Realm;
+
+mod cache;
+mod credentials;
+mod index;
+mod keyring;
+mod middleware;
+mod realm;
+
+// TODO(zanieb): Consider passing a cache explicitly throughout
+
+/// Global authentication cache for a uv invocation
+///
+/// This is used to share credentials across uv clients.
+pub(crate) static CREDENTIALS_CACHE: LazyLock<CredentialsCache> =
+    LazyLock::new(CredentialsCache::default);
+
+/// Populate the global authentication store with credentials on a URL, if there are any.
+///
+/// Returns `true` if the store was updated.
+pub fn store_credentials_from_url(url: &Url) -> bool {
+    if let Some(credentials) = Credentials::from_url(url) {
+        trace!("Caching credentials for {url}");
+        CREDENTIALS_CACHE.insert(url, Arc::new(credentials));
+        true
+    } else {
+        false
+    }
+}
+
+/// Populate the global authentication store with credentials on a URL, if there are any.
+///
+/// Returns `true` if the store was updated.
+pub fn store_credentials(url: &Url, credentials: Arc<Credentials>) {
+    trace!("Caching credentials for {url}");
+    CREDENTIALS_CACHE.insert(url, credentials);
+}
---
+++ packages/uv/uv-0.7.8.tar.gz/uv-0.7.8/crates/uv-pypi-types/src/base_url.rs
@@ -0,0 +1,63 @@
+use serde::{Deserialize, Serialize};
+use url::Url;
+
+/// Join a relative URL to a base URL.
+pub fn base_url_join_relative(base: &str, relative: &str) -> Result<Url, JoinRelativeError> {
+    let base_url = Url::parse(base).map_err(|err| JoinRelativeError::ParseError {
+        original: base.to_string(),
+        source: err,
+    })?;
+
+    base_url
+        .join(relative)
+        .map_err(|err| JoinRelativeError::ParseError {
+            original: format!("{base}/{relative}"),
+            source: err,
+        })
+}
+
+/// An error that occurs when `base_url_join_relative` fails.
+///
+/// The error message includes the URL (`base` or `maybe_relative`) passed to
+/// `base_url_join_relative` that provoked the error.
+#[derive(Clone, Debug, thiserror::Error)]
+pub enum JoinRelativeError {
+    #[error("Failed to parse URL: `{original}`")]
+    ParseError {
+        original: String,
+        source: url::ParseError,
+    },
+}
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub struct BaseUrl(
+    #[serde(
+        serialize_with = "Url::serialize_internal",
+        deserialize_with = "Url::deserialize_internal"
+    )]
+    Url,
+);
+
+impl BaseUrl {
+    /// Return the underlying [`Url`].
+    pub fn as_url(&self) -> &Url {
+        &self.0
+    }
+
+    /// Return the underlying [`Url`] as a serialized string.
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
+impl From<Url> for BaseUrl {
+    fn from(url: Url) -> Self {
+        Self(url)
+    }
+}
+
+impl std::fmt::Display for BaseUrl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
