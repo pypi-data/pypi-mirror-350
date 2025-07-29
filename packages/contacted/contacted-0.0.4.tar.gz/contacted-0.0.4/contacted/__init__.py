f01d2c952665d9aa8c46da3983825c59e5a32c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:38 +0000

    Add package contacted/0.0.4/contacted-0.0.4.tar.gz

---
+++ packages/contacted/contacted-0.0.4.tar.gz/contacted-0.0.4/contacted/__init__.py
@@ -0,0 +1,13 @@
+"""
+ContactedAI Python SDK
+Official Python client for the ContactedAI API
+"""
+
+from .client import ContactedAI
+
+__version__ = "1.0.0"
+__author__ = "ContactedAI"
+__email__ = "support@contacted.io"
+
+# Main export
+__all__ = ["ContactedAI"]
