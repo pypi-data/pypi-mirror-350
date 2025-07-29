7817d371ff8d6886c73d35ca7d0b08819f4d43f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:06 +0000

    Add package whatxtract/0.1.3/whatxtract-0.1.3-py3-none-any.whl

---
+++ packages/whatxtract/whatxtract-0.1.3-py3-none-any.whl/whatxtract/__init__.py
@@ -0,0 +1,21 @@
+"""
+WhatXtract: WhatsApp data extraction and automation toolkit.
+
+WhatsApp Contacts Extractor and Number Checker via WhatsApp Web
+A WhatsApp data extraction and automation toolkit
+
+License: MIT
+Version: 0.1.3
+Author : Hasan Rasel
+Email  : rrss.mahmud@gmail.com
+"""
+
+# fmt: off
+__title__       = 'whatxtract'
+__description__ = 'A WhatsApp data extraction and automation toolkit'
+__version__     = '0.1.3'
+__author__      = 'Hasan Rasel'
+__email__       = 'rrss.mahmud@gmail.com'
+__license__     = 'MIT'
+__url__         = 'https://github.com/bitbytelab/whatxtract'
+__copyright__   = '2025 BitByteLab'
