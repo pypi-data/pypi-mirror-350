b7dde046644a8a88e69ca943ff2accf8e66470c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:10:18 +0000

    Add package fastrtc-whisper-cpp/0.1.1/fastrtc_whisper_cpp-0.1.1-py3-none-any.whl

---
+++ packages/fastrtc-whisper-cpp/fastrtc_whisper_cpp-0.1.1-py3-none-any.whl/fastrtc_whisper_cpp/__init__.py
@@ -0,0 +1,6 @@
+from .speech_to_text import STTModel, WhisperCppSTT, get_stt_model
+from .utils import load_audio
+
+__all__ = ["WhisperCppSTT", "STTModel", "get_stt_model", "load_audio"]
+
+__version__ = "0.1.1"
