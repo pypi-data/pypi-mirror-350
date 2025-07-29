ac44cd20353c38798f59e1b8558a76c978ef975d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:53 +0000

    Add package dycw-utilities/0.124.0/dycw_utilities-0.124.0-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.124.0-py3-none-any.whl/utilities/importlib.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from importlib import import_module
+from importlib.util import find_spec
+
+
+def is_valid_import(module: str, name: str) -> bool:
+    """Check if an import is valid."""
+    spec = find_spec(module)
+    if spec is None:
+        return False
+    mod = import_module(module)
+    return hasattr(mod, name)
+
+
+__all__ = ["is_valid_import"]
