73d339bbe9e5a3c4c158bef51408cac4d949cd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:27 +0000

    Add package custom-python-logger/1.0.9/custom_python_logger-1.0.9.tar.gz

---
+++ packages/custom-python-logger/custom_python_logger-1.0.9.tar.gz/custom_python_logger-1.0.9/custom_python_logger/__init__.py
@@ -0,0 +1,10 @@
+from custom_python_logger.logger import (
+    get_logger, CustomLoggerAdapter, json_pretty_format, yaml_pretty_format
+)
+
+__all__ = [
+    "get_logger",
+    "CustomLoggerAdapter",
+    "json_pretty_format",
+    "yaml_pretty_format"
+]
