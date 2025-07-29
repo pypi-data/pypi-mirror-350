7fd0b11d511fb959f92e13cf625562f7d1579052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:17 +0000

    Add package get-latest-version/1.0.1/get_latest_version-1.0.1.tar.gz

---
+++ packages/get-latest-version/get_latest_version-1.0.1.tar.gz/get_latest_version-1.0.1/src/get_latest_version/__version__.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2025 Joe Pitt
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+"""Version information for get_latest_version"""
+
+__title__ = "get_latest_version"
+__description__ = "Python module to get the latest version for various package types."
+__author__ = "Joe Pitt"
+__copyright__ = "Copyright 2025, Joe Pitt"
+__email__ = "Joe.Pitt@joepitt.co.uk"
+__license__ = "GPL-3.0-only"
+__maintainer__ = "Joe Pitt"
+__status__ = "Production"
+__version__ = "1.0.1"
