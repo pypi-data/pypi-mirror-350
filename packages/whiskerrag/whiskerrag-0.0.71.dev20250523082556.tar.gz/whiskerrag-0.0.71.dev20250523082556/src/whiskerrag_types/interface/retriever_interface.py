304e60ea0eb7d5f0d48a6c41c930e2df58d4752b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:08 +0000

    Add package whiskerrag/0.0.71.dev20250523082556/whiskerrag-0.0.71.dev20250523082556.tar.gz

---
+++ packages/whiskerrag/whiskerrag-0.0.71.dev20250523082556.tar.gz/whiskerrag-0.0.71.dev20250523082556/src/whiskerrag_types/interface/retriever_interface.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import Generic, List, TypeVar
+
+from whiskerrag_types.model.retrieval import RetrievalChunk, RetrievalRequest
+
+T = TypeVar("T", bound=RetrievalRequest)
+R = TypeVar("R", bound=RetrievalChunk)
+
+
+class BaseRetriever(Generic[T, R], ABC):
+    """Retriever interface."""
+
+    @abstractmethod
+    async def retrieve(self, params: T, tenant_id: str) -> List[R]:
+        """
+        Retrieve data based on the given parameters.
+        Args:
+            params (T): The parameters for retrieval.
+            tenant_id (str): The tenant ID.
+        Returns:
+            List[R]: A list of retrieval results.
+        """
+        pass
