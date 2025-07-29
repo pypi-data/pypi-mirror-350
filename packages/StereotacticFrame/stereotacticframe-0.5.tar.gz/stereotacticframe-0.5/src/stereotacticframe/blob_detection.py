dcbe01297187531930f25aaae454c3a6ecb8c87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:29 +0000

    Add package StereotacticFrame/0.5/stereotacticframe-0.5.tar.gz

---
+++ packages/StereotacticFrame/stereotacticframe-0.5.tar.gz/stereotacticframe-0.5/src/stereotacticframe/blob_detection.py
@@ -0,0 +1,30 @@
+import SimpleITK as sitk
+import logging
+
+
+def _get_label_statistics(label_img: sitk.Image, img: sitk.Image):
+    label_statistics = sitk.LabelIntensityStatisticsImageFilter()
+    label_statistics.Execute(label_img, img)
+    return label_statistics
+
+
+modality_thresholds = {"MR": 65, "CT": 750}
+
+
+def detect_blobs(
+    img_slice: sitk.Image, mask_slice: sitk.Image, modality: str
+) -> list[tuple[float, float]]:
+    cc = sitk.ConnectedComponent(mask_slice > 0)
+    label_statistics = _get_label_statistics(cc, img_slice)
+    blobs_list = []
+
+    for label_idx in label_statistics.GetLabels():
+        if not 1 < label_statistics.GetPhysicalSize(label_idx) < 30.0:  # [mm²]
+            continue
+
+        if not label_statistics.GetMean(label_idx) > modality_thresholds[modality]:
+            continue
+
+        blobs_list.append(label_statistics.GetCenterOfGravity(label_idx))
+
+    return blobs_list
