de7608ad82f4ed7f842a86e8e48a06fc06979f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:14 +0000

    Add package sator-core/0.0.13/sator_core-0.0.13-py3-none-any.whl

---
+++ packages/sator-core/sator_core-0.0.13-py3-none-any.whl/sator_core/use_cases/analysis/attributes/product.py
@@ -0,0 +1,40 @@
+from typing import List
+
+from sator_core.models.product.attributes import ProductAttributes
+from sator_core.models.product.references import ProductReferences
+
+from sator_core.models.product.locator import ProductLocator
+
+from sator_core.ports.driven.gateways.oss import OSSGatewayPort
+from sator_core.ports.driven.persistence.storage import StoragePersistencePort
+from sator_core.ports.driving.analysis.attributes.product import ProductAttributesAnalysisPort
+
+
+class ProductAttributesAnalysis(ProductAttributesAnalysisPort):
+    def __init__(self, oss_gateways: List[OSSGatewayPort], storage_port: StoragePersistencePort):
+        self.oss_gateways = oss_gateways
+        self.storage_port = storage_port
+
+    def analyze_product_attributes(self, product_id: str) -> ProductLocator | None:
+        product_locator = self.storage_port.load(ProductLocator, product_id)
+
+        if product_locator:
+            return product_locator
+
+        product_attributes = self.storage_port.load(ProductAttributes, product_id)
+
+        if product_attributes:
+            product_references = self.storage_port.load(ProductReferences, product_id)
+
+            if product_references:
+                for port in self.oss_gateways:
+                    locator = port.get_product_locator_from_urls(
+                        product_id, urls=product_references.repositories + product_references.purls,
+                        product_attributes=product_attributes
+                    )
+
+                    if locator:
+                        self.storage_port.save(locator, product_id)
+                        return locator
+
+        return None
