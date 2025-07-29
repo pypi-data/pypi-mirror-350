3bc5163339f771c7ca3b5b670dcac80bd3e2be35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:25 +0000

    Add package sqlmesh/0.186.1.dev6/sqlmesh-0.186.1.dev6.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.186.1.dev6.tar.gz/sqlmesh-0.186.1.dev6/sqlmesh/lsp/context.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from pathlib import Path
+from sqlmesh.core.context import Context
+import typing as t
+
+
+@dataclass
+class ModelTarget:
+    """Information about models in a file."""
+
+    names: t.List[str]
+
+
+@dataclass
+class AuditTarget:
+    """Information about standalone audits in a file."""
+
+    name: str
+
+
+class LSPContext:
+    """
+    A context that is used for linting. It contains the context and a reverse map of file uri to
+    model names and standalone audit names.
+    """
+
+    def __init__(self, context: Context) -> None:
+        self.context = context
+
+        # Add models to the map
+        model_map: t.Dict[Path, ModelTarget] = {}
+        for model in context.models.values():
+            if model._path is not None:
+                uri = model._path
+                if uri in model_map:
+                    model_map[uri].names.append(model.name)
+                else:
+                    model_map[uri] = ModelTarget(names=[model.name])
+
+        # Add standalone audits to the map
+        audit_map: t.Dict[Path, AuditTarget] = {}
+        for audit in context.standalone_audits.values():
+            if audit._path is not None:
+                uri = audit._path
+                if uri not in audit_map:
+                    audit_map[uri] = AuditTarget(name=audit.name)
+
+        self.map: t.Dict[Path, t.Union[ModelTarget, AuditTarget]] = {
+            **model_map,
+            **audit_map,
+        }
