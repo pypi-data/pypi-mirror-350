1333971a87bb88a25aa5160a2e7c3c07586223d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:29 +0000

    Add package AIModeler/0.93/aimodeler-0.93.tar.gz

---
+++ packages/AIModeler/aimodeler-0.93.tar.gz/aimodeler-0.93/src/aimodeler/__init__.py
@@ -0,0 +1,7 @@
+from . import text_extraction
+from .text_extraction import extract_text
+
+from . import text_chunking
+from .text_chunking import chunk_text
+
+__all__ = ["extract_text", "chunk_text"]
