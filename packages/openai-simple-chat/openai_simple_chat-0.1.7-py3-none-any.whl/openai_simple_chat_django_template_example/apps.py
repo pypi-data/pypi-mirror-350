57008622a76940af5a0cba8fa0da216e38f6b124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:51 +0000

    Add package openai-simple-chat/0.1.7/openai_simple_chat-0.1.7-py3-none-any.whl

---
+++ packages/openai-simple-chat/openai_simple_chat-0.1.7-py3-none-any.whl/openai_simple_chat_django_template_example/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OpenaiSimpleChatDjangoTemplateExampleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'openai_simple_chat_django_template_example'
