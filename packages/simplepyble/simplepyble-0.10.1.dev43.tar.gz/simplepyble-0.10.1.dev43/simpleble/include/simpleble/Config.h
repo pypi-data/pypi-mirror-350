2cd6b04bb8a6fd514f52a730a34971024f8f1318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:03 +0000

    Add package simplepyble/0.10.1.dev43/simplepyble-0.10.1.dev43.tar.gz

---
+++ packages/simplepyble/simplepyble-0.10.1.dev43.tar.gz/simplepyble-0.10.1.dev43/simpleble/include/simpleble/Config.h
@@ -0,0 +1,46 @@
+#pragma once
+
+namespace SimpleBLE {
+namespace Config {
+    namespace SimpleBluez {
+        static void reset() {}
+    }
+
+    namespace WinRT {
+        inline static bool experimental_use_own_mta_apartment = true;
+
+        static void reset() {
+            experimental_use_own_mta_apartment = true;
+        }
+    }
+
+    namespace CoreBluetooth {
+        static void reset() {}
+    }
+
+    namespace Android {
+        enum class ConnectionPriorityRequest {
+            DISABLED = -1,
+            BALANCED = 0,
+            HIGH = 1,
+            LOW_POWER = 2,
+            DCK = 3
+        };
+
+        inline static ConnectionPriorityRequest connection_priority_request = ConnectionPriorityRequest::DISABLED;
+
+        static void reset() {
+            connection_priority_request = ConnectionPriorityRequest::DISABLED;
+        }
+    }
+
+    namespace Base {
+        static void reset_all() {
+            SimpleBluez::reset();
+            WinRT::reset();
+            CoreBluetooth::reset();
+            Android::reset();
+        }
+    }
+}  // namespace Config
+}  // namespace SimpleBLE
---
+++ packages/simplepyble/simplepyble-0.10.1.dev43.tar.gz/simplepyble-0.10.1.dev43/simpleble/src/backends/macos/AdapterBaseMacOS.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#import <CoreBluetooth/CoreBluetooth.h>
+#import <Foundation/Foundation.h>
+
+#include <functional>
+#import "AdapterMac.h"
+
+@interface AdapterBaseMacOS : NSObject<CBCentralManagerDelegate>
+
+@property NSString* uuid;
+
+- (bool)isBluetoothEnabled;
+
+- (instancetype)init:(SimpleBLE::AdapterMac*)adapter;
+
+- (void*)underlying;
+
+- (bool)isPowered;
+
+- (void)scanStart;
+- (void)scanStop;
+- (bool)scanIsActive;
+
+- (NSString*)address;
+
+@end
