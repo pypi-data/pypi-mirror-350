9b513200bc759d4dd6fa33b62057bedaa6be5df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package kittycad/0.7.8/kittycad-0.7.8-py3-none-any.whl

---
+++ packages/kittycad/kittycad-0.7.8-py3-none-any.whl/kittycad/models/entity_linear_pattern_transform.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, ConfigDict
+
+from ..models.face_edge_info import FaceEdgeInfo
+
+
+class EntityLinearPatternTransform(BaseModel):
+    """The response from the `EntityLinearPatternTransform` command."""
+
+    entity_face_edge_ids: Optional[List[FaceEdgeInfo]] = None
+
+    entity_ids: Optional[List[str]] = None
+
+    model_config = ConfigDict(protected_namespaces=())
