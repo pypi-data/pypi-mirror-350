fb3e59c60e0b5cc026a92481fe7378ce6933bbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:01 +0000

    Add package pylibbhavya/0.1/pylibbhavya-0.1.tar.gz

---
+++ packages/pylibbhavya/pylibbhavya-0.1.tar.gz/pylibbhavya-0.1/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pylibbhavya",
+    version=0.1,
+    author="Bhavya Sree Pasumarthi",
+    author_email="pasumarthibhavyasree216@gmail.com",
+    description="A Python package for data analysis and visualization using Pandas, NumPy, Matplotlib, and Seaborn.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "pandas>=1.0",
+        "numpy>=1.18",
+        "matplotlib>=3.0",
+        "seaborn>=0.10"
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
