593b4038fe8e5c5742bd6eb01dcc81b5aca152c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:20:19 +0000

    Add package djangocms-text/0.8.6/djangocms_text-0.8.6-py3-none-any.whl

---
+++ packages/djangocms-text/djangocms_text-0.8.6-py3-none-any.whl/djangocms_text/contrib/text_ckeditor4/__init__.py
@@ -0,0 +1,17 @@
+from djangocms_text.editors import RTEConfig
+
+from cms.utils.urlutils import static_with_version
+
+
+ckeditor4 = RTEConfig(
+    name="ckeditor4",
+    config="CKEDITOR",
+    js=(
+        static_with_version("cms/js/dist/bundle.admin.base.min.js"),
+        "djangocms_text/vendor/ckeditor4/ckeditor.js",
+        "djangocms_text/bundles/bundle.ckeditor4.min.js",
+    ),
+    css={"all": ("djangocms_text/css/cms.ckeditor4.css",)},
+    inline_editing=True,
+    child_plugin_support=True,
+)
