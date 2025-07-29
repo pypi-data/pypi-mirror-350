676ad321b78d58e9d101eda4f90afcb0719d01f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:13 +0000

    Add package vplotlib/0.2/vplotlib-0.2-py3-none-any.whl

---
+++ packages/vplotlib/vplotlib-0.2-py3-none-any.whl/vplotlib/__init__.py
@@ -0,0 +1,1 @@
+from .label import set_labels
---
+++ packages/vplotlib/vplotlib-0.2-py3-none-any.whl/vplotlib/label.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+def set_labels(title="",xlabel="",ylabel=""):
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
