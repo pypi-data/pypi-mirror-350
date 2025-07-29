12c1633d7f916f34d0e021138b75a3b58e07d12c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:07 +0000

    Add package calc-norm/0.1.0/calc_norm-0.1.0-py3-none-any.whl

---
+++ packages/calc-norm/calc_norm-0.1.0-py3-none-any.whl/calc_norm/basic_calc.py
@@ -0,0 +1,13 @@
+class CalcBasic:
+    
+    def add(self, a, b):
+        return a + b
+    
+    def sub(self, a, b):
+        return a - b
+    
+    def mul(self, a, b):
+        return a * b
+    
+    def div(self, a, b):
+        return a / b
