7503b0670305bbd183ed6e046c575f2cb9a8198b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:17 +0000

    Add package wurzel/0.10.5/wurzel-0.10.5.tar.gz

---
+++ packages/wurzel/wurzel-0.10.5.tar.gz/wurzel-0.10.5/wurzel/steps/qdrant/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2025 Deutsche Telekom AG (opensource@telekom.de)
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from wurzel.utils import HAS_QDRANT as _HAS_QDRANT
+
+from .data import QdrantResult  # noqa: F401
+
+if _HAS_QDRANT:
+    from .step import QdrantConnectorStep  # noqa: F401
+    from .step_multi_vector import QdrantConnectorMultiVectorStep  # noqa: F401
+
+else:
+    pass
