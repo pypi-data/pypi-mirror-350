e5d5a5f395a4dc37c088d5e7cb05a703e4535522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:50 +0000

    Add package item-matching/0.0.104/item_matching-0.0.104.tar.gz

---
+++ packages/item-matching/item_matching-0.0.104.tar.gz/item_matching-0.0.104/src/item_matching/func/function_image.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+import polars as pl
+from rich import print
+from core_pro import ImgDownloaderThreadProcess
+from core_pro.ultilities import make_dir
+from core_eda import TextEDA
+
+
+class PipelineImage:
+    def __init__(
+        self,
+        path_image: Path,
+        mode: str = "",
+    ):
+        # path
+        self.path_image = path_image
+        make_dir(self.path_image)
+        self.mode = mode
+        self.folder_image = self.path_image / f"img_{self.mode}"
+
+        print(f"[Image Processing] {mode}")
+
+    def load_images(self) -> pl.DataFrame:
+        # sorted files
+        files_sorted = sorted(
+            [*self.folder_image.glob("*/*.jpg")],
+            key=lambda x: int(x.stem.split("_")[0]),
+        )
+
+        # file to df
+        lst = [(i.stem, str(i), i.exists()) for i in files_sorted]
+        df = pl.DataFrame(
+            lst, orient="row", schema=["path_idx", "file_path", "file_exists"]
+        )
+        if df.shape[0] == 0:
+            print(f"-> Images Errors {self.mode}: {df.shape}")
+        else:
+            print(f"-> Load images from folder {self.mode}: {df.shape}")
+        return df
+
+    def run(
+        self,
+        data,
+        col_image_url: str = "image_url",
+        col_text: str = "item_name",
+        download: bool = False,
+        num_processes: int = 4,
+        num_workers: int = 16,
+    ):
+        # load data
+        data = data.with_row_index("img_index")
+        run = [
+            (i["img_index"], i[col_image_url])
+            for i in data[["img_index", col_image_url]].to_dicts()
+        ]
+        print(f"-> Base data {self.mode}: {data.shape}")
+
+        # download
+        if download:
+            downloader = ImgDownloaderThreadProcess(
+                output_dir=self.folder_image,
+                num_processes=num_processes,
+                threads_per_process=num_workers,
+                resize_size=(224, 224),
+                batch_size=1000,
+                images_per_folder=1000,
+            )
+            downloader.run(run)
+
+        # load data image
+        data_img = self.load_images()
+
+        # join
+        img_key = [f"{i[0]}_{i[1].split("/")[-1]}" for i in run]
+        data = (
+            data.pipe(TextEDA.clean_text_pipeline_polars, col=col_text)
+            .with_columns(pl.Series("path_idx", img_key))
+            .join(data_img, on=["path_idx"], how="left")
+            .filter(pl.col("file_exists"))
+            .drop(["img_index", "path_idx", "file_exists"])
+        )
+        if self.mode != "":
+            data = data.select(pl.all().name.prefix(f"{self.mode}_"))
+
+        print(f"-> Merge and clean images data {self.mode}: {data.shape}")
+        return data, data_img
---
+++ packages/item-matching/item_matching-0.0.104.tar.gz/item_matching-0.0.104/src/item_matching/main_match.py
@@ -0,0 +1,137 @@
+from pathlib import Path
+import polars as pl
+import duckdb
+import re
+from time import perf_counter
+from core_pro.ultilities import rm_all_folder, make_dir
+from .pipeline.build_index_and_query import BuildIndexAndQuery
+from .pipeline.data_loading import DataEmbedding
+from rich import print
+
+
+class PipelineMatch:
+    def __init__(
+        self,
+        path: Path,
+        PATH_Q: Path,
+        PATH_DB: Path,
+        MATCH_BY: str = "text",
+        COL_CATEGORY: str = "",
+        SHARD_SIZE: int = 1_500_000,
+        QUERY_SIZE: int = 50_000,
+        TOP_K: int = 10,
+    ):
+        # path
+        self.PATH_Q = PATH_Q
+        self.PATH_DB = PATH_DB
+        self.ROOT_PATH = path
+        self.MATCH_BY = MATCH_BY
+        self.PATH_RESULT = self.ROOT_PATH / f"result_match_{self.MATCH_BY}"
+        make_dir(self.PATH_RESULT)
+
+        # config
+        self.COL_CATEGORY = COL_CATEGORY
+        self.SHARD_SIZE = SHARD_SIZE
+        self.QUERY_SIZE = QUERY_SIZE
+        self.TOP_K = TOP_K
+
+        self.lst_category = self._category_chunking()
+
+    def _category_chunking(self) -> list:
+        """
+        Read by duckdb to perform lazy load
+        """
+        query = f"""
+        select distinct {{1}}{self.COL_CATEGORY} as category 
+        from read_parquet('{{0}}') 
+        where {{1}}{self.COL_CATEGORY} is not null
+        """
+        query = query.format(self.PATH_Q, "q_")
+        lst_category = duckdb.sql(query).pl()["category"].to_list()
+        return sorted(lst_category)
+
+    def _load_data(self, cat: str, mode: str, file: Path):
+        """
+        Read by polars to prevent special characters in writing query
+        """
+        filter_ = pl.col(f"{mode}_{self.COL_CATEGORY}") == cat
+        return pl.read_parquet(file).filter(filter_)
+
+    def _remove_cache(self):
+        print(f"[Clean Cache]")
+        folder_list = [
+            "index",
+            "result",
+            "db_array",
+            "db_ds",
+            "q_array",
+            "q_ds",
+            "array",
+            "ds",
+        ]
+        for name in folder_list:
+            rm_all_folder(self.ROOT_PATH / name)
+
+    def run(self):
+        # run
+        start = perf_counter()
+        for idx, cat in enumerate(self.lst_category):
+            # logging
+            cat_log = f"[dark_orange]{cat}[/]"
+            batch_log = f"{idx}/{len(self.lst_category) - 1}"
+
+            # check file exists
+            file_result_final = self.PATH_RESULT / f"{cat}.parquet"
+            if file_result_final.exists():
+                print(f"[PIPELINE] File {cat_log} already exists")
+                continue
+
+            # chunk checking
+            print("*" * 50)
+            print(
+                f"🐋 [PIPELINE MATCH BY {self.MATCH_BY}] 🐋 \n"
+                f"-> Category: {cat_log} {batch_log}"
+            )
+            chunk_db = self._load_data(cat=cat, mode="db", file=self.PATH_DB)
+            chunk_q = self._load_data(cat=cat, mode="q", file=self.PATH_Q)
+            print(f"-> Database shape {chunk_db.shape}, Query shape {chunk_q.shape}")
+
+            if chunk_q.shape[0] < 2 or chunk_db.shape[0] < 2:
+                print(f"[PIPELINE] Database/Query have not enough data")
+                continue
+
+            # embeddings
+            DataEmbedding(
+                path=self.ROOT_PATH,
+                MODE="db",
+                MATCH_BY=self.MATCH_BY,
+                SHARD_SIZE=self.SHARD_SIZE,
+            ).load(data=chunk_db)
+
+            DataEmbedding(
+                path=self.ROOT_PATH,
+                MODE="q",
+                MATCH_BY=self.MATCH_BY,
+                SHARD_SIZE=self.SHARD_SIZE,
+            ).load(data=chunk_q)
+
+            # index and query
+            cat = re.sub("/", "", cat)  # special characters
+            build = BuildIndexAndQuery(
+                path=self.ROOT_PATH,
+                file_export_name=cat,
+                MATCH_BY=self.MATCH_BY,
+                QUERY_SIZE=self.QUERY_SIZE,
+                TOP_K=self.TOP_K,
+            )
+            build.build()
+            build.query()
+
+            self._remove_cache()
+
+        time_perf = perf_counter() - start
+        print(
+            f"🐋 [PIPELINE MATCH BY {self.MATCH_BY}] 🐋 \n"
+            f"-> Your files are ready, please find here: {self.PATH_RESULT}"
+        )
+        return {f"time_perf_{self.MATCH_BY}": time_perf}
