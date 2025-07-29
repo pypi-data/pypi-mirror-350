4610e22912ba1c1d1399ac2302b3d3ef88a78a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:10:22 +0000

    Add package eiogram/0.2.8.dev20250523144756/eiogram-0.2.8.dev20250523144756-py3-none-any.whl

---
+++ packages/eiogram/eiogram-0.2.8.dev20250523144756-py3-none-any.whl/eiogram/types/_callback_query.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+from ._user import User
+from ._message import Message
+from ..client import Bot
+
+
+class CallbackQuery(BaseModel):
+    id: str
+    from_user: User = Field(..., alias="from")
+    message: Optional[Message] = None
+    data: Optional[str] = None
+    bot: Optional[Bot] = None
+
+    class Config:
+        validate_by_name = True
+        arbitrary_types_allowed = True
+
+    def __str__(self) -> str:
+        return f"CallbackQuery(id={self.id}, from={self.from_user.full_name}, data={self.data})"
+
+    def answer(
+        self, text: Optional[str] = None, show_alert: Optional[bool] = None
+    ) -> bool:
+        return self.bot.answer_callback(
+            callback_query_id=self.id, text=text, show_alert=show_alert
+        )
