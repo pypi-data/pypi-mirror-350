76c776cc4fbcb84caac2fc0e8e63a1b01e52e34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:20:43 +0000

    Add package pypiquant/0.3.2/pypiquant-0.3.2-cp39-cp39-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pypiquant/pypiquant-0.3.2-cp39-cp39-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/piquant/__init__.py
@@ -0,0 +1,8 @@
+__author__ = 'Mario Sieg'
+__email__ = 'mario.sieg.64@gmail.com'
+__author_email__ = 'mario.sieg.64@gmail.com'
+
+from ._quant import *
+from ._torch import *
+from ._numpy import *
+__version__ = "0.3.2"
