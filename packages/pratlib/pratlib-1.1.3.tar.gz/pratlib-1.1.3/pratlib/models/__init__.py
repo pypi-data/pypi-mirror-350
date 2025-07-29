224ad38277d7480293c162101733e5bb04d9b718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:27 +0000

    Add package pratlib/1.1.3/pratlib-1.1.3.tar.gz

---
+++ packages/pratlib/pratlib-1.1.3.tar.gz/pratlib-1.1.3/pratlib/models/__init__.py
@@ -0,0 +1,15 @@
+# pratlib/models/__init__.py
+from .classification import (
+    LogisticRegressor,
+    DecisionTreeClassifier,
+    RandomForestClassifier,
+    GradientBoostedTreeClassifier,
+    NaiveBayes
+)
+
+from .regression import (
+    LinearRegressor,
+    DecisionTreeRegressor,
+    RandomForestRegressor,
+    GradientBoostedTreeRegressor,
+)
