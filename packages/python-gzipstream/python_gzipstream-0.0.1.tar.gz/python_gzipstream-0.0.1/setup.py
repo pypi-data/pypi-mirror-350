96f1eeba215b963d6cbd3e385123fc292e9932ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:04 +0000

    Add package python-gzipstream/0.0.1/python_gzipstream-0.0.1.tar.gz

---
+++ packages/python-gzipstream/python_gzipstream-0.0.1.tar.gz/python_gzipstream-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-gzipstream',
+    version='0.0.1',
+    author='Guangye Bai',
+    author_email='2253637@tongji.edu.cn',
+    description='An academic experiment package',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://pypi.org/project/python-gzipstream/',
+    packages=find_packages(),
+    license='MIT',
+    keywords=['gzipstream', 'experiment', 'typosquatting', 'security'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
+)
