a5ca675c6f921bcf9eb5f5aeed7e3da3d549239c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:08:39 +0000

    Add package qf-lib/4.0.2/qf_lib-4.0.2.tar.gz

---
+++ packages/qf-lib/qf_lib-4.0.2.tar.gz/qf_lib-4.0.2/demo_scripts/charts/pie_chart_demo.py
@@ -0,0 +1,46 @@
+#     Copyright 2016-present CERN – European Organization for Nuclear Research
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+from matplotlib import pyplot as plt
+
+from qf_lib.containers.series.qf_series import QFSeries
+from qf_lib.plotting.charts.pie_chart import PieChart
+
+
+def demo_with_5_data_points():
+    my_series = QFSeries([10, 20, 3, 8.6, 4], ["Example 1", "Example 2", "Example 3", "Example 4", "Example 5"])
+    pie_chart = PieChart(my_series)
+    pie_chart.plot(figsize=(10, 5))
+    plt.show()
+
+
+def demo_with_15_data_points():
+    my_series = QFSeries([10, 20, 3, 8.6, 4,
+                          10, 20, 3, 8.6, 4,
+                          10, 20, 3, 8.6, 4],
+                         ["Example 11", "Example 12", "Example 13", "Example 14", "Example 15",
+                          "Example 21", "Example 22", "Example 23", "Example 24", "Example 25",
+                          "Example 31", "Example 32", "Example 33", "Example 34", "Example 35"])
+    pie_chart = PieChart(my_series)
+    pie_chart.plot(figsize=(10, 5))
+    plt.show()
+
+
+def main():
+    demo_with_5_data_points()
+    demo_with_15_data_points()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/qf-lib/qf_lib-4.0.2.tar.gz/qf_lib-4.0.2/qf_lib/plotting/decorators/bar_values_decorator.py
@@ -0,0 +1,52 @@
+#     Copyright 2016-present CERN – European Organization for Nuclear Research
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+import matplotlib as mpl
+
+from qf_lib.containers.series.qf_series import QFSeries
+from qf_lib.plotting.decorators.chart_decorator import ChartDecorator
+
+
+class BarValuesDecorator(ChartDecorator):
+    """
+    Adds values next to each bar on the bar chart.
+
+    Parameters
+    ----------
+    series: QFSeries
+        series that is going to be decorated by the cone
+    key: str
+        see ChartDecorator.key.__init__#key
+
+    """
+
+    def __init__(self, series: QFSeries, key: str = None):
+        super().__init__(key)
+        self.series = series
+
+    def decorate(self, chart: "Chart") -> None:
+        font_size = mpl.rcParams['legend.fontsize']
+        max_val = self.series.abs().max()
+        space = max_val * 0.02
+        for i, v in self.series.items():
+            if v < 0:
+                x = space
+            else:
+                x = v + space
+
+            chart.axes.text(x, i, '{:0.1%}'.format(v), verticalalignment='center', size=font_size)
+
+        # move the right limit of the x axis because some labels might go beyond the chart
+        _, x_max = chart.axes.get_xlim()
+        chart.axes.set_xlim(right=x_max + 7 * space)
