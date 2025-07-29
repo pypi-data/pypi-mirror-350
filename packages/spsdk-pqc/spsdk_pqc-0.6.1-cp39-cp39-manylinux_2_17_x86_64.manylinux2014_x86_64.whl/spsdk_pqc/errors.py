018d5116e8eb9b5c4dad587df227f67b75736203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:04 +0000

    Add package spsdk-pqc/0.6.1/spsdk_pqc-0.6.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/spsdk-pqc/spsdk_pqc-0.6.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/spsdk_pqc/errors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2024-2025 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Errors used in the plugin."""
+
+
+class PQCError(Exception):
+    """Base plugin error."""
