8b5e39eb84f1614180b75621b1d735ea4ba2c0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:07:10 +0000

    Add package empire-chain/0.5.3/empire_chain-0.5.3.tar.gz

---
+++ packages/empire-chain/empire_chain-0.5.3.tar.gz/empire_chain-0.5.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="empire-chain",
+    version="0.5.3",    
+    description="An orchestration framework for all your AI needs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Manas Chopra",
+    author_email="manaschopra95826@gmail.com",
+    url="https://github.com/manas95826/empire-chain",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "anthropic",
+        "groq",
+        "python-dotenv",
+        "requests",
+        "PyPDF2",
+        "python-docx",
+        "qdrant-client",
+        "soundfile"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
