3c11bd4cdbae1c622283605824c4e05d8ca9fd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:23 +0000

    Add package Openix/2.0.0/openix-2.0.0.tar.gz

---
+++ packages/Openix/openix-2.0.0.tar.gz/openix-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Openix",
+    version="2.0.0",
+    description="A simple chess openings (ECO) library for Python.",
+    author="0xh7",
+    author_email="fjdjfjd1424@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "python-chess",
+    ],
+    python_requires=">=3.7",
+    include_package_data=True,
+    url="https://github.com/0xh7/Openix-Library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
