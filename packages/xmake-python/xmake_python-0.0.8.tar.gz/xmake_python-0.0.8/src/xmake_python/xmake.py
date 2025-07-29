63038ebefc0c42c56e7c1ff193f674445876834a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:25 +0000

    Add package xmake-python/0.0.8/xmake_python-0.0.8.tar.gz

---
+++ packages/xmake-python/xmake_python-0.0.8.tar.gz/xmake_python-0.0.8/src/xmake_python/xmake.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from subprocess import run
+from pathlib import Path
+from shlex import split
+
+
+@dataclass
+class XMaker:
+    xmake: str = "xmake"
+    command: str = ""
+    tempname: str = ""
+    project: str = ""
+
+    def config(self):
+        text = ""
+        # src/xmake_python/templates/xmake.lua
+        with open(Path(__file__).parent / "templates" / "xmake.lua") as f:
+            text = f.read()
+        text = text.format(project=self.project, root=self.tempname)
+        with open(Path(self.tempname) / "xmake.lua", "w") as f:
+            f.write(text)
+        cmd = [
+            self.xmake,
+            "config",
+            "-y",
+            "-P",
+            self.tempname,
+        ] + split(self.command)
+        run(cmd)
+
+    def build(self):
+        cmd = [self.xmake, "-y", "-P", self.tempname, "--verbose"]
+        run(cmd)
+
+    def install(self):
+        cmd = [self.xmake, "install", "-y", "-P", self.tempname, "-o", self.tempname]
+        run(cmd)
