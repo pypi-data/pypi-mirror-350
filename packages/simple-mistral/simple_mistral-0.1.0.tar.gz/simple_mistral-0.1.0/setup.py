49c1f8c2b78a2ed225da0e542abfc9109203b028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package simple-mistral/0.1.0/simple_mistral-0.1.0.tar.gz

---
+++ packages/simple-mistral/simple_mistral-0.1.0.tar.gz/simple_mistral-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="simple-mistral",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Alexander Balashov",
+    author_email="alaex77@gmail.com",
+    description="A simple async and sync Mistral API client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alaex77/simple-mistral",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
