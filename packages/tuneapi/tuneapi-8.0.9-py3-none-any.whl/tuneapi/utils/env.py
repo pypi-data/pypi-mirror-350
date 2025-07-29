44d551bdfb5e924692b530a8c995a23ea19af250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package tuneapi/8.0.9/tuneapi-8.0.9-py3-none-any.whl

---
+++ packages/tuneapi/tuneapi-8.0.9-py3-none-any.whl/tuneapi/utils/env.py
@@ -0,0 +1,32 @@
+# Copyright © 2024-2025 Frello Technology Private Limited
+# Copyright © 2025-2025 Yash Bonde github.com/yashbonde
+# MIT License
+# REMEMBER: nothing from outside tune should be imported in utils
+
+import os
+
+
+class _ENV:
+    def __init__(self):
+        self.vars_called = set()
+
+    TUNEAPI_TOKEN = lambda _, x="": os.getenv("TUNEAPI_TOKEN", x)
+    TUNEORG_ID = lambda _, x="": os.getenv("TUNEORG_ID", x)
+
+    def __getattr__(self, __name: str):
+        self.vars_called.add(__name)
+        return lambda x="": os.getenv(__name, x)
+
+
+ENV = _ENV()
+"""
+Convinience class to get environment variables
+
+Usage:
+
+.. code-block:: python
+
+    from tuneapi.utils.env import ENV
+    TUNEAPI_TOKEN = ENV.TUNEAPI_TOKEN()   # get defined values
+    MY_ENV = ENV.MY_ENV()               # get your arbitrary env var
+"""
