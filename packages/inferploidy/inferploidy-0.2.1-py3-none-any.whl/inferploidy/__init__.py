b216b745e214ad299e1e652b8b2ee2a80df9cfc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:41 +0000

    Add package inferploidy/0.2.1/inferploidy-0.2.1-py3-none-any.whl

---
+++ packages/inferploidy/inferploidy-0.2.1-py3-none-any.whl/inferploidy/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+# Copyright (c) 2021 (syoon@dku.edu) and contributors
+# https://github.com/combio-dku/MarkerCount/tree/master
+print('https://github.com/combio-dku/InferPloidy')
+
+from .inferploidy import run_infercnv, run_inferploidy
