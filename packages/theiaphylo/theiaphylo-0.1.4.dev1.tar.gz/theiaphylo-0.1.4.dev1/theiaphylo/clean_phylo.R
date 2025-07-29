575ecf7517391292653d477c5979741e25d044b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:04 +0000

    Add package theiaphylo/0.1.4.dev1/theiaphylo-0.1.4.dev1.tar.gz

---
+++ packages/theiaphylo/theiaphylo-0.1.4.dev1.tar.gz/theiaphylo-0.1.4.dev1/theiaphylo/clean_phylo.R
@@ -0,0 +1,20 @@
+#! /usr/bin/env Rscript
+
+library('ape')
+
+# accept command-line args
+args <- commandArgs(trailingOnly = TRUE)
+
+# read tree
+tree <- read.tree(args[1])
+
+# convert 0 branches to polytomy
+multifurc_tree <- di2multi(tree)
+
+# report if tree is bifurcating
+bifurcation <- is.binary(multifurc_tree)
+write(paste('bifurcating:', bifurcation), stdout())
+
+# write tree
+out_path <- gsub('(.*)\\.([^\\.]+)$', '\\1.clean.\\2', args[1])
+write.tree(multifurc_tree, out_path)
