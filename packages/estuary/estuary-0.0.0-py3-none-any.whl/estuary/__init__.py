d55a09ab88aa5dbf4e5c63545bdcb5fe758c964a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:11 +0000

    Add package estuary/0.0.0/estuary-0.0.0-py3-none-any.whl

---
+++ packages/estuary/estuary-0.0.0-py3-none-any.whl/estuary/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2025 Riverlane Ltd. All rights reserved.
+
+estuary: A simple stabilizer circuit generator library
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
