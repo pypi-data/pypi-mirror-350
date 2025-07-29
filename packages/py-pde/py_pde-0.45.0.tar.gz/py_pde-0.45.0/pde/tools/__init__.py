f64e31ace4a0d9a4f24ae0c25cb905e684c21086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:55 +0000

    Add package py-pde/0.45.0/py_pde-0.45.0.tar.gz

---
+++ packages/py-pde/py_pde-0.45.0.tar.gz/py_pde-0.45.0/pde/tools/__init__.py
@@ -0,0 +1,24 @@
+"""Package containing several tools required in py-pde.
+
+.. autosummary::
+   :nosignatures:
+
+   cache
+   config
+   cuboid
+   docstrings
+   expressions
+   ffmpeg
+   math
+   misc
+   mpi
+   numba
+   output
+   parameters
+   parse_duration
+   plotting
+   spectral
+   typing
+
+.. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
+"""
