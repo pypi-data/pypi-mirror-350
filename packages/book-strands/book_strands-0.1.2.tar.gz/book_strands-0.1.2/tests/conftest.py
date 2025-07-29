0aa13199052f0ed54b5fc85c933db19a41981994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:18 +0000

    Add package book-strands/0.1.2/book_strands-0.1.2.tar.gz

---
+++ packages/book-strands/book_strands-0.1.2.tar.gz/book_strands-0.1.2/tests/conftest.py
@@ -0,0 +1,5 @@
+import os
+
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "test_data")
+CORRECT_METADATA_DIR = os.path.join(TEST_DATA_DIR, "correct-metadata")
+MISSING_METADATA_DIR = os.path.join(TEST_DATA_DIR, "missing-metadata")
