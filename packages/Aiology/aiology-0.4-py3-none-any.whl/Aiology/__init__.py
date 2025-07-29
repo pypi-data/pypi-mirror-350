2025559f49918c4ad0f481920e20b6d976a14f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:07:26 +0000

    Add package Aiology/0.4/aiology-0.4-py3-none-any.whl

---
+++ packages/Aiology/aiology-0.4-py3-none-any.whl/Aiology/__init__.py
@@ -0,0 +1,23 @@
+"""
+## Ailogy
+
+Ailogy is a package which pressent you two modules :
+
+## 📄 PDF module
+
+This module use to extract data from pdf files ,or use pdf data to ask question from ai 
+(This module can extract Persian texts too !!)
+
+## 🤖 AI module
+
+You can commiunicate with ai by this module ,this module is a text base module which only 
+supported text input ,and output
+
+## Author
+
+✍️ Seyed Moied Seyedi
+
+😁 I will be glad to see your tricks 
+"""
+from .main import PDF
+from .main import AI
