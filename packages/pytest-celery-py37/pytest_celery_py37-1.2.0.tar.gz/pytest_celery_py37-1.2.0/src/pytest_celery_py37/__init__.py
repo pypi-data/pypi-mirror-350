b6eb64d8459543149d08d172cb9e3b8331f55220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:02 +0000

    Add package pytest-celery-py37/1.2.0/pytest_celery_py37-1.2.0.tar.gz

---
+++ packages/pytest-celery-py37/pytest_celery_py37-1.2.0.tar.gz/pytest_celery_py37-1.2.0/src/pytest_celery_py37/__init__.py
@@ -0,0 +1,142 @@
+"""Official pytest plugin for Celery."""
+
+# flake8: noqa
+
+
+__version__ = "1.2.0"
+__author__ = "Tomer Nosrati"
+__contact__ = "tomer.nosrati@gmail.com"
+__homepage__ = "https://pytest-celery.readthedocs.io/"
+__docformat__ = "restructuredtext"
+
+
+import re
+from collections import namedtuple
+
+from pytest_celery_py37.api.backend import CeleryBackendCluster
+from pytest_celery_py37.api.backend import CeleryTestBackend
+from pytest_celery_py37.api.base import CeleryTestCluster
+from pytest_celery_py37.api.base import CeleryTestNode
+from pytest_celery_py37.api.broker import CeleryBrokerCluster
+from pytest_celery_py37.api.broker import CeleryTestBroker
+from pytest_celery_py37.api.container import CeleryTestContainer
+from pytest_celery_py37.api.setup import CeleryTestSetup
+from pytest_celery_py37.api.worker import CeleryTestWorker
+from pytest_celery_py37.api.worker import CeleryWorkerCluster
+from pytest_celery_py37.defaults import *
+from pytest_celery_py37.fixtures.backend import celery_backend
+from pytest_celery_py37.fixtures.backend import celery_backend_cluster
+from pytest_celery_py37.fixtures.backend import celery_backend_cluster_config
+from pytest_celery_py37.fixtures.broker import celery_broker
+from pytest_celery_py37.fixtures.broker import celery_broker_cluster
+from pytest_celery_py37.fixtures.broker import celery_broker_cluster_config
+from pytest_celery_py37.fixtures.setup import celery_setup
+from pytest_celery_py37.fixtures.setup import celery_setup_app
+from pytest_celery_py37.fixtures.setup import celery_setup_cls
+from pytest_celery_py37.fixtures.setup import celery_setup_config
+from pytest_celery_py37.fixtures.setup import celery_setup_name
+from pytest_celery_py37.fixtures.worker import celery_worker
+from pytest_celery_py37.fixtures.worker import celery_worker_cluster
+from pytest_celery_py37.fixtures.worker import celery_worker_cluster_config
+from pytest_celery_py37.vendors import _is_vendor_installed
+
+if _is_vendor_installed("localstack"):
+    from pytest_celery_py37.vendors.localstack.api import LocalstackTestBroker
+    from pytest_celery_py37.vendors.localstack.container import LocalstackContainer
+    from pytest_celery_py37.vendors.localstack.defaults import *
+    from pytest_celery_py37.vendors.localstack.fixtures import celery_localstack_broker
+    from pytest_celery_py37.vendors.localstack.fixtures import default_localstack_broker
+    from pytest_celery_py37.vendors.localstack.fixtures import default_localstack_broker_cls
+    from pytest_celery_py37.vendors.localstack.fixtures import default_localstack_broker_env
+    from pytest_celery_py37.vendors.localstack.fixtures import default_localstack_broker_image
+    from pytest_celery_py37.vendors.localstack.fixtures import default_localstack_broker_ports
+
+if _is_vendor_installed("memcached"):
+    from pytest_celery_py37.vendors.memcached.api import MemcachedTestBackend
+    from pytest_celery_py37.vendors.memcached.container import MemcachedContainer
+    from pytest_celery_py37.vendors.memcached.defaults import *
+    from pytest_celery_py37.vendors.memcached.fixtures import celery_memcached_backend
+    from pytest_celery_py37.vendors.memcached.fixtures import default_memcached_backend
+    from pytest_celery_py37.vendors.memcached.fixtures import default_memcached_backend_cls
+    from pytest_celery_py37.vendors.memcached.fixtures import default_memcached_backend_env
+    from pytest_celery_py37.vendors.memcached.fixtures import default_memcached_backend_image
+    from pytest_celery_py37.vendors.memcached.fixtures import default_memcached_backend_ports
+
+if _is_vendor_installed("rabbitmq"):
+    from pytest_celery_py37.vendors.rabbitmq.api import RabbitMQTestBroker
+    from pytest_celery_py37.vendors.rabbitmq.container import RabbitMQContainer
+    from pytest_celery_py37.vendors.rabbitmq.defaults import *
+    from pytest_celery_py37.vendors.rabbitmq.fixtures import celery_rabbitmq_broker
+    from pytest_celery_py37.vendors.rabbitmq.fixtures import default_rabbitmq_broker
+    from pytest_celery_py37.vendors.rabbitmq.fixtures import default_rabbitmq_broker_cls
+    from pytest_celery_py37.vendors.rabbitmq.fixtures import default_rabbitmq_broker_env
+    from pytest_celery_py37.vendors.rabbitmq.fixtures import default_rabbitmq_broker_image
+    from pytest_celery_py37.vendors.rabbitmq.fixtures import default_rabbitmq_broker_ports
+
+if _is_vendor_installed("redis"):
+    from pytest_celery_py37.vendors.redis.backend.api import RedisTestBackend
+    from pytest_celery_py37.vendors.redis.backend.defaults import *
+    from pytest_celery_py37.vendors.redis.backend.fixtures import celery_redis_backend
+    from pytest_celery_py37.vendors.redis.backend.fixtures import default_redis_backend
+    from pytest_celery_py37.vendors.redis.backend.fixtures import default_redis_backend_cls
+    from pytest_celery_py37.vendors.redis.backend.fixtures import default_redis_backend_command
+    from pytest_celery_py37.vendors.redis.backend.fixtures import default_redis_backend_env
+    from pytest_celery_py37.vendors.redis.backend.fixtures import default_redis_backend_image
+    from pytest_celery_py37.vendors.redis.backend.fixtures import default_redis_backend_ports
+    from pytest_celery_py37.vendors.redis.broker.api import RedisTestBroker
+    from pytest_celery_py37.vendors.redis.broker.defaults import *
+    from pytest_celery_py37.vendors.redis.broker.fixtures import celery_redis_broker
+    from pytest_celery_py37.vendors.redis.broker.fixtures import default_redis_broker
+    from pytest_celery_py37.vendors.redis.broker.fixtures import default_redis_broker_cls
+    from pytest_celery_py37.vendors.redis.broker.fixtures import default_redis_broker_command
+    from pytest_celery_py37.vendors.redis.broker.fixtures import default_redis_broker_env
+    from pytest_celery_py37.vendors.redis.broker.fixtures import default_redis_broker_image
+    from pytest_celery_py37.vendors.redis.broker.fixtures import default_redis_broker_ports
+    from pytest_celery_py37.vendors.redis.container import RedisContainer
+    from pytest_celery_py37.vendors.redis.defaults import *
+
+if _is_vendor_installed("worker"):
+    from pytest_celery_py37.vendors.worker.container import CeleryWorkerContainer
+    from pytest_celery_py37.vendors.worker.content import app
+    from pytest_celery_py37.vendors.worker.content import utils
+    from pytest_celery_py37.vendors.worker.fixtures import celery_base_worker_image
+    from pytest_celery_py37.vendors.worker.fixtures import celery_setup_worker
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_app
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_app_module
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_celery_log_level
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_celery_version
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_celery_worker_name
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_celery_worker_queue
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_cls
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_command
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_container
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_container_cls
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_container_session_cls
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_env
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_initial_content
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_ports
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_signals
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_tasks
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_utils_module
+    from pytest_celery_py37.vendors.worker.fixtures import default_worker_volume
+    from pytest_celery_py37.vendors.worker.tasks import *
+    from pytest_celery_py37.vendors.worker.volume import WorkerInitialContent
+
+
+version_info_t = namedtuple(
+    "version_info_t",
+    (
+        "major",
+        "minor",
+        "micro",
+        "releaselevel",
+        "serial",
+    ),
+)
+
+
+# Required for RTD to build
+_temp = re.match(r"v?(\d+)\.(\d+)\.(\d+)([a-zA-Z0-9]+)?", __version__).groups()  # type: ignore
+VERSION = version_info = version_info_t(int(_temp[0]), int(_temp[1]), int(_temp[2]), _temp[3] or "", "")
+del _temp
+del re
