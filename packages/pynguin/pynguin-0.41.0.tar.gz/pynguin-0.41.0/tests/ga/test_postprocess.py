acef62d146cf33480a4ec4bf5ed83fba9ef3ab58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:19 +0000

    Add package pynguin/0.41.0/pynguin-0.41.0.tar.gz

---
+++ packages/pynguin/pynguin-0.41.0.tar.gz/pynguin-0.41.0/tests/ga/test_postprocess.py
@@ -0,0 +1,240 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2025 Pynguin Contributors
+#
+#  SPDX-License-Identifier: MIT
+#
+import ast
+
+from unittest import mock
+from unittest.mock import MagicMock
+from unittest.mock import call
+
+import pytest
+
+import pynguin.ga.postprocess as pp
+import pynguin.ga.testcasechromosome as tcc
+import pynguin.testcase.defaulttestcase as dtc
+import pynguin.testcase.statement as stmt
+
+from pynguin.analyses.module import ModuleTestCluster
+from pynguin.assertion.assertion import ExceptionAssertion
+from pynguin.large_language_model.parsing.astscoping import VariableRefAST
+from pynguin.utils.orderedset import OrderedSet
+
+
+def test_not_failing():
+    trunc = pp.ExceptionTruncation()
+    test_case = MagicMock()
+    chromosome = MagicMock(test_case=test_case)
+    chromosome.is_failing.return_value = False
+    trunc.visit_test_case_chromosome(chromosome)
+    test_case.chop.assert_not_called()
+
+
+def test_simple_chop():
+    trunc = pp.ExceptionTruncation()
+    test_case = MagicMock()
+    chromosome = MagicMock(test_case=test_case)
+    chromosome.is_failing.return_value = True
+    chromosome.get_last_mutatable_statement.return_value = 42
+    trunc.visit_test_case_chromosome(chromosome)
+    test_case.chop.assert_called_once_with(42)
+
+
+def test_suite_chop():
+    trunc = pp.ExceptionTruncation()
+    chromosome = MagicMock()
+    suite = MagicMock(test_case_chromosomes=[chromosome, chromosome])
+    trunc.visit_test_suite_chromosome(suite)
+    chromosome.accept.assert_has_calls([call(trunc), call(trunc)])
+
+
+def test_suite_assertion_minimization():
+    ass_min = pp.AssertionMinimization()
+    chromosome = MagicMock()
+    suite = MagicMock(test_case_chromosomes=[chromosome, chromosome])
+    ass_min.visit_test_suite_chromosome(suite)
+    chromosome.accept.assert_has_calls([call(ass_min), call(ass_min)])
+
+
+def test_test_case_assertion_minimization(default_test_case):
+    ass_min = pp.AssertionMinimization()
+    statement = stmt.IntPrimitiveStatement(default_test_case)
+
+    assertion_1 = MagicMock(checked_instructions=[MagicMock(lineno=1), MagicMock(lineno=2)])
+    assertion_2 = MagicMock(checked_instructions=[MagicMock(lineno=1)])
+
+    statement.add_assertion(assertion_1)
+    statement.add_assertion(assertion_2)
+    default_test_case.add_statement(statement)
+
+    chromosome = tcc.TestCaseChromosome(test_case=default_test_case)
+    ass_min.visit_test_case_chromosome(chromosome)
+
+    assert ass_min.remaining_assertions == OrderedSet([assertion_1])
+    assert ass_min.deleted_assertions == OrderedSet([assertion_2])
+    assert default_test_case.get_assertions() == [assertion_1]
+
+
+def test_test_case_assertion_minimization_does_not_remove_exception_assertion(
+    default_test_case,
+):
+    ass_min = pp.AssertionMinimization()
+    statement = stmt.IntPrimitiveStatement(default_test_case)
+
+    assertion_1 = MagicMock(checked_instructions=[MagicMock(lineno=1), MagicMock(lineno=2)])
+    assertion_2 = MagicMock(spec=ExceptionAssertion, checked_instructions=[MagicMock(lineno=1)])
+
+    statement.add_assertion(assertion_1)
+    statement.add_assertion(assertion_2)
+    default_test_case.add_statement(statement)
+
+    chromosome = tcc.TestCaseChromosome(test_case=default_test_case)
+    ass_min.visit_test_case_chromosome(chromosome)
+
+    assert ass_min.remaining_assertions == OrderedSet([assertion_1, assertion_2])
+    assert ass_min.deleted_assertions == OrderedSet()
+    assert default_test_case.get_assertions() == [assertion_1, assertion_2]
+
+
+def test_test_case_assertion_minimization_does_not_remove_empty_assertion(
+    default_test_case,
+):
+    ass_min = pp.AssertionMinimization()
+    statement = stmt.IntPrimitiveStatement(default_test_case)
+
+    assertion_1 = MagicMock(checked_instructions=[])
+
+    statement.add_assertion(assertion_1)
+    default_test_case.add_statement(statement)
+
+    chromosome = tcc.TestCaseChromosome(test_case=default_test_case)
+    ass_min.visit_test_case_chromosome(chromosome)
+
+    assert ass_min.remaining_assertions == OrderedSet([assertion_1])
+    assert ass_min.deleted_assertions == OrderedSet()
+    assert default_test_case.get_assertions() == [assertion_1]
+
+
+def test_test_case_postprocessor_suite():
+    dummy_visitor = MagicMock()
+    tcpp = pp.TestCasePostProcessor([dummy_visitor])
+    chromosome = MagicMock()
+    suite = MagicMock(test_case_chromosomes=[chromosome, chromosome])
+    tcpp.visit_test_suite_chromosome(suite)
+    chromosome.accept.assert_has_calls([call(tcpp), call(tcpp)])
+
+
+def test_test_case_postprocessor_test():
+    dummy_visitor = MagicMock()
+    tcpp = pp.TestCasePostProcessor([dummy_visitor])
+    test_case = MagicMock()
+    test_chromosome = MagicMock(test_case=test_case)
+    tcpp.visit_test_case_chromosome(test_chromosome)
+    test_case.accept.assert_has_calls([call(dummy_visitor)])
+
+
+def test_unused_primitives_visitor():
+    visitor = pp.UnusedStatementsTestCaseVisitor()
+    statement = MagicMock()
+    test_case = MagicMock(statements=[statement])
+    visitor.visit_default_test_case(test_case)
+    assert statement.accept.call_count == 1
+
+
+# TODO(fk) replace with ast_to_stmt
+def test_remove_integration(constructor_mock):
+    cluster = ModuleTestCluster(0)
+    test_case = dtc.DefaultTestCase(cluster)
+    test_case.add_statement(stmt.IntPrimitiveStatement(test_case))
+    test_case.add_statement(stmt.FloatPrimitiveStatement(test_case))
+    int0 = stmt.IntPrimitiveStatement(test_case)
+    test_case.add_statement(int0)
+    list0 = stmt.ListStatement(
+        test_case, cluster.type_system.convert_type_hint(list[int]), [int0.ret_val]
+    )
+    test_case.add_statement(list0)
+    float0 = stmt.FloatPrimitiveStatement(test_case)
+    test_case.add_statement(float0)
+    ctor0 = stmt.ConstructorStatement(
+        test_case, constructor_mock, {"foo": float0.ret_val, "bar": list0.ret_val}
+    )
+    test_case.add_statement(ctor0)
+    assert test_case.size() == 6
+    visitor = pp.UnusedStatementsTestCaseVisitor()
+    test_case.accept(visitor)
+    assert test_case.statements == [int0, list0, float0, ctor0]
+
+
+def test_visit_ast_assign_statement():
+    """Test that the visit_ast_assign_statement method is called correctly."""
+    # Create a test case
+    cluster = ModuleTestCluster(0)
+    test_case = dtc.DefaultTestCase(cluster)
+
+    # Create an ASTAssignStatement with a variable reference
+    # First, create a variable reference
+    int_stmt = stmt.IntPrimitiveStatement(test_case)
+    test_case.add_statement(int_stmt)
+    var_ref = int_stmt.ret_val
+
+    # Create a name node and ref_dict for the variable reference
+    name_node = ast.Name(id="test_var", ctx=ast.Load())
+    ref_dict = {"test_var": var_ref}
+
+    # Create the ASTAssignStatement that uses the variable reference
+    var_ref_ast = VariableRefAST(name_node, ref_dict)
+    ast_assign = stmt.ASTAssignStatement(test_case, var_ref_ast, {})
+    test_case.add_statement(ast_assign)
+
+    # Create a visitor and visit the ASTAssignStatement
+    visitor = pp.UnusedPrimitiveOrCollectionStatementVisitor()
+    ast_assign.accept(visitor)
+
+    # Verify that _handle_remaining was called by checking that the variable reference
+    # was added to the _used_references set
+    assert var_ref in visitor._used_references
+
+    # Also verify that the statement was not removed
+    assert ast_assign in test_case.statements
+
+
+@pytest.mark.parametrize(
+    "statement_type, func",
+    [
+        ("visit_int_primitive_statement", "_handle_collection_or_primitive"),
+        ("visit_float_primitive_statement", "_handle_collection_or_primitive"),
+        ("visit_string_primitive_statement", "_handle_collection_or_primitive"),
+        ("visit_bytes_primitive_statement", "_handle_collection_or_primitive"),
+        ("visit_boolean_primitive_statement", "_handle_collection_or_primitive"),
+        ("visit_enum_statement", "_handle_collection_or_primitive"),
+        ("visit_none_statement", "_handle_collection_or_primitive"),
+        ("visit_constructor_statement", "_handle_remaining"),
+        ("visit_method_statement", "_handle_remaining"),
+        ("visit_function_statement", "_handle_remaining"),
+        ("visit_list_statement", "_handle_collection_or_primitive"),
+        ("visit_set_statement", "_handle_collection_or_primitive"),
+        ("visit_tuple_statement", "_handle_collection_or_primitive"),
+        ("visit_dict_statement", "_handle_collection_or_primitive"),
+        ("visit_ast_assign_statement", "_handle_remaining"),
+    ],
+)
+def test_all_statements(statement_type, func):
+    visitor = pp.UnusedPrimitiveOrCollectionStatementVisitor()
+    with mock.patch.object(visitor, func) as f:
+        visitor.__getattribute__(statement_type)(MagicMock())  # noqa: PLC2801
+        f.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "statement_type",
+    [
+        "visit_field_statement",
+        "visit_assignment_statement",
+    ],
+)
+def test_not_implemented_statements(statement_type):
+    visitor = pp.UnusedPrimitiveOrCollectionStatementVisitor()
+    with pytest.raises(NotImplementedError):
+        visitor.__getattribute__(statement_type)(MagicMock())  # noqa: PLC2801
---
+++ packages/pynguin/pynguin-0.41.0.tar.gz/pynguin-0.41.0/tests/utils/test_type_utils.py
@@ -0,0 +1,241 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2025 Pynguin Contributors
+#
+#  SPDX-License-Identifier: MIT
+#
+import enum
+import inspect
+
+from unittest.mock import MagicMock
+
+import pytest
+
+from pynguin.analyses.typesystem import InferredSignature
+from pynguin.utils.type_utils import given_exception_matches
+from pynguin.utils.type_utils import is_assertable
+from pynguin.utils.type_utils import is_bytes
+from pynguin.utils.type_utils import is_collection_type
+from pynguin.utils.type_utils import is_dict
+from pynguin.utils.type_utils import is_enum
+from pynguin.utils.type_utils import is_ignorable_type
+from pynguin.utils.type_utils import is_list
+from pynguin.utils.type_utils import is_none_type
+from pynguin.utils.type_utils import is_numeric
+from pynguin.utils.type_utils import is_optional_parameter
+from pynguin.utils.type_utils import is_primitive_type
+from pynguin.utils.type_utils import is_set
+from pynguin.utils.type_utils import is_string
+from pynguin.utils.type_utils import is_tuple
+
+
+@pytest.mark.parametrize(
+    "type_, result",
+    [
+        (int, True),
+        (float, True),
+        (str, True),
+        (bool, True),
+        (complex, True),
+        (type, False),
+        (None, False),
+    ],
+)
+def test_is_primitive_type(type_, result):
+    assert is_primitive_type(type_) == result
+
+
+@pytest.mark.parametrize(
+    "type_, result",
+    [
+        (type(None), True),
+        (None, False),
+        (str, False),
+    ],
+)
+def test_is_none_type(type_, result):
+    assert is_none_type(type_) == result
+
+
+@pytest.mark.parametrize(
+    "value, result",
+    [(5, True), (5.5, True), ("test", False), (None, False)],
+)
+def test_is_numeric(value, result):
+    assert is_numeric(value) == result
+
+
+@pytest.mark.parametrize(
+    "value, result",
+    [(5, False), (5.5, False), ("test", True), (None, False)],
+)
+def test_is_string(value, result):
+    assert is_string(value) == result
+
+
+@pytest.mark.parametrize(
+    "value, result",
+    [(b"5", True), ("foo", False), (bytearray("test", "ascii"), True), (None, False)],
+)
+def test_is_bytes(value, result):
+    assert is_bytes(value) == result
+
+
+@pytest.mark.parametrize(
+    "value, result",
+    [
+        (["foo", "bar"], True),
+        ({"foo", "bar"}, False),
+        ({"foo": "bar"}, False),
+        (("foo", "bar"), False),
+    ],
+)
+def test_is_list(value, result):
+    assert is_list(type(value)) == result
+
+
+@pytest.mark.parametrize(
+    "value, result",
+    [
+        (["foo", "bar"], False),
+        ({"foo", "bar"}, True),
+        ({"foo": "bar"}, False),
+        (("foo", "bar"), False),
+    ],
+)
+def test_is_set(value, result):
+    assert is_set(type(value)) == result
+
+
+@pytest.mark.parametrize(
+    "value, result",
+    [
+        (["foo", "bar"], False),
+        ({"foo", "bar"}, False),
+        ({"foo": "bar"}, True),
+        (("foo", "bar"), False),
+    ],
+)
+def test_is_dict(value, result):
+    assert is_dict(type(value)) == result
+
+
+@pytest.mark.parametrize(
+    "value, result",
+    [
+        (["foo", "bar"], False),
+        ({"foo", "bar"}, False),
+        ({"foo": "bar"}, False),
+        (("foo", "bar"), True),
+    ],
+)
+def test_is_tuple(value, result):
+    assert is_tuple(type(value)) == result
+
+
+def test_is_enum():
+    class Foo(enum.Enum):
+        pass
+
+    assert is_enum(Foo)
+
+
+@pytest.mark.parametrize(
+    "param_name,result",
+    [
+        ("normal", False),
+        ("args", True),
+        ("kwargs", True),
+        ("default", True),
+    ],
+)
+def test_should_skip_parameter(param_name, result):
+    def inner_func(normal: str, *args, default="foo", **kwargs):
+        pass  # pragma: no cover
+
+    inf_sig = MagicMock(InferredSignature, signature=inspect.signature(inner_func))
+    assert is_optional_parameter(inf_sig, param_name) == result
+
+
+@pytest.mark.parametrize(
+    "type_,result",
+    [
+        (list, True),
+        (set, True),
+        (dict, True),
+        (tuple, True),
+        (list[str], True),
+        (set[str], True),
+        (tuple[str], True),
+        (dict[str, str], True),
+        (str, False),
+    ],
+)
+def test_is_collection_type(type_, result):
+    assert is_collection_type(type_) == result
+
+
+def test_is_ignorable_type():
+    def generator():  # pragma: no cover
+        yield from range(10)
+
+    generator_type = type(generator())
+    assert is_ignorable_type(generator_type)
+
+
+def test_is_ignorable_type_async():
+    async def async_generator():  # pragma: no cover  # noqa: RUF029
+        yield "foo"
+
+    generator_type = type(async_generator())
+    assert is_ignorable_type(generator_type)
+
+
+def test_is_ignorable_type_false():
+    assert not is_ignorable_type(str)
+
+
+@pytest.mark.parametrize(
+    "exception,ex_match,result",
+    [
+        (ValueError, ValueError, True),
+        (ValueError(), ValueError, True),
+        (ValueError(), Exception, True),
+        (ValueError(), NameError, False),
+        (None, None, False),
+    ],
+)
+def test_given_exception_matches(exception, ex_match, result):
+    assert given_exception_matches(exception, ex_match) == result
+
+
+@pytest.mark.parametrize(
+    "value,result",
+    [
+        (1, True),
+        (MagicMock(), False),
+        (enum.Enum("Dummy", "a").a, True),
+        ({1, 2}, True),
+        ({1, MagicMock()}, False),
+        ([1, 2], True),
+        ([1, MagicMock()], False),
+        ((1, 2), True),
+        ((1, MagicMock()), False),
+        ({1: 2}, True),
+        ({1: MagicMock()}, False),
+        ([[[[[[[[]]]]]]]], False),
+        ((), True),
+        (set(), True),
+        ({}, True),
+        ([], True),
+        ([[]], True),
+        ("foobar", True),
+        (["a", "b", ["a", "b", MagicMock()]], False),
+        (1.5, False),
+        ([1, 1.5], False),
+        (None, True),
+        ([None], True),
+    ],
+)
+def test_is_assertable(value, result):
+    assert is_assertable(value) == result
