89584a0699a8dbfe2b1d5b7792aa0cae4fa69dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:49 +0000

    Add package pyjuiz/0.0.2/pyjuiz-0.0.2.tar.gz

---
+++ packages/pyjuiz/pyjuiz-0.0.2.tar.gz/pyjuiz-0.0.2/juiz/__init__.py
@@ -0,0 +1,5 @@
+
+# from . import proxy
+from .juiz import *
+
+from .decorators import juiz_process, juiz_container, juiz_container_process, juiz_component_container, juiz_component_container_process
