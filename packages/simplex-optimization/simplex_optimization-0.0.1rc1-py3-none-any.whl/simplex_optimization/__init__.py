8e536d63ccced3e5cf089f3524c9d77885e496ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:48 +0000

    Add package simplex-optimization/0.0.1rc1/simplex_optimization-0.0.1rc1-py3-none-any.whl

---
+++ packages/simplex-optimization/simplex_optimization-0.0.1rc1-py3-none-any.whl/simplex_optimization/__init__.py
@@ -0,0 +1,9 @@
+"""
+simplex_optimization
+=================
+This is a placeholder for a simplex-normalized gradient descent algorithm.
+"""
+__version__ = "0.0.1"
+
+def info():
+    return "simplex_optimization is a placeholder. Full release coming soon."
