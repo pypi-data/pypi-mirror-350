2cb7301b37b178aac9e2a097cd5b6e6a98e2277d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:00 +0000

    Add package spiderman/0.0.1/spiderman-0.0.1.tar.gz

---
+++ packages/spiderman/spiderman-0.0.1.tar.gz/spiderman-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="spiderman",
+    author="Arjun Shankar",
+    author_email="arjun.sha2425@gmail.com",
+    description="",
+    license="Apache-2.0",
+    packages=find_packages(),
+    version="0.0.1"
+)
