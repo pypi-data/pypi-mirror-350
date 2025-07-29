3387e721b487afb043da4a1d3337f63409d16267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:09:28 +0000

    Add package gthelp/1.0.6/gthelp-1.0.6.tar.gz

---
+++ packages/gthelp/gthelp-1.0.6.tar.gz/gthelp-1.0.6/publish.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+import subprocess
+import toml
+
+# Remove dist directory if it exists
+if os.path.exists('dist'):
+    shutil.rmtree('dist')
+    
+version = '1.0.0'
+with open('pyproject.toml', 'r') as f:
+    data = toml.load(f)
+    # Parse current version and increment build number
+    version_parts = data['project']['version'].split('.')
+    version_base = '.'.join(version_parts[:-1]) + '.'
+    build_num = int(version_parts[-1]) + 1
+    new_version = f"{version_base}{build_num}"
+    version = new_version
+    data['project']['version'] = new_version
+    
+    with open('pyproject.toml', 'w') as f:
+        toml.dump(data, f)
+
+
+with open('src/gthelp/__init__.py', 'w') as f:
+    f.write(f"__version__ = '{version}'")
+
+# Build package
+subprocess.run(['python', '-m', 'build'], check=True)
+
+# Upload to PyPI
+subprocess.run(['python', '-m', 'twine', 'upload', 'dist/*', '--verbose'], check=True)
