2bb504e8b1459f55bf6e0a50993569144b084d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:55 +0000

    Add package cloudly/0.3.7b4/cloudly-0.3.7b4-py3-none-any.whl

---
+++ packages/cloudly/cloudly-0.3.7b4-py3-none-any.whl/cloudly/gcp/secretmanager.py
@@ -0,0 +1,13 @@
+__all__ = ['get_secret']
+
+from google.cloud import secretmanager
+
+from .auth import get_project_id
+
+
+def get_secret(name: str, *, version: str = 'latest') -> str:
+    client = secretmanager.SecretManagerServiceClient()
+    secret_uri = f'projects/{get_project_id()}/secrets/{name}/versions/{version}'
+    response = client.access_secret_version(request={'name': secret_uri})
+    # This may raise `google.api_core.exceptions.NotFound`
+    return response.payload.data.decode('utf-8')
