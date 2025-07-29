2fe53d112227bb6e69e3bf7796dc06b4da614037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:05 +0000

    Add package thames/0.0.0/thames-0.0.0-py3-none-any.whl

---
+++ packages/thames/thames-0.0.0-py3-none-any.whl/thames/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2025 Riverlane Ltd. All rights reserved.
+
+thames: SDK for Quantum Error Correction
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
