3aec91301199eca6d38017330543aadbc8dbe39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:43 +0000

    Add package tess-asteroids/1.2.1/tess_asteroids-1.2.1.tar.gz

---
+++ packages/tess-asteroids/tess_asteroids-1.2.1.tar.gz/tess_asteroids-1.2.1/src/tess_asteroids/__init__.py
@@ -0,0 +1,17 @@
+import logging
+from os import path
+
+import pandas as pd
+
+# Configure logging
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+# Read in straps table
+loc = path.abspath(path.dirname(__file__))
+straps = pd.read_csv(f"{loc}/data/straps.csv", comment="#")
+
+__version__ = "1.2.1"
+__all__ = ["MovingTPF"]
+
+from .movingtpf import MovingTPF  # noqa: E402
