f0365c37196e5834b583363002e3b248b7e8c43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:43 +0000

    Add package clickhouse-df/0.1.4/clickhouse_df-0.1.4.tar.gz

---
+++ packages/clickhouse-df/clickhouse_df-0.1.4.tar.gz/clickhouse_df-0.1.4/clickhouse_df/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------------
+Created on 2025/5/23 13:11
+@author: ZhangYundi
+@email: yundi.xxii@outlook.com
+---------------------------------------------
+"""
+
+from .yck import connect, to_pandas, to_polars
+
+__version__ = "v0.1.4"
+
+__all__ = ["connect", "to_pandas", "to_polars"]
