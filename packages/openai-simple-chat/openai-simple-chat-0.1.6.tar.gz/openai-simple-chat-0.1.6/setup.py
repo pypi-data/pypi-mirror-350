c5d735ebed5b1ce21baca348358bbe23b9d06fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:56 +0000

    Add package openai-simple-chat/0.1.6/openai-simple-chat-0.1.6.tar.gz

---
+++ packages/openai-simple-chat/openai-simple-chat-0.1.6.tar.gz/openai-simple-chat-0.1.6/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import os
+from io import open
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), "r", encoding="utf-8") as fobj:
+    long_description = fobj.read()
+
+with open(os.path.join(here, "requirements.txt"), "r", encoding="utf-8") as fobj:
+    requires = [x.strip() for x in fobj.readlines() if x.strip()]
+
+
+setup(
+    name="openai-simple-chat",
+    version="0.1.6",
+    description="大模型对话标准接口。支持模板对话、JSON对话等。",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="rRR0VrFP",
+    maintainer="rRR0VrFP",
+    license="Apache License, Version 2.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords=["openai-simple-chat"],
+    packages=find_packages("."),
+    install_requires=requires,
+    zip_safe=False,
+    include_package_data=True,
+)
