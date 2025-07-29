bac8d8b2e3db15075a8d6c4c6b543aa5ef9ee8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:13 +0000

    Add package skypilot-nightly/1.0.0.dev20250524/skypilot_nightly-1.0.0.dev20250524-py3-none-any.whl

---
+++ packages/skypilot-nightly/skypilot_nightly-1.0.0.dev20250524-py3-none-any.whl/sky/dashboard/out/_next/static/chunks/pages/jobs-73d5e0c369d00346.js
@@ -0,0 +1,16 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[142],{1446:function(t,n,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/jobs",function(){return e(8929)}])},6521:function(t,n,e){"use strict";e.d(n,{Z:function(){return r}});/**
+ * @license lucide-react v0.407.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,e(998).Z)("FileSearch",[["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M4.268 21a2 2 0 0 0 1.727 1H18a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v3",key:"ms7g94"}],["path",{d:"m9 18-1.5-1.5",key:"1j6qii"}],["circle",{cx:"5",cy:"14",r:"3",key:"ufru5t"}]])},3610:function(t,n,e){"use strict";e.d(n,{Z:function(){return r}});/**
+ * @license lucide-react v0.407.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,e(998).Z)("MonitorPlay",[["path",{d:"M10 7.75a.75.75 0 0 1 1.142-.638l3.664 2.249a.75.75 0 0 1 0 1.278l-3.664 2.25a.75.75 0 0 1-1.142-.64z",key:"1pctta"}],["path",{d:"M12 17v4",key:"1riwvh"}],["path",{d:"M8 21h8",key:"1ev6f3"}],["rect",{x:"2",y:"3",width:"20",height:"14",rx:"2",key:"x3v2xh"}]])},3293:function(t,n,e){"use strict";e.d(n,{Z:function(){return r}});/**
+ * @license lucide-react v0.407.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,e(998).Z)("RefreshCcw",[["path",{d:"M21 12a9 9 0 0 0-9-9 9.75 9.75 0 0 0-6.74 2.74L3 8",key:"14sxne"}],["path",{d:"M3 3v5h5",key:"1xhq8a"}],["path",{d:"M3 12a9 9 0 0 0 9 9 9.75 9.75 0 0 0 6.74-2.74L21 16",key:"1hlbsb"}],["path",{d:"M16 16h5v5",key:"ccwih5"}]])},8929:function(t,n,e){"use strict";e.r(n),e.d(n,{default:function(){return h}});var r=e(5893),i=e(9008),c=e.n(i),a=e(938);function h(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)(c(),{children:(0,r.jsx)("title",{children:"Managed Jobs | SkyPilot Dashboard"})}),(0,r.jsx)(a.Nk,{})]})}},9008:function(t,n,e){t.exports=e(7219)}},function(t){t.O(0,[573,488,480,734,938,888,774,179],function(){return t(t.s=1446)}),_N_E=t.O()}]);
