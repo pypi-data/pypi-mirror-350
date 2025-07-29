d75c1ab878193a20571961edd52f11ae1b2b629c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:28 +0000

    Add package pPEGpy/0.3.4/ppegpy-0.3.4.tar.gz

---
+++ packages/pPEGpy/ppegpy-0.3.4.tar.gz/ppegpy-0.3.4/examples/same_again.py
@@ -0,0 +1,45 @@
+from pPEGpy import peg
+
+# Context Sensitive Grammars
+
+# using <@name> to match a name rule result the same-again
+
+# Markdown code quotes...
+
+code = peg.compile("""
+    Code = tics code tics
+    code = ~<@tics>*
+    tics = [`]+
+""")
+
+print(code.parse("```abcc``def```"))
+
+# Rust raw string syntax:
+
+raw = peg.compile("""
+    Raw   = fence '"' raw '"' fence
+    raw   = ~('"' <@fence>)*
+    fence = '#'+
+""")
+
+print(raw.parse("""##"abcc#"x"#def"##"""))
+
+# indented blocks...
+
+blocks = peg.compile("""
+    Blk    = inset line (more / inlay)*
+    more   = <@inset> !' ' line
+    inlay  = &(<@inset> ' ') Blk
+    inset  = ' '+
+    line   = ~[\n\r]* '\r'? '\n'?
+""")
+
+p = blocks.parse("""  line one
+  line two
+    inset 2.1
+      inset 3.1
+    inset 2.2
+  line three
+""")
+
+print(p)
