0385c062a0c810daa94447ee72930d75460f3c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:20:18 +0000

    Add package btutils/0.1.0/btutils-0.1.0-py3-none-any.whl

---
+++ packages/btutils/btutils-0.1.0-py3-none-any.whl/btutils/__init__.py
@@ -0,0 +1,10 @@
+"""
+BTUtils - A lightweight Python library for backtesting analysis and visualization
+"""
+
+__version__ = "0.1.0"
+
+from .btutils import Backtest, Stats, Plots
+
+if __name__ == "__main__":
+    pass
