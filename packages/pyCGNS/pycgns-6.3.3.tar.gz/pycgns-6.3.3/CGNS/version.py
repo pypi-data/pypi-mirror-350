7a74ec12723533b39a0d473e49f119c3f09d5085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:29 +0000

    Add package pyCGNS/6.3.3/pycgns-6.3.3.tar.gz

---
+++ packages/pyCGNS/pycgns-6.3.3.tar.gz/pycgns-6.3.3/CGNS/version.py
@@ -0,0 +1,8 @@
+
+version = "6.3.3"
+__version__ = version
+full_version = version
+
+git_revision = "b9e979829f35e9df0e1e02870bf14b4b6d9b48bf"
+release = 'dev' not in version and '+' not in version
+short_version = version.split("+")[0]
