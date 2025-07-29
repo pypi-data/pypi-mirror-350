714f28492b89aa15a9343769eb52ca8c3a8e4639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:51 +0000

    Add package jpyc-core-sdk/1.0.1/jpyc_core_sdk-1.0.1-py3-none-any.whl

---
+++ packages/jpyc-core-sdk/jpyc_core_sdk-1.0.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,15 @@
+# from importlib.metadata import version
+
+from .client import SdkClient
+from .jpyc import JPYC
+
+# __version__ = version("jpyc-core-sdk")
+__all__ = [
+    # "__version__",
+    # client
+    "SdkClient",
+    # jpyc
+    "JPYC",
+    # utils
+    "utils",
+]
