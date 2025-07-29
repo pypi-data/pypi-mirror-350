0edc984609d75f88e80044d4f51d281fc08ea86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:43 +0000

    Add package sap-ai-sdk-gen/2.9.9/sap_ai_sdk_gen-2.9.9.tar.gz

---
+++ packages/sap-ai-sdk-gen/sap_ai_sdk_gen-2.9.9.tar.gz/sap_ai_sdk_gen-2.9.9/setup.py
@@ -0,0 +1,47 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+
+def generate_metadata():
+    classifiers = """\
+    Development Status :: 1 - Planning
+    License :: Other/Proprietary License
+    Operating System :: MacOS
+    Operating System :: Microsoft :: Windows
+    Operating System :: POSIX :: Linux
+    Programming Language :: Python
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3.9
+    Programming Language :: Python :: 3.10
+    Programming Language :: Python :: 3.11
+    Programming Language :: Python :: 3.12
+    Topic :: Software Development :: Libraries :: Python Modules"""
+    metadata = dict(
+        summary="SDK for access to large language models in the Generative AI Hub",
+        description="SAP Cloud SDK for AI (Python): generative AI SDK",
+        long_description="# SAP Cloud SDK for AI (Python):generative AI SDK\n  will be published here soon ...",
+        long_description_content_type='text/markdown',
+        keywords="SAP AI Core, SAP AI Core API",
+        url="https://www.sap.com/",
+        author="SAP SE",
+        download_url="https://pypi.python.org/pypi/sap-ai-sdk-gen",
+        license='SAP DEVELOPER LICENSE AGREEMENT',
+        classifiers=[_f for _f in classifiers.split('\n') if _f],
+        platforms=["Windows", "Linux", "Mac OS-X", "Unix"],
+        version="2.9.9",
+        python_requires='>=3.9',
+        zip_safe=False,
+    )
+    return metadata
+
+
+setup(name="sap-ai-sdk-gen",
+      include_package_data=True,
+      **generate_metadata()
+      )
