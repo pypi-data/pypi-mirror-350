b255133932013e7eda27001b6bf7c9c0805243e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:48 +0000

    Add package mbe-mcp-server/0.1.5/mbe_mcp_server-0.1.5.tar.gz

---
+++ packages/mbe-mcp-server/mbe_mcp_server-0.1.5.tar.gz/mbe_mcp_server-0.1.5/README.md
@@ -0,0 +1,2 @@
+TODO
+韬略mcpserver 
---
+++ packages/mbe-mcp-server/mbe_mcp_server-0.1.5.tar.gz/mbe_mcp_server-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mbe_mcp_server",  # 项目名称
+    version="0.1.5",  # 版本号
+    description="韬略服务对外Mcp服务",  # 简要描述
+    long_description=open('README.md', encoding='utf-8').read(),
+    author="yanan.wya",  # 作者名字
+    author_email="wya0556@qq.com",  # 作者邮箱
+    url="",  # 项目
+    packages=find_packages(),  # 自动发现所有包
+    entry_points={
+        'console_scripts': [
+            # 定义命令行工具，运行 uvx mbe-mcp-server 时会执行 mbe_mcp_server.main:main
+            'mbe-mcp-server=mbe_mcp_server.main:main',
+        ],
+    },
+    install_requires=[
+        "mcp",  # 所依赖的第三方库，例如 mcp 库
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # 支持的 Python 版本
+)
