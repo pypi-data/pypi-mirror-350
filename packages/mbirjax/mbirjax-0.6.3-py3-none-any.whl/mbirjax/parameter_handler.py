1e7a74c33d28634fcff02004276211da368aedfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:09 +0000

    Add package mbirjax/0.6.3/mbirjax-0.6.3-py3-none-any.whl

---
+++ packages/mbirjax/mbirjax-0.6.3-py3-none-any.whl/mbirjax/parameter_handler.py
@@ -0,0 +1,359 @@
+import jax.numpy as jnp
+import numpy as np
+from ruamel.yaml import YAML
+import mbirjax._utils as utils
+import mbirjax as mj
+import warnings
+import copy
+import logging
+import io
+
+
+class ParameterHandler():
+    array_prefix = ':ARRAY:'
+
+    def __init__(self):
+
+        self.params = utils.get_default_params()
+        self.logger = None
+        self.log_buffer = None
+
+    def setup_logger(self, *, logfile_path: str = "./logs/recon.log", print_logs: bool = True):
+        """
+        Initialize self.logger and self.log_buffer.
+
+        Args:
+            logfile_path: Path to the log file. If None or empty, file logging is skipped.
+            verbosity: 0 -> WARNING, 1 -> INFO, 2+ -> DEBUG
+            print_logs: If True, emit logs to console.
+
+        Raises:
+            Exception: If logfile_path directory cannot be created.
+        """
+        # Map verbosity to logging level
+        verbose = self.get_params('verbose')
+        if verbose < 1:
+            level = logging.WARNING
+        elif verbose < 2:
+            level = logging.INFO
+        else:
+            level = logging.DEBUG
+
+        # Configure logger
+        logger = logging.getLogger(self.__class__.__name__)
+        logger.setLevel(level)
+        logger.handlers.clear()
+
+        # In-memory buffer handler (always enabled)
+        self.log_buffer = io.StringIO()
+        buffer_handler = logging.StreamHandler(self.log_buffer)
+        buffer_handler.setLevel(level)
+        buffer_formatter = logging.Formatter('%(message)s')
+        buffer_handler.setFormatter(buffer_formatter)
+        logger.addHandler(buffer_handler)
+
+        # Console handler
+        if print_logs:
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(level)
+            console_formatter = logging.Formatter('%(message)s')
+            console_handler.setFormatter(console_formatter)
+            logger.addHandler(console_handler)
+
+        # File handler (optional)
+        if logfile_path:
+            mj.makedirs(logfile_path)
+            file_handler = logging.FileHandler(logfile_path, mode='w')
+            file_handler.setLevel(level)
+            file_formatter = logging.Formatter('%(message)s')
+            file_handler.setFormatter(file_formatter)
+            logger.addHandler(file_handler)
+
+        self.logger = logger
+
+    def print_params(self):
+        """
+        Prints out the parameters of the model.
+        """
+        verbose = self.get_params('verbose')
+        print("----")
+        for key, entry in self.params.items():
+            if verbose < 3 and key == 'view_params_array':
+                continue
+            param_val = entry.get('val')
+            if verbose < 3:
+                print("{} = {}".format(key, param_val))
+            else:
+                recompile_flag = entry.get('recompile_flag')
+                print("{} = {}, recompile_flag = {}".format(key, param_val, recompile_flag))
+        print("----")
+
+    @staticmethod
+    def convert_arrays_to_strings(cur_params):
+        """
+        Replaces any jax or numpy arrays in cur_params with a flattened string representation and the array shape.
+        Args:
+            cur_params (dict): Parameter dictionary
+
+        Returns:
+            dict: The same dictionary with arrays replaced by strings.
+        """
+        for key, entry in cur_params.items():
+            param_val = entry.get('val')
+            if isinstance(param_val, (jnp.ndarray, np.ndarray)):
+                # Get the array values, then flatten them and put them in a string.
+                cur_array = np.array(param_val)
+                formatted_string = " ".join(f"{x:.7f}" for x in cur_array.flatten())
+                # Include a prefix for identification upon reading
+                new_val = ParameterHandler.array_prefix + formatted_string
+                cur_params[key]['val'] = new_val
+                cur_params[key]['shape'] = param_val.shape
+
+            # Also convert np.floats to native python floats
+            if isinstance(param_val, np.floating):
+                new_val = param_val.item()
+                cur_params[key]['val'] = new_val
+
+        return cur_params
+
+    @staticmethod
+    def convert_strings_to_arrays(cur_params):
+        """
+        Convert the string representation of an array back to an array.
+        Args:
+            cur_params (dict): Parameter dictionary
+
+        Returns:
+            dict: The same dictionary with array strings replaced by arrays.
+        """
+        array_prefix = ParameterHandler.array_prefix
+        for key, entry in cur_params.items():
+            param_val = entry.get('val')
+            # CHeck for a string with the array marker as prefix.
+            if type(param_val) is str and param_val[0:len(array_prefix)] == array_prefix:
+                # Strip the prefix, then remove the delimiters
+                param_str = param_val[len(array_prefix):]
+                clean_str = param_str.replace('[', '').replace(']', '').strip()
+                # Read to a flat array, then reshape
+                new_val = jnp.array(np.fromstring(clean_str + ' ', sep=' '))
+                new_shape = cur_params[key]['shape']
+                # Save the value and remove the 'shape' key, which is needed only for the yaml file.
+                cur_params[key]['val'] = new_val.reshape(new_shape)
+                del cur_params[key]['shape']
+
+        return cur_params
+
+    def save_params(self, filename=None):
+        """
+        Serialize parameters to YAML. If filename is provided, write to file; otherwise return YAML text.
+
+        Args:
+            filename (str or None): Path to save YAML file (must end in .yml/.yaml). If None, return YAML string.
+
+        Returns:
+            None if filename was provided; otherwise, YAML-formatted string of parameters.
+
+        Raises:
+            ValueError: If filename is invalid.
+        """
+        # Prepare parameter dict
+        output_params = ParameterHandler.convert_arrays_to_strings(copy.deepcopy(self.params))
+        for key in output_params:
+            val = output_params[key]['val']
+            if isinstance(val, list):
+                output_params[key]['val'] = tuple(val)
+
+        yaml = YAML()
+        yaml.default_flow_style = False
+
+        if filename:
+            if not filename.lower().endswith(('.yml', '.yaml')):
+                raise ValueError(f"Filename must end in .yaml or .yml: {filename}")
+            # Ensure output directory exists
+            mj.makedirs(filename)
+            with open(filename, 'w') as file:
+                yaml.dump(output_params, file)
+            return None
+        else:
+            stream = io.StringIO()
+            yaml.dump(output_params, stream)
+            return stream.getvalue()
+
+    @staticmethod
+    def load_param_dict(filename, required_param_names=None, values_only=True):
+        """
+        Load parameter dictionary from yaml file.
+
+        Args:
+            filename (str): Path to load to store the parameter dictionary.  Must end in .yml or .yaml
+            required_param_names (list of strings): List of parameter names that are required for a class.
+            values_only (bool):  If True, then extract and return the values of each entry only.
+
+        Returns:
+            required_params (dict): Dictionary of required parameter entries.
+            params (dict): Dictionary of all other parameters.
+        """
+        # Determine file type
+        if filename[-4:] == '.yml' or filename[-5:] == '.yaml':
+            # Save the full parameter dictionary
+            with open(filename, 'r') as file:
+                yaml = YAML(typ="safe")
+                params = yaml.load(file)
+                params = ParameterHandler.convert_strings_to_arrays(params)
+
+        # Convert any lists to tuples for consistency with save
+        for key in params.keys():
+            if isinstance(params[key]['val'], list):
+                params[key]['val'] = tuple(params[key]['val'])
+
+        # Separate the required parameters into a new dict and delete those entries from the original
+        required_params = dict()
+        for name in required_param_names:
+            required_params[name] = params[name]
+            del params[name]
+
+        if values_only:
+            for key in required_params.keys():
+                required_params[key] = required_params[key]['val']
+            for key in params.keys():
+                params[key] = params[key]['val']
+        return required_params, params
+
+    def set_params(self, no_warning=False, no_compile=False, **kwargs):
+        """
+        Updates parameters using keyword arguments.
+        After setting parameters, it checks if key geometry-related parameters have changed and, if so, recompiles the projectors.
+
+        Args:
+            no_warning (bool, optional, default=False): This is used internally to allow for some initial parameter setting.
+            no_compile (bool, optional, default=False): Prevent (re)compiling the projectors.  Used for initialization.
+            **kwargs: Arbitrary keyword arguments where keys are parameter names and values are the new parameter values.
+        """
+        # Get initial geometry parameters
+        recompile = False
+        regularization_parameter_change = False
+        meta_parameter_change = False
+
+        # Set all the given parameters
+        for key, val in kwargs.items():
+            # Default to forcing a recompile for new parameters
+            recompile_flag = True
+
+            if key in self.params.keys():
+                recompile_flag = self.params[key]['recompile_flag']
+            elif not no_warning:  # Check if this is a valid parameter.  This is disabled for initialization.
+                error_message = '{} is not a recognized parameter'.format(key)
+                error_message += '\nValid parameters are: \n'
+                for valid_key in self.params.keys():
+                    error_message += '   {}\n'.format(valid_key)
+                raise ValueError(error_message)
+
+            new_entry = {'val': val, 'recompile_flag': recompile_flag}
+            self.params[key] = new_entry
+
+            # Handle special cases
+            if recompile_flag:
+                recompile = True
+            elif key in ["sigma_y", "sigma_x", "sigma_prox"]:
+                regularization_parameter_change = True
+            elif key in ["sharpness", "snr_db"]:
+                meta_parameter_change = True
+
+        # Check for valid parameters
+        if not no_warning:
+            self.verify_valid_params()
+
+        # Handle case if any regularization parameter changed
+        if regularization_parameter_change:
+            if not no_warning:
+                self.set_params(auto_regularize_flag=False)
+                warnings.warn('You are directly setting regularization parameters, sigma_x, sigma_y or sigma_prox. '
+                              'This is an advanced feature that will disable auto-regularization.')
+
+        # Handle case if any meta regularization parameter changed
+        if meta_parameter_change:
+            if self.get_params('auto_regularize_flag') is False:
+                self.set_params(auto_regularize_flag=True)
+                if not no_warning:
+                    warnings.warn('You have re-enabled auto-regularization by setting sharpness or snr_db. '
+                                  'It was previously disabled')
+
+        # Return a flag to signify recompiling
+        recompile_flag = False
+        if recompile and not no_compile:
+            recompile_flag = True
+
+        return recompile_flag
+
+    @staticmethod
+    def get_params_from_dict(param_dict, parameter_names):
+        """
+        Get the values of the listed parameter names from the supplied dict.
+        Raises an exception if a parameter name is not defined in parameters.
+
+        Args:
+            param_dict (dict): The dictionary of parameters
+            parameter_names (str or list of str): String or list of strings
+
+        Returns:
+            Single value or list of values
+        """
+        if isinstance(parameter_names, str):
+            if parameter_names in param_dict.keys():
+                value = param_dict[parameter_names]['val']
+            else:
+                raise NameError('"{}" is not a recognized argument'.format(parameter_names))
+            return value
+        values = []
+        for name in parameter_names:
+            if name in param_dict.keys():
+                values.append(param_dict[name]['val'])
+            else:
+                raise NameError('"{}" is not a recognized argument'.format(name))
+        return values
+
+    def get_params(self, parameter_names):
+        """
+        Get the values of the listed parameter names from the internal parameter dict.
+        Raises an exception if a parameter name is not defined in parameters.
+
+        Args:
+            parameter_names (str or list of str): String or list of strings
+
+        Returns:
+            Single value or list of values
+        """
+        param_values = ParameterHandler.get_params_from_dict(self.params, parameter_names)
+        return param_values
+
+    def get_magnification(self):
+        """
+        Compute the scale factor from a voxel at iso (at the origin on the center of rotation) to
+        its projection on the detector.  For parallel beam, this is 1, but it may be parameter-dependent
+        for other geometries.
+
+        Returns:
+            (float): magnification
+        """
+        raise NotImplementedError('get_magnification is not implemented.')
+
+    def verify_valid_params(self):
+        """
+        Verify any conditions that must be satisfied among parameters for correct projections.
+
+        Subclasses of TomographyModel should call super().verify_valid_params() before checking any
+        subclass-specific conditions.
+
+        Note:
+            Raises ValueError for invalid parameters.
+        """
+
+        sinogram_shape = self.get_params('sinogram_shape')
+        if len(sinogram_shape) != 3:
+            error_message = "sinogram_shape must be (views, rows, channels). \n"
+            error_message += "Got {} for sinogram shape.".format(sinogram_shape)
+            raise ValueError(error_message)
+
+        geometry_type = self.get_params('geometry_type')
+        if geometry_type != str(type(self)):
+            raise ValueError('Parameters are associated with {}, but the current model is {}'.format(geometry_type, str(type(self))))
---
+++ packages/mbirjax/mbirjax-0.6.3-py3-none-any.whl/tests/test_projectors.py
@@ -0,0 +1,242 @@
+import numpy as np
+import os
+import jax
+import jax.numpy as jnp
+import mbirjax
+import unittest
+
+
+class TestProjectors(unittest.TestCase):
+    """
+    Test the adjoint property of the forward and back projectors, both the full versions and the sparse voxel version.
+    This means if x is an image, and y is a sinogram, then <y, Ax> = <Aty, x>.
+    The code below verifies this for the full forward and back projectors and the versions that specify a
+    subset of voxels in x.
+    This code also verifies that first applying the full back projector and selecting the voxels to get (Aty)[ss]
+    is the same as using the subset back projector with the specified set of voxels.
+    """
+
+    def setUp(self):
+        """Set up before each test method."""
+        # Choose the geometry type
+        self.geometry_types = mbirjax._utils._geometry_types_for_tests
+
+        # Set parameters
+        self.num_views = 64
+        self.num_det_rows = 40
+        self.num_det_channels = 128
+        self.sharpness = 0.0
+
+        # These can be adjusted to describe the geometry in the cone beam case.
+        # np.Inf is an allowable value, in which case this is essentially parallel beam
+        self.source_detector_dist = 4 * self.num_det_channels
+        self.source_iso_dist = self.source_detector_dist
+
+        # Initialize sinogram
+        self.sinogram_shape = (self.num_views, self.num_det_rows, self.num_det_channels)
+        self.angles = None
+
+    def tearDown(self):
+        """Clean up after each test method."""
+        pass
+
+    def set_angles(self, geometry_type):
+        if geometry_type == 'cone':
+            detector_cone_angle = 2 * np.arctan2(self.num_det_channels / 2, self.source_detector_dist)
+        else:
+            detector_cone_angle = 0
+        start_angle = -(np.pi + detector_cone_angle) * (1 / 2)
+        end_angle = (np.pi + detector_cone_angle) * (1 / 2)
+        self.angles = jnp.linspace(start_angle, end_angle, self.num_views, endpoint=False)
+
+    def get_model(self, geometry_type):
+        if geometry_type == 'cone':
+            ct_model = mbirjax.ConeBeamModel(self.sinogram_shape, self.angles,
+                                             source_detector_dist=self.source_detector_dist,
+                                             source_iso_dist=self.source_iso_dist)
+        elif geometry_type == 'parallel':
+            ct_model = mbirjax.ParallelBeamModel(self.sinogram_shape, self.angles)
+        else:
+            raise ValueError('Invalid geometry type.  Expected cone or parallel, got {}'.format(geometry_type))
+
+        return ct_model
+
+    def test_all_adjoints(self):
+        for geometry_type in self.geometry_types:
+            with self.subTest(geometry_type=geometry_type):
+                print("Testing adjoint with", geometry_type)
+                self.verify_adjoint(geometry_type)
+
+    def test_all_hessians(self):
+        for geometry_type in self.geometry_types:
+            with self.subTest(geometry_type=geometry_type):
+                print("Testing Hessian with", geometry_type)
+                self.verify_hessian(geometry_type)
+
+    def test_save_load(self):
+        for geometry_type in self.geometry_types:
+            with self.subTest(geometry_type=geometry_type):
+                print("Testing save/load with", geometry_type)
+                self.verify_save_load(geometry_type)
+
+    def verify_save_load(self, geometry_type):
+        """
+        Verify the adjoint property of the projectors:
+        Choose a random phantom, x, and a random sinogram, y, and verify that <y, Ax> = <Aty, x>.
+        """
+        self.set_angles(geometry_type)
+        ct_model = self.get_model(geometry_type)
+
+        # Generate phantom
+        recon_shape = ct_model.get_params('recon_shape')
+        num_recon_rows, num_recon_cols, num_recon_slices = recon_shape[:3]
+
+        # Get the vector of indices
+        indices = jnp.arange(num_recon_rows * num_recon_cols)
+
+        # ##########################
+        # Do a forward and back projection from a single pixel
+        i, j = num_recon_rows // 4, num_recon_cols // 3
+        x = jnp.zeros(recon_shape)
+        x = x.at[i, j, :].set(1)
+        voxel_values = x.reshape((-1, num_recon_slices))[indices]
+
+        Ax = ct_model.sparse_forward_project(voxel_values, indices)
+        Aty = ct_model.sparse_back_project(Ax, indices)
+        Aty = ct_model.reshape_recon(Aty)
+
+        # Save the model
+        filename = 'saved_model_test.yaml'
+        ct_model.to_file(filename)
+
+        # Load the model
+        new_model = self.get_model(geometry_type)
+        import warnings
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            new_model = new_model.from_file(filename)
+        if os.path.exists(filename):
+            os.remove(filename)
+
+        # Compare parameters
+        for key, entry in ct_model.params.items():
+            if isinstance(entry['val'], list):
+                entry['val'] = tuple(entry['val'])
+            loaded_entry = new_model.params[key]
+            if isinstance(entry['val'], type(jnp.zeros(1))):
+                assert(jnp.allclose(entry['val'], loaded_entry['val']))
+            else:
+                assert(entry == loaded_entry)
+
+        # Do a forward and back projection with loaded model
+        Ax_new = new_model.sparse_forward_project(voxel_values, indices)
+        Aty_new = new_model.sparse_back_project(Ax_new, indices)
+        Aty_new = new_model.reshape_recon(Aty_new)
+
+        # Compare to original
+        assert(np.allclose(Aty, Aty_new, atol=1e-4))
+
+    def verify_adjoint(self, geometry_type):
+        """
+        Verify the adjoint property of the projectors:
+        Choose a random phantom, x, and a random sinogram, y, and verify that <y, Ax> = <Aty, x>.
+        """
+        self.set_angles(geometry_type)
+        ct_model = self.get_model(geometry_type)
+
+        # Initialize a random key
+        seed_value = np.random.randint(1000000)
+        key = jax.random.PRNGKey(seed_value)
+
+        # Generate phantom
+        recon_shape = ct_model.get_params('recon_shape')
+        num_recon_rows, num_recon_cols, num_recon_slices = recon_shape[:3]
+        phantom = mbirjax.gen_cube_phantom(recon_shape)
+
+        # Generate indices of pixels
+        num_subsets = 1
+        full_indices = mbirjax.gen_pixel_partition(recon_shape, num_subsets)
+
+        # Generate sinogram data
+        voxel_values = phantom.reshape((-1,) + recon_shape[2:])[full_indices]
+
+        # Compute forward projection
+        sinogram = ct_model.sparse_forward_project(voxel_values[0], full_indices[0])
+
+        # Get the vector of indices
+        indices = jnp.arange(num_recon_rows * num_recon_cols)
+        num_trials = 3
+        indices = jnp.mod(np.arange(num_trials, dtype=int).reshape((-1, 1)) + indices.reshape((1, -1)),
+                          num_recon_rows * num_recon_cols)
+
+        # Convert to jax arrays
+        sinogram = jnp.array(sinogram)
+        indices = jnp.array(indices)
+
+        # Run once to finish compiling and get backprojection shape
+        bp = ct_model.sparse_back_project(sinogram, indices[0])
+
+        # ##########################
+        # Test the adjoint property
+        # Get a random 3D phantom to test the adjoint property
+        key, subkey = jax.random.split(key)
+        x = jax.random.uniform(subkey, shape=bp.shape)
+        key, subkey = jax.random.split(key)
+        y = jax.random.uniform(subkey, shape=sinogram.shape)
+
+        # Do a forward projection, then a backprojection
+        voxel_values = x.reshape((-1, num_recon_slices))[indices[0]]
+        Ax = ct_model.sparse_forward_project(voxel_values, indices[0])
+        Aty = ct_model.sparse_back_project(y, indices[0])
+
+        # Calculate <Aty, x> and <y, Ax>
+        Aty_x = jnp.sum(Aty * x)
+        y_Ax = jnp.sum(y * Ax)
+
+        # Determine if property holds
+        adjoint_test_result = np.allclose(Aty_x, y_Ax)
+        self.assertTrue(adjoint_test_result)
+
+    def verify_hessian(self, geometry_type):
+        """
+        Verify the hessian property of the back projector:
+        Choose a random pixel, set it to epsilon, apply A^T A and compare to the value from compute_hessian_diagaonal.
+        """
+        self.set_angles(geometry_type)
+        ct_model = self.get_model(geometry_type)
+
+        # ## Test the hessian against a finite difference approximation ## #
+        hessian = ct_model.compute_hessian_diagonal()
+
+        # Initialize a random key
+        seed_value = np.random.randint(1000000)
+        key = jax.random.PRNGKey(seed_value)
+
+        recon_shape = ct_model.get_params('recon_shape')
+        num_recon_rows, num_recon_cols, num_recon_slices = recon_shape[:3]
+        x = jnp.zeros(recon_shape)
+        key, subkey = jax.random.split(key)
+        i, j = jax.random.randint(subkey, shape=(2,), minval=0, maxval=num_recon_rows)
+        key, subkey = jax.random.split(key)
+        k = jax.random.randint(subkey, shape=(), minval=0, maxval=num_recon_slices)
+
+        # Get the vector of indices
+        indices = jnp.arange(num_recon_rows * num_recon_cols)
+        # num_trials = 3
+        # indices = jnp.mod(np.arange(num_trials, dtype=int).reshape((-1, 1)) + indices.reshape((1, -1)),
+        #                   num_recon_rows * num_recon_cols)
+
+        eps = 0.01
+        x = x.at[i, j, k].set(eps)
+        voxel_values = x.reshape((-1, num_recon_slices))[indices]
+        Ax = ct_model.sparse_forward_project(voxel_values, indices)
+        AtAx = ct_model.sparse_back_project(Ax, indices).reshape(x.shape)
+        finite_diff_hessian = AtAx[i, j, k] / eps
+
+        # Determine if property holds
+        hessian_test_result = jnp.allclose(hessian.reshape(x.shape)[i, j, k], finite_diff_hessian)
+        self.assertTrue(hessian_test_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
