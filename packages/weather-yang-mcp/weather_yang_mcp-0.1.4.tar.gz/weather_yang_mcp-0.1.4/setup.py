01e9b9152876e01c3904498b76ecd2ab5f0dc9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:10:06 +0000

    Add package weather-yang-mcp/0.1.4/weather_yang_mcp-0.1.4.tar.gz

---
+++ packages/weather-yang-mcp/weather_yang_mcp-0.1.4.tar.gz/weather_yang_mcp-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name="weather_yang_mcp",  # 包名，pip install 时用这个
+    version="0.1.4",
+    description="A get weather tool",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="jokul",
+    author_email="test@qq.com",
+    url="https://github.com/jokulyang/mytool",  # 可选：放 GitHub 仓库地址
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
