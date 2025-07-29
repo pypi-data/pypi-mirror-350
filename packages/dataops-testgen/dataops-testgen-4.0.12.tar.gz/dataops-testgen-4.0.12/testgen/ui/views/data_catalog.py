260e291b64accb0c3c7212af1da089117ee609f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:07 +0000

    Add package dataops-testgen/4.0.12/dataops-testgen-4.0.12.tar.gz

---
+++ packages/dataops-testgen/dataops-testgen-4.0.12.tar.gz/dataops-testgen-4.0.12/testgen/ui/views/data_catalog.py
@@ -0,0 +1,312 @@
+import json
+import typing
+from collections import defaultdict
+from datetime import datetime
+from functools import partial
+
+import pandas as pd
+import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
+
+import testgen.ui.services.database_service as db
+import testgen.ui.services.query_service as dq
+from testgen.ui.components import widgets as testgen
+from testgen.ui.components.widgets import testgen_component
+from testgen.ui.navigation.menu import MenuItem
+from testgen.ui.navigation.page import Page
+from testgen.ui.queries import project_queries
+from testgen.ui.queries.profiling_queries import TAG_FIELDS, get_column_by_id, get_hygiene_issues, get_table_by_id
+from testgen.ui.services import user_session_service
+from testgen.ui.session import session
+from testgen.ui.views.dialogs.data_preview_dialog import data_preview_dialog
+from testgen.ui.views.dialogs.run_profiling_dialog import run_profiling_dialog
+from testgen.utils import friendly_score, score
+
+PAGE_ICON = "dataset"
+PAGE_TITLE = "Data Catalog"
+
+
+class DataCatalogPage(Page):
+    path = "data-catalog"
+    can_activate: typing.ClassVar = [
+        lambda: session.authentication_status,
+        lambda: "project_code" in st.query_params,
+    ]
+    menu_item = MenuItem(icon=PAGE_ICON, label=PAGE_TITLE, section="Data Profiling", order=0)
+
+    def render(self, project_code: str, table_group_id: str | None = None, selected: str | None = None, **_kwargs) -> None:
+        testgen.page_header(
+            PAGE_TITLE,
+        )
+
+        user_can_navigate = not user_session_service.user_has_catalog_role()
+
+        if render_empty_state(project_code, user_can_navigate):
+            return
+
+        group_filter_column, _, loading_column = st.columns([.3, .5, .2], vertical_alignment="center")
+
+        with group_filter_column:
+            table_groups_df = get_table_group_options(project_code)
+            table_group_id = testgen.select(
+                options=table_groups_df,
+                value_column="id",
+                display_column="table_groups_name",
+                default_value=table_group_id,
+                required=True,
+                label="Table Group",
+                bind_to_query="table_group_id",
+            )
+
+        with loading_column:
+            columns_df = get_table_group_columns(table_group_id)
+            selected_item = get_selected_item(selected, table_group_id)
+            if selected_item:
+                selected_item["project_code"] = project_code
+                selected_item["connection_id"] = str(
+                    table_groups_df.loc[table_groups_df["id"] == table_group_id].iloc[0]["connection_id"])
+            else:
+                self.router.set_query_params({ "selected": None })
+
+        if columns_df.empty:
+            table_group = table_groups_df.loc[table_groups_df["id"] == table_group_id].iloc[0]
+            testgen.empty_state(
+                label="No profiling data yet",
+                icon=PAGE_ICON,
+                message=testgen.EmptyStateMessage.Profiling,
+                action_label="Run Profiling",
+                action_disabled=not user_session_service.user_can_edit(),
+                button_onclick=partial(run_profiling_dialog, project_code, table_group),
+                button_icon="play_arrow",
+            )
+        else:
+            def on_item_selected(item_id):
+                self.router.set_query_params({ "selected": item_id })
+
+            testgen_component(
+                "data_catalog",
+                props={
+                    "columns": columns_df.to_json(orient="records"),
+                    "selected": json.dumps(selected_item),
+                    "tag_values": get_tag_values(),
+                    "last_saved_timestamp": st.session_state.get("data_catalog:last_saved_timestamp"),
+                    "permissions": {
+                        "can_edit": user_session_service.user_can_disposition(),
+                        "can_navigate": user_can_navigate,
+                    },
+                },
+                on_change_handlers={
+                    "ItemSelected": on_item_selected,
+                    "DataPreviewClicked": lambda item: data_preview_dialog(
+                        item["table_group_id"],
+                        item["schema_name"],
+                        item["table_name"],
+                        item.get("column_name"),
+                    ),
+                },
+                event_handlers={ "TagsChanged": partial(on_tags_changed, loading_column) },
+            )
+
+
+def on_tags_changed(spinner_container: DeltaGenerator, payload: dict) -> None:
+    attributes = ["description"]
+    attributes.extend(TAG_FIELDS)
+    cde_value_map = {
+        True: "TRUE",
+        False: "FALSE",
+        None: "NULL",
+    }
+
+    tags = payload["tags"]
+    set_attributes = [ f"{key} = NULLIF('{tags.get(key) or ''}', '')" for key in attributes if key in tags ]
+    if "critical_data_element" in tags:
+        set_attributes.append(f"critical_data_element = {cde_value_map[tags.get('critical_data_element')]}")
+
+    tables = []
+    columns = []
+    for item in payload["items"]:
+        id_list = tables if item["type"] == "table" else columns
+        id_list.append(item["id"])
+
+    schema = st.session_state["dbschema"]
+
+    with spinner_container:
+        with st.spinner("Saving tags"):
+            if tables:
+                db.execute_sql(f"""
+                UPDATE {schema}.data_table_chars
+                SET {', '.join(set_attributes)}
+                WHERE table_id IN ({", ".join([ f"'{item}'" for item in tables ])});
+                """)
+
+            if columns:
+                db.execute_sql(f"""
+                UPDATE {schema}.data_column_chars
+                SET {', '.join(set_attributes)}
+                WHERE column_id IN ({", ".join([ f"'{item}'" for item in columns ])});
+                """)
+
+    for func in [ get_table_group_columns, get_table_by_id, get_column_by_id, get_tag_values ]:
+        func.clear()
+    st.session_state["data_catalog:last_saved_timestamp"] = datetime.now().timestamp()
+    st.rerun()
+
+
+def render_empty_state(project_code: str, user_can_navigate: bool) -> bool:
+    project_summary_df = project_queries.get_summary_by_code(project_code)
+    if project_summary_df["profiling_runs_ct"]: # Without profiling, we don't have any table and column information in db
+        return False
+
+    label="Your project is empty"
+    testgen.whitespace(5)
+    if not project_summary_df["connections_ct"]:
+        testgen.empty_state(
+            label=label,
+            icon=PAGE_ICON,
+            message=testgen.EmptyStateMessage.Connection,
+            action_label="Go to Connections",
+            action_disabled=not user_can_navigate,
+            link_href="connections",
+            link_params={ "project_code": project_code },
+        )
+    else:
+        testgen.empty_state(
+            label=label,
+            icon=PAGE_ICON,
+            message=testgen.EmptyStateMessage.Profiling if project_summary_df["table_groups_ct"] else testgen.EmptyStateMessage.TableGroup,
+            action_label="Go to Table Groups",
+            action_disabled=not user_can_navigate,
+            link_href="connections:table-groups",
+            link_params={ "connection_id": str(project_summary_df["default_connection_id"]) }
+        )
+    return True
+
+
+@st.cache_data(show_spinner=False)
+def get_table_group_options(project_code):
+    schema = st.session_state["dbschema"]
+    return dq.run_table_groups_lookup_query(schema, project_code)
+
+
+@st.cache_data(show_spinner="Loading data ...")
+def get_table_group_columns(table_group_id: str) -> pd.DataFrame:
+    schema = st.session_state["dbschema"]
+    sql = f"""
+    SELECT CONCAT('column_', column_chars.column_id) AS column_id,
+        CONCAT('table_', table_chars.table_id) AS table_id,
+        column_chars.column_name,
+        table_chars.table_name,
+        column_chars.general_type,
+        column_chars.functional_data_type,
+        column_chars.drop_date,
+        table_chars.drop_date AS table_drop_date,
+        column_chars.critical_data_element,
+        table_chars.critical_data_element AS table_critical_data_element,
+        {", ".join([ f"column_chars.{tag}" for tag in TAG_FIELDS ])},
+        {", ".join([ f"table_chars.{tag} AS table_{tag}" for tag in TAG_FIELDS ])}
+    FROM {schema}.data_column_chars column_chars
+        LEFT JOIN {schema}.data_table_chars table_chars ON (
+            column_chars.table_id = table_chars.table_id
+        )
+    WHERE column_chars.table_groups_id = '{table_group_id}'
+    ORDER BY table_name, ordinal_position;
+    """
+    return db.retrieve_data(sql)
+
+
+def get_selected_item(selected: str, table_group_id: str) -> dict | None:
+    if not selected:
+        return None
+
+    item_type, item_id = selected.split("_", 2)
+
+    if item_type == "table":
+        item = get_table_by_id(item_id, table_group_id)
+    elif item_type == "column":
+        item = get_column_by_id(item_id, table_group_id, include_tags=True, include_has_test_runs=True, include_scores=True)
+    else:
+        return None
+
+    if item:
+        item["dq_score"] = friendly_score(score(item["dq_score_profiling"], item["dq_score_testing"]))
+        item["dq_score_profiling"] = friendly_score(item["dq_score_profiling"])
+        item["dq_score_testing"] = friendly_score(item["dq_score_testing"])
+        item["hygiene_issues"] = get_hygiene_issues(item["profile_run_id"], item["table_name"], item.get("column_name"))
+        item["test_issues"] = get_latest_test_issues(item["table_group_id"], item["table_name"], item.get("column_name"))
+        return item
+
+
+@st.cache_data(show_spinner=False)
+def get_latest_test_issues(table_group_id: str, table_name: str, column_name: str | None = None) -> dict | None:
+    schema = st.session_state["dbschema"]
+
+    column_condition = ""
+    if column_name:
+        column_condition = f"AND column_names = '{column_name}'"
+
+    sql = f"""
+    SELECT test_results.id::VARCHAR(50),
+        column_names AS column_name,
+        test_name_short AS test_name,
+        result_status,
+        result_message,
+        test_suite,
+        test_results.test_run_id::VARCHAR(50),
+        EXTRACT(EPOCH FROM test_starttime) * 1000 AS test_run_date
+    FROM {schema}.test_suites
+        LEFT JOIN {schema}.test_runs ON (
+            test_suites.last_complete_test_run_id = test_runs.id
+        )
+        LEFT JOIN {schema}.test_results ON (
+            test_runs.id = test_results.test_run_id
+        )
+        LEFT JOIN {schema}.test_types ON (
+            test_results.test_type = test_types.test_type
+        )
+    WHERE test_suites.table_groups_id = '{table_group_id}'
+        AND table_name = '{table_name}'
+        {column_condition}
+        AND result_status <> 'Passed'
+        AND COALESCE(disposition, 'Confirmed') = 'Confirmed'
+    ORDER BY
+        CASE result_status
+            WHEN 'Failed' THEN 1
+            WHEN 'Warning' THEN 2
+            ELSE 3
+        END,
+        column_name;
+    """
+
+    df = db.retrieve_data(sql)
+    return [row.to_dict() for _, row in df.iterrows()]
+
+
+@st.cache_data(show_spinner=False)
+def get_tag_values() -> dict[str, list[str]]:
+    schema = st.session_state["dbschema"]
+
+    quote = lambda v: f"'{v}'"
+    sql = f"""
+    SELECT DISTINCT
+        UNNEST(array[{', '.join([quote(t) for t in TAG_FIELDS])}]) as tag,
+        UNNEST(array[{', '.join(TAG_FIELDS)}]) AS value
+    FROM {schema}.data_column_chars
+    UNION
+    SELECT DISTINCT
+        UNNEST(array[{', '.join([quote(t) for t in TAG_FIELDS])}]) as tag,
+        UNNEST(array[{', '.join(TAG_FIELDS)}]) AS value
+    FROM {schema}.data_table_chars
+    UNION
+    SELECT DISTINCT
+        UNNEST(array[{', '.join([quote(t) for t in TAG_FIELDS if t != 'aggregation_level'])}]) as tag,
+        UNNEST(array[{', '.join([ t for t in TAG_FIELDS if t != 'aggregation_level'])}]) AS value
+    FROM {schema}.table_groups
+    ORDER BY value
+    """
+    df = db.retrieve_data(sql)
+
+    values = defaultdict(list)
+    for _, row in df.iterrows():
+        if row["tag"] and row["value"]:
+            values[row["tag"]].append(row["value"])
+    return values
---
+++ packages/dataops-testgen/dataops-testgen-4.0.12.tar.gz/dataops-testgen-4.0.12/testgen/ui/views/profiling_results.py
@@ -0,0 +1,256 @@
+import json
+import typing
+
+import streamlit as st
+
+import testgen.ui.queries.profiling_queries as profiling_queries
+import testgen.ui.services.database_service as db
+import testgen.ui.services.form_service as fm
+from testgen.common import date_service
+from testgen.ui.components import widgets as testgen
+from testgen.ui.components.widgets.testgen_component import testgen_component
+from testgen.ui.navigation.page import Page
+from testgen.ui.services import project_service, user_session_service
+from testgen.ui.session import session
+from testgen.ui.views.dialogs.data_preview_dialog import data_preview_dialog
+
+FORM_DATA_WIDTH = 400
+
+
+class ProfilingResultsPage(Page):
+    path = "profiling-runs:results"
+    can_activate: typing.ClassVar = [
+        lambda: session.authentication_status,
+        lambda: not user_session_service.user_has_catalog_role(),
+        lambda: "run_id" in st.query_params or "profiling-runs",
+    ]
+
+    def render(self, run_id: str, table_name: str | None = None, column_name: str | None = None, **_kwargs) -> None:
+        run_df = profiling_queries.get_run_by_id(run_id)
+        if run_df.empty:
+            self.router.navigate_with_warning(
+                f"Profiling run with ID '{run_id}' does not exist. Redirecting to list of Profiling Runs ...",
+                "profiling-runs",
+            )
+            return
+
+        run_date = date_service.get_timezoned_timestamp(st.session_state, run_df["profiling_starttime"])
+        project_service.set_sidebar_project(run_df["project_code"])
+
+        testgen.page_header(
+            "Data Profiling Results",
+            "view-data-profiling-results",
+            breadcrumbs=[
+                { "label": "Profiling Runs", "path": "profiling-runs", "params": { "project_code": run_df["project_code"] } },
+                { "label": f"{run_df['table_groups_name']} | {run_date}" },
+            ],
+        )
+
+        table_filter_column, column_filter_column, sort_column, export_button_column = st.columns(
+            [.3, .3, .08, .32], vertical_alignment="bottom"
+        )
+
+        with table_filter_column:
+            # Table Name filter
+            df = get_profiling_run_tables(run_id)
+            table_name = testgen.select(
+                options=df,
+                value_column="table_name",
+                default_value=table_name,
+                bind_to_query="table_name",
+                label="Table Name",
+            )
+
+        with column_filter_column:
+            # Column Name filter
+            df = get_profiling_run_columns(run_id, table_name)
+            column_name = testgen.select(
+                options=df,
+                value_column="column_name",
+                default_value=column_name,
+                bind_to_query="column_name",
+                label="Column Name",
+                disabled=not table_name,
+            )
+
+        with sort_column:
+            sortable_columns = (
+                ("Schema Name", "schema_name"),
+                ("Table Name", "table_name"),
+                ("Column Name", "column_name"),
+                ("Column Type", "column_type"),
+                ("Semantic Data Type", "semantic_data_type"),
+                ("Hygiene Issues", "hygiene_issues"),
+            )
+            default_sorting = [(sortable_columns[i][1], "ASC") for i in (0, 1, 2)]
+            sorting_columns = testgen.sorting_selector(sortable_columns, default_sorting)
+
+        # Use SQL wildcard to match all values
+        if not table_name:
+            table_name = "%%"
+        if not column_name:
+            column_name = "%%"
+
+        # Display main results grid
+        df = profiling_queries.get_profiling_results(run_id, table_name, column_name, sorting_columns)
+        show_columns = [
+            "schema_name",
+            "table_name",
+            "column_name",
+            "column_type",
+            "semantic_data_type",
+            "hygiene_issues",
+        ]
+
+        # Show CREATE script button
+        if len(df) > 0 and table_name != "%%":
+            with st.expander("📜 **Table CREATE script with suggested datatypes**"):
+                st.code(generate_create_script(df), "sql")
+
+        selected_row = fm.render_grid_select(
+            df,
+            show_columns,
+            bind_to_query_name="selected",
+            bind_to_query_prop="id",
+        )
+
+        with export_button_column:
+            testgen.flex_row_end()
+            render_export_button(df)
+
+        # Display profiling for selected row
+        if not selected_row:
+            st.markdown(":orange[Select a row to see profiling details.]")
+        else:
+            item = selected_row[0]
+            item["hygiene_issues"] = profiling_queries.get_hygiene_issues(run_id, item["table_name"], item.get("column_name"))
+            testgen_component(
+                "column_profiling_results",
+                props={ "column": json.dumps(item), "data_preview": True },
+                on_change_handlers={
+                    "DataPreviewClicked": lambda item: data_preview_dialog(
+                        item["table_group_id"],
+                        item["schema_name"],
+                        item["table_name"],
+                        item.get("column_name"),
+                    ),
+                },
+            )
+
+
+def render_export_button(df):
+    export_columns = [
+        "schema_name",
+        "table_name",
+        "column_name",
+        "position",
+        "hygiene_issues",
+        # Characteristics
+        "general_type",
+        "column_type",
+        "semantic_table_type",
+        "semantic_data_type",
+        "datatype_suggestion",
+        # Value Counts
+        "record_ct",
+        "value_ct",
+        "distinct_value_ct",
+        "null_value_ct",
+        "zero_value_ct",
+        # Alpha
+        "zero_length_ct",
+        "filled_value_ct",
+        "includes_digit_ct",
+        "numeric_ct",
+        "date_ct",
+        "quoted_value_ct",
+        "lead_space_ct",
+        "embedded_space_ct",
+        "avg_embedded_spaces",
+        "min_length",
+        "max_length",
+        "avg_length",
+        "min_text",
+        "max_text",
+        "distinct_std_value_ct",
+        "distinct_pattern_ct",
+        "std_pattern_match",
+        "top_freq_values",
+        "top_patterns",
+        # Numeric
+        "min_value",
+        "min_value_over_0",
+        "max_value",
+        "avg_value",
+        "stdev_value",
+        "percentile_25",
+        "percentile_50",
+        "percentile_75",
+        # Date
+        "min_date",
+        "max_date",
+        "before_1yr_date_ct",
+        "before_5yr_date_ct",
+        "before_20yr_date_ct",
+        "within_1yr_date_ct",
+        "within_1mo_date_ct",
+        "future_date_ct",
+        # Boolean
+        "boolean_true_ct",
+        # Extra
+        "distinct_value_hash",
+        "fractional_sum",
+        "date_days_present",
+        "date_weeks_present",
+        "date_months_present",
+    ]
+    wrap_columns = ["top_freq_values", "top_patterns"]
+    caption = "{TIMESTAMP}"
+    fm.render_excel_export(df, export_columns, "Profiling Results", caption, wrap_columns)
+
+
+def generate_create_script(df):
+    ddf = df[["schema_name", "table_name", "column_name", "column_type", "datatype_suggestion"]].copy()
+    ddf.fillna("", inplace=True)
+
+    ddf["comment"] = ddf.apply(
+        lambda row: "-- WAS " + row["column_type"] if row["column_type"] != row["datatype_suggestion"] else "", axis=1
+    )
+    max_len_name = ddf.apply(lambda row: len(row["column_name"]), axis=1).max() + 3
+    max_len_type = ddf.apply(lambda row: len(row["datatype_suggestion"]), axis=1).max() + 3
+
+    str_header = f"CREATE TABLE {df.at[0, 'schema_name']}.{ddf.at[0, 'table_name']} ( "
+    col_defs = ddf.apply(
+        lambda row: f"     {row['column_name']:<{max_len_name}} {row['datatype_suggestion']:<{max_len_type}},    {row['comment']}",
+        axis=1,
+    ).tolist()
+    str_footer = ");"
+    # Drop final comma in column definitions
+    col_defs[-1] = col_defs[-1].replace(",    --", "    --")
+
+    return "\n".join([str_header, *list(col_defs), str_footer])
+
+
+@st.cache_data(show_spinner=False)
+def get_profiling_run_tables(profiling_run_id: str):
+    schema: str = st.session_state["dbschema"]
+    query = f"""
+    SELECT DISTINCT table_name
+        FROM {schema}.profile_results
+    WHERE profile_run_id = '{profiling_run_id}'
+    ORDER BY table_name
+    """
+    return db.retrieve_data(query)
+
+
+@st.cache_data(show_spinner=False)
+def get_profiling_run_columns(profiling_run_id: str, table_name: str):
+    schema: str = st.session_state["dbschema"]
+    query = f"""
+    SELECT DISTINCT column_name
+        FROM {schema}.profile_results
+    WHERE profile_run_id = '{profiling_run_id}'
+        AND table_name = '{table_name}'
+    ORDER BY column_name
+    """
+    return db.retrieve_data(query)
