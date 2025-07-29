56fec09b1b5f212a8faaf5c38e3bcc01fad34b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:02 +0000

    Add package maleo-foundation/0.2.52/maleo_foundation-0.2.52.tar.gz

---
+++ packages/maleo-foundation/maleo_foundation-0.2.52.tar.gz/maleo_foundation-0.2.52/maleo_foundation/models/transfers/parameters/general.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from maleo_foundation.models.schemas.general import BaseGeneralSchemas
+from maleo_foundation.models.schemas.parameter import BaseParameterSchemas
+
+class BaseGeneralParametersTransfers:
+    class FieldExpansionProcessor(
+        BaseParameterSchemas.Expand,
+        BaseParameterSchemas.Data
+    ): pass
+
+    class GetSingleQuery(BaseParameterSchemas.Statuses): pass
+
+    class BaseGetSingle(
+        BaseParameterSchemas.IdentifierValue,
+        BaseParameterSchemas.IdentifierType
+    ):
+        pass
+
+    class GetSingle(BaseParameterSchemas.Statuses, BaseGetSingle): pass
+
+    class StatusUpdate(BaseGeneralSchemas.Status): pass
