f981145ee081be3077a83a4cb9edd0ff0cb37913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:00 +0000

    Add package dsp-tools/15.0.0.post9/dsp_tools-15.0.0.post9.tar.gz

---
+++ packages/dsp-tools/dsp_tools-15.0.0.post9.tar.gz/dsp_tools-15.0.0.post9/src/dsp_tools/cli/args.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class ServerCredentials:
+    """Contains credentials to connect to a server."""
+
+    user: str
+    password: str
+    server: str
+    dsp_ingest_url: str = "http://0.0.0.0:3340"
+
+
+@dataclass(frozen=True)
+class ValidateDataConfig:
+    """Contains the configuration for validate data."""
+
+    xml_file: Path
+    save_graph_dir: Path | None
+    severity: ValidationSeverity
+
+
+@dataclass
+class ValidationSeverity(Enum):
+    ERROR = 3
+    WARNING = 2
+    INFO = 1
