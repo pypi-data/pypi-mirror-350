b96bed7705001df13cbcb30c0e418b09a26440e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:53 +0000

    Add package fasr/0.3.3/fasr-0.3.3.tar.gz

---
+++ packages/fasr/fasr-0.3.3.tar.gz/fasr-0.3.3/fasr/models/asr/__init__.py
@@ -0,0 +1,14 @@
+from .paraformer import ParaformerForASR
+from .sensevoice import SensevoiceForASR
+from .seaco_paraformer import SeacoParaformerForASR
+from .firered.firered_aed import FireRedAEDForASR
+from .dolphin import DolphinSmallForASR
+
+
+__all__ = [
+    "ParaformerForASR",
+    "SensevoiceForASR",
+    "SeacoParaformerForASR",
+    "FireRedAEDForASR",
+    "DolphinSmallForASR",
+]
