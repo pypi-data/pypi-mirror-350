0c7a5be8171c00de3c3a5cc77dd19b26791309fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:38 +0000

    Add package xiaokang/0.1.0/xiaokang-0.1.0.tar.gz

---
+++ packages/xiaokang/xiaokang-0.1.0.tar.gz/xiaokang-0.1.0/tests/test_1.py
@@ -0,0 +1,15 @@
+from xiaokang.ChangYong import BaoCuoXingxi, xk
+
+
+def test_BaoCuoXingxi():
+    try:
+        print('1'+1)
+    except:
+        print(BaoCuoXingxi('json'))
+
+
+def test_xk():
+    xk()
+
+
+test_BaoCuoXingxi()
---
+++ packages/xiaokang/xiaokang-0.1.0.tar.gz/xiaokang-0.1.0/xiaokang/__init__.py
@@ -0,0 +1,4 @@
+from .ChangYong import BaoCuoXingxi,xk
+
+# 定义外部可访问的内容（非必须，但推荐）
+__all__ = ["BaoCuoXingxi","hello","xk"]
