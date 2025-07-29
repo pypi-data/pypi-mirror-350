1c42055b8a6564c97e7e3d0703acde91ee5508ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:40 +0000

    Add package nnetflow/0.3.0/nnetflow-0.3.0-py3-none-any.whl

---
+++ packages/nnetflow/nnetflow-0.3.0-py3-none-any.whl/nnetflow/optim.py
@@ -0,0 +1,18 @@
+from .engine import Tensor as tensor 
+
+"""add optimizers here"""
+
+class SGD:
+    def __init__(self,params,lr=0.01):
+        self.params = params
+        self.lr = lr
+    def step(self):
+        for p in self.params:
+            p.data -= self.lr * p.grad
+    def zero_grad(self):
+        for p in self.params:
+            p.zero_grad() 
+        
+
+
+
