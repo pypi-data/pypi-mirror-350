645473fc541d40b081416340591a5cd48c645be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:53 +0000

    Add package coveo-settings/3.0/coveo_settings-3.0.tar.gz

---
+++ packages/coveo-settings/coveo_settings-3.0.tar.gz/coveo_settings-3.0/README.md
@@ -0,0 +1,201 @@
+# coveo-settings
+
+Whenever you want the user to be able to configure something through an environment variable, this module has your back:
+
+```python
+from coveo_settings import StringSetting, BoolSetting
+
+DATABASE_URL = StringSetting('project.database.url')
+DATABASE_USE_SSL = BoolSetting('project.database.ssl')
+```
+
+The user can then configure the environment variables `project.database.url` and `project.database.ssl` to configure the application.
+
+When accessed, the values are automatically converted to the desired type:
+
+- `StringSetting` will always be a string
+- `BoolSetting` is either True or False, but accepts "yes|no|true|false|1|0" as input (case-insensitive, of course)
+- `IntSetting` and `FloatSetting` are self-explanatory
+- `DictSetting` allows you to use JSON maps
+- `PathSetting` gives a Path instance, and also implements PathLike and the `/` operator
+
+If the input cannot be converted to the value type, an `TypeConversionConfigurationError` exception is raised.
+
+A default (fallback) value may be specified. The fallback may be a `callable`.
+
+A validation callback may be specified for custom logic and error messages.
+
+Not limited to environment variables; supports redirection to custom implementations.
+
+**A setting can be set as sensitive for logging purposes. When logging, use repr(setting) to get the correct representation.**
+
+
+
+## Accessing the value
+
+There are various ways to obtain the value:
+
+```python
+from coveo_settings import BoolSetting
+
+DATABASE_USE_SSL = BoolSetting('project.database.ssl')
+
+# this method will raise an exception if the setting has no value and no fallback
+use_ssl = bool(DATABASE_USE_SSL)
+use_ssl = DATABASE_USE_SSL.get_or_raise()
+assert use_ssl in [True, False]
+
+# this method will not raise an exception
+use_ssl = DATABASE_USE_SSL.value
+assert use_ssl in [True, False, None]
+
+# use "is_set" to check if there is a value set for this setting; skips validation check
+if DATABASE_USE_SSL.is_set:
+    use_ssl = bool(DATABASE_USE_SSL)
+
+# use "is_valid" to verify if the value passes the validation callback. implies is_set.
+if not DATABASE_USE_SSL.is_valid:
+    ...
+```
+
+
+## Loose environment key matching
+
+Matching the key of the environment variable `project.database.ssl` is done very loosely:
+
+- case-insensitive
+- dots and underscores are ignored completely (`foo_bar` and `f__ooba.r` are equal)
+    - useful for some runners that don't support dots in environment variable keys
+
+
+## Use ready validation
+
+You can quickly validate that a string is in a specific list like this:
+
+```python
+from coveo_settings.settings import StringSetting
+from coveo_settings.validation import InSequence
+
+ENV = StringSetting("environment", fallback="dev", validation=InSequence("prod", "staging", "dev"))
+```
+
+
+## Redirection
+
+You can register custom redirection schemes in order to support any data source.
+
+Much like `https://` is a clear scheme, you may register callback functions to trigger when the value of a setting
+starts with the scheme(s) you define. For instance, let's support a custom API and a file storage:
+
+```python
+from coveo_settings import settings_adapter, StringSetting, ConfigValue
+
+
+@settings_adapter("internal-api::")
+def internal_api_adapter(key: str) -> ConfigValue:
+    # the scheme was automatically removed for convenience; only the resource remains
+    assert "internal-api::" not in key
+    return "internal api"  # implement logic to obtain value from internal api
+
+
+@settings_adapter("file::", strip_scheme=False)
+def file_adapter(key: str) -> ConfigValue:
+    # you can keep the scheme by specifying `strip_scheme=False`
+    assert key.startswith("file::")
+    return "file adapter"  # implement logic to parse the key and retrieve the setting value
+
+
+assert StringSetting('...', fallback="internal-api::settings/user-name").value == "internal api"
+assert StringSetting('...', fallback="file::settings.yaml/user-name").value == "file adapter"
+
+
+# even though we used `fallback` above, the redirection is driven by the user:
+import os
+
+REDIRECT_ME = StringSetting('test')
+os.environ['test'] = "file::user.json::name"
+assert REDIRECT_ME.value == "file adapter"
+os.environ['test'] = "internal-api::url"
+assert REDIRECT_ME.value == "internal api"
+```
+
+Keep in mind that there's no restriction on the prefix scheme; it's your responsibility to pick something unique
+that can be set as the value of an environment variable.
+
+
+### Redirection is recursive
+
+The value of a redirection may be another redirection and may juggle between adapters.
+A limit of 50 redirections is supported:
+
+```python
+import os
+
+from coveo_settings import StringSetting
+
+
+os.environ["expected"] = "final value"
+os.environ["redirected"] = "env->expected"
+os.environ["my-setting"] = "env->redirected"
+
+assert StringSetting("my-setting").value == "final value"
+```
+
+### Builtin environment redirection
+
+The builtin redirection scheme `env->` can be used to redirect to a different environment variable.
+The example below demonstrates the deprecation/migration of `my-setting` into `new-setting`:
+
+```python
+import os
+
+from coveo_settings import StringSetting
+
+os.environ["new-setting"] = "correct-value"
+os.environ["my-setting"] = "env->new-setting"
+
+assert StringSetting("my-setting").value == "correct-value"
+```
+
+## Cached
+
+You can set a setting to cache the first valid value with `cached=True`.
+This is particularly useful in redirection scenarios to avoid repeating requests too often.
+
+
+## Setting the value
+
+You can override the value using `setting.value = "some value"` and clear the override with `setting.value = None`.
+Clearing the override resumes the normal behavior of the environment variables and the fallback value, if set.
+
+This is typically used as a way to propagate CLI switches globally.
+For mocking scenarios, refer to the `Mocking` section below.
+
+
+## Mocking
+
+When you need a setting value for a test, use the `mock_config_value` context manager:
+
+```python
+from coveo_settings import StringSetting
+from coveo_settings.mock import mock_config_value
+
+SETTING = StringSetting(...)
+
+assert not SETTING.is_set
+with mock_config_value(SETTING, 'new-value'):
+    assert SETTING.is_set
+```
+
+You can also clear the value:
+
+```python
+from coveo_settings import StringSetting
+from coveo_settings.mock import mock_config_value
+
+SETTING = StringSetting(..., fallback='test')
+
+assert SETTING.is_set
+with mock_config_value(SETTING, None):
+    assert not SETTING.is_set
+```
---
+++ packages/coveo-settings/coveo_settings-3.0.tar.gz/coveo_settings-3.0/coveo_settings/setting_abc.py
@@ -0,0 +1,381 @@
+"""Create application settings that can be overridden through environment variables or custom adapters."""
+
+from __future__ import annotations
+
+import os
+import re
+
+import logging
+from abc import abstractmethod
+from copy import copy
+from functools import partial
+from typing import (
+    Any,
+    Optional,
+    Union,
+    SupportsInt,
+    SupportsFloat,
+    Generic,
+    Collection,
+    Callable,
+    Container,
+    Iterator,
+    Iterable,
+    Dict,
+    Final,
+    Pattern,
+)
+
+from coveo_settings.annotations import ConfigValue, T, Validation, ValidationCallback
+from coveo_settings.exceptions import (
+    InvalidConfiguration,
+    TypeConversionConfigurationError,
+    ValidationConfigurationError,
+    ValidationCallbackError,
+    MandatoryConfigurationError,
+    DuplicatedScheme,
+    TooManyRedirects,
+)
+from coveo_settings.validation import InSequence
+
+
+ENVIRONMENT_VARIABLE_SEPARATORS = "._"
+
+log = logging.getLogger(__name__)
+
+
+def _find_setting(*keys: str) -> Optional[str]:
+    """Attempts to find a variable in the environment variables. The casing, dots (.) and the underline character (_)
+    are not significant. For instance, "ut.test.setting" will match "UTTESTSETTING" and also "UT_teST.._setting".
+    """
+    if not keys:
+        return None
+    main_key = keys[0]
+
+    def _normalize(key_: str) -> str:
+        """Returns a lowercase version of key without separators."""
+        return "".join(char.lower() for char in key_ if char not in ENVIRONMENT_VARIABLE_SEPARATORS)
+
+    if not keys:
+        raise InvalidConfiguration("Key should not be empty.")
+
+    for potential_key in keys:
+        return_value: Optional[str] = os.environ.get(potential_key)
+        if return_value is None:
+            stripped = _normalize(potential_key)
+            for key, value in os.environ.items():
+                if _normalize(key) == stripped:
+                    log.debug(f"Setting {main_key} retrieved from the environment variable: {key}")
+                    return value
+        else:
+            log.debug(
+                f"Setting {main_key} retrieved from the environment variable: {potential_key}"
+            )
+            return return_value
+
+    return None
+
+
+def _no_validation(_: ConfigValue) -> Optional[str]:
+    """Default validation callback"""
+    return None
+
+
+class Setting(SupportsInt, SupportsFloat, Generic[T], Container, Iterable):
+    """
+    Base class for magic type-checked settings.
+
+    To enforce type safety and play along with mypy:
+        - If the setting is required, use int/float/str/== to obtain or evaluate the value. If it's missing,
+          an exception will be raised.
+        - If the setting is optional, use `.value` instead: the value will be None if missing.
+
+    Evaluating __bool__ evaluates the general python-truth of `.value`. Thus, missing is evaluated as bool(None).
+
+    If you need to verify the presence/absence of a setting, use `is_set` instead.
+
+    All methods read the value and raise an exception on bad formats.
+    """
+
+    def __init__(
+        self,
+        key: str,
+        fallback: Optional[Union[ConfigValue, Callable[[], Optional[ConfigValue]]]] = None,
+        alternate_keys: Optional[Collection[str]] = None,
+        sensitive: bool = False,
+        validation: Validation = _no_validation,
+        cached: bool = False,
+    ) -> None:
+        """Initializes a setting."""
+        self._key: str = key
+        self._alternate_keys: Collection[str] = alternate_keys or tuple()
+        self._fallback = fallback
+        self._override: Optional[ConfigValue] = None
+        self._validation_callback: ValidationCallback = self._resolve_validation_callback(
+            validation
+        )
+        self._sensitive = sensitive
+        self._cached = cached
+        self._cache_validated: Optional[T] = None
+        self._last_value: Optional[ConfigValue] = None
+        # cast fallback values so that it breaks on import (e.g.: during tests)
+        # however, do not trigger any callables or validation to promote a just-in-time evaluation at runtime
+        if fallback is not None and not callable(fallback) and not self.is_redirect:
+            self._cast_or_raise(fallback)
+
+    @property
+    def key(self) -> str:
+        """Return the key of this setting."""
+        return self._key
+
+    @property
+    def value(self) -> Optional[T]:
+        """Returns the validated value of the setting, or None when not set."""
+        value = settings_adapter.evaluate(self._get_value_before_redirections())
+        return None if value is None else self._cast_and_validate(value)
+
+    @value.setter
+    def value(self, value: Optional[ConfigValue]) -> None:
+        """Sets the value so that it overrides environment or fallback, if any.
+
+        This is useful in CLI applications to propagate global flags such as "DryRun" or "Verbose".
+
+        If it is `None`, the normal behavior is restored (reading from environment + fallback).
+        To simulate/override as "unset", use `mock_config_value` with `None` instead.
+        """
+        self._override = value
+
+    @property
+    def is_set(self) -> bool:
+        """
+        Indicates if the value is set. This doesn't mean the value is valid.
+
+        Notes:
+            - Values with defaults are always set
+            - Values that should redirect to a custom adapter are always set
+        """
+        return self._get_value_before_redirections() is not None
+
+    @property
+    def is_valid(self) -> bool:
+        """True if value is set and valid."""
+        try:
+            return self.value is not None
+        except InvalidConfiguration:
+            return False
+
+    @property
+    def is_redirect(self) -> bool:
+        """True if the value invokes a custom adapter."""
+        return settings_adapter.is_redirect(self._get_value_before_redirections())
+
+    def get_or_raise(self) -> T:
+        """Return the value or raise an MandatoryConfigurationError if not set."""
+        self.raise_if_missing()
+        return self.value
+
+    def get_if_set(self, default: T) -> T:
+        """Return the value, or a default if not set."""
+        return self.value if self.is_set else default
+
+    @staticmethod
+    @abstractmethod
+    def _cast(value: ConfigValue) -> T:
+        """Casts a value to the appropriate type."""
+
+    def _cast_or_raise(self, value: ConfigValue) -> T:
+        """Cast the value or raise an exception."""
+        try:
+            return self._cast(value)
+        except (TypeError, ValueError) as exception:
+            raise TypeConversionConfigurationError(
+                f"{self._pretty_repr(value)}: Conversion to desired type failed."
+            ) from exception
+
+    def _validate_or_raise(self, value: T) -> T:
+        """Launches the custom validation callback on a value. Raises ValidationConfigurationError on failure."""
+        if self._cache_validated != value:
+            error_message = self._validation_callback(value)
+            if error_message:
+                raise ValidationConfigurationError(f"{self._pretty_repr(value)}: {error_message}")
+            self._cache_validated = copy(value)
+        return value
+
+    def _cast_and_validate(self, value: ConfigValue) -> T:
+        """Cast and validate the value or raise an exception."""
+        return self._validate_or_raise(self._cast_or_raise(value))
+
+    def _get_value_before_redirections(self) -> Optional[ConfigValue]:
+        """Returns the raw value/fallback/override of this setting, else None."""
+        if self._cached and self._cache_validated is not None:
+            log.debug(f"Setting {self.key} retrieved from cache.")
+            return copy(self._cache_validated)
+
+        value = (
+            _find_setting(self.key, *self._alternate_keys)
+            if self._override is None
+            else self._override
+        )
+        if value is None and self._fallback is not None:
+            log.debug(f"Setting {self.key} retrieved from fallback.")
+            value = self._fallback() if callable(self._fallback) else self._fallback
+        elif value is None:
+            log.debug(f"Setting {self.key} is not set.")
+        elif settings_adapter.is_redirect(value):
+            log.debug(f"Setting {self.key} is a redirection.")
+
+        self._last_value = copy(value)
+        return value
+
+    def _resolve_validation_callback(self, validation: Validation) -> ValidationCallback:
+        if callable(validation):
+            return validation
+
+        try:
+            if isinstance(validation, str):
+                raise TypeError(validation)
+            iterable = iter(validation)
+        except TypeError as exception:
+            raise ValidationCallbackError("Unsupported validation callback type") from exception
+
+        return InSequence(*iterable)
+
+    def raise_if_missing(self) -> None:
+        """Raises an MandatoryConfigurationError exception if the setting is required but missing."""
+        if not self.is_set:
+            raise MandatoryConfigurationError(f'Mandatory config item "{self.key}" is missing.')
+
+    def _pretty_repr(self, value: Optional[ConfigValue]) -> str:
+        value_str = "<not-set>" if value is None else "<sensitive>" if self._sensitive else value
+        return f"{self.__class__.__name__}[{self.key}] = {value_str}"
+
+    def __repr__(self) -> str:
+        """Returns a readable representation of the item for debugging."""
+        # we are overly careful in not triggering mechanics (e.g.: _get_value()) from here.
+        # value is only shown if already computed.
+        value = next(
+            v for v in (self._cache_validated, self._last_value, "<not-evaluated>") if v is not None
+        )
+        return self._pretty_repr(value)
+
+    def __eq__(self, other: Any) -> bool:
+        """Indicates if the value is equal to another one."""
+        self.raise_if_missing()
+        equal = other == self.value
+        if isinstance(equal, bool):
+            return equal
+        return NotImplemented
+
+    def __bool__(self) -> bool:
+        """Indicates if the value is True (in python's terms). Missing values are False."""
+        return bool(self.value)
+
+    def __str__(self) -> str:
+        """Returns the value, blindly converted to a string."""
+        self.raise_if_missing()
+        return str(self.value)
+
+    def __int__(self) -> int:
+        """Returns the value, blindly converted to int."""
+        self.raise_if_missing()
+        return int(self.value)  # type: ignore[call-overload, no-any-return]
+
+    def __float__(self) -> float:
+        """Return the value, blindly converted to float."""
+        self.raise_if_missing()
+        return float(self.value)  # type: ignore[arg-type]
+
+    def __iter__(self) -> Iterator:
+        """Return the iterator for `value`. Will raise on unsupported types or missing values.
+        Note: T will not be used here, because in the case of e.g. Dictionaries you would get strings,
+        or a List of str would give back str...
+        """
+        self.raise_if_missing()
+        return iter(self.value)  # type: ignore[no-any-return,call-overload]
+
+    def __contains__(self, item: Any) -> bool:
+        """Tells if item is in `value`. Will raise on unsupported types or missing values."""
+        self.raise_if_missing()
+        return item in self.value  # type: ignore[operator]
+
+
+AdapterHandler = Callable[[str], Optional[ConfigValue]]
+
+
+class _SchemeDispatch:
+    """Decorator that registers a function as a callback for a specific scheme."""
+
+    matchers: Final[Dict[Pattern, AdapterHandler]] = {}
+
+    def __init__(self, scheme: str, strip_scheme: bool = True) -> None:
+        """
+        Reasonable schemes will use the same delimiter pattern within an application for consistency and will
+        contain several characters for uniqueness.
+
+        Examples:
+            - ssm://
+            - s3->
+            - {api}
+
+        The scheme is removed from the value given to the adapter. To keep it, set `strip_scheme` to False.
+        """
+        pattern = (
+            rf"^{re.escape(scheme)}(?P<resource>.+)$"
+            if strip_scheme
+            else rf"^(?P<resource>{re.escape(scheme)}.+)$"
+        )
+        self.matcher = re.compile(pattern, flags=re.IGNORECASE)
+        self.scheme = scheme
+
+    def __call__(self, fn: AdapterHandler) -> AdapterHandler:
+        """Register the function into the class registry. The function is returned; no wrapping occurs."""
+        if self.matcher in self.matchers:
+            raise DuplicatedScheme(self.scheme)
+        self.matchers[self.matcher] = fn
+        return fn
+
+    @classmethod
+    def evaluate(cls, value: ConfigValue) -> Optional[ConfigValue]:
+        """Evaluates the config value against the registered handlers."""
+        if not isinstance(value, str):
+            return value
+
+        try:
+            return cls._evaluate(value)
+        except RecursionError as exception:
+            raise TooManyRedirects(value) from exception
+
+    @classmethod
+    def _evaluate(cls, value: ConfigValue) -> Optional[ConfigValue]:
+        """Evaluates value recursively."""
+        handler = cls._get_handler(value)
+        if handler:
+            redirected_value = handler()
+            if redirected_value != value:
+                # recurse when the value changes
+                return cls._evaluate(redirected_value)
+
+        return value
+
+    @classmethod
+    def is_redirect(cls, value: ConfigValue) -> bool:
+        """Indicates if the value would match a registered adapter."""
+        return cls._get_handler(value) is not None
+
+    @classmethod
+    def _get_handler(cls, value: Any) -> Optional[Callable[[], Optional[ConfigValue]]]:
+        if isinstance(value, str):
+            for matcher, handler in cls.matchers.items():
+                match = matcher.match(value)
+                if match:
+                    return partial(handler, match["resource"])
+        return None
+
+
+settings_adapter = _SchemeDispatch
+
+
+@settings_adapter("env->")
+def default_environ_adapter(value: str) -> Optional[ConfigValue]:
+    """Custom adapter that defers to another environment variable."""
+    return os.getenv(value)
