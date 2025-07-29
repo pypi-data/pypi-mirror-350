242a4afd03fad60e973c30bdc9d075980b58326d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:56 +0000

    Add package lark-bot-sdk/0.1.0/lark_bot_sdk-0.1.0-py3-none-any.whl

---
+++ packages/lark-bot-sdk/lark_bot_sdk-0.1.0-py3-none-any.whl/feishusdk/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+飞书SDK
+提供简单的飞书消息发送功能
+"""
+
+from .bot import BotMessage, init_bot
+
+__version__ = "0.1.0"
+__all__ = ["BotMessage", "init_bot"]
