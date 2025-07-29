59d452076b6d10b1e28ddd093d714211ff759cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:08 +0000

    Add package ScaleHD/1.3.0/scalehd-1.3.0.tar.gz

---
+++ packages/ScaleHD/scalehd-1.3.0.tar.gz/scalehd-1.3.0/ScaleHD/__init__.py
@@ -0,0 +1,6 @@
+from .sherpa import *
+__all__ = ['sherpa.py',
+		   '__backend.py',
+		   '__alleleface.py',
+		   '__init__.py',
+		   '__main__.py']
---
+++ packages/ScaleHD/scalehd-1.3.0.tar.gz/scalehd-1.3.0/ScaleHD/__main__.py
@@ -0,0 +1,2 @@
+from .sherpa import main
+main()
