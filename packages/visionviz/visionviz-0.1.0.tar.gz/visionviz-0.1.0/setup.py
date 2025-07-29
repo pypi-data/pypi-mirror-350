09c9c3540ecdbca622d5ea53ada74372dfb58ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:28 +0000

    Add package visionviz/0.1.0/visionviz-0.1.0.tar.gz

---
+++ packages/visionviz/visionviz-0.1.0.tar.gz/visionviz-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="visionviz",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add your package dependencies here
+    ],
+    author="Intel Open Edge Platform",
+    author_email="open-edge-platform@intel.com",
+    description="Visualization tools for computer vision applications",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/intel/visionai",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    python_requires=">=3.8",
+    license="Apache-2.0",
+)
