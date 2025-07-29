dd5e43b51280556f89767200fb63067a28eeebec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:26 +0000

    Add package pratlib/1.0.11/pratlib-1.0.11-py3-none-any.whl

---
+++ packages/pratlib/pratlib-1.0.11-py3-none-any.whl/pratlib/models/classification/naive_bayes.py
@@ -0,0 +1,29 @@
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.classification import NaiveBayes as SparkNaiveBayes
+
+class NaiveBayesClassifier:
+    def __init__(self, **kwargs):
+        self.model = SparkNaiveBayes(**kwargs)
+        self.assembler = None
+        self.fitted_model = None
+        self.feature_cols = None
+
+    def fit(self, df, label_col='label'):
+        """
+        Fit the Naive Bayes model using inferred feature columns from the input DataFrame.
+        """
+        self.feature_cols = [col for col in df.columns if col != label_col]
+        self.assembler = VectorAssembler(inputCols=self.feature_cols, outputCol='features')
+        df_vectorized = self.assembler.transform(df)
+        self.model.setFeaturesCol('features').setLabelCol(label_col)
+        self.fitted_model = self.model.fit(df_vectorized)
+        return self
+
+    def predict(self, df):
+        """
+        Predict using the trained Naive Bayes model.
+        """
+        if not self.fitted_model or not self.assembler:
+            raise ValueError("Model must be fitted before predicting.")
+        df_vectorized = self.assembler.transform(df)
+        return self.fitted_model.transform(df_vectorized)
