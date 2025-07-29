8c9a874cdc3c5ddb2a7637a318c8faf7fbe1668c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:03 +0000

    Add package streamlit-product-card/0.1.2/streamlit_product_card-0.1.2-py3-none-any.whl

---
+++ packages/streamlit-product-card/streamlit_product_card-0.1.2-py3-none-any.whl/streamlit_product_card/frontend/build/precache-manifest.4ecd783d47582bad189cc4b422adb2b6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "88ce9da20540f9a131c4",
+    "url": "./static/js/main.daf9ec54.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4e7fdb760cc86868f4cb",
+    "url": "./static/js/2.cc2bfc1d.chunk.js"
+  },
+  {
+    "revision": "221dc0e9bbec25db2dc8ad020369ac77",
+    "url": "./index.html"
+  }
+];
