ae858ad77e5d6c175663a631b0636145c5c5b567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:57 +0000

    Add package RouteX/0.1.0/routex-0.1.0-py3-none-any.whl

---
+++ packages/RouteX/routex-0.1.0-py3-none-any.whl/routex/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 23/5/2025 at 11:43
+Author: Abdelouahed Ben Mhamed
+Email: a.benmhamed@intelligentica.net
+Company: Intelligentica
+"""
+def hello():
+    return "Hello RouteX"
