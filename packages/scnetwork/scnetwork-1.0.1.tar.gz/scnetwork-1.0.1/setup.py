1ea2f01ed27dc517fc00b80d6475ba536eec1814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:41 +0000

    Add package scnetwork/1.0.1/scnetwork-1.0.1.tar.gz

---
+++ packages/scnetwork/scnetwork-1.0.1.tar.gz/scnetwork-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+package_name = 'scnetwork'
+version = '1.0.1'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    install_requires=['tqdm', 'requests', 'hurnet', 'semantic-comparison-network', 'psutil', 'ijson', 'tiktoken', 'torch', 'torch-xla', 'numpy'],
+    url='https://github.com/',
+    license='Proprietary Software'
+)
