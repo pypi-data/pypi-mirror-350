10b9041af45acc7866e223877f0323a890dc4a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:01 +0000

    Add package fabricatio-capabilities/0.1.3.dev8/fabricatio_capabilities-0.1.3.dev8-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/fabricatio-capabilities/fabricatio_capabilities-0.1.3.dev8-cp312-cp312-manylinux_2_34_x86_64.whl/fabricatio_capabilities/__init__.py
@@ -0,0 +1,7 @@
+"""Fabricatio is a Python library for building llm app using event-based agent structure."""
+
+from fabricatio_capabilities import capabilities
+
+__all__ = [
+    "capabilities",
+]
