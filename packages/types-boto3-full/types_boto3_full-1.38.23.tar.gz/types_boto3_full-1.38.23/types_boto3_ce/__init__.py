5f95ef96dcf3b2c1a5030847a5a72d1a52093e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:09:33 +0000

    Add package types-boto3-full/1.38.23/types_boto3_full-1.38.23.tar.gz

---
+++ packages/types-boto3-full/types_boto3_full-1.38.23.tar.gz/types_boto3_full-1.38.23/types_boto3_ce/__init__.py
@@ -0,0 +1,45 @@
+"""
+Main interface for ce service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_ce/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_ce import (
+        Client,
+        CostExplorerClient,
+        GetAnomaliesPaginator,
+        GetAnomalyMonitorsPaginator,
+        GetAnomalySubscriptionsPaginator,
+    )
+
+    session = Session()
+    client: CostExplorerClient = session.client("ce")
+
+    get_anomalies_paginator: GetAnomaliesPaginator = client.get_paginator("get_anomalies")
+    get_anomaly_monitors_paginator: GetAnomalyMonitorsPaginator = client.get_paginator("get_anomaly_monitors")
+    get_anomaly_subscriptions_paginator: GetAnomalySubscriptionsPaginator = client.get_paginator("get_anomaly_subscriptions")
+    ```
+"""
+
+from .client import CostExplorerClient
+from .paginator import (
+    GetAnomaliesPaginator,
+    GetAnomalyMonitorsPaginator,
+    GetAnomalySubscriptionsPaginator,
+)
+
+Client = CostExplorerClient
+
+
+__all__ = (
+    "Client",
+    "CostExplorerClient",
+    "GetAnomaliesPaginator",
+    "GetAnomalyMonitorsPaginator",
+    "GetAnomalySubscriptionsPaginator",
+)
