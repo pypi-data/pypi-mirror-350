31d8d751062747e24d290af07974ec879174e13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:14 +0000

    Add package jk_datamatrix/0.2025.5.24/jk_datamatrix-0.2025.5.24-py3-none-any.whl

---
+++ packages/jk_datamatrix/jk_datamatrix-0.2025.5.24-py3-none-any.whl/jk_datamatrix/__init__.py
@@ -0,0 +1,27 @@
+﻿
+
+__author__ = "Jürgen Knauth"
+__version__ = "0.2025.5.24"
+__email__ = "pubsrc@binary-overflow.de"
+__license__ = "Apache2"
+__copyright__ = "Copyright (c) 2021-2025, Jürgen Knauth"
+
+
+
+# the writers. they don't have a dependency to DataMatrix.
+from .DataMatrixCSVWriter import DataMatrixCSVWriter
+from .DataMatrixJSONWriter import DataMatrixJSONWriter
+
+# the DataMatrix and associated classes
+from .DataMatrixRow import DataMatrixRow
+from .DataMatrix import DataMatrix
+
+# the loaders. they require an already defined DataMatrix.
+from .DataMatrixJSONLoader import DataMatrixJSONLoader
+
+
+
+# convenience methods
+loadFromJSONFile = DataMatrixJSONLoader.loadFromJSONFile
+loadFromJSONStr = DataMatrixJSONLoader.loadFromJSONStr
+
