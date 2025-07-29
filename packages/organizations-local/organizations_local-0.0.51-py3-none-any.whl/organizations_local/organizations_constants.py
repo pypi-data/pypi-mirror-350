e6fc87ea6ca9d9d042e36109e46a4168b70b96de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:02 +0000

    Add package organizations-local/0.0.51/organizations_local-0.0.51-py3-none-any.whl

---
+++ packages/organizations-local/organizations_local-0.0.51-py3-none-any.whl/organizations_local/organizations_constants.py
@@ -0,0 +1,19 @@
+from logger_local.LoggerComponentEnum import LoggerComponentEnum
+
+ORGANIZATIONS_LOCAL_PYTHON_COMPONENT_ID = 286
+ORGANIZATIONS_LOCAL_PYTHON_COMPONENT_NAME = "organization-local-python-package"
+DEVELOPER_EMAIL = "tal.g@circ.zone"
+ORGANIZATIONS_PYTHON_PACKAGE_CODE_LOGGER_OBJECT = {
+    "component_id": ORGANIZATIONS_LOCAL_PYTHON_COMPONENT_ID,
+    "component_name": ORGANIZATIONS_LOCAL_PYTHON_COMPONENT_NAME,
+    "component_category": LoggerComponentEnum.ComponentCategory.Code.value,
+    "developer_email": DEVELOPER_EMAIL,
+}
+
+ORGANIZATIONS_PYTHON_PACKAGE_TEST_LOGGER_OBJECT = {
+    "component_id": ORGANIZATIONS_LOCAL_PYTHON_COMPONENT_ID,
+    "component_name": ORGANIZATIONS_LOCAL_PYTHON_COMPONENT_NAME,
+    "component_category": LoggerComponentEnum.ComponentCategory.Unit_Test.value,
+    "testing_framework": LoggerComponentEnum.testingFramework.pytest.value,
+    "developer_email": DEVELOPER_EMAIL,
+}
