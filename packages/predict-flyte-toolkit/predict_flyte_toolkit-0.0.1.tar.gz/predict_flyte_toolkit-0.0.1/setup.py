3adeced427763ecdd44054e94739a70142f5ab4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:47 +0000

    Add package predict-flyte-toolkit/0.0.1/predict_flyte_toolkit-0.0.1.tar.gz

---
+++ packages/predict-flyte-toolkit/predict_flyte_toolkit-0.0.1.tar.gz/predict_flyte_toolkit-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='predict-flyte-toolkit',
+    version='0.0.1',
+    description='PyPi package created by Schibsted\'s Product & Application Security team.',
+    long_description='',
+    url='https://schibsted.com',
+    author='Olivia Mattsson',
+    author_email='olivia.mattsson@schibsted.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
