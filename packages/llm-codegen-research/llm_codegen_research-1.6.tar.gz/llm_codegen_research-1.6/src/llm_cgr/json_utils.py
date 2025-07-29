5a42b9af143ea75d8013665ed925175a962b00ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:37 +0000

    Add package llm-codegen-research/1.6/llm_codegen_research-1.6.tar.gz

---
+++ packages/llm-codegen-research/llm_codegen_research-1.6.tar.gz/llm_codegen_research-1.6/src/llm_cgr/json_utils.py
@@ -0,0 +1,30 @@
+"""Simple utility functions to save and read json files."""
+
+import json
+
+
+def save_json(
+    data: dict | list,
+    file_path: str,
+):
+    """
+    Utility to save python dictionary or list to a json file.
+    """
+    with open(file_path, mode="w", encoding="utf-8") as f:
+        json.dump(obj=data, fp=f, indent=4)
+
+
+def load_json(
+    file_path: str,
+) -> dict:
+    """
+    Utility to load a python dictionary or list from a json file.
+    """
+    with open(file_path, mode="r", encoding="utf-8") as f:
+        _json = json.load(fp=f)
+
+    if isinstance(_json, list):
+        # always return a dict
+        return {"data": _json}
+
+    return _json
