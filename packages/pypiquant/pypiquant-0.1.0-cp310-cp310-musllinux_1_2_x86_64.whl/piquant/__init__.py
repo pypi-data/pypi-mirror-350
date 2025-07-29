cbdde45fa9b37117d3ff8f9f1f0786cb430981d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:34 +0000

    Add package pypiquant/0.1.0/pypiquant-0.1.0-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/pypiquant/pypiquant-0.1.0-cp310-cp310-musllinux_1_2_x86_64.whl/piquant/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.0'
+__author__ = 'Mario Sieg'
+__email__ = 'mario.sieg.64@gmail.com'
+__author_email__ = 'mario.sieg.64@gmail.com'
+
+from ._quant import *
+from ._torch import *
+from ._numpy import *
