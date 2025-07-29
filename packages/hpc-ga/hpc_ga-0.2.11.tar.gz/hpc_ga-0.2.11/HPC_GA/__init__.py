bc8331ccaa21bbaa646d25fb9619ed581df9a504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:12 +0000

    Add package hpc-ga/0.2.11/hpc_ga-0.2.11.tar.gz

---
+++ packages/hpc-ga/hpc_ga-0.2.11.tar.gz/hpc_ga-0.2.11/HPC_GA/__init__.py
@@ -0,0 +1,34 @@
+"""
+fastGA-hpc - Framework for Genetic Algorithms
+
+Expose les classes principales :
+- GeneticAlgorithm : Algorithme séquentiel de base
+- ParallelGeneticAlgorithm : Version parallèle
+- Chromosome : Classe de base pour les solutions
+"""
+
+from .core.genetic_algorithm import GeneticAlgorithm
+from .parallel.parallel_ga import MasterSlaveModel, CellularModel, IslandModel, ParallelGA
+from .common.chromosome import Chromosome
+from .common.chromosome import Population
+from .core.operators import Crossover, Mutator
+from .utils import default_stopping_condition, basic_aspiration, diversification_aspiration, frequency_based_intensification, restart_based_diversification
+
+
+__version__ = "0.2.11"
+__all__ = [
+    'GeneticAlgorithm',
+    'MasterSlaveModel',
+    'CellularModel',
+    'IslandModel',
+    'ParallelGA',
+    'Chromosome',
+    'Population',
+    'Crossover',
+    'Mutator',
+    'default_stopping_condition',
+    'basic_aspiration',
+    'diversification_aspiration',
+    'frequency_based_intensification',
+    'restart_based_diversification',
+]
