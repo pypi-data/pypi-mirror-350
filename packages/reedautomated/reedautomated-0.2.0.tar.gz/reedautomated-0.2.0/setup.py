3086c549af9d678b1b8a3eaa516c5437613a5572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:07:29 +0000

    Add package reedautomated/0.2.0/reedautomated-0.2.0.tar.gz

---
+++ packages/reedautomated/reedautomated-0.2.0.tar.gz/reedautomated-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="reedautomated",
+    version="0.2.0",  
+     packages=find_packages(include=['reedautomated', 'reedautomated.*']),
+     install_requires=[
+        "selenium",
+        "schedule"
+    ],
+      entry_points={
+        "console_scripts": [
+        "run-reedautoassistant = reedautomated.main:run_as_script"
+        ]
+    },
+)
