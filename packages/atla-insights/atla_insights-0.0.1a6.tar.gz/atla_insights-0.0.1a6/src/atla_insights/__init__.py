23316c1dcb73c7fa817ce07a84d0f321ff7e71f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:00 +0000

    Add package atla-insights/0.0.1a6/atla_insights-0.0.1a6.tar.gz

---
+++ packages/atla-insights/atla_insights-0.0.1a6.tar.gz/atla_insights-0.0.1a6/src/atla_insights/__init__.py
@@ -0,0 +1,20 @@
+"""Atla package for PyPI distribution."""
+
+from logfire import instrument
+
+from ._main import (
+    configure,
+    instrument_litellm,
+    instrument_openai,
+    mark_failure,
+    mark_success,
+)
+
+__all__ = [
+    "configure",
+    "instrument",
+    "instrument_litellm",
+    "instrument_openai",
+    "mark_failure",
+    "mark_success",
+]
