f008ec83deec6a44d448a5a4470b573cb93c5778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:33 +0000

    Add package oelint-adv/8.0.2/oelint_adv-8.0.2.tar.gz

---
+++ packages/oelint-adv/oelint_adv-8.0.2.tar.gz/oelint_adv-8.0.2/tests/test_class_oelint_task_heredocs.py
@@ -0,0 +1,52 @@
+import pytest  # noqa: I900
+
+from .base import TestBaseClass
+
+
+class TestClassOelintTaskHeredocs(TestBaseClass):
+
+    @pytest.mark.parametrize('id_', ['oelint.task.heredocs'])
+    @pytest.mark.parametrize('occurrence', [1])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     do_foo() {
+                                         cat    >  ${T}/some.files <<   abchhehdhhe
+                                         kfkdfkd
+                                         abchhehdhhe
+                                     }
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     do_install_append() {
+                                         cat    <<   EOF    >${T}/some.files
+                                         abc
+                                         EOF
+                                     }
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_bad(self, input_, id_, occurrence):
+        self.check_for_id(self._create_args(input_), id_, occurrence)
+
+    @pytest.mark.parametrize('id_', ['oelint.task.heredocs'])
+    @pytest.mark.parametrize('occurrence', [0])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     do_install() {
+                                         abc
+                                     }
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_good(self, input_, id_, occurrence):
+        self.check_for_id(self._create_args(input_), id_, occurrence)
