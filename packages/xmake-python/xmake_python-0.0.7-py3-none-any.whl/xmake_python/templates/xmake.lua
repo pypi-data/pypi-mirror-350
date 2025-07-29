c0dcef804293c2314b62fa73902110228324ed2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:35 +0000

    Add package xmake-python/0.0.7/xmake_python-0.0.7-py3-none-any.whl

---
+++ packages/xmake-python/xmake_python-0.0.7-py3-none-any.whl/xmake_python/templates/xmake.lua
@@ -0,0 +1,31 @@
+option("xmake-platlib")
+do
+    set_default("/../platlib")
+end
+option("xmake-scripts")
+do
+    set_default("/bin")
+end
+option("xmake-headers")
+do
+    set_default("/include")
+end
+option("xmake-data")
+do
+    set_default("/")
+end
+option("xmake-metadata")
+do
+    set_default("/../metadata")
+end
+option("xmake-null")
+do
+    set_default("/../null")
+end
+option("xmake-root")
+do
+    set_default("{root}")
+end
+option_end()
+set_prefixdir("/data", {{bindir = "bin", libdir = "lib", includedir = "include"}})
+includes("{project}")
