bec91de9c7b4b87c9cb58cc58fbd3cc7e581f70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:59 +0000

    Add package ssm-simulators/0.9.0b3/ssm_simulators-0.9.0b3-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/ssm-simulators/ssm_simulators-0.9.0b3-cp311-cp311-macosx_10_9_x86_64.whl/ssms/__init__.py
@@ -0,0 +1,5 @@
+from . import basic_simulators, config, dataset_generators, support_utils  # noqa: D104
+
+__version__ = "0.9.0"  # importlib.metadata.version(__package__ or __name__)
+
+__all__ = ["basic_simulators", "dataset_generators", "config", "support_utils"]
