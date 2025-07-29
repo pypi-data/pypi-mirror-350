bbd8528cab1619e26082396b5cc99d30ab7a586c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:34 +0000

    Add package pyseoa/0.1.0/pyseoa-0.1.0.tar.gz

---
+++ packages/pyseoa/pyseoa-0.1.0.tar.gz/pyseoa-0.1.0/pyseoa/__init__.py
@@ -0,0 +1,16 @@
+"""
+pyseoa: Python SEO Analyzer
+
+https://github.com/sempre76/pyseoa
+"""
+
+from .analyzer import SEOAnalyzer, BatchSEOAnalyzer
+
+__all__         = ['SEOAnalyzer','BatchSEOAnalyzer']
+
+__description__ = "A seo analysis tool library written in python, with CLI support."
+__version__     = "0.1.0"
+__author__      = "masem"
+__email__       = "contact@masem.at"
+__url__         = "https://github.com/sempre76/pyseoa"
+__license__     = "MIT"
