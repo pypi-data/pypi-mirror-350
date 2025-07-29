4f8be51823215cc0aa9faed6610164eb2946367e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:07:09 +0000

    Add package metaworkflows/0.1.7/metaworkflows-0.1.7-py3-none-any.whl

---
+++ packages/metaworkflows/metaworkflows-0.1.7-py3-none-any.whl/metaworkflows/io/__init__.py
@@ -0,0 +1,5 @@
+# Expose common IO classes if needed, or keep it for structuring.
+# from .base import BaseReader, BaseWriter
+# from .file import FileReader, FileWriter
+# from .database import DatabaseReader, DatabaseWriter
+# ... etc.
