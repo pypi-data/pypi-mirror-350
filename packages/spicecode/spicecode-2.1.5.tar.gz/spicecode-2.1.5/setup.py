1e573a475b48f26c9d97bdb92f645b244e3c5c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:02 +0000

    Add package spicecode/2.1.5/spicecode-2.1.5.tar.gz

---
+++ packages/spicecode/spicecode-2.1.5.tar.gz/spicecode-2.1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt using utf-16 encoding to handle the BOM correctly
+with open("requirements.txt", encoding="utf-16") as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="spicecode",
+    version="2.1.5", # version 2.0.0 = all features from N2 
+    packages=find_packages(exclude=["spicecode-venv", "spicecode.egg-info"]),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'spice = cli.main:main',  # This tells Python to run the main function in cli.main
+        ],
+    },
+
+)
