0abae1f148ef3245e06561aeefbe2eaf3a5b6305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:48 +0000

    Add package chatgpt-mirai-qq-bot-scheduler/0.3.9/chatgpt-mirai-qq-bot-scheduler-0.3.9.tar.gz

---
+++ packages/chatgpt-mirai-qq-bot-scheduler/chatgpt-mirai-qq-bot-scheduler-0.3.9.tar.gz/chatgpt-mirai-qq-bot-scheduler-0.3.9/README.md
@@ -0,0 +1,24 @@
+# scheduler for ChatGPT-Mirai-QQ-Bot
+
+本项目是 [ChatGPT-Mirai-QQ-Bot](https://github.com/lss233/chatgpt-mirai-qq-bot) 的一个插件，用于在工作流中配置定时任务，同时提供auto_tool的聚合block，实现对定时任务增删查的自动调用。
+
+## 安装
+
+```bash
+pip install chatgpt-mirai-qq-bot-scheduler
+```
+
+## 使用
+
+在 chatgpt-mirai-qq-bot的web_ui中配置  
+使用示例请参考 [scheduler/example/normalWithTool.yml](scheduler/example/normalWithTool.yaml)    
+
+## 开源协议
+
+本项目基于 [ChatGPT-Mirai-QQ-Bot](https://github.com/lss233/chatgpt-mirai-qq-bot) 开发，遵循其 [开源协议](https://github.com/lss233/chatgpt-mirai-qq-bot/blob/master/LICENSE)
+
+## 感谢
+
+感谢 [ChatGPT-Mirai-QQ-Bot](https://github.com/lss233/chatgpt-mirai-qq-bot) 的作者 [lss233](https://github.com/lss233) 提供框架支持
+
+
---
+++ packages/chatgpt-mirai-qq-bot-scheduler/chatgpt-mirai-qq-bot-scheduler-0.3.9.tar.gz/chatgpt-mirai-qq-bot-scheduler-0.3.9/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import io
+import os
+
+version = os.environ.get('RELEASE_VERSION', '0.3.9'
+'').lstrip('v')
+
+setup(
+    name="chatgpt-mirai-qq-bot-scheduler",
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,  # 这行很重要
+    package_data={
+        "scheduler": ["example/*.yaml", "example/*.yml"],
+    },
+    install_requires=[
+        "apscheduler",
+        "kirara-ai>3.2.0",
+    ],
+    entry_points={
+        'chatgpt_mirai.plugins': [
+            'scheduler = scheduler:SchedulerPlugin'
+        ]
+    },
+    author="chuanSir",
+    author_email="416448943@qq.com",
+
+    description="定时任务 for lss233/chatgpt-mirai-qq-bot，提供聚合block实现定时任务的增删查(同时也可以配置其他安装的block实现自动调用)",
+    long_description=io.open("README.md", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/chuanSir123/scheduler",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/chuanSir123/scheduler/issues",
+        "Documentation": "https://github.com/chuanSir123/scheduler/wiki",
+        "Source Code": "https://github.com/chuanSir123/scheduler",
+    },
+    python_requires=">=3.8",
+)
