1f305d8a44ff8d87e9bfe5f7a3c066e5bd74007e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:56 +0000

    Add package repcosn/1.5.5/repcosn-1.5.5.tar.gz

---
+++ packages/repcosn/repcosn-1.5.5.tar.gz/repcosn-1.5.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    description = f.read()
+
+setup(
+    name = "repcosn",
+    version = "1.5.5",
+    author = "Abhidnya Tambe",
+    packages = find_packages(),
+    install_requires = [
+        #Nil
+    ],
+
+    long_description = description,
+    long_description_content_type="text/markdown",
+)
