e0b4e05837776ee2a779eda21d572b9f351c0aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:01 +0000

    Add package ommlx/0.0.0.dev320/ommlx-0.0.0.dev320-py3-none-any.whl

---
+++ packages/ommlx/ommlx-0.0.0.dev320-py3-none-any.whl/ommlx/minichain/__init__.py
@@ -0,0 +1,214 @@
+# fmt: off
+
+from .backends.manifests import (  # noqa
+    backend_of,
+    new_backend,
+)
+
+from .chat.formats import (  # noqa
+    JSON_RESPONSE_FORMAT,
+    JsonResponseFormat,
+    ResponseFormat,
+    TEXT_RESPONSE_FORMAT,
+    TextResponseFormat,
+)
+
+from .chat.history import (  # noqa
+    ChatHistory,
+    ChatHistoryService,
+    ListChatHistory,
+)
+
+from .chat.messages import (  # noqa
+    AiMessage,
+    Chat,
+    Message,
+    SystemMessage,
+    ToolExecResultMessage,
+    UserMessage,
+)
+
+from .chat.choices import (  # noqa
+    AiChoice,
+    AiChoices,
+)
+
+from .chat.services import (  # noqa
+    ChatRequest,
+    ChatResponse,
+    ChatService,
+    ChatService_,
+)
+
+from .chat.streaming import (  # noqa
+    ChatStreamRequest,
+    ChatStreamResponse,
+
+    ChatStreamService,
+    ChatStreamService_,
+)
+
+from .chat.templating import (  # noqa
+    ChatTemplatePart,
+    ChatTemplate,
+
+    MessageTemplate,
+    MessagePlaceholder,
+
+    ChatTemplater,
+)
+
+from .chat.tools import (  # noqa
+    Tool,
+)
+
+from .chat.types import (  # noqa
+    ChatRequestOption,
+    ChatResponseOutput,
+)
+
+from .completion import (  # noqa
+    CompletionRequest,
+    CompletionRequestOption,
+    CompletionResponse,
+    CompletionResponseOutput,
+    CompletionService,
+)
+
+from .configs import (  # noqa
+    Config,
+
+    consume_configs,
+)
+
+from .content.content import (  # noqa
+    Content,
+    ExtendedContent,
+)
+
+from .content.images import (  # noqa
+    Image,
+)
+
+from .content.rendering import (  # noqa
+    StringRenderer,
+)
+
+from .content.transforms import (  # noqa
+    ContentTransform,
+
+    StringFnContentTransform,
+    transform_content_strings,
+)
+
+from .envs import (  # noqa
+    Env,
+    EnvKey,
+)
+
+from .llms.tokens import (  # noqa
+    Token,
+    Tokens,
+)
+
+from .llms.services import (  # noqa
+    LlmRequestOption,
+
+    TopK,
+    Temperature,
+    MaxTokens,
+
+    LlmResponseOutput,
+
+    FinishReason,
+    FinishReasonOutput,
+
+    TokenUsage,
+    TokenUsageOutput,
+)
+
+from .services import (  # noqa
+    Request,
+    RequestOption,
+    Response,
+    ResponseOutput,
+    Service,
+    Service_,
+)
+
+from .standard import (  # noqa
+    ModelSpecifier,
+    ModelName,
+    ModelPath,
+
+    ApiKey,
+
+    DefaultRequestOptions,
+)
+
+from .streaming import (  # noqa
+    StreamResponse,
+)
+
+from .tools import (  # noqa
+    ToolDtype,
+    ToolExecRequest,
+    ToolParam,
+    ToolSpec,
+)
+
+from .vectors.embeddings import (  # noqa
+    EmbeddingRequest,
+    EmbeddingRequestOption,
+    EmbeddingResponse,
+    EmbeddingResponseOutput,
+    EmbeddingService,
+)
+
+from .vectors.index import (  # noqa
+    VectorIndexRequest,
+    VectorIndexRequestOption,
+    VectorIndexResponse,
+    VectorIndexResponseOutput,
+    VectorIndexService,
+    VectorIndexed,
+)
+
+from .vectors.search import (  # noqa
+    VectorHit,
+    VectorHits,
+    VectorSearch,
+    VectorSearchRequest,
+    VectorSearchRequestOption,
+    VectorSearchResponse,
+    VectorSearchResponseOutput,
+    VectorSearchService,
+    VectorSearchSimilarity,
+)
+
+from .vectors.similarity import (  # noqa
+    CALC_NP_SIMILARITIES_FUNCS,
+    Similarity,
+    calc_np_cosine_similarities,
+    calc_np_dot_similarities,
+    calc_np_similarities,
+)
+
+from .vectors.vectors import (  # noqa
+    Vector,
+    Vectorable,
+)
+
+from .vectors.stores import (  # noqa
+    VectorStore,
+)
+
+
+##
+
+
+from omlish.lang.imports import _register_conditional_import  # noqa
+
+_register_conditional_import('omlish.marshal', '.chat.marshal', __package__)
+_register_conditional_import('omlish.marshal', '.content.marshal', __package__)
+_register_conditional_import('omlish.marshal', '.llms.marshal', __package__)
