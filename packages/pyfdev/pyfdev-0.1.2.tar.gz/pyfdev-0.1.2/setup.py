26d214dc8e2ab46ff5854287ed15ad7de3d27cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:37 +0000

    Add package pyfdev/0.1.2/pyfdev-0.1.2.tar.gz

---
+++ packages/pyfdev/pyfdev-0.1.2.tar.gz/pyfdev-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import io
+from setuptools import setup, find_packages
+
+setup(
+    name="pyfdev",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "pyfdev = pyfdev.compiler.cli:main",
+        ],
+    },
+    author="Priyanshu Rauth",
+    author_email="raut.priyanshu30@gmail.com",
+    description="Pyfdev transpiler and CLI",
+    long_description=io.open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
