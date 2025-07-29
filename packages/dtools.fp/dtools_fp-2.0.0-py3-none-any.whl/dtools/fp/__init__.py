f223712881925100bbd3746d5eeb9706eb332584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:01 +0000

    Add package dtools.fp/2.0.0/dtools_fp-2.0.0-py3-none-any.whl

---
+++ packages/dtools.fp/dtools_fp-2.0.0-py3-none-any.whl/dtools/fp/__init__.py
@@ -0,0 +1,30 @@
+# Copyright 2023-2025 Geoffrey R. Scheller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""### Developer Tools - Pythonic functional programming
+
+Tools to aid with functional programming in Python yet still endeavoring to
+remain Pythonic.
+
+- *module* dtools.fp.bool: boolean with sub-types
+- *module* dtools.fp.function: tools to combine and partially apply functions
+- *module* dtools.fp.lazy: Lazy (non-strict) function evaluation
+- *module* dtools.fp.singletons: useful types with but one instance
+- *module* dtools.fp.state: classic implementation of the state monad
+
+"""
+
+__author__ = 'Geoffrey R. Scheller'
+__copyright__ = 'Copyright (c) 2023-2025 Geoffrey R. Scheller'
+__license__ = 'Apache License 2.0'
