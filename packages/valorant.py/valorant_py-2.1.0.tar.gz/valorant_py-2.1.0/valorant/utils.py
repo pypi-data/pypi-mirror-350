f24283beade070809f427962b24a4e450d5f73b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:18 +0000

    Add package valorant.py/2.1.0/valorant_py-2.1.0.tar.gz

---
+++ packages/valorant.py/valorant_py-2.1.0.tar.gz/valorant_py-2.1.0/valorant/utils.py
@@ -0,0 +1,12 @@
+import json
+
+try:
+    import msgspec
+except ImportError:  # pragma: no cover
+    msgspec = None
+
+
+if msgspec is None:  # pragma: no cover  # noqa: SIM108
+    _from_json = json.loads
+else:
+    _from_json = msgspec.json.decode
