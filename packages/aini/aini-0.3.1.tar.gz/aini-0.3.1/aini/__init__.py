f51be4f31f7470ec855057c76fd4c1b9cc52d31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:27 +0000

    Add package aini/0.3.1/aini-0.3.1.tar.gz

---
+++ packages/aini/aini-0.3.1.tar.gz/aini-0.3.1/aini/__init__.py
@@ -0,0 +1,7 @@
+"""xinit package for AI component initialization and visualization."""
+
+from aini.builder import aini
+from aini.viewer import aview, ameth
+
+__version__ = '0.3.1'
+__all__ = ['aini', 'aview', 'ameth']
