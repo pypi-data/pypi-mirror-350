00d102ec58827eccd9402f7930cd41208bcb71fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:08:13 +0000

    Add package hugo-nbconvert/2025.5.24.2/hugo_nbconvert-2025.5.24.2.tar.gz

---
+++ packages/hugo-nbconvert/hugo_nbconvert-2025.5.24.2.tar.gz/hugo_nbconvert-2025.5.24.2/hugo_nbconvert/resource.py
@@ -0,0 +1,48 @@
+content = r"""
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "ec2a032c",
+   "metadata": {},
+   "source": [
+    "---\n",
+    "date: '{date}'\n",
+    "title: '{title}'\n",
+    "---"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
+"""
+
+content_md = r"""---
+date: '{date}'
+title: '{title}'
+---
+"""
+content_md_toml = r"""+++
+date = '{date}'
+title = '{title}'
++++
+"""
+
+content_json = r"""
+{
+    "cell_type": "markdown",
+    "id": "ec2a032c",
+    "metadata": {},
+    "source": [
+        "---\n",
+        "date: '{date}'\n",
+        "title: '{title}'\n",
+        "---"
+    ]
+}"""
