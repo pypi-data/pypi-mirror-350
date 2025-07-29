4975f969f3c315c93f953d7b0019e1c758051ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:25 +0000

    Add package logisticpca/0.2.6/logisticpca-0.2.6-py3-none-any.whl

---
+++ packages/logisticpca/logisticpca-0.2.6-py3-none-any.whl/logisticpca/__init__.py
@@ -0,0 +1,4 @@
+from .logistic_pca import LogisticPCA, SparseLogisticPCA, LatentSparseLogisticPCA, LogisticPCA_SVT
+from .ppca import prob_pca
+
+__all__ = ["LogisticPCA", "SparseLogisticPCA", "LatentSparseLogisticPCA", "prob_pca", "LogisticPCA_SVT", CollinsLogisticPCA]
