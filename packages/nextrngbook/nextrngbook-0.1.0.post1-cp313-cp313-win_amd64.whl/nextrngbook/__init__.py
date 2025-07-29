cf4ceb26c60c32f5f1637e110a1f1db21ed691a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:47 +0000

    Add package nextrngbook/0.1.0.post1/nextrngbook-0.1.0.post1-cp313-cp313-win_amd64.whl

---
+++ packages/nextrngbook/nextrngbook-0.1.0.post1-cp313-cp313-win_amd64.whl/nextrngbook/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# MIT License
+# Copyright (c) 2025 chintunglin
+
+"""
+NextRNGBook is a random number generator (RNG) package compatible with NumPy.
+It currently includes an implementation of the DX generator for random number
+generation, with plans to introduce additional generators in the future.
+
+**Subpackages:**
+
+- [`dx_generator`](dx_generator.md): Implements the DX algorithm for random 
+number generation.
+"""
+
+from . import dx_generator
