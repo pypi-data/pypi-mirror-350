3a12d0f7258388c02b73fb22713bbb2905005b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:38 +0000

    Add package invenio-audit-logs/0.2.0/invenio_audit_logs-0.2.0-py2.py3-none-any.whl

---
+++ packages/invenio-audit-logs/invenio_audit_logs-0.2.0-py2.py3-none-any.whl/invenio_audit_logs/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio-Audit-Logs is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Module providing audit logging features for Invenio.."""
+
+from .ext import InvenioAuditLogs
+
+__version__ = "0.2.0"
+
+__all__ = (
+    "__version__",
+    "InvenioAuditLogs",
+)
