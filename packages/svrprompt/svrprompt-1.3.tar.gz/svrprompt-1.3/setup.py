ba12df533014062a9baec97d9c4629bdbcfebdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:16 +0000

    Add package svrprompt/1.3/svrprompt-1.3.tar.gz

---
+++ packages/svrprompt/svrprompt-1.3.tar.gz/svrprompt-1.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='svrprompt',
+	version='1.3',
+	author='afkdev',
+	author_email='afk256rec@gmail.com',
+	description='svrprompt',
+	packages=['svrprompt'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.9',
+)
