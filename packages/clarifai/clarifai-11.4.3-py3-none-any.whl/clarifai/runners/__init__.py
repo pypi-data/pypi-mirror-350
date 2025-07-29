2e240af7d5f2df30ccd6a852d3d694416197b067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:08 +0000

    Add package clarifai/11.4.3/clarifai-11.4.3-py3-none-any.whl

---
+++ packages/clarifai/clarifai-11.4.3-py3-none-any.whl/clarifai/runners/__init__.py
@@ -0,0 +1,9 @@
+from .models.model_builder import ModelBuilder
+from .models.model_class import ModelClass
+from .models.model_runner import ModelRunner
+
+__all__ = [
+    "ModelRunner",
+    "ModelBuilder",
+    "ModelClass",
+]
