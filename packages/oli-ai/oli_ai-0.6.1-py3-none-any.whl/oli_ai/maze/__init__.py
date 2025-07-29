5fe244bebc2117c8e9ecbbba72c3d62d9ea25b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:42 +0000

    Add package oli-ai/0.6.1/oli_ai-0.6.1-py3-none-any.whl

---
+++ packages/oli-ai/oli_ai-0.6.1-py3-none-any.whl/oli_ai/maze/__init__.py
@@ -0,0 +1,5 @@
+from .maze import Square, random_maze, plot_maze, search_, search_animated, search_solution
+
+
+# Se vuoi esporlo con il wildcard (*)
+__all__ = ["Square","random_maze", "plot_maze", "search_", "search_animated", "searh_solution" ]
