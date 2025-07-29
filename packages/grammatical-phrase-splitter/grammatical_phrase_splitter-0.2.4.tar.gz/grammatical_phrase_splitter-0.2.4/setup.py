f9c417b9ad8a0d48c460523cbb27d608f34073a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:54 +0000

    Add package grammatical-phrase-splitter/0.2.4/grammatical_phrase_splitter-0.2.4.tar.gz

---
+++ packages/grammatical-phrase-splitter/grammatical_phrase_splitter-0.2.4.tar.gz/grammatical_phrase_splitter-0.2.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grammatical-phrase-splitter",
+    version="0.2.4",
+    packages=find_packages(
+        include=['grammatical_phrase_splitter', 'grammatical_phrase_splitter.*']
+    ),
+    install_requires=[
+        "openai",       
+        "spacy"        
+    ],
+    entry_points={
+        'console_scripts': [
+            'split-phrase=grammatical_phrase_splitter.grammatical_phrase_splitter:main',
+        ],
+    },
+    include_package_data=True,
+    description="A package to split sentences into grammatical segments using OpenAI API",
+    author="Ziwei Gu",
+    author_email="ziweigu@g.harvard.edu",
+    url="https://github.com/ZiweiGu/GP-TSM", 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
