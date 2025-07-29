93fa7a6e84c28c18826c58c5cdce75162fe59234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:48 +0000

    Add package qxmt/0.5.2/qxmt-0.5.2-py3-none-any.whl

---
+++ packages/qxmt/qxmt-0.5.2-py3-none-any.whl/qxmt/devices/builder.py
@@ -0,0 +1,47 @@
+from qxmt.configs import DeviceConfig
+from qxmt.devices.base import BaseDevice
+
+
+class DeviceBuilder:
+    """
+    Builder class for quantum devices.
+
+    Examples:
+        >>> from qxmt.configs import DeviceConfig
+        >>> from qxmt.devices.builder import DeviceBuilder
+        >>> config = DeviceConfig(
+        ...     platform="pennylane",
+        ...     device_name="default.qubit",
+        ...     backend_name=None,
+        ...     n_qubits=2,
+        ...     shots=1000,
+        ...     random_seed=42,
+        ... )
+        >>> device = DeviceBuilder(config).build()
+    """
+
+    def __init__(
+        self,
+        config: DeviceConfig,
+    ) -> None:
+        """Initialize the device builder.
+
+        Args:
+            config (DeviceConfig): Configuration for the quantum device. It is element of the ExperimentConfig.
+        """
+        self.config: DeviceConfig = config
+
+    def build(self) -> BaseDevice:
+        """Build a quantum device. it can be a general-purpose device overseeing multiple platforms.
+
+        Returns:
+            BaseDevice: General-purpose device overseeing multiple platforms
+        """
+        return BaseDevice(
+            platform=self.config.platform,
+            device_name=self.config.device_name,
+            backend_name=self.config.backend_name,
+            n_qubits=self.config.n_qubits,
+            shots=self.config.shots,
+            random_seed=self.config.random_seed,
+        )
