e1d7620a843f5e003b5c683f0145982e0c2b77b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:48 +0000

    Add package measure-space-api/0.1.0/measure_space_api-0.1.0-py3-none-any.whl

---
+++ packages/measure-space-api/measure_space_api-0.1.0-py3-none-any.whl/measure_space_api/constants.py
@@ -0,0 +1,10 @@
+
+URL_MAPPING = {
+    'hourly_weather': 'https://q0np6mu0vi.execute-api.us-east-1.amazonaws.com/prd/global-hourly-weather-forecast',
+    'daily_weather': 'https://4y0sy5lved.execute-api.us-east-1.amazonaws.com/prd/global-daily-weather-forecast',
+    'daily_climate': 'https://d43arqqeh8.execute-api.us-east-1.amazonaws.com/prd/global-daily-climate-forecast',
+    'hourly_air_quality': 'https://pawsqe3sob.execute-api.us-east-1.amazonaws.com/prd/global-hourly-air-quality-forecast',
+    'daily_air_quality': 'https://pawsqe3sob.execute-api.us-east-1.amazonaws.com/prd/global-daily-air-quality-forecast',
+    'geocoding_nearest_city': 'https://ncstsm9hel.execute-api.us-east-1.amazonaws.com/prd/nearest-city',
+    'geocoding_autocomplete': 'https://ncstsm9hel.execute-api.us-east-1.amazonaws.com/prd/autocomplete',
+}
