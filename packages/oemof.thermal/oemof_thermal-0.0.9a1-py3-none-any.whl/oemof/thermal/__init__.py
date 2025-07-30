fbe2f3e9d4cb9950d46cfabf2e2e8717b669fdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:20 +0000

    Add package oemof.thermal/0.0.9a1/oemof_thermal-0.0.9a1-py3-none-any.whl

---
+++ packages/oemof.thermal/oemof_thermal-0.0.9a1-py3-none-any.whl/oemof/thermal/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.0.9a1"
+__project__ = "oemof.thermal"
+
+from . import absorption_heatpumps_and_chillers
+from . import cogeneration
+from . import compression_heatpumps_and_chillers
+from . import concentrating_solar_power
+from . import facades
+from . import solar_thermal_collector
+from . import stratified_thermal_storage
+
+__all__ = [
+    "absorption_heatpumps_and_chillers",
+    "compression_heatpumps_and_chillers",
+    "facades",
+    "stratified_thermal_storage",
+    "cogeneration",
+    "concentrating_solar_power",
+    "solar_thermal_collector",
+]
