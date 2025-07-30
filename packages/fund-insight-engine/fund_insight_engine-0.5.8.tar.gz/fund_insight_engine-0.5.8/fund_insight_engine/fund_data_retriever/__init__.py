a74acbfe7ab38cf3a2ecbdc3a73619f29a7f2891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:42 +0000

    Add package fund-insight-engine/0.5.8/fund_insight_engine-0.5.8.tar.gz

---
+++ packages/fund-insight-engine/fund_insight_engine-0.5.8.tar.gz/fund_insight_engine-0.5.8/fund_insight_engine/fund_data_retriever/__init__.py
@@ -0,0 +1,7 @@
+from .menu_data import *
+from .fund_codes import *
+from .portfolio import *
+from .firm_aum import *
+from .fund_info import *
+from .fund_mappings import *
+from .timeseries_manager import *
