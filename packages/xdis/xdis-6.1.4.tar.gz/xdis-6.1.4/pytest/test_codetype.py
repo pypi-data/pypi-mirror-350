d9e75d75a861ea2076bc5e83eef1377b2ab78a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:14 +0000

    Add package xdis/6.1.4/xdis-6.1.4.tar.gz

---
+++ packages/xdis/xdis-6.1.4.tar.gz/xdis-6.1.4/pytest/test_codetype.py
@@ -0,0 +1,33 @@
+"""
+Check interoperability of native an emulated code type.
+"""
+import types
+
+import xdis.codetype
+
+
+def five():
+    return 5
+
+
+def test_codeType2Portable():
+    if hasattr(five, "__code__"):
+        # Python 2.6+
+        five_code = five.__code__
+    elif hasattr(five, "func_code"):
+        # Python 2.6-
+        five_code = five.func_code
+
+    xdis.codetype.portableCodeType()
+    cc = xdis.codetype.codeType2Portable(five_code)
+    assert xdis.codetype.portableCodeType() == type(cc)
+    new_code = cc.to_native()
+    assert types.CodeType == type(new_code)
+    assert eval(new_code) == 5
+    cc_new = cc.replace(co_name="five_renamed")
+    assert cc_new.co_name == "five_renamed"
+    assert eval(cc_new.to_native()) == 5
+
+
+if __name__ == "__main__":
+    test_codeType2Portable()
