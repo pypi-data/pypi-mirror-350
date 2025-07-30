a1f3047c3a73e7f9fd7747353651aa03cf11748e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:44 +0000

    Add package maleo-metadata/0.1.6/maleo_metadata-0.1.6.tar.gz

---
+++ packages/maleo-metadata/maleo_metadata-0.1.6.tar.gz/maleo_metadata-0.1.6/maleo_metadata/models/schemas/service.py
@@ -0,0 +1,14 @@
+from pydantic import Field
+from maleo_foundation.models.schemas.general import BaseGeneralSchemas
+from maleo_foundation.models.schemas.parameter import BaseParameterSchemas
+from maleo_metadata.enums.service import MaleoMetadataServiceEnums
+
+class MaleoMetadataServiceSchemas:
+    class IdentifierType(BaseParameterSchemas.IdentifierType):
+        identifier:MaleoMetadataServiceEnums.IdentifierType = Field(..., description="Service's identifier type")
+
+    class Key(BaseGeneralSchemas.Key):
+        key:str = Field(..., max_length=20, description="Service's key")
+
+    class Name(BaseGeneralSchemas.Name):
+        name:str = Field(..., max_length=20, description="Service's name")
