c61d2074ab68405e4695fd92ec19ddd4cf8251c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:54 +0000

    Add package kaiserlift/0.1.0/kaiserlift-0.1.0.tar.gz

---
+++ packages/kaiserlift/kaiserlift-0.1.0.tar.gz/kaiserlift-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kaiserlift',
+    version='0.1.0',
+    description='Data-driven progressive overload',
+    author='Douglas Kaiser',
+    author_email='douglastkaiser@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'kaiserlift-cli = kaiserlift.main:main',
+        ],
+    },
+)
