6f2f0b17622a9f25444ea6dac34328df2dc66d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:50 +0000

    Add package mcp-server-baidu-digitalhuman/0.1.0/mcp_server_baidu_digitalhuman-0.1.0-py3-none-any.whl

---
+++ packages/mcp-server-baidu-digitalhuman/mcp_server_baidu_digitalhuman-0.1.0-py3-none-any.whl/mcp_server_baidu_digitalhuman/__init__.py
@@ -0,0 +1,16 @@
+# __init__.py
+"""
+# MCP Server from Baidu Digital Human
+# File: __init__.py.py
+# Authors: digithuman
+# Date: 2025/5/21 10:07
+"""
+
+from .dhserver import start
+
+def main():
+    """Run the MCP server."""
+    start()
+
+if __name__ == "__main__":
+    main()
---
+++ packages/mcp-server-baidu-digitalhuman/mcp_server_baidu_digitalhuman-0.1.0-py3-none-any.whl/mcp_server_baidu_digitalhuman/__main__.py
@@ -0,0 +1,10 @@
+"""
+# MCP Server from Baidu Digital Human
+# File: __main__.py.py
+# Authors: digithuman
+# Date: 2025/5/21 10:07
+"""
+
+from mcp_server_baidu_digitalhuman import main
+
+main()
