b07dd4074b795ddd5ec86144aa20b07448b47404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:05 +0000

    Add package ptulsconv/2.2.1/ptulsconv-2.2.1.tar.gz

---
+++ packages/ptulsconv/ptulsconv-2.2.1.tar.gz/ptulsconv-2.2.1/ptulsconv/__init__.py
@@ -0,0 +1,9 @@
+"""
+Parse and convert Pro Tools text exports
+"""
+
+__version__ = '2.2.1'
+__author__ = 'Jamie Hardt'
+__license__ = 'MIT'
+__copyright__ = "%s %s (c) 2025 %s. All rights reserved." \
+        % (__name__, __version__, __author__)
