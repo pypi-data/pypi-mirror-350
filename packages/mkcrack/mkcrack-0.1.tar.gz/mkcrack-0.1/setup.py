3b43dbd89359f3fbf4e483901b1a7af1def9513d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:38 +0000

    Add package mkcrack/0.1/mkcrack-0.1.tar.gz

---
+++ packages/mkcrack/mkcrack-0.1.tar.gz/mkcrack-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkcrack',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple yescrypt hash cracker by Manju',
+    author='Manju',
+    author_email='kotabagimanju240@gmail.com',
+    python_requires='>=3.6',
+)
