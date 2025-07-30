6af41b104ca758e65163271278f340d4d7b00e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:36 +0000

    Add package truefoundry/0.9.1/truefoundry-0.9.1-py3-none-any.whl

---
+++ packages/truefoundry/truefoundry-0.9.1-py3-none-any.whl/truefoundry/deploy/cli/commands/__init__.py
@@ -0,0 +1,16 @@
+from truefoundry.deploy.cli.commands.apply_command import get_apply_command
+from truefoundry.deploy.cli.commands.ask_command import get_ask_command
+from truefoundry.deploy.cli.commands.build_command import get_build_command
+from truefoundry.deploy.cli.commands.delete_command import get_delete_command
+from truefoundry.deploy.cli.commands.deploy_command import get_deploy_command
+from truefoundry.deploy.cli.commands.deploy_init_command import get_deploy_init_command
+from truefoundry.deploy.cli.commands.get_command import get_get_command
+from truefoundry.deploy.cli.commands.login_command import get_login_command
+from truefoundry.deploy.cli.commands.logout_command import get_logout_command
+from truefoundry.deploy.cli.commands.logs_command import get_logs_command
+from truefoundry.deploy.cli.commands.patch_application_command import (
+    get_patch_application_command,
+)
+from truefoundry.deploy.cli.commands.patch_command import get_patch_command
+from truefoundry.deploy.cli.commands.terminate_comand import get_terminate_command
+from truefoundry.deploy.cli.commands.trigger_command import get_trigger_command
