823bc7722500f7bda93a691c7b1d74b80f89fd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:18 +0000

    Add package nsepythonserver/2.97/nsepythonserver-2.97.tar.gz

---
+++ packages/nsepythonserver/nsepythonserver-2.97.tar.gz/nsepythonserver-2.97/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import os
+version = os.environ.get("PACKAGE_VERSION", "0.0.0")  # fallback if not set
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'nsepythonserver',
+    packages=setuptools.find_packages(),
+    version = version,
+    license='GNU',
+    include_package_data=True,
+    description = 'Python library for NSE India APIs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  author = 'Aeron7',
+    author_email = 'dexter@unofficed.com',
+    url = 'https://github.com/aeron7/nsepythonserver',
+    install_requires=['requests', 'pandas','scipy'],
+    keywords = ['nseindia', 'nse', 'python', 'sdk', 'trading', 'stock markets'],
+    classifiers=[
+      'Intended Audience :: Developers',
+      'Natural Language :: English',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: Implementation :: PyPy',
+      'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
