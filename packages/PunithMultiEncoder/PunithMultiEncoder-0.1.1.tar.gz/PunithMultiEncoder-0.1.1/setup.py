6fa5537b83038f06032645a7e1f74c3582733333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:36:23 +0000

    Add package PunithMultiEncoder/0.1.1/PunithMultiEncoder-0.1.1.tar.gz

---
+++ packages/PunithMultiEncoder/PunithMultiEncoder-0.1.1.tar.gz/PunithMultiEncoder-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PunithMultiEncoder',
+    version='0.1.1',
+    description='A multilingual encryption and decryption library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Punithan',
+    author_email='punithanae@gmail.com',
+    packages=find_packages(),
+    keywords=['encryption', 'multilingual', 'decryption', 'language', 'security'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
+
+
