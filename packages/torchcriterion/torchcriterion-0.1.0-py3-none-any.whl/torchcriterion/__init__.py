b7a8032bbec4be9481561c1a70ae2c65e7cfdd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:06 +0000

    Add package torchcriterion/0.1.0/torchcriterion-0.1.0-py3-none-any.whl

---
+++ packages/torchcriterion/torchcriterion-0.1.0-py3-none-any.whl/torchcriterion/__init__.py
@@ -0,0 +1,12 @@
+from torchcriterion.regression.mse import MSELoss
+
+__all__ = [
+    "CrossEntropyLoss",
+    "FocalLoss",
+    "MSELoss",
+    "HuberLoss",
+    "DiceLoss",
+    "TverskyLoss",
+    "TripletLoss",
+    "ContrastiveLoss",
+]
