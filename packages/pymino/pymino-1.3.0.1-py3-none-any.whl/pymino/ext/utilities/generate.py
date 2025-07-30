4ba5c653a73fb47855a240a121c8a3aadc699c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:44 +0000

    Add package pymino/1.3.0.1/pymino-1.3.0.1-py3-none-any.whl

---
+++ packages/pymino/pymino-1.3.0.1-py3-none-any.whl/pymino/ext/utilities/generate.py
@@ -0,0 +1,88 @@
+from hmac import new
+from hashlib import sha1
+from base64 import b64encode
+from secrets import token_hex
+from typing import Union
+import requests
+
+class Generator:
+    def __init__(
+        self,
+        prefix:        Union[str, int],
+        device_key:    str,
+        signature_key: str,
+        service_key: str
+        ) -> None:
+        self.PREFIX = bytes.fromhex(str(prefix))
+        self.DEVICE_KEY = bytes.fromhex(device_key)
+        self.SIGNATURE_KEY = bytes.fromhex(signature_key)
+        self.SERVICE_KEY = service_key
+
+    def device_id(self) -> str:
+        """
+        `generate_device_id` Generates a device ID based on a specific string.
+
+        `**Returns**`
+        - `str` - Returns a device ID as a string.
+        """
+        encoded_data = sha1(str(token_hex(20)).encode('utf-8')).hexdigest()
+
+        digest = new(
+            self.DEVICE_KEY,
+            self.PREFIX + bytes.fromhex(encoded_data),
+            sha1).hexdigest()
+
+        return f"{bytes.hex(self.PREFIX)}{encoded_data}{digest}".upper()
+
+    def signature(self, data: str) -> str:
+        """
+        `signature` Generates a signature based on a specific string.
+        
+        `**Parameters**`
+        - `data` - Data to generate a signature from
+        `**Returns**`
+        - `str` - Returns a signature as a string.
+        """
+
+        signature = [self.PREFIX[0]]  
+        signature.extend(new(
+            self.SIGNATURE_KEY,
+            str(data).encode("utf-8"), sha1).digest())
+
+        return b64encode(bytes(signature)).decode("utf-8")
+    
+    def update_device(self, device: str) -> str:
+        """
+        Update a device ID to new prefix.
+
+        :param device: The device ID to update.
+        :type device: str
+        :return: The updated device ID as a string.
+        :rtype: str
+        """
+        encoded_data = sha1(str(bytes.fromhex(device[2:42])).encode('utf-8')).hexdigest()
+
+        digest = new(
+            self.DEVICE_KEY,
+            self.PREFIX + bytes.fromhex(encoded_data),
+            sha1).hexdigest()
+
+        return f"{bytes.hex(self.PREFIX)}{encoded_data}{digest}".upper()
+    
+    def sign_data(self, data: str, auid: str, sid: str, deviceid: str) -> str:
+        if any(not i for i in (data, auid, sid, deviceid)):
+            return None
+        response = requests.post(
+            "https://friendify.ninja/api/v1/g/s/security/public_key",
+            headers={
+                "SID": sid,
+                "NDCDEVICEID": deviceid,
+                "AUID": auid,
+                "key": self.SERVICE_KEY
+            },
+            data=str(data).encode("utf-8")
+        )
+        
+        if response.status_code != 200:
+            raise Exception(response.text)
+        return response.text
