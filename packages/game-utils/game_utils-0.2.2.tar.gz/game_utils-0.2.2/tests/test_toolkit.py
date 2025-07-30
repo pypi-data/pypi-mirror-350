b456d3820cca4e764a4eb0c78969861dbbda0286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:07 +0000

    Add package game-utils/0.2.2/game_utils-0.2.2.tar.gz

---
+++ packages/game-utils/game_utils-0.2.2.tar.gz/game_utils-0.2.2/tests/test_toolkit.py
@@ -0,0 +1,42 @@
+import shutil
+import os
+import src.game_utils.toolkit as toolkit
+
+TEST_PATH = os.path.dirname(os.path.abspath(__file__))
+TEST_GAME_PATH = os.path.join(TEST_PATH, "test_game")
+
+
+def test_batocera_package_path():
+    def _clear():
+        shutil.rmtree(os.path.join(TEST_GAME_PATH, "game_utils"))
+        os.remove(os.path.join(TEST_GAME_PATH, "testgame.pygame"))
+        os.remove(os.path.join(TEST_GAME_PATH, "testgame.zip"))
+
+    lib_path = os.path.join(TEST_GAME_PATH, "game_utils")
+    if os.path.exists(lib_path):
+        _clear()
+
+    assert os.path.exists(TEST_GAME_PATH)
+    assert os.path.exists(os.path.join(TEST_GAME_PATH, "testgame.py"))
+
+    toolkit.package_for_batocera("testgame", TEST_GAME_PATH)
+
+    assert os.path.exists(lib_path)
+    assert len(os.listdir(lib_path)) > 0
+    assert os.path.exists(os.path.join(TEST_GAME_PATH, "testgame.pygame"))
+    assert os.path.exists(os.path.join(TEST_GAME_PATH, "testgame.zip"))
+
+    _clear()
+
+
+def test_default_package():
+    def _clear():
+        os.remove(os.path.join(TEST_GAME_PATH, "testgame.zip"))
+
+    if os.path.exists(os.path.join(TEST_GAME_PATH, "testgame.zip")):
+        _clear()
+
+    toolkit.default_package("testgame", TEST_GAME_PATH)
+    assert os.path.exists(os.path.join(TEST_GAME_PATH, "testgame.zip"))
+
+    _clear()
