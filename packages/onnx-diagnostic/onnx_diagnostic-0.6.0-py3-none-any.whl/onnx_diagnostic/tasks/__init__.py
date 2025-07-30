c8612a9993622a31fa8e33398dcae59009a5ae19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:31 +0000

    Add package onnx-diagnostic/0.6.0/onnx_diagnostic-0.6.0-py3-none-any.whl

---
+++ packages/onnx-diagnostic/onnx_diagnostic-0.6.0-py3-none-any.whl/onnx_diagnostic/tasks/__init__.py
@@ -0,0 +1,56 @@
+from typing import Any, Callable, Dict, List, Tuple
+from . import (
+    automatic_speech_recognition,
+    feature_extraction,
+    fill_mask,
+    image_classification,
+    image_text_to_text,
+    mixture_of_expert,
+    object_detection,
+    sentence_similarity,
+    summarization,
+    text_classification,
+    text_generation,
+    text2text_generation,
+    zero_shot_image_classification,
+)
+
+__TASKS__ = [
+    automatic_speech_recognition,
+    feature_extraction,
+    fill_mask,
+    image_classification,
+    image_text_to_text,
+    mixture_of_expert,
+    object_detection,
+    sentence_similarity,
+    summarization,
+    text_classification,
+    text_generation,
+    text2text_generation,
+    zero_shot_image_classification,
+]
+
+
+def supported_tasks() -> List[str]:
+    "Returns the list of supported tasks."
+    return sorted(mod.__TASK__ for mod in __TASKS__)
+
+
+def reduce_model_config(config: Any, task: str) -> Dict[str, Any]:
+    """Reduces a model size."""
+    tasks = {mod.__TASK__: mod.reduce_model_config for mod in __TASKS__}
+    assert task in tasks, f"Task {task!r} not found in {sorted(tasks)}"
+    return tasks[task](config)
+
+
+def random_input_kwargs(config: Any, task: str) -> Tuple[Dict[str, Any], Callable]:
+    """
+    Inputs kwargs.
+    If the configuration is None, the function selects typical dimensions.
+    It returns parameters and a function. The function creates dummy inputs
+    if it receives the parameters returned as a first result.
+    """
+    tasks = {mod.__TASK__: mod.random_input_kwargs for mod in __TASKS__}
+    assert task in tasks, f"Task {task!r} not found in {sorted(tasks)}"
+    return tasks[task](config)
---
+++ packages/onnx-diagnostic/onnx_diagnostic-0.6.0-py3-none-any.whl/onnx_diagnostic/torch_export_patches/patch_module_helper.py
@@ -0,0 +1,69 @@
+import ast
+from typing import Any, List, Optional
+
+
+class OrToBitOrTransformer(ast.NodeTransformer):
+    def visit_BoolOp(self, node):
+        self.generic_visit(node)
+        if isinstance(node.op, ast.Or):
+            new_node = node.values[0]
+            for value in node.values[1:]:
+                new_node = ast.BinOp(left=new_node, op=ast.BitOr(), right=value)
+            return ast.copy_location(new_node, node)
+        return node
+
+
+def ast_or_into_bitor(node: "ast.Node") -> "ast.Node":
+    """Replaces every operator ``or`` into ``|``."""
+    new_node = OrToBitOrTransformer().visit(node)
+    return new_node
+
+
+def _rewrite_bart_encoder_layer():
+    "BartEncoderLayer, PLBartEncoderLayer"
+    import transformers
+
+    bd = dict(
+        filter_node=(
+            lambda node: isinstance(node, ast.If) and not isinstance(node.test, ast.Name)
+        ),
+        pre_rewriter=ast_or_into_bitor,
+    )
+
+    def _add(f):
+        g = bd.copy()
+        g["function"] = f
+        return g
+
+    return [
+        _add(transformers.models.bart.modeling_bart.BartEncoderLayer.forward),
+        _add(transformers.models.plbart.modeling_plbart.PLBartEncoderLayer.forward),
+    ]
+
+
+def code_needing_rewriting(cls_name: str) -> Optional[List[Any]]:
+    """
+    Returns a known list of methods or functions to rewrite because of control flow
+    for a specific model class.
+
+    :param cls_name: name of the class
+    :return: a list of rewriting
+
+    .. runpython::
+        :showcode:
+
+        import pprint
+        from onnx_diagnostic.torch_export_patches.patch_module_helper import (
+            code_needing_rewriting,
+        )
+
+        pprint.pprint(code_needing_rewriting("BartForConditionalGeneration"))
+    """
+    if cls_name in {
+        "BartEncoderLayer",
+        "BartForConditionalGeneration",
+        "PLBartEncoderLayer",
+        "PLBartForConditionalGeneration",
+    }:
+        return _rewrite_bart_encoder_layer()
+    return None
