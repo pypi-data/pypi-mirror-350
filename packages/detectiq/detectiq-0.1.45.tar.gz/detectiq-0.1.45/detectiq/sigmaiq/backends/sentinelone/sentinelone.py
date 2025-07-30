07cbf1ad5493f8676ffe41d11d80a5cd81f0585f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:01 +0000

    Add package detectiq/0.1.45/detectiq-0.1.45.tar.gz

---
+++ packages/detectiq/detectiq-0.1.45.tar.gz/detectiq-0.1.45/detectiq/sigmaiq/backends/sentinelone/sentinelone.py
@@ -0,0 +1,17 @@
+from sigma.backends.sentinelone import SentinelOneBackend
+from sigma.conversion.state import ConversionState
+from sigma.rule import SigmaRule
+
+from detectiq.sigmaiq.backends.sigmaiq_abstract_backend import (
+    AbstractGenericSigmAIQBackendClass,
+)
+
+
+class SigmAIQSentinelOneBackend(AbstractGenericSigmAIQBackendClass, SentinelOneBackend):
+    custom_formats = {}
+    associated_pipelines = ["sentinelone"]
+    default_pipeline = "sentinelone"
+
+    # Fix SentinelOne json output format. "id" key in JSON is of type UUID, should be str
+    def finalize_query_json(self, rule: SigmaRule, query: str, index: int, state: ConversionState) -> dict:
+        return {"query": query, "title": rule.title, "id": str(rule.id), "description": rule.description}
