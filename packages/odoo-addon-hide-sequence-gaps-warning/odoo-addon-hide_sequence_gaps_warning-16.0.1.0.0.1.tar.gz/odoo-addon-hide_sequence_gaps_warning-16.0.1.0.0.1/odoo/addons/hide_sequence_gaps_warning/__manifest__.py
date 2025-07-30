a407f432ac8c266b7f5b0906a3982e6b29489df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:41 +0000

    Add package odoo-addon-hide-sequence-gaps-warning/16.0.1.0.0.1/odoo-addon-hide_sequence_gaps_warning-16.0.1.0.0.1.tar.gz

---
+++ packages/odoo-addon-hide-sequence-gaps-warning/odoo-addon-hide_sequence_gaps_warning-16.0.1.0.0.1.tar.gz/odoo-addon-hide_sequence_gaps_warning-16.0.1.0.0.1/odoo/addons/hide_sequence_gaps_warning/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Hide Sequence Gaps Warning",
+    "version": "16.0.1.0.0",
+    "depends": ["account"],
+    "author": "Coopdevs Treball SCCL, " "Som Connexió SCCL",
+    "website": "https://coopdevs.org",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "data": ["views/account_journal_dashboard_view.xml"],
+    "demo": [],
+    "external_dependencies": {},
+}
