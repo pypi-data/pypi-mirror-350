a11a23e2f7afe65be92d9106a979a1afaf0ef16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:28 +0000

    Add package hosted-flasks/0.5.0/hosted_flasks-0.5.0.tar.gz

---
+++ packages/hosted-flasks/hosted_flasks-0.5.0.tar.gz/hosted_flasks-0.5.0/hosted_flasks/utils.py
@@ -0,0 +1,16 @@
+class DottedDict:
+  """
+  wrapper around a normal dict, allowing dotted key access
+  >>> d = { "a" : 1, "b" : { "b1" : 1, "b2" : 2 }}
+  >>> dd = DottedDict(d)
+  >>> dd["b.b2"]
+  2
+  """
+  def __init__(self, wrapped_dict):
+    self.wrapped_dict = wrapped_dict
+    
+  def __getitem__(self, path):
+    value = self.wrapped_dict
+    for step in path.split("."):
+      value = value[step]
+    return value
