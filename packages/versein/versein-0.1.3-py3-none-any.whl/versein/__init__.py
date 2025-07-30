2946db089b06f85fa509ff31bd6367b92960f291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:16 +0000

    Add package versein/0.1.3/versein-0.1.3-py3-none-any.whl

---
+++ packages/versein/versein-0.1.3-py3-none-any.whl/versein/__init__.py
@@ -0,0 +1,12 @@
+"""Top level API.
+
+.. data:: __version__
+    :type: str
+
+    Version number as calculated by https://github.com/pypa/setuptools_scm
+"""
+
+from ._version import __version__
+from .greeting import Greeter
+
+__all__ = ["__version__","Greeter"]
---
+++ packages/versein/versein-0.1.3-py3-none-any.whl/versein/greeting.py
@@ -0,0 +1,6 @@
+class Greeter:
+    def __init__(self, name="Developer"):
+        self.name = name
+
+    def say_hello(self):
+        return f"Hello, {self.name}! Welcome to Versein SDK."
