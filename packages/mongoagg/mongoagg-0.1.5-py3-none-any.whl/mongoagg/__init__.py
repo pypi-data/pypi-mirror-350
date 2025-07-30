96ecd91f3f1df22edab1a59e2cfa3f2430bc9ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:30 +0000

    Add package mongoagg/0.1.5/mongoagg-0.1.5-py3-none-any.whl

---
+++ packages/mongoagg/mongoagg-0.1.5-py3-none-any.whl/mongoagg/__init__.py
@@ -0,0 +1,20 @@
+"""
+MongoDB Aggregation Pipeline Builder
+"""
+
+__version__ = "0.1.5"
+
+from .core import AggBuilder
+from .exceptions import AggBuilderError, InvalidLookupError, PipelineSerializationError, InvalidStageError
+from .models import Expr, Pipeline, ConditionBuilder
+
+__all__ = [
+    "AggBuilder",
+    "AggBuilderError",
+    "InvalidLookupError",
+    "PipelineSerializationError",
+    "InvalidStageError",
+    "Expr",
+    "Pipeline",
+    "ConditionBuilder",
+]
