ae7eecec26e2791602138180fffdf49d21754d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:31 +0000

    Add package bokcolmaps/4.3.0/bokcolmaps-4.3.0-py3-none-any.whl

---
+++ packages/bokcolmaps/bokcolmaps-4.3.0-py3-none-any.whl/bokcolmaps/Examples/ColourMapLPSliderExample.py
@@ -0,0 +1,15 @@
+"""
+To run this example at the command line enter:
+python ColourMapLPSLiderExample.py
+"""
+
+from bokeh.plotting import show
+
+from bokcolmaps.ColourMapLPSlider import ColourMapLPSlider
+from bokcolmaps.Examples import example_data
+
+x, y, z, D = example_data()
+
+cm = ColourMapLPSlider(x, y, z, D, xlab='x val', ylab='y val', zlab='power val', dmlab='Function val')
+
+show(cm)
