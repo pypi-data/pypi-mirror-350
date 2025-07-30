341f0bf6b043102b4d9ebc6c41a50787485f9cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:15 +0000

    Add package ryry-cli/3.2/ryry_cli-3.2.tar.gz

---
+++ packages/ryry-cli/ryry_cli-3.2.tar.gz/ryry_cli-3.2/ryry/constant.py
@@ -0,0 +1,10 @@
+#!!!!! do not change this file !!!!!
+app_version="3.2"
+app_bulld_anchor="Noh_2025-05-27 17:13:14.294129"
+app_name="ryry-cli"
+import sys, os
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
