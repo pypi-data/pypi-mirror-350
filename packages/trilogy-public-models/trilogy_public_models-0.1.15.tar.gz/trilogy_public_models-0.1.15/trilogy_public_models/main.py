3e7c7f60b0e1d0408d1a982d4e60f979e34f6efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:32 +0000

    Add package trilogy-public-models/0.1.15/trilogy_public_models-0.1.15.tar.gz

---
+++ packages/trilogy-public-models/trilogy_public_models-0.1.15.tar.gz/trilogy_public_models-0.1.15/trilogy_public_models/main.py
@@ -0,0 +1,39 @@
+from trilogy import Executor, Dialects
+from trilogy_public_models.discovery import data_models
+from trilogy_public_models.models import LazyEnvironment, QueryType
+
+
+def get_executor(
+    model: str, executor: Executor | None = None, run_setup: bool = True
+) -> Executor:
+
+    if "bigquery" in model:
+        dialect = Dialects.BIGQUERY
+    elif "duckdb" in model:
+        dialect = Dialects.DUCK_DB
+    else:
+        raise NotImplementedError(f"Model {model} not supported")
+    loaded = data_models[model]
+    if executor is None:
+        executor = dialect.default_executor(environment=loaded.environment)
+    else:
+        executor.environment = loaded.environment
+    if isinstance(loaded.environment, LazyEnvironment):
+        loaded.environment._load()
+
+    if isinstance(loaded.setup, list):
+        queries = loaded.setup
+    else:
+        queries = loaded.setup()
+    if run_setup:
+        for x in queries:
+            if x.type == QueryType.SQL:
+                z = executor.execute_raw_sql(x.query)
+                z.fetchall()
+            elif x.type == QueryType.TRILOGY:
+                z = executor.execute_query(x.query)
+                z.fetchall()
+            else:
+                z = executor.execute_query(x)
+                z.fetchall()
+    return executor
