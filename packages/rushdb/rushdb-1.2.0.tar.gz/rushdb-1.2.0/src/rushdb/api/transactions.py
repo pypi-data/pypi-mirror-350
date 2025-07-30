ccc95bdcb6b0c05d2070d38142f3ba97db44bd23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:42 +0000

    Add package rushdb/1.2.0/rushdb-1.2.0.tar.gz

---
+++ packages/rushdb/rushdb-1.2.0.tar.gz/rushdb-1.2.0/src/rushdb/api/transactions.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from ..models.transaction import Transaction
+from .base import BaseAPI
+
+
+class TransactionsAPI(BaseAPI):
+    """API for managing transactions in RushDB."""
+
+    def begin(self, ttl: Optional[int] = None) -> Transaction:
+        """Begin a new transaction.
+
+        Returns:
+            Transaction object
+        """
+        response = self.client._make_request("POST", "/tx", {"ttl": ttl or 5000})
+        return Transaction(self.client, response.get("data")["id"])
+
+    def _commit(self, transaction_id: str) -> None:
+        """Internal method to commit a transaction."""
+        return self.client._make_request("POST", f"/tx/{transaction_id}/commit", {})
+
+    def _rollback(self, transaction_id: str) -> None:
+        """Internal method to rollback a transaction."""
+        return self.client._make_request("POST", f"/tx/{transaction_id}/rollback", {})
