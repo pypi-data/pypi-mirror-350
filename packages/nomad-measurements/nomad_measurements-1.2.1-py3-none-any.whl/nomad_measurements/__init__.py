79755f7d6742254ab5903e19244fb67f9cfcdcc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:45 +0000

    Add package nomad-measurements/1.2.1/nomad_measurements-1.2.1-py3-none-any.whl

---
+++ packages/nomad-measurements/nomad_measurements-1.2.1-py3-none-any.whl/nomad_measurements/__init__.py
@@ -0,0 +1,14 @@
+from nomad.config.models.plugins import SchemaPackageEntryPoint
+
+
+class GeneralSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from nomad_measurements.general import m_package
+
+        return m_package
+
+
+schema = GeneralSchemaPackageEntryPoint(
+    name='General Schema',
+    description='Schema package containing basic classes used around in the plugin.',
+)
