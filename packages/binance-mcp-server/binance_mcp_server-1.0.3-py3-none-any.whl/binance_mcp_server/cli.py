1e393ed1bc7bb85e39edbf1af90f82b1e770b54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:54 +0000

    Add package binance-mcp-server/1.0.3/binance_mcp_server-1.0.3-py3-none-any.whl

---
+++ packages/binance-mcp-server/binance_mcp_server-1.0.3-py3-none-any.whl/binance_mcp_server/cli.py
@@ -0,0 +1,24 @@
+import os
+import typer
+from dotenv import load_dotenv
+from binance_mcp_server import mcp
+
+
+app = typer.Typer(add_completion=True)
+
+
+@app.command()
+def binance_mcp_server(
+        api_key = typer.Option("", "--api-key", "-k", help="Binance API key", prompt=True, envvar="BINANCE_API_KEY"),
+        api_secret = typer.Option("", "--api-secret", "-s", help="Binance API secret", prompt=True, envvar="BINANCE_API_SECRET")
+    ):
+    load_dotenv()
+
+    os.environ["api_key"] = str(api_key)
+    os.environ["api_secret"] = api_secret
+
+    mcp.run(transport="stdio")
+
+
+if __name__ == "__main__":
+    app()
