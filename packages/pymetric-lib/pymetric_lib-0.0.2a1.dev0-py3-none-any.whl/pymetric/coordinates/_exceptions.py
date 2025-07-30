697995a693bdaf3059e8571277be678b07ac50eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:25 +0000

    Add package pymetric-lib/0.0.2a1.dev0/pymetric_lib-0.0.2a1.dev0-py3-none-any.whl

---
+++ packages/pymetric-lib/pymetric_lib-0.0.2a1.dev0-py3-none-any.whl/pymetric/coordinates/_exceptions.py
@@ -0,0 +1,12 @@
+"""
+Specialized exceptions for coordinate systems.
+"""
+
+
+class CoordinateClassException(Exception):
+    """
+    A specialized exception which is raised when a coordinate class fails to be
+    initialized properly.
+    """
+
+    pass
