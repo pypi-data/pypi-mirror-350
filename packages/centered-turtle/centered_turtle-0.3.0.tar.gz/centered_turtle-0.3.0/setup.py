548ac10e5c8553195e8551920b9095b876408f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:05:57 +0000

    Add package centered-turtle/0.3.0/centered_turtle-0.3.0.tar.gz

---
+++ packages/centered-turtle/centered_turtle-0.3.0.tar.gz/centered_turtle-0.3.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='centered-turtle',
+    version='0.3.0',
+    author='KilRoadWay',
+    author_email='kilroadway09@gmail.com',
+    description='A simple turtle-based drawing library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',  # GitHub 주소 등
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Education',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires='>=3.10',
+)
