ac7ad98d0ecb607dcee2cba597d438fc2185917f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:23 +0000

    Add package scalar_doc/0.1.4/scalar_doc-0.1.4-py3-none-any.whl

---
+++ packages/scalar_doc/scalar_doc-0.1.4-py3-none-any.whl/scalar_doc/__init__.py
@@ -0,0 +1,15 @@
+from .core import (
+    ScalarColorSchema,
+    ScalarConfiguration,
+    ScalarDoc,
+    ScalarHeader,
+    ScalarTheme,
+)
+
+__all__ = [
+    ScalarColorSchema,
+    ScalarConfiguration,
+    ScalarDoc,
+    ScalarHeader,
+    ScalarTheme,
+]
