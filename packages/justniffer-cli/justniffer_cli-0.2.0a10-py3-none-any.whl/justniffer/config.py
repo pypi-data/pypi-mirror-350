be4154bbfeda58c6f929fb7c6d66ea62cb8f5d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:38 +0000

    Add package justniffer-cli/0.2.0a10/justniffer_cli-0.2.0a10-py3-none-any.whl

---
+++ packages/justniffer-cli/justniffer_cli-0.2.0a10-py3-none-any.whl/justniffer/config.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from functools import cache
+import yaml
+from justniffer.logging import logger
+
+
+
+class Config(BaseModel):
+    selectors: dict[str, dict | None] | None = None
+    extractors: dict[str, dict | None] | None = None
+    formatter: str | dict | None = None
+
+
+@cache
+def load_config(filepath: str | None) -> Config:
+    logger.debug(f'load_config {filepath=}')
+    if filepath is None:
+        return Config()
+    else:
+        with open(filepath, 'r') as f:
+            obj = yaml.safe_load(f)
+        return Config.model_validate(obj)
