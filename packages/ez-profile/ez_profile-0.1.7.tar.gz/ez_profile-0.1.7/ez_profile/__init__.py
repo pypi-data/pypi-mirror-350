c3d84c034f34d5b92e7ee5c00c1d7d67ae947a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:00 +0000

    Add package ez-profile/0.1.7/ez_profile-0.1.7.tar.gz

---
+++ packages/ez-profile/ez_profile-0.1.7.tar.gz/ez_profile-0.1.7/ez_profile/__init__.py
@@ -0,0 +1,43 @@
+from sys import argv
+
+if "--ignore" not in argv:
+    from subprocess import Popen
+    from time import sleep
+    import sys
+    import signal
+
+    output_file_name = "stats.prof"
+    if "--fname" in argv:
+        try:
+            output_file_name = argv[argv.index("--fname") + 1]
+        except IndexError:
+            raise RuntimeError("Unspecified filename")
+
+    gui_option = "snakeviz"
+    if "--gui" in argv:
+        try:
+            gui_option = argv[argv.index("--gui") + 1]
+        except IndexError:
+            raise RuntimeError("Unspecified GUI option")
+
+    profile_proc = Popen(
+        [
+            f"{sys.executable}",
+            "-m",
+            "cProfile",
+            "-o",
+            output_file_name,
+            f"{argv[0]}",
+            "--ignore",
+        ]
+    )
+    try:
+        profile_proc.wait()
+    except KeyboardInterrupt:
+        pass
+
+    sv_proc = Popen([sys.executable, "-m", gui_option, output_file_name], shell=True)
+    sleep(10)
+    sv_proc.send_signal(signal.CTRL_C_EVENT)
+
+    sys.exit()
---
+++ packages/ez-profile/ez_profile-0.1.7.tar.gz/ez_profile-0.1.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+# try:
+#     import pypandoc
+#     long_description = pypandoc.convert_file("README.md", "rst")
+# except (IOError, ImportError):
+long_description = open("README.md").read()
+
+__ver = "0.1.7"
+
+setup(
+    name="ez_profile",
+    packages=find_packages(include=["ez_profile"]),
+    version=__ver,
+    description="A basic wrapper around cProfile with optional snakeviz integration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    data_files=[('', ['README.md'])],
+    author="oddbookworm",
+    author_email="andrewryancoffey@hotmail.com",
+    license="MIT",
+    install_requires=['snakeviz', 'tuna'],
+    url="https://github.com/oddbookworm/ez_profile",
+    download_url="https://github.com/oddbookworm/ez_profile/archive/refs/tags/v{__ver}.tar.gz",
+    keywords=["ez_profile"],
+    classifiers=[],
+)
+
+# release procedure:
+# create github release
+# python setup.py sdist
+# twine upload dist/*
