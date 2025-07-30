b3a76a2e20a2e4a634035b04c2699999fad74834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:23 +0000

    Add package loopypy/0.0.1/loopypy-0.0.1.tar.gz

---
+++ packages/loopypy/loopypy-0.0.1.tar.gz/loopypy-0.0.1/README.md
@@ -0,0 +1,2 @@
+# loopy-py
+The Python wrapper for api.loopy5418.dev
---
+++ packages/loopypy/loopypy-0.0.1.tar.gz/loopypy-0.0.1/src/loopypy_loopy5418/__init__.py
@@ -0,0 +1,2 @@
+from .app import checkStatus, setApiKey, getApiKey, ai, airesp
+__all__ = ["checkStatus", "setApiKey", "getApiKey", "ai", "airesp"]
