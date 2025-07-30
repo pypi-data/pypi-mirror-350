96fa0d1e657f73befe02cd37dd032f450bbfe03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:40 +0000

    Add package escudeiro/0.2.3/escudeiro-0.2.3-cp313-cp313-musllinux_1_1_x86_64.whl

---
+++ packages/escudeiro/escudeiro-0.2.3-cp313-cp313-musllinux_1_1_x86_64.whl/escudeiro/data/__init__.py
@@ -0,0 +1,30 @@
+from .field_ import field, private
+from .helpers import (
+    call_init,
+    get_fields,
+    init_hooks,
+    resolve_typevars,
+    squire_method,
+    update_refs,
+)
+from .main import data
+from .methods import MethodBuilder
+from .slots import slot
+from .utils.factory import factory
+from .utils.typedef import UNINITIALIZED
+
+__all__ = [
+    "MethodBuilder",
+    "factory",
+    "UNINITIALIZED",
+    "data",
+    "field",
+    "private",
+    "slot",
+    "call_init",
+    "squire_method",
+    "get_fields",
+    "init_hooks",
+    "update_refs",
+    "resolve_typevars",
+]
