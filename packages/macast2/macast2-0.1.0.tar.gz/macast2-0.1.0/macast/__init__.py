33fda79c320fc7b9aafd29772984b145bd631961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:09 +0000

    Add package macast2/0.1.0/macast2-0.1.0.tar.gz

---
+++ packages/macast2/macast2-0.1.0.tar.gz/macast2-0.1.0/macast/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021 by xfangfang. All Rights Reserved.
+
+
+from .macast import Service, Macast, gui, cli
+from .utils import SETTING_DIR, Setting, SettingProperty
+from .renderer import Renderer, RendererSetting
+from .plugin import RendererPlugin
+from .gui import App, MenuItem, Platform
