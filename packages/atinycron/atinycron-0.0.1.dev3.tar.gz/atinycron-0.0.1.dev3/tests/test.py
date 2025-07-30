723235ea6db3a8f15759c7d6a9b34c9570e131c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:45 +0000

    Add package atinycron/0.0.1.dev3/atinycron-0.0.1.dev3.tar.gz

---
+++ packages/atinycron/atinycron-0.0.1.dev3.tar.gz/atinycron-0.0.1.dev3/tests/test.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+import asyncio
+import logging
+
+from atinycron import AbstractTask
+
+logging.basicConfig(level=logging.INFO)
+
+
+class TestTask(AbstractTask):
+    async def setup(self):
+        print('Setup')
+
+    async def teardown(self):
+        print('Teardown')
+
+    async def run(self):
+        print('Running task')
+        await asyncio.sleep(2)
+        print('Task finished')
+
+
+if __name__ == '__main__':
+    test_task = TestTask(name='test_task', allow_concurrent=False)
+    test_task.cron_config_set()
+    asyncio.run(test_task.schedule_foreground())
