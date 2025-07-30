1ade5b01cdfa8c51b9eddf2cb06e3334f601c9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:37 +0000

    Add package draive/0.65.2/draive-0.65.2-py3-none-any.whl

---
+++ packages/draive/draive-0.65.2-py3-none-any.whl/draive/stages/__init__.py
@@ -0,0 +1,21 @@
+from draive.stages.stage import Stage
+from draive.stages.types import (
+    StageCondition,
+    StageContextTransforming,
+    StageException,
+    StageExecution,
+    StageMerging,
+    StageResultTransforming,
+    StageStateAccessing,
+)
+
+__all__ = (
+    "Stage",
+    "StageCondition",
+    "StageContextTransforming",
+    "StageException",
+    "StageExecution",
+    "StageMerging",
+    "StageResultTransforming",
+    "StageStateAccessing",
+)
