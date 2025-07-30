e10f801ccd19b370d46e3c61babcd4a2d6c6a0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:49 +0000

    Add package migipo/0.0.1/migipo-0.0.1-py3-none-any.whl

---
+++ packages/migipo/migipo-0.0.1-py3-none-any.whl/migipo/core/_load.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+
+class Load:
+
+    def __init__(self, verbose: bool = False) -> None:
+        self.verbose = verbose
+
+    # def load_from_file(path: str) -> int:
+    #     pass
