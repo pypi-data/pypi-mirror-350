7a9aa6a1525040867c541fa6701656f332a074c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:08 +0000

    Add package freeride/0.1.0/freeride-0.1.0.tar.gz

---
+++ packages/freeride/freeride-0.1.0.tar.gz/freeride-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+def get_version():
+    with open("freeride/__init__.py", "r") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return eval(line.split("=")[-1].strip())
+
+setuptools.setup(
+    name='freeride',
+    version=get_version(),
+	description='A package for undergraduate microeconomics.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/alexanderthclark/FreeRide',
+    author='Alexander Clark',
+    install_requires=['matplotlib', 'numpy', 'papermill', 'IPython', 'bokeh'],
+    author_email='',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+    zip_safe=False,
+)
