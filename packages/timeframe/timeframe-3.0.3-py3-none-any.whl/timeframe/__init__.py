761934ab3f5c983ec1408a9e0f5af6f7ad0ddc2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:21 +0000

    Add package timeframe/3.0.3/timeframe-3.0.3-py3-none-any.whl

---
+++ packages/timeframe/timeframe-3.0.3-py3-none-any.whl/timeframe/__init__.py
@@ -0,0 +1,10 @@
+from .timeframe import BatchTimeFrame, TimeFrame
+
+__all__ = (
+    "BatchTimeFrame",
+    "TimeFrame",
+)
+
+__name__ = "timeframe"
+__author__ = "Meysam Azad"
+__author_email__ = "MeysamAzad81@gmail.com"
