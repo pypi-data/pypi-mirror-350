8c277e3ae0637b37019ae501fd6ba6ff60a99b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:51 +0000

    Add package Lavender-CcGAN/0.0.2/lavender_ccgan-0.0.2.tar.gz

---
+++ packages/Lavender-CcGAN/lavender_ccgan-0.0.2.tar.gz/lavender_ccgan-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Lavender-CcGAN",
+    version="0.0.2",
+    description="A pip package of model CcGAN.",
+    long_description=open(
+        "README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Zeyu Xie",
+    author_email="xie.zeyu20@gmail.com",
+    url="https://github.com/Zeyu-Xie/Lavender-CcGAN",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
