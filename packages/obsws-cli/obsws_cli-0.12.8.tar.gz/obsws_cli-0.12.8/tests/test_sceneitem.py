51fdd2afaef17bb0e0c1029e28a161b69981f05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:10 +0000

    Add package obsws-cli/0.12.8/obsws_cli-0.12.8.tar.gz

---
+++ packages/obsws-cli/obsws_cli-0.12.8.tar.gz/obsws_cli-0.12.8/tests/test_sceneitem.py
@@ -0,0 +1,33 @@
+"""Unit tests for the item command in the OBS WebSocket CLI."""
+
+from typer.testing import CliRunner
+
+from obsws_cli.app import app
+
+runner = CliRunner()
+
+
+def test_sceneitem_list():
+    """Test the sceneitem list command."""
+    result = runner.invoke(app, ['sceneitem', 'list', 'pytest'])
+    assert result.exit_code == 0
+    assert 'pytest_input' in result.stdout
+    assert 'pytest_input_2' in result.stdout
+
+
+def test_sceneitem_transform():
+    """Test the sceneitem transform command."""
+    result = runner.invoke(
+        app,
+        [
+            'sceneitem',
+            'transform',
+            '--rotation=60',
+            'pytest',
+            'pytest_input_2',
+        ],
+    )
+    assert result.exit_code == 0
+    assert (
+        "Item 'pytest_input_2' in scene 'pytest' has been transformed" in result.stdout
+    )
