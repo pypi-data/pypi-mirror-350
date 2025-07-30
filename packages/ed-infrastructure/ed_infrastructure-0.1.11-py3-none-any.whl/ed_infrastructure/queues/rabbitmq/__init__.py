08f95b6306dff3d9a768b94d36645e6219a8c088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:13 +0000

    Add package ed-infrastructure/0.1.11/ed_infrastructure-0.1.11-py3-none-any.whl

---
+++ packages/ed-infrastructure/ed_infrastructure-0.1.11-py3-none-any.whl/ed_infrastructure/queues/rabbitmq/__init__.py
@@ -0,0 +1,7 @@
+from ed_infrastructure.queues.rabbitmq.producer import RabbitMQProducer
+from ed_infrastructure.queues.rabbitmq.subscriber import RabbitMQSubscriber
+
+__all__ = [
+    "RabbitMQProducer",
+    "RabbitMQSubscriber",
+]
