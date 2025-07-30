c4e2c4b3fea6a02905273be3af858dfb4020d03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:10 +0000

    Add package pulp-ansible/0.26.0/pulp_ansible-0.26.0.tar.gz

---
+++ packages/pulp-ansible/pulp_ansible-0.26.0.tar.gz/pulp_ansible-0.26.0/pulp_ansible/app/__init__.py
@@ -0,0 +1,12 @@
+from pulpcore.plugin import PulpPluginAppConfig
+
+
+class PulpAnsiblePluginAppConfig(PulpPluginAppConfig):
+    """
+    Entry point for pulp_ansible plugin.
+    """
+
+    name = "pulp_ansible.app"
+    label = "ansible"
+    version = "0.26.0"
+    python_package_name = "pulp-ansible"
