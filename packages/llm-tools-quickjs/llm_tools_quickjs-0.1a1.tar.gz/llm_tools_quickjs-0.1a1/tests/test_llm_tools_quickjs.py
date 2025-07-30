57d8259696b84e18340e8f3c80d8fa654e1dcbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:19 +0000

    Add package llm-tools-quickjs/0.1a1/llm_tools_quickjs-0.1a1.tar.gz

---
+++ packages/llm-tools-quickjs/llm_tools_quickjs-0.1a1.tar.gz/llm_tools_quickjs-0.1a1/tests/test_llm_tools_quickjs.py
@@ -0,0 +1,35 @@
+import llm
+import json
+from llm_tools_quickjs import QuickJS
+
+
+def test_tool():
+    model = llm.get_model("echo")
+    code = """
+    function execute() {
+        return "hello: " + (45 * 16);
+    }
+    execute()
+    """
+    chain_response = model.chain(
+        json.dumps(
+            {
+                "tool_calls": [
+                    {
+                        "name": "QuickJS_execute_javascript",
+                        "arguments": {"javascript": code},
+                    }
+                ]
+            }
+        ),
+        tools=[QuickJS()],
+    )
+    responses = list(chain_response.responses())
+    tool_results = json.loads(responses[-1].text())["tool_results"]
+    assert tool_results == [
+        {
+            "name": "QuickJS_execute_javascript",
+            "output": "hello: 720",
+            "tool_call_id": None,
+        }
+    ]
