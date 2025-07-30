282ac7fbab84cb65abfa9fc0f9c5e87d9df09632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:56 +0000

    Add package gumicorn/23.0.0/gumicorn-23.0.0-py3-none-any.whl

---
+++ packages/gumicorn/gumicorn-23.0.0-py3-none-any.whl/gumicorn/http/__init__.py
@@ -0,0 +1,8 @@
+#
+# This file is part of gumicorn released under the MIT license.
+# See the NOTICE for more information.
+
+from gumicorn.http.message import Message, Request
+from gumicorn.http.parser import RequestParser
+
+__all__ = ['Message', 'Request', 'RequestParser']
