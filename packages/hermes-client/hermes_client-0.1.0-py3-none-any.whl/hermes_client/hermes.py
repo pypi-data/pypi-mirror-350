fca0e312537cc701af3d090bc70fb79c02eb31e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:36:39 +0000

    Add package hermes-client/0.1.0/hermes_client-0.1.0-py3-none-any.whl

---
+++ packages/hermes-client/hermes_client-0.1.0-py3-none-any.whl/hermes_client/hermes.py
@@ -0,0 +1,156 @@
+import logging
+from uuid import UUID
+
+from hermes_client.base import BaseClient, NotFound
+
+
+class HermesClient(BaseClient):
+    def __init__(self,
+                 url: str,
+                 timeout: int | None = None):
+        """
+        Initialize Class.
+
+        Args:
+            url:        URL of the hermes webservice
+            timeout:    after how long, contacting the webservice should
+                        be aborted
+        """
+        self.url = url
+        self._timeout = timeout
+        self.logger = logging.getLogger(__name__)
+
+    def list_projects(self):
+        """
+        List all projects.
+
+        Returns:
+            list: list of projects
+        """
+        request_url = f'{self.url}/v1/projects'
+        data = self._get(request_url)
+
+        return data
+
+    def get_project_by_name(self, project_name: str):
+        """
+        Get a project by name.
+
+        Args:
+            project_name: name of the project.
+        Returns:
+            The project.
+        """
+        request_url = f'{self.url}/v1/projects'
+        data = self._get(request_url)
+        project = next((p for p in data if p['name'] == project_name),
+                       None)
+        if project is None:
+            raise NotFound(
+                f'Project with name "{project_name}" not found. '
+                'Please provide a valid project name.')
+        return project
+
+    def get_project(self, project_oid: UUID | str):
+        """
+        Get a project by oid.
+
+        Args:
+            project_oid: oid of the project.
+        Returns:
+            The project.
+        """
+        request_url = f'{self.url}/v1/projects/{str(project_oid)}'
+
+        try:
+            project = self._get(request_url)
+        except NotFound:
+            raise NotFound(
+                f'Project with oid "{project_oid}" not found. '
+                'Please provide a valid UUID.')
+
+        return project
+
+    def list_forecastseries(self, project: UUID | str):
+        """
+        List all ForecastSeries for a project.
+
+        Args:
+            project: oid or name of the project.
+
+        Returns:
+            All ForecastSeries for the project.
+        """
+
+        try:
+            if not isinstance(project, UUID):
+                UUID(project)
+        except ValueError:
+            project = self.get_project_by_name(project)['oid']
+
+        request_url = f'{self.url}/v1/projects/{str(project)}/forecastseries'
+
+        try:
+            data = self._get(request_url)
+        except NotFound:
+            raise NotFound(
+                f'Project with oid "{project}" not found. '
+                'Please provide a valid UUID.')
+
+        return data
+
+    def list_modelconfigs(self):
+        """
+        List all model configurations.
+
+        Returns:
+            list: list of model configurations
+        """
+        request_url = f'{self.url}/v1/modelconfigs'
+        data = self._get(request_url)
+
+        return data
+
+    def get_forecastseries_by_name(self,
+                                   project_name: str,
+                                   forecastseries_name: str):
+        """
+        Get a ForecastSeries by name.
+
+        Args:
+            project_name: name of the project.
+            forecastseries_name: name of the forecast series.
+        Returns:
+            The ForecastSeries.
+        """
+        project = self.get_project_by_name(project_name)['oid']
+
+        request_url = f'{self.url}/v1/projects/{project}/forecastseries'
+        data = self._get(request_url)
+        fs = next(
+            (fs for fs in data if fs['name'] == forecastseries_name),
+            None)
+        if fs is None:
+            raise NotFound(
+                f'ForecastSeries with name "{forecastseries_name}" not found. '
+                'Please provide a valid ForecastSeries name.')
+        return fs
+
+    def get_forecastseries(self,
+                           forecastseries_oid: UUID | str):
+        """
+        Get a ForecastSeries by oid.
+
+        Args:
+            forecastseries_oid: oid of the forecast series.
+        Returns:
+            The ForecastSeries.
+        """
+        request_url = f'{self.url}/v1/forecastseries/{str(forecastseries_oid)}'
+        try:
+            fs = self._get(request_url)
+        except NotFound:
+            raise NotFound(
+                f'ForecastSeries with oid "{forecastseries_oid}" not found. '
+                'Please provide a valid UUID.')
+        return fs
---
+++ packages/hermes-client/hermes_client-0.1.0-py3-none-any.whl/hermes_client/modelrun.py
@@ -0,0 +1,92 @@
+import logging
+
+from hydws.parser import BoreholeHydraulics
+
+from hermes_client.base import BaseClient
+from hermes_client.schemas import ModelRunInfo
+
+
+class ModelRunClient(BaseClient):
+    def __init__(self,
+                 url: str,
+                 modelrun: dict,
+                 forecast_client: object | None = None,
+                 timeout: int | None = None):
+        self._metadata = modelrun
+        self.url = url
+        self._timeout = timeout
+        self.logger = logging.getLogger(__name__)
+
+        # if the reference to the forecast_client exists, we can
+        # use it to get the injectionplan and modelconfig
+        # instead of fetching them again from the API
+        self._forecast_client = forecast_client
+        self._injectionplan = None
+        self._modelconfig = None
+
+    def __repr__(self):
+        return \
+            f"ModelRun({self.metadata.status}, " \
+            f"modelconfig={self.metadata.modelconfig}, " \
+            f"injectionplan={self.metadata.injectionplan})"
+
+    @property
+    def metadata(self) -> dict:
+        """
+        Metadata of the model run.
+        """
+        return ModelRunInfo.model_validate(self._metadata)
+
+    @property
+    def injectionplan(self) -> BoreholeHydraulics | None:
+        """
+        InjectionPlan of the ModelRun.
+        """
+        if self._forecast_client is not None:
+            return self._forecast_client.injectionplans[
+                self.metadata.injectionplan]
+
+        if self._injectionplan is None:
+            print('fetching')
+            ip = self._get_injectionplan()
+            self._injectionplan = BoreholeHydraulics(ip) if ip else None
+
+        return self._injectionplan
+
+    def _get_injectionplan(self) -> dict:
+        """
+        Get all injection plans for the forecast.
+        """
+        request_url = f'{self.url}/v1/modelruns/' \
+            f'{self._metadata["oid"]}/injectionplan'
+
+        data = self._get(request_url)
+
+        return data
+
+    @property
+    def modelconfig(self) -> dict | None:
+        """
+        ModelConfig of the ModelRun.
+        """
+        if self._forecast_client is not None:
+            return self._forecast_client.modelconfigs[
+                self.metadata.modelconfig]
+
+        if self._modelconfig is None:
+            mc = self._get_modelconfig()
+            self._modelconfig = mc if mc else None
+
+        return self._modelconfig.copy()
+
+    @property
+    def _get_modelconfig(self) -> dict:
+        """
+        Get all model configs for the forecast.
+        """
+        request_url = f'{self.url}/v1/modelruns/' \
+            f'{self._metadata["oid"]}/modelconfig'
+
+        data = self._get(request_url)
+
+        return data
