38b30755a69db7e02ce023442d798cc73d1270eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:19 +0000

    Add package ctextlib/1.0.25/ctextlib-1.0.25.tar.gz

---
+++ packages/ctextlib/ctextlib-1.0.25.tar.gz/ctextlib-1.0.25/CMakeFiles/ctextlib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ctextlib.dir/src/module.cpp.o"
+  "CMakeFiles/ctextlib.dir/src/module.cpp.o.d"
+  "ctextlib.cpython-37m-x86_64-linux-gnu.so"
+  "ctextlib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ctextlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
