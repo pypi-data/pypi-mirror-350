3d57d2b2e9e693b94e6ec1027288f2edefd5a744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:30 +0000

    Add package mcp-server-pubtator3/0.1.9/mcp_server_pubtator3-0.1.9.tar.gz

---
+++ packages/mcp-server-pubtator3/mcp_server_pubtator3-0.1.9.tar.gz/mcp_server_pubtator3-0.1.9/src/mcp_server_pubtator3/__main__.py
@@ -0,0 +1,10 @@
+import sys
+import asyncio
+
+# from server import main as async_main
+from .server import main as async_main
+def main():
+    sys.exit(asyncio.run(async_main()))
+
+if __name__ == '__main__':
+    main()
