6dea52bdb693a2b6b2afe56cedb95e07cf0708be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:00 +0000

    Add package hurnet/1.0.8/hurnet-1.0.8-py3-none-any.whl

---
+++ packages/hurnet/hurnet-1.0.8-py3-none-any.whl/hurnet/__init__.py
@@ -0,0 +1,29 @@
+"""
+    ############################################################################################################################################################################################################################
+    # This algorithm represents a revolutionary new architecture of artificial neural networks created by Sapiens Technology®, which is significantly faster and more accurate than conventional neural network architectures. #
+    # The HurNet network, which uses back-division calculations where the computation starts from the output layer and goes back to the previous layers up to the input layer,                                                 #
+    # dividing each current neuron by the immediately previous one in a single iteration,                                                                                                                                      #
+    # manages to be significantly faster than traditional neural networks that use several iterations of weight adjustment through backpropagation.                                                                            #
+    # The neural network architecture in question has been named HurNet (Hur, from Ben-Hur Varriano, who designed the mathematical architecture and the network algorithm; and Net, from neural network).                      #
+    # The HurNet neural network does not rely on backpropagation or gradient calculations, achieving optimal weight adjustments in a single iteration.                                                                         #
+    # This approach dramatically reduces demands for computational processing and can potentially increase training speed.                                                                                                     #
+    # This algorithm is protected by copyright, and any public commentary, disclosure, or distribution of the HurNet algorithm code without prior authorization from Sapiens Technology® is strictly prohibited.               #
+    # Reverse engineering and the creation of derivative algorithms through adaptations or inspirations based on the original calculations and code are also not allowed.                                                      #
+    # Any violation of these prohibitions will be subject to legal action by our legal department.                                                                                                                             #
+    ############################################################################################################################################################################################################################
+"""
+from .hurnet import *
+"""
+    ############################################################################################################################################################################################################################
+    # This algorithm represents a revolutionary new architecture of artificial neural networks created by Sapiens Technology®, which is significantly faster and more accurate than conventional neural network architectures. #
+    # The HurNet network, which uses back-division calculations where the computation starts from the output layer and goes back to the previous layers up to the input layer,                                                 #
+    # dividing each current neuron by the immediately previous one in a single iteration,                                                                                                                                      #
+    # manages to be significantly faster than traditional neural networks that use several iterations of weight adjustment through backpropagation.                                                                            #
+    # The neural network architecture in question has been named HurNet (Hur, from Ben-Hur Varriano, who designed the mathematical architecture and the network algorithm; and Net, from neural network).                      #
+    # The HurNet neural network does not rely on backpropagation or gradient calculations, achieving optimal weight adjustments in a single iteration.                                                                         #
+    # This approach dramatically reduces demands for computational processing and can potentially increase training speed.                                                                                                     #
+    # This algorithm is protected by copyright, and any public commentary, disclosure, or distribution of the HurNet algorithm code without prior authorization from Sapiens Technology® is strictly prohibited.               #
+    # Reverse engineering and the creation of derivative algorithms through adaptations or inspirations based on the original calculations and code are also not allowed.                                                      #
+    # Any violation of these prohibitions will be subject to legal action by our legal department.                                                                                                                             #
+    ############################################################################################################################################################################################################################
+"""
