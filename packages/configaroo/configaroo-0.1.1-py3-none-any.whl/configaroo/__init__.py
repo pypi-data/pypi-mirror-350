8a0dd14da9d4d51b9e97b637feb1fe99e01a9e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:37 +0000

    Add package configaroo/0.1.1/configaroo-0.1.1-py3-none-any.whl

---
+++ packages/configaroo/configaroo-0.1.1-py3-none-any.whl/configaroo/__init__.py
@@ -0,0 +1,10 @@
+"""Bouncy configuration handling"""
+
+from configaroo.configuration import Configuration  # noqa
+from configaroo.exceptions import (  # noqa
+    ConfigarooException,
+    MissingEnvironmentVariableError,
+    UnsupportedLoaderError,
+)
+
+__version__ = "0.1.1"
