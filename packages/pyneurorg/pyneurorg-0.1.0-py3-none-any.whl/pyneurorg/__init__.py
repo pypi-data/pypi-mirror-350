222d9fcbc7f4cdde9385c669968dc4e50213ae48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:58 +0000

    Add package pyneurorg/0.1.0/pyneurorg-0.1.0-py3-none-any.whl

---
+++ packages/pyneurorg/pyneurorg-0.1.0-py3-none-any.whl/pyneurorg/__init__.py
@@ -0,0 +1,8 @@
+# src/pyneurorg/__init__.py
+__version__ = "0.1.0"
+
+# Opcional: importe classes/funções chave para o nível superior do pacote
+from .organoid.organoid import Organoid
+from .simulation.simulator import Simulator
+from .mea.mea import MEA
+# ... etc.
