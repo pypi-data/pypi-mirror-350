b5eb2bf0c4c5f08334cdbf516d1b860e8bfd74c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:15 +0000

    Add package mondip-bhabhor/1.0.0/mondip_bhabhor-1.0.0.tar.gz

---
+++ packages/mondip-bhabhor/mondip_bhabhor-1.0.0.tar.gz/mondip_bhabhor-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mondip_bhabhor',
+    version='1.0.0',  # Replace with your package's version
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
