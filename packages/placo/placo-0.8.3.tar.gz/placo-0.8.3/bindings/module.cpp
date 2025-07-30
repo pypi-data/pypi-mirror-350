b3d53020adc8d79f0169048fe9d5cad7429d191b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:07 +0000

    Add package placo/0.8.3/placo-0.8.3.tar.gz

---
+++ packages/placo/placo-0.8.3.tar.gz/placo-0.8.3/bindings/module.cpp
@@ -0,0 +1,23 @@
+#include <pinocchio/fwd.hpp>
+
+#include <Eigen/Dense>
+#include <boost/python.hpp>
+#include <ostream>
+#include <pinocchio/bindings/python/spatial/se3.hpp>
+
+#include "module.h"
+
+BOOST_PYTHON_MODULE(placo)
+{
+  using namespace boost::python;
+
+  exposeEigen();
+  exposeTools();
+  exposeDynamics();
+  exposeFootsteps();
+  exposeProblem();
+  exposeRobotWrapper();
+  exposeParameters();
+  exposeKinematics();
+  exposeWalkPatternGenerator();
+}
