26bf5d80bee7919bd6285838b3c5b3199a2a0ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:08 +0000

    Add package seatools-starter-sqlalchemy/1.0.9/seatools_starter_sqlalchemy-1.0.9.tar.gz

---
+++ packages/seatools-starter-sqlalchemy/seatools_starter_sqlalchemy-1.0.9.tar.gz/seatools_starter_sqlalchemy-1.0.9/README.md
@@ -0,0 +1,66 @@
+# Seatools-starter-sqlalchemy
+
+The SQLAlchemy starter for seatools ioc.
+
+[中文文档](./README_zh.md)
+
+## Repository Addresses:
+1. https://github.com/seatools-py/seatools-starter-sqlalchemy
+2. https://gitee.com/seatools-py/seatools-starter-sqlalchemy
+
+## Usage Guide
+1. Install with `poetry add seatools-starter-sqlalchemy`
+2. Configure the `config/application.yml` file as follows:
+```yaml
+# seatools configuration
+seatools:
+  # sqlalchemy configuration
+  sqlalchemy:
+    echo: false
+    pool_cycle: 3600
+  # database configuration
+  datasource:
+    # database bean name
+    demo_db:
+      host: xxx
+      port: 1234
+      user: xxx
+      password: sss
+      database: xxx
+      driver: sqlite # sqlalchemy schema database driver, examples: sqlite+aiosqlite (asynchronous), mysql+pymysql, hive, clickhouse, etc.
+      primary: false # whether it is the default bean
+      is_async: false # whether it is asynchronous, if the driver is asynchronous then this value needs to be set to true
+      # override configuration for individual databases' sqlalchemy
+      config:
+        echo: true
+        # 1.0.6 version supports custom Session, default synchronous is sqlalchemy.orm.Session, asynchronous is sqlalchemy.ext.asyncio.AsyncSession
+        session_cls: sqlalchemy.orm.Session # can be changed to other Sessions, for example: sqlmodel.orm.Session (sqlmodel needs to be installed)
+```
+3. Usage example:
+```python
+from seatools.ioc import run, Autowired
+from seatools.sqlalchemy.decorators.ioc import new_session
+from sqlalchemy.orm import Session
+from seatools.sqlalchemy import SqlAlchemyClient
+
+
+# Start ioc
+run(scan_package_names=['seatools.ioc.starters.sqlalchemy'], config='./config')
+
+"""
+Method one, using decorator, recommended
+"""
+@new_session
+def xxx(session: Session):
+    ...
+
+# Call
+xxx()
+
+
+# Method two, not recommended
+client = Autowired(cls=SqlAlchemyClient)
+
+with client.session() as session:
+    ...
+```
---
+++ packages/seatools-starter-sqlalchemy/seatools_starter_sqlalchemy-1.0.9.tar.gz/seatools_starter_sqlalchemy-1.0.9/seatools/ioc/starters/sqlalchemy/sqlalchemy.py
@@ -0,0 +1,51 @@
+from typing import Optional
+from seatools.ioc.config import cfg
+from seatools.ioc.base import get_bean_factory
+from seatools.ioc.injects import Bean
+from seatools.sqlalchemy.utils import new_client
+from seatools.utils.dict_utils import deep_update
+from loguru import logger
+from seatools.sqlalchemy.dbconfig import CommonDBConfig
+
+
+class SqlalchemyDBConfig(CommonDBConfig):
+    config: Optional[dict] = None
+
+
+@Bean
+def init_db_beans():
+    db_config, sqlalchemy_config = None, None
+    config = cfg()
+    if 'seatools' in config and 'datasource' in config['seatools']:
+        db_config = config['seatools']['datasource']
+    # Backward compatibility with old configuration
+    elif 'db' in config:
+        db_config = config['db']
+
+    # Backward compatibility with old sqlalchemy configuration
+    sqlalchemy_config = (config.get('seatools') or {}).get('sqlalchemy') or config.get('sqlalchemy')
+
+    if not db_config:
+        logger.warning(
+            'Configuration [seatools.datasource] does not exist, unable to automatically initialize database bean instances.')
+        return
+    if not isinstance(db_config, dict):
+        logger.error(
+            'Configuration [seatools.datasource] property is not of dict type, unable to automatically initialize database bean instances.')
+        exit(1)
+    bean_factory = get_bean_factory()
+    for name, v in db_config.items():
+        config = SqlalchemyDBConfig(**v)
+        if config.config:
+            if not sqlalchemy_config:
+                sqlalchemy_config = config.config
+            else:
+                sqlalchemy_config = deep_update(sqlalchemy_config, config.config)
+        elif config.sqlalchemy:
+            if not sqlalchemy_config:
+                sqlalchemy_config = config.sqlalchemy
+            else:
+                sqlalchemy_config = deep_update(sqlalchemy_config, config.sqlalchemy)
+        client = new_client(config, config=sqlalchemy_config)
+        # Register bean, not a lazy registration
+        bean_factory.register_bean(name=config.name or name, cls=client, primary=config.primary, lazy=False)
