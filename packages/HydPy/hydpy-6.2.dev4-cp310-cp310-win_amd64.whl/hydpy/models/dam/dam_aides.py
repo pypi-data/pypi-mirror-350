9261679e4048dffce1a9591cca927fde1f37afdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:34 +0000

    Add package HydPy/6.2.dev4/hydpy-6.2.dev4-cp310-cp310-win_amd64.whl

---
+++ packages/HydPy/hydpy-6.2.dev4-cp310-cp310-win_amd64.whl/hydpy/models/dam/dam_aides.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-module-docstring
+
+# import...
+# ...from HydPy
+from hydpy.core import sequencetools
+
+
+class SurfaceArea(sequencetools.AideSequence):
+    """Surface area [km²]."""
+
+    NDIM, NUMERIC, SPAN = 0, True, (None, None)
+
+
+class AllowedDischarge(sequencetools.AideSequence):
+    """Discharge threshold not to be overcut by the actual discharge [m³/s]."""
+
+    NDIM, NUMERIC, SPAN = 0, True, (None, None)
