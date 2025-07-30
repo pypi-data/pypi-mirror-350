3db6ba30c9cd3b1ed76a8d656654bec18cfb9fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:11 +0000

    Add package driveup/0.9.2/driveup-0.9.2.tar.gz

---
+++ packages/driveup/driveup-0.9.2.tar.gz/driveup-0.9.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# ...
+
+with open("README.md", "r") as fh:
+    README_DESCRIPTION = fh.read()
+
+# ...
+
+setup(
+    long_description=README_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    name='driveup',
+    version='0.9.2',
+    author='Raúl M.R.',
+    author_email="raul.martin4bc@gmail.com",
+    url="https://github.com/raul-martin-dev/Driveup",
+    license="MIT",
+    description='Python package for uploading files and folders to Google Drive.',
+    packages=find_packages(include=["Driveup","Driveup.features"]),
+    install_requires=[
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+    ],
+)
