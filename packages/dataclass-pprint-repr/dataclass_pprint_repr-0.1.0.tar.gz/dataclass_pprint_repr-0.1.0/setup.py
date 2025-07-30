cd2f48cba652a102c07bdfeefdb04bb634371d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:58 +0000

    Add package dataclass-pprint-repr/0.1.0/dataclass_pprint_repr-0.1.0.tar.gz

---
+++ packages/dataclass-pprint-repr/dataclass_pprint_repr-0.1.0.tar.gz/dataclass_pprint_repr-0.1.0/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dataclass_pprint_repr",
+    version="0.1.0",
+    author="Your Name",
+    description="A decorator to enhance dataclass __repr__ formatting using pprint.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # or whichever license you choose
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
