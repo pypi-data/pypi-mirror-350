b5f3dd22174a4a7ae1bda5f50079e7b936322e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:10:07 +0000

    Add package hyppo/0.5.2/hyppo-0.5.2.tar.gz

---
+++ packages/hyppo/hyppo-0.5.2.tar.gz/hyppo-0.5.2/hyppo/__init__.py
@@ -0,0 +1,12 @@
+import hyppo.discrim
+import hyppo.independence
+import hyppo.ksample
+import hyppo.time_series
+import hyppo.kgof
+import hyppo.tools
+import hyppo.d_variate
+import hyppo.conditional
+import hyppo.causal
+
+
+__version__ = "0.5.2"
