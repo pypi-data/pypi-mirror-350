2e1d16c038f15a2f4fdb90be8f4bea79d80ff1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:41 +0000

    Add package odoo-addon-marginalized-groups-somconnexio/16.0.1.0.0.5/odoo-addon-marginalized_groups_somconnexio-16.0.1.0.0.5.tar.gz

---
+++ packages/odoo-addon-marginalized-groups-somconnexio/odoo-addon-marginalized_groups_somconnexio-16.0.1.0.0.5.tar.gz/odoo-addon-marginalized_groups_somconnexio-16.0.1.0.0.5/odoo/addons/marginalized_groups_somconnexio/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Odoo Som Connexió Marginalized Groups customizations",
+    "version": "16.0.1.0.0",
+    "depends": [
+        "cooperator_somconnexio",
+    ],
+    "author": "Coopdevs Treball SCCL, " "Som Connexió SCCL",
+    "website": "https://coopdevs.org",
+    "category": "Cooperative management",
+    "license": "AGPL-3",
+    "data": [
+        "data/mail_activity_type.xml",
+        "data/res_partner_category.xml",
+        "reports/CM_50_conditions.xml",
+        "reports/CM_100_conditions.xml",
+        "reports/CM_mixed_conditions.xml",
+        "reports/crm_lead_creation_CM_email_template.xml",
+        "views/res_partner_view.xml",
+    ],
+}
