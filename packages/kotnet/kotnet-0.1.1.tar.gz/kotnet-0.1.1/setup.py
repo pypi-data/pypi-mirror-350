3de4e9db3c87a8f065f52de9035be84e663f1913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:34 +0000

    Add package kotnet/0.1.1/kotnet-0.1.1.tar.gz

---
+++ packages/kotnet/kotnet-0.1.1.tar.gz/kotnet-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kotnet",
+    version="0.1.1",
+    description="Console text formatting and animations library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="EnderDragon",
+    author_email="rhdhf909@gmail.com",
+    packages=find_packages(),
+    packges=["kotnet"],
+    include_package_data=True,  
+    install_requires=[
+        'colorama >= 0.4.4',
+        'tqdm >= 4.0.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
