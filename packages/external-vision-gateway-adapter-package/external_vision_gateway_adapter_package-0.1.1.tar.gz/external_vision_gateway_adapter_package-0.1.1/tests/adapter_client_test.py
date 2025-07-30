497e226eeea17975113871533849428636fcaec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:11 +0000

    Add package external-vision-gateway-adapter-package/0.1.1/external_vision_gateway_adapter_package-0.1.1.tar.gz

---
+++ packages/external-vision-gateway-adapter-package/external_vision_gateway_adapter_package-0.1.1.tar.gz/external_vision_gateway_adapter_package-0.1.1/tests/adapter_client_test.py
@@ -0,0 +1,28 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+from datetime import datetime, timezone
+from evga.server_client_adapter import ServerClientAdapter
+
+def load_image(image_path):
+	"""Read an image file as binary data."""
+	with open(image_path, "rb") as f:
+		return f.read()
+
+
+def send_image(image_path):
+	# Load image data
+	image_data = [load_image(img_path) for img_path in image_path]
+
+	datetime_now = datetime.now(timezone.utc)
+
+	response = ServerClientAdapter(port=50051, timeout=2).send_request(datetime=datetime_now, image_bytes_list=image_data, tool_name='ingrind')
+
+	print(response)
+
+
+if __name__ == '__main__':
+
+	image_path = ["test.bmp", "test2.bmp"]  # Replace with your image file path
+	send_image(image_path)
