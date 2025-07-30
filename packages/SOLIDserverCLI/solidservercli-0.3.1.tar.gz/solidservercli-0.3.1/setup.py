e556ec34a471ef571cc642954f4c07da88e98620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:30 +0000

    Add package SOLIDserverCLI/0.3.1/solidservercli-0.3.1.tar.gz

---
+++ packages/SOLIDserverCLI/solidservercli-0.3.1.tar.gz/solidservercli-0.3.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='SOLIDserverCLI',
+    version='0.3.1',
+    description='EfficientIP SOLIDserver cli',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Alex Chauvin',
+    author_email='ach@efficientip.com',
+    project_urls={
+        'GitHub': 'https://gitlab.com/efficientip/cli-for-solidserver',
+        'Changelog': 'https://gitlab.com/efficientip/cli-for-solidserver/-/raw/master/CHANGELOG.md',
+    },
+    url='https://gitlab.com/efficientip/cli-for-solidserver',
+    license=license,
+    packages=['sds'],
+
+    entry_points={
+        'console_scripts': [
+            'sds = sds.cli:main',
+        ],
+    },
+
+    python_requires='>=3.10',
+    install_requires=['typer', 'rich', 'SOLIDserverRest'],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers"
+    ],
+)
