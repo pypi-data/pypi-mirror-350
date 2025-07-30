605eb9a2e8807ba068b170a4a1405a84c2b21999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:45 +0000

    Add package narwhals/1.41.0/narwhals-1.41.0.tar.gz

---
+++ packages/narwhals/narwhals-1.41.0.tar.gz/narwhals-1.41.0/tests/expr_and_series/shift_test.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import pyarrow as pa
+import pytest
+
+import narwhals as nw
+from tests.utils import (
+    DUCKDB_VERSION,
+    POLARS_VERSION,
+    Constructor,
+    ConstructorEager,
+    assert_equal_data,
+)
+
+data = {
+    "i": [0, 1, 2, 3, 4],
+    "a": [0, 1, 2, 3, 4],
+    "b": [1, 2, 3, 5, 3],
+    "c": [5, 4, 3, 2, 1],
+}
+
+
+def test_shift(constructor_eager: ConstructorEager) -> None:
+    df = nw.from_native(constructor_eager(data))
+    result = df.with_columns(nw.col("a", "b", "c").shift(2)).filter(nw.col("i") > 1)
+    expected = {"i": [2, 3, 4], "a": [0, 1, 2], "b": [1, 2, 3], "c": [5, 4, 3]}
+    assert_equal_data(result, expected)
+
+
+def test_shift_lazy(constructor: Constructor) -> None:
+    if "polars" in str(constructor) and POLARS_VERSION < (1, 10):
+        pytest.skip()
+    if "duckdb" in str(constructor) and DUCKDB_VERSION < (1, 3):
+        pytest.skip()
+    df = nw.from_native(constructor(data))
+    result = df.with_columns(nw.col("a", "b", "c").shift(2).over(order_by="i")).filter(
+        nw.col("i") > 1
+    )
+    expected = {"i": [2, 3, 4], "a": [0, 1, 2], "b": [1, 2, 3], "c": [5, 4, 3]}
+    assert_equal_data(result, expected)
+
+
+def test_shift_series(constructor_eager: ConstructorEager) -> None:
+    df = nw.from_native(constructor_eager(data), eager_only=True)
+    result = df.with_columns(df["a"].shift(2), df["b"].shift(2), df["c"].shift(2)).filter(
+        nw.col("i") > 1
+    )
+    expected = {"i": [2, 3, 4], "a": [0, 1, 2], "b": [1, 2, 3], "c": [5, 4, 3]}
+    assert_equal_data(result, expected)
+
+
+def test_shift_multi_chunk_pyarrow() -> None:
+    tbl = pa.table({"a": [1, 2, 3]})
+    tbl = pa.concat_tables([tbl, tbl, tbl])
+    df = nw.from_native(tbl, eager_only=True)
+
+    result = df.select(nw.col("a").shift(1))
+    expected = {"a": [None, 1, 2, 3, 1, 2, 3, 1, 2]}
+    assert_equal_data(result, expected)
+
+    result = df.select(nw.col("a").shift(-1))
+    expected = {"a": [2, 3, 1, 2, 3, 1, 2, 3, None]}
+    assert_equal_data(result, expected)
+
+    result = df.select(nw.col("a").shift(0))
+    expected = {"a": [1, 2, 3, 1, 2, 3, 1, 2, 3]}
+    assert_equal_data(result, expected)
---
+++ packages/narwhals/narwhals-1.41.0.tar.gz/narwhals-1.41.0/tests/series_only/scatter_test.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import pandas as pd
+import pytest
+
+import narwhals as nw
+from tests.utils import ConstructorEager, assert_equal_data
+
+
+def test_scatter(
+    constructor_eager: ConstructorEager, request: pytest.FixtureRequest
+) -> None:
+    if "modin" in str(constructor_eager):
+        # https://github.com/modin-project/modin/issues/7392
+        request.applymarker(pytest.mark.xfail)
+    df = nw.from_native(
+        constructor_eager({"a": [1, 2, 3], "b": [142, 124, 132]}), eager_only=True
+    )
+    result = df.with_columns(
+        df["a"].scatter([0, 1], [999, 888]), df["b"].scatter([0, 2, 1], df["b"])
+    )
+    expected = {"a": [999, 888, 3], "b": [142, 132, 124]}
+    assert_equal_data(result, expected)
+
+
+def test_scatter_indices() -> None:
+    s = nw.from_native(pd.Series([2, 3, 6], index=[1, 0, 2]), series_only=True)
+    result = s.scatter([1, 0, 2], s)
+    expected = pd.Series([3, 2, 6], index=[1, 0, 2])
+    pd.testing.assert_series_equal(result.to_native(), expected)
+
+
+def test_scatter_unchanged(constructor_eager: ConstructorEager) -> None:
+    df = nw.from_native(
+        constructor_eager({"a": [1, 2, 3], "b": [142, 124, 132]}), eager_only=True
+    )
+    df.with_columns(
+        df["a"].scatter([0, 1], [999, 888]), df["b"].scatter([0, 2, 1], [142, 124, 132])
+    )
+    expected = {"a": [1, 2, 3], "b": [142, 124, 132]}
+    assert_equal_data(df, expected)
+
+
+def test_single_series(constructor_eager: ConstructorEager) -> None:
+    df = nw.from_native(
+        constructor_eager({"a": [1, 2, 3], "b": [142, 124, 132]}), eager_only=True
+    )
+    s = df["a"]
+    s.scatter([0, 1], [999, 888])
+    expected = {"a": [1, 2, 3]}
+    assert_equal_data({"a": s}, expected)
+
+
+def test_scatter_integer(constructor_eager: ConstructorEager) -> None:
+    df = nw.from_native(
+        constructor_eager({"a": [1, 2, 3], "b": [142, 124, 132]}), eager_only=True
+    )
+    s = df["a"]
+    result = s.scatter(0, 999)
+    expected = {"a": [999, 2, 3]}
+    assert_equal_data({"a": result}, expected)
+
+
+def test_scatter_unordered_indices(
+    constructor_eager: ConstructorEager, request: pytest.FixtureRequest
+) -> None:
+    if "modin" in str(constructor_eager):
+        # bugged
+        request.applymarker(pytest.mark.xfail)
+    data = {"a": [16, 12, 10, 9, 6, 5, 2]}
+    indices = [6, 1, 0, 5, 3, 2, 4]
+    df = nw.from_native(constructor_eager(data))
+    result = df["a"].scatter(indices, df["a"])
+    assert_equal_data({"a": result}, {"a": [10, 12, 5, 6, 2, 9, 16]})
