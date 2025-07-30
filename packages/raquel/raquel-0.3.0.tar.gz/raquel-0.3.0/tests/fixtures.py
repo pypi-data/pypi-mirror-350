dea884d17bbb7a0704779bf1d97547fa257f4865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:28 +0000

    Add package raquel/0.3.0/raquel-0.3.0.tar.gz

---
+++ packages/raquel/raquel-0.3.0.tar.gz/raquel-0.3.0/tests/fixtures.py
@@ -0,0 +1,39 @@
+import logging
+
+import pytest
+import pytest_asyncio
+
+from raquel import Raquel, AsyncRaquel
+
+
+@pytest.fixture
+def rq_sqlite():
+    logging.getLogger("raquel").setLevel(logging.DEBUG)
+
+    instance = Raquel("sqlite://")
+    instance.create_all()
+    return instance
+
+
+@pytest.fixture
+def rq_psycopg2(postgres_dsn_sync):
+    logging.getLogger("raquel").setLevel(logging.DEBUG)
+
+    instance = Raquel(postgres_dsn_sync)
+    try:
+        instance.create_all()
+        yield instance
+    finally:
+        instance.drop_all()
+
+
+@pytest_asyncio.fixture
+async def rq_asyncpg(postgres_dsn_async):
+    logging.getLogger("raquel").setLevel(logging.DEBUG)
+
+    instance = AsyncRaquel(postgres_dsn_async)
+    try:
+        await instance.create_all()
+        yield instance
+    finally:
+        await instance.drop_all()
