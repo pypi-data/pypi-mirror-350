f6b0d85d2441583c2ec31bd954ad65652a525c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:55 +0000

    Add package rtgym/0.16/rtgym-0.16-py3-none-any.whl

---
+++ packages/rtgym/rtgym-0.16-py3-none-any.whl/rtgym/__init__.py
@@ -0,0 +1,16 @@
+from rtgym.envs.real_time_env import RealTimeGymInterface, DEFAULT_CONFIG_DICT
+from rtgym.tuto.dummy_drone import DummyRCDrone
+import logging
+from gymnasium.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+register(
+    id='real-time-gym-v1',
+    entry_point='rtgym.envs:RealTimeEnv',
+)
+
+register(
+    id='real-time-gym-ts-v1',
+    entry_point='rtgym.envs:RealTimeEnvTS',
+)
