e83351999cce0feebb3fc89b81080ff2a8d5a54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:39 +0000

    Add package django-jqgrid/1.0.0/django_jqgrid-1.0.0.tar.gz

---
+++ packages/django-jqgrid/django_jqgrid-1.0.0.tar.gz/django_jqgrid-1.0.0/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+
+from django_jqgrid.views import GridFilterViewSet
+
+app_name = 'django_jqgrid'
+
+router = DefaultRouter()
+router.register(r'grid-filter', GridFilterViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    # Example page showcasing standalone grid
+    path('example/', TemplateView.as_view(template_name='django_jqgrid/example_page.html'), name='jqgrid_example'),
+]
