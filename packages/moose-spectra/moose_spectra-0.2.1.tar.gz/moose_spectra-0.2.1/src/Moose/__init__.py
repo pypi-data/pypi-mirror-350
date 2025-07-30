bee58a1b4aa76e4681416c3de893ac954c685e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:18 +0000

    Add package moose-spectra/0.2.1/moose_spectra-0.2.1.tar.gz

---
+++ packages/moose-spectra/moose_spectra-0.2.1.tar.gz/moose_spectra-0.2.1/src/Moose/__init__.py
@@ -0,0 +1,46 @@
+"""
+Moose allows you to simulate thermal diatomic spectra based on line-by-line databases.
+
+[Simulation](./Simulation) contains the core functionality to work with these databases.
+
+
+When you import `Moose`, it will check if the [`lmfit`](https://lmfit.github.io/lmfit-py/) package is installed in your environment.
+
+If so, it will import some additional functions in the `Moose` namespace for your convenience.
+
+[FitUtil](./FitUtil) contains these additional functions.
+
+To see `Moose` in action, check out the [examples](../../examples)
+"""
+
+import importlib.util
+from importlib import resources
+from .Simulation import (
+    default_params,
+    thermal_default_params,
+    query_DB,
+    create_stick_spectrum,
+    equidistant_mesh,
+    apply_voigt,
+    match_spectra,
+    model_for_fit,
+)
+
+database_path = resources.files("Moose").joinpath("data")
+database_files = [p.stem for p in database_path.glob("*.db")]  # + [p.stem for p in database_path.glob("*.par")]
+
+__all__ = [
+    "default_params",
+    "thermal_default_params",
+    "query_DB",
+    "create_stick_spectrum",
+    "equidistant_mesh",
+    "apply_voigt",
+    "match_spectra",
+    "model_for_fit",
+]
+
+if importlib.util.find_spec("lmfit") is not None:
+    from .FitUtil import set_param, set_params, make_model
+
+    __all__ += ["set_param", "set_params", "make_model"]
