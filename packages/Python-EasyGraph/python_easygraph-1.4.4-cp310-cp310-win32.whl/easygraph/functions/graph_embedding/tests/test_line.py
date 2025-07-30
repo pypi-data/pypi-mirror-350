49c859dad147d1b42e61a2331707292efbf27aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:48:34 +0000

    Add package Python-EasyGraph/1.4.4/python_easygraph-1.4.4-cp310-cp310-win32.whl

---
+++ packages/Python-EasyGraph/python_easygraph-1.4.4-cp310-cp310-win32.whl/easygraph/functions/graph_embedding/tests/test_line.py
@@ -0,0 +1,13 @@
+import unittest
+
+import easygraph as eg
+import numpy as np
+
+
+class Test_Line(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
