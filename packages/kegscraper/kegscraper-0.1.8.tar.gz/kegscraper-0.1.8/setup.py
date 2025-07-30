2c7abd4eb42a2ba243f8bebd1b4a5482241e3eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:51 +0000

    Add package kegscraper/0.1.8/kegscraper-0.1.8.tar.gz

---
+++ packages/kegscraper/kegscraper-0.1.8.tar.gz/kegscraper-0.1.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+
+setup(
+    name='kegscraper',
+    version='v0.1.8',
+    packages=['kegscraper'] +
+             [f"kegscraper.{subdir}" for subdir in next(os.walk("kegscraper"))[1] if subdir != "__pycache__"],
+    url='https://kegs.org.uk/',
+    license=open("LICENSE").read(),
+    author='BigPotatoPizzaHey',
+    author_email="poo@gmail.com",
+    description="The ultimate KEGS webscraping module",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    requires=[
+        'requests',
+        'bs4',
+        'beautifulsoup4',
+        'dateparser',
+        'setuptools',
+        'pypdf',
+        'pyperclip'
+    ]
+)
