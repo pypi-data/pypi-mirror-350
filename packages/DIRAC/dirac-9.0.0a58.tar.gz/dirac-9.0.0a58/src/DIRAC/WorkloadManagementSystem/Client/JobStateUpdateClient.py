e634268115dcddeabea4d6159ef1c90afa47e2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:37 +0000

    Add package DIRAC/9.0.0a58/dirac-9.0.0a58.tar.gz

---
+++ packages/DIRAC/dirac-9.0.0a58.tar.gz/dirac-9.0.0a58/src/DIRAC/WorkloadManagementSystem/Client/JobStateUpdateClient.py
@@ -0,0 +1,30 @@
+""" Class that contains client access to the JobStateUpdate handler. """
+
+from DIRAC.Core.Base.Client import Client, createClient
+from DIRAC.WorkloadManagementSystem.FutureClient.JobStateUpdateClient import (
+    JobStateUpdateClient as futureJobStateUpdateClient,
+)
+
+
+@createClient("WorkloadManagement/JobStateUpdate")
+class JobStateUpdateClient(Client):
+    """JobStateUpdateClient sets url for the JobStateUpdateHandler."""
+
+    diracxClient = futureJobStateUpdateClient
+
+    def __init__(self, url=None, **kwargs):
+        """
+        Sets URL for JobStateUpdate handler
+
+        :param self: self reference
+        :param url: url of the JobStateUpdateHandler
+        :param kwargs: forwarded to the Base Client class
+        """
+
+        super().__init__(**kwargs)
+
+        if not url:
+            self.serverURL = "WorkloadManagement/JobStateUpdate"
+
+        else:
+            self.serverURL = url
