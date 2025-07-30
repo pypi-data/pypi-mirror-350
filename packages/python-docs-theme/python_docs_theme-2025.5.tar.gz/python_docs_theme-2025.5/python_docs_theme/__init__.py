c4fb03dce5443c375158742f530ef4caeaf8ab4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:14 +0000

    Add package python-docs-theme/2025.5/python_docs_theme-2025.5.tar.gz

---
+++ packages/python-docs-theme/python_docs_theme-2025.5.tar.gz/python_docs_theme-2025.5/python_docs_theme/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+TYPE_CHECKING = False
+if TYPE_CHECKING:
+    from sphinx.application import Sphinx
+    from sphinx.util.typing import ExtensionMetadata
+
+__version__ = "2025.5"
+
+THEME_PATH = Path(__file__).resolve().parent
+
+
+def setup(app: Sphinx) -> ExtensionMetadata:
+    app.require_sphinx("7.3")
+
+    app.add_html_theme("python_docs_theme", str(THEME_PATH))
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
