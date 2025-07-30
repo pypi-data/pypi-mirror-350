773376d373716a454df437bbc8c6d4322b44896e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package feasc/1.1.1/feasc-1.1.1.tar.gz

---
+++ packages/feasc/feasc-1.1.1.tar.gz/feasc-1.1.1/FeaSc/__init__.py
@@ -0,0 +1,8 @@
+from importlib.metadata import version
+
+__version__ = "1.1.1"  # 版本号
+
+from FeaSc.pyFeaSc import *
+from FeaSc.pySaaSc import *
+from FeaSc.pymca import *
+from FeaSc.scamp import *
