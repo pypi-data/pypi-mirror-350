a2e0d8edac0e4d2b4a051e885ec055671b14db2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:31 +0000

    Add package jinja2-getenv-extension/1.0.3/jinja2_getenv_extension-1.0.3-py3-none-any.whl

---
+++ packages/jinja2-getenv-extension/jinja2_getenv_extension-1.0.3-py3-none-any.whl/jinja2_getenv_extension/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+from jinja2.ext import Extension
+
+try:
+    from jinja2 import pass_eval_context as eval_context
+except ImportError:
+    from jinja2 import evalcontextfilter as eval_context
+
+
+@eval_context
+def getenv(eval_ctx, value, default=None):
+    result = os.environ.get(value, default)
+    if result is None:
+        raise Exception("can't find %s environnement variable" % value)
+    return result
+
+
+class GetenvExtension(Extension):
+
+    def __init__(self, environment):
+        super(GetenvExtension, self).__init__(environment)
+        environment.filters['getenv'] = getenv
