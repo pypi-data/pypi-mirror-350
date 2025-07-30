35f7ee19485adad1a9cad39e9f25ec4ad692c45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:33 +0000

    Add package liteboty/0.2.2a2/liteboty-0.2.2a2-py3-none-any.whl

---
+++ packages/liteboty/liteboty-0.2.2a2-py3-none-any.whl/liteboty/core/utils.py
@@ -0,0 +1,27 @@
+import asyncio
+
+
+class TimerLoop:
+    def __init__(self,  name, interval, callback, count=None):
+        self.interval = interval
+        self.callback = callback
+        self.name = name
+        self.count = count
+
+    def __str__(self):
+        return f"TimerLoop({self.name}, {self.interval})"
+
+    async def run(self):
+        run_count = 0
+        while self.count is None or run_count < self.count:
+            start_time = asyncio.get_event_loop().time()
+            await self.callback()
+            end_time = asyncio.get_event_loop().time()
+            elapsed_time = end_time - start_time
+            await asyncio.sleep(max(0, self.interval - elapsed_time))
+
+            if self.count is not None:
+                run_count += 1
+
+    def stop(self):
+        self.count = -1
