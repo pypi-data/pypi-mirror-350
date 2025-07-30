3134ad99ed114d0d0e1bce9b195523c89695b021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:43 +0000

    Add package calcing/1.0.0/calcing-1.0.0.tar.gz

---
+++ packages/calcing/calcing-1.0.0.tar.gz/calcing-1.0.0/README.md
@@ -0,0 +1,8 @@
+-- documantation for calcing --
+
+import calcing
+calcing.plus(numb1=1, numb2=5) # --  1 + 5 = 6
+calcing.minus(numb1=1, numb2=5) # --  1 - 5 = -4
+calcing.dil(numb1=1, numb2=5) # -- 1 / 5 = 0.2
+calcing.diln(numb1=1, numb2=5) # -- 1 // 5 = 0
+
---
+++ packages/calcing/calcing-1.0.0.tar.gz/calcing-1.0.0/calcing.py
@@ -0,0 +1,14 @@
+def plus(numb1, numb2):
+    return numb1 + numb2
+
+def minus(numb1, numb2):
+    return numb1 - numb2
+
+def mnoz(numb1, numb2):
+    return numb1 * numb2
+
+def dil(numb1, numb2):
+    return numb1 / numb2
+
+def diln(numb1, numb2):
+    return numb1 // numb2
