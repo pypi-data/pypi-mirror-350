b83924578a83a3b4fb93126b32fb8508a43b9f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:11 +0000

    Add package vectordbcloud/1.1.2/vectordbcloud-1.1.2.tar.gz

---
+++ packages/vectordbcloud/vectordbcloud-1.1.2.tar.gz/vectordbcloud-1.1.2/vectordbcloud/__init__.py
@@ -0,0 +1,12 @@
+"""
+VectorDBCloud Python SDK
+Official Python SDK for VectorDBCloud API
+Version: 1.1.0
+Updated: 2025-05-26T01:56:35.560274
+"""
+
+from .client import VectorDBCloud
+from .exceptions import VectorDBCloudError, AuthenticationError, APIError
+
+__version__ = "1.1.2"
+__all__ = ["VectorDBCloud", "VectorDBCloudError", "AuthenticationError", "APIError"]
