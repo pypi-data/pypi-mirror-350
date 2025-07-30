c71b47b7eea4b277d4eeddbd0a83960274d822ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:16 +0000

    Add package kaiserlift/0.1.1/kaiserlift-0.1.1.tar.gz

---
+++ packages/kaiserlift/kaiserlift-0.1.1.tar.gz/kaiserlift-0.1.1/kaiserlift/__init__.py
@@ -0,0 +1,19 @@
+from .helpers import (
+    calculate_1rm,
+    highest_weight_per_rep,
+    estimate_weight_from_1rm,
+    add_1rm_column,
+    dougs_next_pareto,
+    get_closest_exercise,
+    plot_df
+)
+
+__all__ = [
+    "calculate_1rm",
+    "highest_weight_per_rep",
+    "estimate_weight_from_1rm",
+    "add_1rm_column",
+    "dougs_next_pareto",
+    "get_closest_exercise",
+    "plot_df"
+]
