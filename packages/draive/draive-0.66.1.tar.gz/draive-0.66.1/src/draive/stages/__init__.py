e0e281ab7785f03e0f94e4afdd6b8cf9edf20dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:41 +0000

    Add package draive/0.66.1/draive-0.66.1.tar.gz

---
+++ packages/draive/draive-0.66.1.tar.gz/draive-0.66.1/src/draive/stages/__init__.py
@@ -0,0 +1,22 @@
+from draive.stages.stage import Stage, stage
+from draive.stages.types import (
+    StageCondition,
+    StageContextTransforming,
+    StageException,
+    StageExecution,
+    StageMerging,
+    StageResultTransforming,
+    StageStateAccessing,
+)
+
+__all__ = (
+    "Stage",
+    "StageCondition",
+    "StageContextTransforming",
+    "StageException",
+    "StageExecution",
+    "StageMerging",
+    "StageResultTransforming",
+    "StageStateAccessing",
+    "stage",
+)
