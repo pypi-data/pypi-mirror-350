1a09e729ec0390a386b24dbb93512632a5e8c89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:14 +0000

    Add package superduper-snowflake/0.6.4/superduper_snowflake-0.6.4-py3-none-any.whl

---
+++ packages/superduper-snowflake/superduper_snowflake-0.6.4-py3-none-any.whl/superduper_snowflake/connect.py
@@ -0,0 +1,69 @@
+import os
+import re
+
+from snowflake.snowpark import Session
+from superduper import logging
+
+
+def connect(uri):
+    """Connect to Snowflake using the Snowpark session.
+
+    :param uri: The URI of the Snowflake connection.
+
+    If the URI is 'snowflake://', the connection parameters are read
+    from environment variables.
+
+    - SNOWFLAKE_HOST: The Snowflake host.
+    - SNOWFLAKE_PORT: The Snowflake port.
+    - SNOWFLAKE_ACCOUNT: The Snowflake account.
+    - SNOWFLAKE_WAREHOUSE: The Snowflake warehouse.
+    - SNOWFLAKE_DATABASE: The Snowflake database.
+    - SUPERDUPER_DATA_SCHEMA: The Snowflake schema.
+    - /snowflake/session/token: The Snowflake OAuth token.
+    """
+    logging.info(
+        'Creating Snowpark session for' ' snowflake data-backend implementation'
+    )
+    if uri == 'snowflake://':
+        try:
+            connection_parameters = dict(
+                host=os.environ['SNOWFLAKE_HOST'],
+                port=int(os.environ['SNOWFLAKE_PORT']),
+                account=os.environ['SNOWFLAKE_ACCOUNT'],
+                authenticator='oauth',
+                token=open('/snowflake/session/token').read(),
+                warehouse=os.environ['SNOWFLAKE_WAREHOUSE'],
+                database=os.environ['SNOWFLAKE_DATABASE'],
+                schema=os.environ['SUPERDUPER_DATA_SCHEMA'],
+            )
+            schema = os.environ['SUPERDUPER_DATA_SCHEMA']
+        except KeyError as e:
+            all_snowflake_envs = [
+                key for key in os.environ.keys() if 'SNOWFLAKE' in key
+            ]
+            raise KeyError(
+                f'Environment variable {e} not set. '
+                f'Available snowflake environment variables: {all_snowflake_envs}. '
+                'Please set the environment variables for Snowflake connection.'
+            )
+    else:
+        if '?warehouse=' not in uri:
+            match = re.match('^snowflake:\/\/(.*):(.*)\@(.*)\/(.*)\/(.*)$', uri)
+            user, password, account, database, schema = match.groups()
+            warehouse = None
+        else:
+            match = re.match(
+                '^snowflake://(.*):(.*)@(.*)/(.*)/(.*)?warehouse=(.*)$', uri
+            )
+            user, password, account, database, schema, warehouse = match.groups()
+
+        connection_parameters = dict(
+            user=user,
+            password=password,
+            account=account,
+            warehouse=warehouse,
+            database=database,
+            schema=schema,
+        )
+
+    return Session.builder.configs(connection_parameters).create(), schema
---
+++ packages/superduper-snowflake/superduper_snowflake-0.6.4-py3-none-any.whl/superduper_snowflake/vector_search.py
@@ -0,0 +1,198 @@
+import numpy as np
+import os
+import re
+import typing as t
+
+from snowflake.snowpark import Session
+from superduper import CFG
+from superduper.backends.base.vector_search import BaseVectorSearcher, VectorItem
+
+if t.TYPE_CHECKING:
+    from superduper.components.vector_index import VectorIndex
+
+
+class SnowflakeVectorSearcher(BaseVectorSearcher):
+    """Vector searcher implementation of atlas vector search.
+
+    :param identifier: Unique string identifier of index
+    :param collection: Collection name
+    :param dimensions: Dimension of the vector embeddings
+    :param measure: measure to assess similarity
+    :param output_path: Path to the output
+    """
+
+    native_service: t.ClassVar[bool] = False
+
+    def __init__(
+        self,
+        identifier: str,
+        collection: str,
+        dimensions: t.Optional[int],
+        measure: t.Optional[str] = None,
+        output_path: t.Optional[str] = None,
+    ):
+        self.identifier = identifier
+        vector_search_uri = CFG.data_backend
+        assert vector_search_uri, "Vector search URI is required"
+
+        self.session = SnowflakeVectorSearcher.create_session(vector_search_uri)
+
+        assert output_path
+        self.output_path = output_path
+        self.collection = collection
+        if measure != 'l2':
+            raise TypeError(
+                "NOTE: Only L2 similary function is supported by ",
+                "snowflake vector search",
+            )
+
+        self.measure = measure
+        self.dimensions = dimensions
+        self._cache = {}
+        self._db = None
+
+    @classmethod
+    def create_session(cls, vector_search_uri):
+        """Creates a snowflake session.
+
+        :param vector_search_uri: Connection URI.
+        """
+        if vector_search_uri == 'snowflake://':
+            host = os.environ['SNOWFLAKE_HOST']
+            port = int(os.environ['SNOWFLAKE_PORT'])
+            account = os.environ['SNOWFLAKE_ACCOUNT']
+            token = open('/snowflake/session/token').read()
+            warehouse = os.environ['SNOWFLAKE_WAREHOUSE']
+            database = os.environ['SNOWFLAKE_DATABASE']
+            schema = os.environ['SUPERDUPER_DATA_SCHEMA']
+
+            connection_parameters = {
+                "token": token,
+                "account": account,
+                "database": database,
+                "schema": schema,
+                "warehouse": warehouse,
+                "authenticator": "oauth",
+                "port": port,
+                "host": host,
+            }
+        else:
+            pattern = r"snowflake://(?P<user>[^:]+):(?P<password>[^@]+)@(?P<account>[^/]+)/(?P<database>[^/]+)/(?P<schema>[^/]+)"
+            match = re.match(pattern, vector_search_uri)
+            schema = match.group("schema")
+            database = match.group("database")
+            if match:
+                connection_parameters = {
+                    "user": match.group("user"),
+                    "password": match.group("password"),
+                    "account": match.group("account"),
+                    "database": match.group("database"),
+                    "schema": match.group("schema"),
+                    # TODO: check warehouse
+                    "warehouse": "base",
+                }
+
+            else:
+                raise ValueError(f"URI `{vector_search_uri}` is invalid!")
+
+        session = Session.builder.configs(connection_parameters).create()
+        return session
+
+    def __len__(self):
+        pass
+
+    def is_initialized(self, identifier):
+        """Check if vector index initialized."""
+        return True
+
+    def drop(self):
+        """Drop the vector index."""
+        pass
+
+    @classmethod
+    def from_component(cls, vi: "VectorIndex"):
+        """Create a vector searcher from a vector index.
+
+        :param vi: VectorIndex instance
+        """
+        from superduper.components.listener import Listener
+
+        assert isinstance(vi.indexing_listener, Listener)
+        assert vi.indexing_listener.select is not None
+        path = collection = vi.indexing_listener.outputs
+
+        return SnowflakeVectorSearcher(
+            identifier=vi.identifier,
+            dimensions=vi.dimensions,
+            measure=vi.measure,
+            output_path=path,
+            collection=collection,
+        )
+
+    def add(self, items: t.Sequence[VectorItem], cache: bool = False) -> None:
+        """
+        Add items to the index.
+
+        :param items: t.Sequence of VectorItems
+        """
+        # NOTE: Since we will be doing vector search on tables directly
+        # seperate vector search is not required.
+
+    def delete(self, ids: t.Sequence[str]) -> None:
+        """Remove items from the index.
+
+        :param ids: t.Sequence of ids of vectors.
+        """
+
+    def find_nearest_from_id(self, id: str, n=100, within_ids=None):
+        """Find the nearest vectors to the given ID.
+
+        :param id: ID of the vector
+        :param n: number of nearest vectors to return
+        :param within_ids: list of IDs to search within
+        """
+        result = self.session.sql(
+            f"""
+        SELECT "{self.output_path}"
+        FROM "{self.collection}"
+        WHERE id = '{id}'
+        LIMIT 1
+        """
+        ).collect()
+        return self.find_nearest_from_array(result, n=n, within_ids=within_ids)
+
+    def find_nearest_from_array(self, h, n=100, within_ids=None):
+        """Find the nearest vectors to the given vector.
+
+        :param h: vector
+        :param n: number of nearest vectors to return
+        :param within_ids: list of IDs to search within
+        """
+        from snowflake.snowpark.functions import col, lit, vector_l2_distance
+        from snowflake.snowpark.types import VectorType
+
+        if isinstance(h, np.ndarray):
+            h = h.tolist()
+
+        vector_table = self.session.table(f'"{self.collection}"')
+        result_df = (
+            vector_table.select(
+                '"_source"',
+                f'"{self.output_path}"',
+                vector_l2_distance(
+                    col(f'"{self.output_path}"').cast(
+                        VectorType(float, self.dimensions)
+                    ),
+                    lit(h).cast(VectorType(float, self.dimensions)),
+                ).as_("distance"),
+            )
+            .sort("distance")
+            .limit(n)
+        )
+        result_list = result_df.collect()
+        ids = [row["_source"] for row in result_list]
+        scores = [-row["distance".upper()] for row in result_list]
+        return ids, scores
+
+    def initialize(self):
+        """Initialize vector search."""
