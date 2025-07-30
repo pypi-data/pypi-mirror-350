f6bfd7d1d99e21b930edb464e1b91206a2cdf5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:52 +0000

    Add package bazoo/0.1.0/bazoo-0.1.0-py3-none-any.whl

---
+++ packages/bazoo/bazoo-0.1.0-py3-none-any.whl/bazoo/__init__.py
@@ -0,0 +1,16 @@
+"""
+Bazoo - An unofficial Python library for BazooStore API
+
+This library provides interfaces to search and retrieve bots from BazooStore service.
+"""
+
+from .core import (
+    GetBestBots,
+    GetNewBots,
+    GetRandomBazoo,
+    SearchBazoo,
+    GetDataBot
+)
+
+__version__ = "0.1.0"
+__author__ = "Hamidreza"
