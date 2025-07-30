2c217901bed7aa68df18aa12b2f90b284930282e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:28 +0000

    Add package nbforge/0.1.10/nbforge-0.1.10.tar.gz

---
+++ packages/nbforge/nbforge-0.1.10.tar.gz/nbforge-0.1.10/src/nbforge/utils/constants.py
@@ -0,0 +1,7 @@
+# Prompt names
+NB_TO_MODULE_PROMPT = "nb_to_module"
+NB_TO_MODULE_STREAMLIT_PROMPT = "nb_to_module_with_streamlit"
+NB_TO_MODULE_FASTAPI_PROMPT =  "nb_to_module_with_fastapi"
+# Annotations 
+STREAMLIT_BLOCK_CONSTANT = "NBForge_Streamlit"
+FASTAPI_BLOCK_CONSTANT = "NBForge_FastAPI"
