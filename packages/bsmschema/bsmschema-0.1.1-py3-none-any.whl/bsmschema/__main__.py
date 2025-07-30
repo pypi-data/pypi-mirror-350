0a1012afda9a55ca8a179b9e5796be3ad6e5e705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:24 +0000

    Add package bsmschema/0.1.1/bsmschema-0.1.1-py3-none-any.whl

---
+++ packages/bsmschema/bsmschema-0.1.1-py3-none-any.whl/bsmschema/__main__.py
@@ -0,0 +1,18 @@
+import json
+import sys
+from pathlib import Path
+from bsmschema import models
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python -m bsmschema /path/to/schemadir')
+        sys.exit(1)
+
+    schemadir = Path(sys.argv[1])
+    schemadir.mkdir(parents=True, exist_ok=True)
+    for mname in models.__all__:
+        model = getattr(models, mname)
+        Path.write_text(
+            schemadir / f'{mname}.json',
+            json.dumps(model.model_json_schema(), indent=2),
+        )
