b4c2ab9d5d69a351bb7339d2dd45d3fc0dcfecdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:15 +0000

    Add package pyrobale/0.3.7/pyrobale-0.3.7-py3-none-any.whl

---
+++ packages/pyrobale/pyrobale-0.3.7-py3-none-any.whl/pyrobale/objects/enums.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+
+class UpdatesTypes(Enum):
+    """Types of updates."""
+
+    MESSAGE = "message"
+    MESSAGE_EDITED = "message_edited"
+    CALLBACK_QUERY = "callback_query"
+    PRE_CHECKOUT_QUERY = "pre_checkout_query"
+    MEMBER_JOINED = "member_joined"
+    MEMBER_LEFT = "member_left"
+    SUCCESSFUL_PAYMENT = "successful_payment"
+
+
+class Filters(Enum):
+    """Filters that you can use in handlers"""
+
+    TEXT = "text"
+    PHOTO = "photo"
+    VIDEO = "video"
+    AUDIO = "audio"
+    VOICE = "voice"
+    CONTACT = "contact"
+    LOCATION = "location"
+
+
+class ChatAction(Enum):
+    """Actions of a user in a chat."""
+
+    TYPING = "typing"
+    PHOTO = "upload_photo"
+    VIDEO = "upload_video"
+    REVORDVIDEO = "record_video"
+    VOICE = "upload_voice"
+    DOCUMENT = "upload_document"
+
+
+class ChatType(Enum):
+    """Types of chats."""
+
+    PRIVATE = "private"
+    GROUP = "group"
+    CHANNEL = "channel"
