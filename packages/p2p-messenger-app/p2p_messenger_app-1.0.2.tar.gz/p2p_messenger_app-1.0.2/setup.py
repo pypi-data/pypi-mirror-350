31044a44186a856a5d16289c597d8e1093f0c69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:46 +0000

    Add package p2p-messenger-app/1.0.2/p2p_messenger_app-1.0.2.tar.gz

---
+++ packages/p2p-messenger-app/p2p_messenger_app-1.0.2.tar.gz/p2p_messenger_app-1.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="p2p-messenger-app",
+    version="1.0.2",
+    packages=find_packages(),
+    py_modules=['node'],
+    install_requires=[
+        'cryptography==42.0.2',
+        'pycryptodome==3.20.0',
+        'psutil',
+    ],
+    entry_points={
+        'console_scripts': [
+            'p2p_messenger=node:main',
+        ],
+    },
+    author="Alfonso Hernandez",
+    author_email="tu@email.com",
+    description="Aplicación de mensajería P2P descentralizada",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tu-usuario/p2p_messenger",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_data={
+        'p2p_messenger': ['*.py'],
+    },
+    include_package_data=True,
+) 
