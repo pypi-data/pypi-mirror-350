331e8b7e8fd95fd8cd4e8151cd91c7b061ffab7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:39 +0000

    Add package gemini-webapi/1.12.3/gemini_webapi-1.12.3.tar.gz

---
+++ packages/gemini-webapi/gemini_webapi-1.12.3.tar.gz/gemini_webapi-1.12.3/tests/test_save_image.py
@@ -0,0 +1,48 @@
+import os
+import unittest
+import logging
+
+from httpx import HTTPError
+from loguru import logger
+
+from gemini_webapi import GeminiClient, AuthError, set_log_level
+
+logging.getLogger("asyncio").setLevel(logging.ERROR)
+set_log_level("DEBUG")
+
+
+class TestGeminiClient(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.geminiclient = GeminiClient(
+            os.getenv("SECURE_1PSID"), os.getenv("SECURE_1PSIDTS")
+        )
+
+        try:
+            await self.geminiclient.init()
+        except AuthError:
+            self.skipTest("Test was skipped due to invalid cookies")
+
+    async def test_save_web_image(self):
+        response = await self.geminiclient.generate_content(
+            "Show me some pictures of random subjects"
+        )
+        self.assertTrue(response.images)
+        for i, image in enumerate(response.images):
+            self.assertTrue(image.url)
+            try:
+                await image.save(verbose=True, skip_invalid_filename=True)
+            except HTTPError as e:
+                logger.warning(e)
+
+    async def test_save_generated_image(self):
+        response = await self.geminiclient.generate_content(
+            "Generate some pictures of random subjects"
+        )
+        self.assertTrue(response.images)
+        for i, image in enumerate(response.images):
+            self.assertTrue(image.url)
+            await image.save(verbose=True)
+
+
+if __name__ == "__main__":
+    unittest.main()
