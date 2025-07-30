077ebd7dd7893642e92f274988267280cc0e2f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:10:09 +0000

    Add package cursor-multi/1.1.7/cursor_multi-1.1.7-py3-none-any.whl

---
+++ packages/cursor-multi/cursor_multi-1.1.7-py3-none-any.whl/cursor_multi/init_readme.py
@@ -0,0 +1,12 @@
+init_readme = """# {__name__}
+
+## Getting started
+
+This repo is a [multi](https://github.com/montaguegabe/cursor-multi) parent repo to manage multiple sub-repositories:
+
+{__repo_list__}
+
+To get started, install cursor-multi with `brew install cursor-multi` or `pipx install cursor-multi`.
+
+Then install the (extension)[https://marketplace.visualstudio.com/items?itemName=montaguegabe.cursor-multi] in Cursor or VS Code.  When you make new cursor rules in the repos, or changes to the VS Code configurations, they will be automatically synced to the parent repo.
+"""
