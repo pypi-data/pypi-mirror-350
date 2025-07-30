ef1d518e86d6ac54c6634c23520664fdd9e6a1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:10 +0000

    Add package pyhermes/0.7.0/pyhermes-0.7.0.tar.gz

---
+++ packages/pyhermes/pyhermes-0.7.0.tar.gz/pyhermes-0.7.0/pyhermes/__init__.py
@@ -0,0 +1,10 @@
+from pyhermes.decorators import publisher, subscriber
+from pyhermes.publishing import publish
+
+__version__ = '0.7.0'
+
+__all__ = [
+    'publish',
+    'publisher',
+    'subscriber'
+]
