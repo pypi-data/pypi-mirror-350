be09566ef34f6a49f7a2a33a78c2e8239af86d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:15 +0000

    Add package mcp-server-nqc/1.0.3/mcp_server_nqc-1.0.3.tar.gz

---
+++ packages/mcp-server-nqc/mcp_server_nqc-1.0.3.tar.gz/mcp_server_nqc-1.0.3/setup.py
@@ -0,0 +1,30 @@
+
+
+
+from setuptools import setup, find_packages
+print(find_packages())
+
+
+setup(
+    name='mcp-server-nqc',
+    version='1.0.3',
+    description='A simple Python project',
+    long_description=open('README.md',encoding='utf8').read(),
+    long_description_content_type="text/markdown",
+    author='zcl',
+    author_email='597854439@qq.com',
+    url='https://github.com/yourusername/my_project',
+    packages=find_packages(
+        where='',
+        # include=['mcp-server-nqc']   # 仅包含主包及其子包
+    ),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'fastmcp>=2.5.1','argparse'
+    ],
+)
