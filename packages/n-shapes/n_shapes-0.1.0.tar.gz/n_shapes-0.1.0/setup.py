964867f02b4837dc8d1f4c79e988719c97d20b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:12 +0000

    Add package n-shapes/0.1.0/n_shapes-0.1.0.tar.gz

---
+++ packages/n-shapes/n_shapes-0.1.0.tar.gz/n_shapes-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='n_shapes',
+    version='0.1.0',
+    author='Naitik',
+    description='Simple turtle-based shape drawing package',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
