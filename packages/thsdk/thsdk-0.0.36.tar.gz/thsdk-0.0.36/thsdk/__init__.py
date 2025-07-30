e92c608fe3e4a6e2bcdb62e64f1d90fe48a1f1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:22:58 +0000

    Add package thsdk/0.0.36/thsdk-0.0.36.tar.gz

---
+++ packages/thsdk/thsdk-0.0.36.tar.gz/thsdk-0.0.36/thsdk/__init__.py
@@ -0,0 +1,8 @@
+from .ths_api import *  # Import everything from ths_api
+from .ths_api import __all__ as ths_api_all  # Import __all__ from ths_api
+
+__all__ = (
+    *ths_api_all,
+)
+
+__version__ = '1.0.0'
