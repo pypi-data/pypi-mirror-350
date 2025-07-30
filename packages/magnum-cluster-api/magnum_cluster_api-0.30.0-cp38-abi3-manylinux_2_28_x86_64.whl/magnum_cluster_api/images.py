22f237dde1b82c59d7301f21c2bda5e29cafac8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:17 +0000

    Add package magnum-cluster-api/0.30.0/magnum_cluster_api-0.30.0-cp38-abi3-manylinux_2_28_x86_64.whl

---
+++ packages/magnum-cluster-api/magnum_cluster_api-0.30.0-cp38-abi3-manylinux_2_28_x86_64.whl/magnum_cluster_api/images.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2023 VEXXHOST, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import semver
+
+from magnum_cluster_api import conf
+
+CONF = conf.CONF
+
+PAUSE = "registry.k8s.io/pause:3.9"
+
+
+def get_cluster_autoscaler_image(version: str):
+    parsed_version = semver.VersionInfo.parse(version[1:])
+    config_option = f"v{parsed_version.major}_{parsed_version.minor}_image"
+
+    if hasattr(CONF.auto_scaling, config_option):
+        return getattr(CONF.auto_scaling, config_option)
+
+    raise ValueError(
+        f"Unsupported Kubernetes version: {version}. "
+        "Please specify a supported version in the cluster template."
+    )
