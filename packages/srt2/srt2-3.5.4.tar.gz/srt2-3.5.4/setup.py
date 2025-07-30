1a8fbb92da3ac9ed094397f359ac2d2fc963aae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:14 +0000

    Add package srt2/3.5.4/srt2-3.5.4.tar.gz

---
+++ packages/srt2/srt2-3.5.4.tar.gz/srt2-3.5.4/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import codecs
+
+from setuptools import setup
+
+with codecs.open("README.rst", encoding="utf8") as readme_f:
+    README = readme_f.read()
+
+setup(
+    name="srt2",
+    version="3.5.4",
+    python_requires=">=2.7",
+    description="A tiny library for parsing, modifying, and composing SRT files.",
+    long_description=README,
+    author="Chris Down",
+    author_email="chris@chrisdown.name",
+    url="https://github.com/RaSan147/srt2",
+    py_modules=["srt"],
+    packages=["srt_tools"],
+    scripts=[
+        "srt_tools/srt",
+        "srt_tools/srt-deduplicate",
+        "srt_tools/srt-normalise",
+        "srt_tools/srt-fixed-timeshift",
+        "srt_tools/srt-linear-timeshift",
+        "srt_tools/srt-lines-matching",
+        "srt_tools/srt-mux",
+        "srt_tools/srt-play",
+        "srt_tools/srt-process",
+    ],
+    license="MIT",
+    keywords="srt",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Text Processing",
+    ],
+)
