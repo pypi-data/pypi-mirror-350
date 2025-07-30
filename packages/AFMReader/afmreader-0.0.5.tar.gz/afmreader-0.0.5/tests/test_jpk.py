65a1ccc48d2c37e87ab114edbfa696d5a5e57c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:23 +0000

    Add package AFMReader/0.0.5/afmreader-0.0.5.tar.gz

---
+++ packages/AFMReader/afmreader-0.0.5.tar.gz/afmreader-0.0.5/tests/test_jpk.py
@@ -0,0 +1,47 @@
+"""Test the loading of jpk files."""
+
+from pathlib import Path
+import pytest
+
+import numpy as np
+
+from AFMReader import jpk
+
+BASE_DIR = Path.cwd()
+RESOURCES = BASE_DIR / "tests" / "resources"
+
+
+@pytest.mark.parametrize(
+    ("file_name", "channel", "pixel_to_nm_scaling", "image_shape", "image_dtype", "image_sum"),
+    [
+        pytest.param(
+            "sample_0.jpk", "height_trace", 1.2770176335964876, (256, 256), float, 219242202.8256843, id="test image 0"
+        )
+    ],
+)
+def test_load_jpk(
+    file_name: str,
+    channel: str,
+    pixel_to_nm_scaling: float,
+    image_shape: tuple[int, int],
+    image_dtype: type,
+    image_sum: float,
+) -> None:
+    """Test the normal operation of loading a .jpk file."""
+    result_image = np.ndarray
+    result_pixel_to_nm_scaling = float
+
+    file_path = RESOURCES / file_name
+    result_image, result_pixel_to_nm_scaling = jpk.load_jpk(file_path, channel)  # type: ignore
+
+    assert result_pixel_to_nm_scaling == pixel_to_nm_scaling
+    assert isinstance(result_image, np.ndarray)
+    assert result_image.shape == image_shape
+    assert result_image.dtype == image_dtype
+    assert result_image.sum() == image_sum
+
+
+def test_load_jpk_file_not_found() -> None:
+    """Ensure FileNotFound error is raised."""
+    with pytest.raises(FileNotFoundError):
+        jpk.load_jpk("nonexistant_file.jpk", channel="TP")
