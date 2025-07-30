c11e056773939700bfae3a8f8d71605e63c0c3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:49 +0000

    Add package algosinpy/0.0.1/algosinpy-0.0.1.tar.gz

---
+++ packages/algosinpy/algosinpy-0.0.1.tar.gz/algosinpy-0.0.1/src/test/algorithm/dynamic_programming/test_longest_increasing_subsequence.py
@@ -0,0 +1,11 @@
+import unittest
+from src.algosinpy.algorithm.dynamic_programming.longest_increasing_subsequence import *
+
+
+class LISTestCase(unittest.TestCase):
+    def test_lis(self):
+        self.assertEqual(lis([3, -2, 3, 1, 2, 5, 2, 4]), [-2, 1, 2, 4])
+
+
+if __name__ == "__main__":
+    unittest.main()
