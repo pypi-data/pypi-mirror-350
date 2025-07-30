00f54d881ee609b3cb7f6a2b94c5d200b652e1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:59 +0000

    Add package flexcpi/0.1.0/flexcpi-0.1.0-py3-none-any.whl

---
+++ packages/flexcpi/flexcpi-0.1.0-py3-none-any.whl/flexcpi/__init__.py
@@ -0,0 +1,14 @@
+from .toolkit import (
+    load_catalog_tables,
+    load_weight_tables,
+    match_series_ids_to_weights,
+    fetch_cpi_series_data,
+    compute_custom_cpi_index,
+    plot_custom_cpi,
+    assign_manual_weights,
+    compute_inflation_rate,
+    plot_inflation_comparison,
+    auto_select_series,
+    export_cpi_data,
+    forecast_custom_cpi
+)
