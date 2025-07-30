00e354285a1774d760cf923a4bf75401010c4332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:33 +0000

    Add package nonebot-plugin-zssm/0.3.6/nonebot_plugin_zssm-0.3.6.tar.gz

---
+++ packages/nonebot-plugin-zssm/nonebot_plugin_zssm-0.3.6.tar.gz/nonebot_plugin_zssm-0.3.6/nonebot_plugin_zssm/__init__.py
@@ -0,0 +1,24 @@
+from nonebot import get_driver, require
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+
+from .browser import install_browser
+from .config import Config
+
+require("nonebot_plugin_alconna")
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot-plugin-zssm",
+    description="这是什么？问一下！用 ai 来解释群友发送的「未知」内容",
+    usage="对着任意你不懂的内容发送「zssm」即可",
+    homepage="https://github.com/djkcyl/nonebot-plugin-zssm",
+    type="application",
+    config=Config,
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
+    extra={"author": "djkcyl", "version": "0.3.6"},
+)
+
+
+driver = get_driver()
+driver.on_startup(install_browser)
+
+from . import handle  # noqa: E402, F401
