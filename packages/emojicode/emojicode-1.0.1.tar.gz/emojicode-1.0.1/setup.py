07a712ff6576a7f3629ee555c7efa7481620465e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:40 +0000

    Add package emojicode/1.0.1/emojicode-1.0.1.tar.gz

---
+++ packages/emojicode/emojicode-1.0.1.tar.gz/emojicode-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='emojicode',
+    version='1.0.1',
+    description='A fun emoji-based text encoder and decoder.',
+    author='Abbas',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
