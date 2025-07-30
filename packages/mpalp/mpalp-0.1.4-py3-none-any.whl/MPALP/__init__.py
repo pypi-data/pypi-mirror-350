4b4076761eaf7988405be4841cdeede8889962d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:35 +0000

    Add package mpalp/0.1.4/mpalp-0.1.4-py3-none-any.whl

---
+++ packages/mpalp/mpalp-0.1.4-py3-none-any.whl/MPALP/__init__.py
@@ -0,0 +1,5 @@
+from .accuracy.classificationutils import *
+from .lossfunction.lossfunction import *
+from .utils.timeutils import *
+from .trainer.train import *
+from .trainer.trainutils import *
