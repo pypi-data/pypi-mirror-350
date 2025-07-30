c1b07e99634794374c00f3fead75ea7c6b45926b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:39 +0000

    Add package jinja2-from-json-extension/1.0.1/jinja2_from_json_extension-1.0.1-py3-none-any.whl

---
+++ packages/jinja2-from-json-extension/jinja2_from_json_extension-1.0.1-py3-none-any.whl/jinja2_from_json_extension/__init__.py
@@ -0,0 +1,20 @@
+import json
+from jinja2.ext import Extension
+
+
+try:
+    from jinja2 import pass_eval_context as eval_context
+except ImportError:
+    from jinja2 import evalcontextfilter as eval_context
+
+
+@eval_context
+def from_json(eval_ctx, value):
+    return json.loads(value)
+
+
+class FromJsonExtension(Extension):
+
+    def __init__(self, environment):
+        super(FromJsonExtension, self).__init__(environment)
+        environment.filters['from_json'] = from_json
