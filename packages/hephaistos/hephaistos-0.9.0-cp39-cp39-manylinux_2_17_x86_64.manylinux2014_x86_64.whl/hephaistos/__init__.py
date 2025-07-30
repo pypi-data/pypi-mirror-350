f1b4603e28a8f6e52e6a1001e43227ab8e26b87e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:12 +0000

    Add package hephaistos/0.9.0/hephaistos-0.9.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/hephaistos/hephaistos-0.9.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/hephaistos/__init__.py
@@ -0,0 +1,26 @@
+from .pyhephaistos import *
+from .util import (
+    ArrayBuffer,
+    ArrayTensor,
+    StructureBuffer,
+    StructureTensor,
+)
+
+
+# make the Program.bindParams more pythonic
+def _bindParams(program: Program, *params, **namedparams) -> None:
+    """
+    Helper function to bind a list of params in a program.
+    """
+    for i, p in enumerate(params):
+        p.bindParameter(program, i)
+
+    names = [b.name for b in program.bindings]
+    for name, p in namedparams.items():
+        if name not in names:
+            continue
+        p.bindParameter(program, name)
+
+
+# register function in class
+Program.bindParams = _bindParams
