5d922cf50e710dcda5b61d8056f589651ca70e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:38 +0000

    Add package jypypkg/0.1.2/jypypkg-0.1.2.tar.gz

---
+++ packages/jypypkg/jypypkg-0.1.2.tar.gz/jypypkg-0.1.2/tests/test_jypypkg.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `jypypkg` package."""
+
+
+import unittest
+
+from jypypkg import jypypkg
+
+
+class TestJypypkg(unittest.TestCase):
+    """Tests for `jypypkg` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
