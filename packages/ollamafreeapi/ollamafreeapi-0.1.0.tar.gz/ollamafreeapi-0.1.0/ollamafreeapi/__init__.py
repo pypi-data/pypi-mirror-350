d7a17913c2f27c7b401564187acad497bcab2066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:20 +0000

    Add package ollamafreeapi/0.1.0/ollamafreeapi-0.1.0.tar.gz

---
+++ packages/ollamafreeapi/ollamafreeapi-0.1.0.tar.gz/ollamafreeapi-0.1.0/ollamafreeapi/__init__.py
@@ -0,0 +1,4 @@
+from .client import OllamaFreeAPI
+
+__all__ = ['OllamaFreeAPI']
+__version__ = '0.1.0'
---
+++ packages/ollamafreeapi/ollamafreeapi-0.1.0.tar.gz/ollamafreeapi-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ollamafreeapi",
+    version="0.1.0",
+    packages=find_packages(),
+    package_data={
+        'ollamafreeapi': ['ollama_json/*.json'],
+    },
+    install_requires=[
+        'ollama>=0.1.0',
+    ],
+    author="Mohammed Foud",
+    author_email="mfoud444",
+    description="A lightweight client for interacting with LLMs served via Ollama",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mfoud444/ollamafreeapi",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
