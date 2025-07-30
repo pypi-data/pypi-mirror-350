4c80270a1bc53cc4930d3f8c86bb50b5aa624916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:22 +0000

    Add package omniTask/0.7.1/omnitask-0.7.1-py3-none-any.whl

---
+++ packages/omniTask/omnitask-0.7.1-py3-none-any.whl/omniTask/__init__.py
@@ -0,0 +1,9 @@
+from .core.workflow import Workflow
+from .core.registry import TaskRegistry
+from .core.task import Task
+from .models.task_result import TaskResult
+from .models.task_group import TaskGroupConfig, TaskGroup
+from .utils.logging import setup_task_logging, TaskLogFormatter
+from .utils.workflow_checker import WorkflowChecker 
+
+__version__ = "0.7.1"
