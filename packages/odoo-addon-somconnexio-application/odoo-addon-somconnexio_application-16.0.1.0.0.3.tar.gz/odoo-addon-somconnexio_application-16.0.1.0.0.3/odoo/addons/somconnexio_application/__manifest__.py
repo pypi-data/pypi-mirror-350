6e0213743d072797e04ce31fedc62aef7584fa9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:41 +0000

    Add package odoo-addon-somconnexio-application/16.0.1.0.0.3/odoo-addon-somconnexio_application-16.0.1.0.0.3.tar.gz

---
+++ packages/odoo-addon-somconnexio-application/odoo-addon-somconnexio_application-16.0.1.0.0.3.tar.gz/odoo-addon-somconnexio_application-16.0.1.0.0.3/odoo/addons/somconnexio_application/__manifest__.py
@@ -0,0 +1,39 @@
+{
+    "version": "16.0.1.0.0",
+    "name": "SomConnexio application",
+    "summary": """
+        Som Connexio's application with customely developed odoo
+        addons as dependencies that fulfill the cooperative requirements.
+    """,
+    "depends": [
+        "base_rest_somconnexio",
+        "contract_api_somconnexio",
+        "contract_group_somconnexio",
+        "cooperator_api_somconnexio",
+        "cooperator_somconnexio",
+        "correos_somconnexio",
+        "crm_lead_api_somconnexio",
+        "delivery_somconnexio",
+        "hr_assistance_somconnexio",
+        "invoice_somconnexio",
+        "marginalized_groups_somconnexio",
+        "mass_mailing_somconnexio",
+        "oficinavirtual_somconnexio",
+        "opencell_somconnexio",
+        "otrs_somconnexio",
+        "res_partner_api_somconnexio",
+        "somconnexio",
+        "switchboard_somconnexio",
+    ],
+    "author": """
+        Som Connexió SCCL,
+        Coopdevs Treball SCCL,
+    """,
+    "category": "Cooperative Management",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "data": [],
+    "demo": [],
+    "application": True,
+    "installable": True,
+}
