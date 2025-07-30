fbe67004ae8096d382f2c5036dc887724c090e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:48:31 +0000

    Add package Python-EasyGraph/1.4.4/python_easygraph-1.4.4-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/Python-EasyGraph/python_easygraph-1.4.4-cp310-cp310-macosx_11_0_arm64.whl/easygraph/functions/graph_embedding/tests/test_deepwalk.py
@@ -0,0 +1,21 @@
+import unittest
+
+import easygraph as eg
+import numpy as np
+
+
+class Test_Deepwalk(unittest.TestCase):
+    def setUp(self):
+        self.ds = eg.datasets.get_graph_karateclub()
+        self.edges = [(1, 4), (2, 4)]
+        self.test_graphs = []
+        self.test_graphs.append(eg.classes.DiGraph(self.edges))
+        self.shs = eg.common_greedy(self.ds, int(len(self.ds.nodes) / 3))
+
+    def test_deepwalk(self):
+        for i in self.test_graphs:
+            print(eg.deepwalk(i))
+
+
+if __name__ == "__main__":
+    unittest.main()
