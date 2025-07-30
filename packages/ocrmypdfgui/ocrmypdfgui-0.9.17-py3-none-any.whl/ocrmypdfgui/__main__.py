fafb9118d36cbdd77d7e1280b2b352aeb752d6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:35 +0000

    Add package ocrmypdfgui/0.9.17/ocrmypdfgui-0.9.17-py3-none-any.whl

---
+++ packages/ocrmypdfgui/ocrmypdfgui-0.9.17-py3-none-any.whl/ocrmypdfgui/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+from ocrmypdfgui.gui import run
+#from gui import run
+
+def main(args=None):
+	if args is None:
+		args = sys.argv[1:]
+		run()
+
+if __name__ == '__main__':
+	sys.exit(main())
