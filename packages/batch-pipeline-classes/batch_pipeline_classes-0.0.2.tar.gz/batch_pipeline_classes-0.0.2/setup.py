a0d8dfe565dabfdf0604c2d63134b822c9ee4176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:51 +0000

    Add package batch-pipeline-classes/0.0.2/batch_pipeline_classes-0.0.2.tar.gz

---
+++ packages/batch-pipeline-classes/batch_pipeline_classes-0.0.2.tar.gz/batch_pipeline_classes-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='batch-pipeline-classes',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[
+        "google-cloud-bigquery"
+    ],
+)
