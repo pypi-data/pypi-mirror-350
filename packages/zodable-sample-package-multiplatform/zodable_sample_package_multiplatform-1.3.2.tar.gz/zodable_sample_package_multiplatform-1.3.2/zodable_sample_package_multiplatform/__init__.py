50149dcc17d533757c77ef945a3599dfce8648bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:59 +0000

    Add package zodable-sample-package-multiplatform/1.3.2/zodable_sample_package_multiplatform-1.3.2.tar.gz

---
+++ packages/zodable-sample-package-multiplatform/zodable_sample_package_multiplatform-1.3.2.tar.gz/zodable_sample_package_multiplatform-1.3.2/zodable_sample_package_multiplatform/__init__.py
@@ -0,0 +1,2 @@
+from zodable_sample_package_multiplatform.digital.guimauve.example.MultiplatformType import MultiplatformType
+from zodable_sample_package_multiplatform.digital.guimauve.example.MultiplatformUser import MultiplatformUser
