72368593888ac4a9bf8f395ac3eb07d14378a677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:06 +0000

    Add package grpcless/0.0.1/grpcless-0.0.1-py3-none-any.whl

---
+++ packages/grpcless/grpcless-0.0.1-py3-none-any.whl/grpcless/middleware/errortrace.py
@@ -0,0 +1,33 @@
+from importlib import metadata
+from typing import Callable, Coroutine, Any
+import traceback
+
+from .. import log
+from grpclib.exceptions import GRPCError, StreamTerminatedError
+from grpclib.const import Status
+
+
+def ErrorTraceMiddleware(func: Callable[[Any], Coroutine]) -> Callable:
+    async def warpper(request) -> Coroutine:
+        try:
+            ret = await func(request)
+        except GRPCError as e:
+            raise e
+        except StreamTerminatedError as e:
+            raise e
+        except Exception as e:
+            error_str = traceback.format_exc().strip("\n")
+            for i in error_str.split("\n"):
+                log.log_error("    "+i)
+
+            raise GRPCError(
+                status=Status.INTERNAL,
+                message="Server Error: "+str(e),
+                details={
+                    "fullname": repr(e),
+                    "middleware": "grpce.middleware.ErrorTraceMiddleware",
+                    "server": "grpcE"
+                }
+            )
+        return ret
+    return warpper
