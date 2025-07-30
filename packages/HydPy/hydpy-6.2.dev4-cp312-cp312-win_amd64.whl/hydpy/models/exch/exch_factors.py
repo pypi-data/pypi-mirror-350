48d3e873e1696280d04fb823a85ad24f1ca0f910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:33 +0000

    Add package HydPy/6.2.dev4/hydpy-6.2.dev4-cp312-cp312-win_amd64.whl

---
+++ packages/HydPy/hydpy-6.2.dev4-cp312-cp312-win_amd64.whl/hydpy/models/exch/exch_factors.py
@@ -0,0 +1,26 @@
+# pylint: disable=missing-module-docstring
+# import...
+# ...from HydPy
+from hydpy.core import sequencetools
+from hydpy.core import variabletools
+
+
+class WaterLevels(variabletools.MixinFixedShape, sequencetools.FactorSequence):
+    """The water level at two locations [m].
+
+    After each simulation step, the value of |exch_factors.WaterLevels| corresponds to
+    the value of the |LoggedWaterLevels| of the previous simulation step.
+    """
+
+    NDIM = 1
+    SHAPE = (2,)
+
+
+class DeltaWaterLevel(sequencetools.FactorSequence):
+    """Effective difference of the two water levels [m].
+
+    After each simulation step, the value of |DeltaWaterLevel| corresponds to the value
+    of the |LoggedWaterLevels| of the previous simulation step.
+    """
+
+    NDIM = 0
