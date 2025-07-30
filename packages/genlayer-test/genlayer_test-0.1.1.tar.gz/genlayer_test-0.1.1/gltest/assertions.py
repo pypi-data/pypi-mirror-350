94c6243fead69d2656aeb4cad76a3749063b9e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:08 +0000

    Add package genlayer-test/0.1.1/genlayer_test-0.1.1.tar.gz

---
+++ packages/genlayer-test/genlayer_test-0.1.1.tar.gz/genlayer_test-0.1.1/gltest/assertions.py
@@ -0,0 +1,16 @@
+from genlayer_py.types import GenLayerTransaction
+
+
+def tx_execution_succeeded(result: GenLayerTransaction) -> bool:
+    if "consensus_data" not in result:
+        return False
+    if "leader_receipt" not in result["consensus_data"]:
+        return False
+    if "execution_result" not in result["consensus_data"]["leader_receipt"]:
+        return False
+    execution_result = result["consensus_data"]["leader_receipt"]["execution_result"]
+    return execution_result == "SUCCESS"
+
+
+def tx_execution_failed(result: GenLayerTransaction) -> bool:
+    return not tx_execution_succeeded(result)
