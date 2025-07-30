f06ab62310f809a4b76b43603778cde312c6dd3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:39 +0000

    Add package zenml-nightly/0.82.1.dev20250526/zenml_nightly-0.82.1.dev20250526-py3-none-any.whl

---
+++ packages/zenml-nightly/zenml_nightly-0.82.1.dev20250526-py3-none-any.whl/zenml/zen_server/dashboard/assets/ProBadge-Cp4hb1YT.js
@@ -0,0 +1,1 @@
+import{j as e}from"./@radix-C7hRs6Kx.js";import{R as o}from"./index-D2iSHVZq.js";function a(){return e.jsx(o,{color:"yellow",rounded:!1,className:"rounded-md",children:e.jsx("span",{className:"font-semibold uppercase",children:"Pro"})})}export{a as P};
