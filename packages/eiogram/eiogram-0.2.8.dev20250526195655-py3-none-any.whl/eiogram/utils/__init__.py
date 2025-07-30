a467637191f45a44e39ecf32d84dd2907e32947f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:09:09 +0000

    Add package eiogram/0.2.8.dev20250526195655/eiogram-0.2.8.dev20250526195655-py3-none-any.whl

---
+++ packages/eiogram/eiogram-0.2.8.dev20250526195655-py3-none-any.whl/eiogram/utils/__init__.py
@@ -0,0 +1,22 @@
+from .callback_data import CallbackData
+from .exceptions import (
+    NetworkError,
+    TimeoutError,
+    TelegramError,
+    RateLimitError,
+    InvalidTokenError,
+    UnauthorizedError,
+)
+from .inline_builder import InlineKeyboardButton, InlineKeyboardBuilder
+
+__all__ = [
+    "NetworkError",
+    "TimeoutError",
+    "TelegramError",
+    "RateLimitError",
+    "InvalidTokenError",
+    "UnauthorizedError",
+    "InlineKeyboardButton",
+    "InlineKeyboardBuilder",
+    "CallbackData",
+]
