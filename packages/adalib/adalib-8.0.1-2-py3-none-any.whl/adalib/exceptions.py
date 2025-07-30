f8e36f75e0aa44386a3ef3a87ec12172b3f22901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:11 +0000

    Add package adalib/8.0.1/adalib-8.0.1-2-py3-none-any.whl

---
+++ packages/adalib/adalib-8.0.1-2-py3-none-any.whl/adalib/exceptions.py
@@ -0,0 +1,13 @@
+"""General module for custom exceptions.
+"""
+
+
+class QueryLimitReached(ValueError):
+    """Exception to signal that the limit of queries in Superset has been
+    reached.
+
+    :param ValueError: The ValueError of the exception
+    :type ValueError: ValueError
+    """
+
+    pass
