71822dd8696073bbf36954f850f155faf61a24fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:21 +0000

    Add package arkhelm-tml/0.0.2/arkhelm_tml-0.0.2.tar.gz

---
+++ packages/arkhelm-tml/arkhelm_tml-0.0.2.tar.gz/arkhelm_tml-0.0.2/arkhelm_tml/__init__.py
@@ -0,0 +1,7 @@
+from .arkhelm_tml import add_title, add_orb, add_curse, add_blessing, add_aura, add_sword, add_magicsword, add_shield, add_knuckles, add_bow, add_armor, add_monster, add_recipe, add_item, mod_passcode
+
+__all__ = [
+    "add_title", "add_orb", "add_curse", "add_blessing", "add_aura", "add_sword", 
+    "add_magicsword", "add_shield", "add_knuckles", "add_bow", "add_armor", "add_monster", 
+    "add_recipe", "add_item", "mod_passcode"
+]
---
+++ packages/arkhelm-tml/arkhelm_tml-0.0.2.tar.gz/arkhelm_tml-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="arkhelm-tml",
+    version="0.0.2",
+    author="Taireru LLC",
+    author_email="tairerullc@gmail.com",
+    description="Arkhelm Online Modding.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TaireruLLC/arkhelm_tml",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    license="MIT",
+)
