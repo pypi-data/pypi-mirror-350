6d42998e0d48b167fd0061c693e778e6aa179a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:53 +0000

    Add package MLTuneX/0.1.2/mltunex-0.1.2.tar.gz

---
+++ packages/MLTuneX/mltunex-0.1.2.tar.gz/mltunex-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "MLTuneX",
+    version = "0.1.2",
+    author = "Ayush Nashine",
+    author_email = "ayush.nashine@gmail.com",
+    description = "A package for machine learning tuning and optimization.",
+    long_description = open("README.md").read(),
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/yourusername/MLTuneX",
+    packages = find_packages(where='src'),
+    package_dir = {'': 'src'},
+    install_requires = [
+        "scikit-learn",
+        "pandas",
+        "numpy",
+        "langchain",
+        "openai",
+        "langchain-openai",
+        "langchain-community",
+        "langchain-core",
+        "optuna",
+        "python-dotenv"
+    ],
+    python_requires='>=3.8',
+)
