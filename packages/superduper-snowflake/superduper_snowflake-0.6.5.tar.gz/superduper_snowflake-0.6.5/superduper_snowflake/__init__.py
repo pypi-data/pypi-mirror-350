e0c18ea40c1daca2f5b22d16b8a8282c0b154811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:34 +0000

    Add package superduper-snowflake/0.6.5/superduper_snowflake-0.6.5.tar.gz

---
+++ packages/superduper-snowflake/superduper_snowflake-0.6.5.tar.gz/superduper_snowflake-0.6.5/superduper_snowflake/__init__.py
@@ -0,0 +1,11 @@
+from .data_backend import SnowflakeDataBackend as DataBackend
+from .secrets import check_secret_updates
+from .vector_search import SnowflakeVectorSearcher as VectorSearcher
+
+__version__ = "0.6.5"
+
+__all__ = [
+    "VectorSearcher",
+    "DataBackend",
+    "check_secret_updates",
+]
