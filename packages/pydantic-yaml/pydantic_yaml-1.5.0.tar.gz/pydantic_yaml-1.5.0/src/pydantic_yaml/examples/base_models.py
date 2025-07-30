2b97ebf9efc6bf1c1258cfdad9c97edd34dc9688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:12 +0000

    Add package pydantic-yaml/1.5.0/pydantic_yaml-1.5.0.tar.gz

---
+++ packages/pydantic-yaml/pydantic_yaml-1.5.0.tar.gz/pydantic_yaml-1.5.0/src/pydantic_yaml/examples/base_models.py
@@ -0,0 +1,52 @@
+"""Models used for testing, just normal `pydantic.BaseModel` objects."""
+
+__all__ = [
+    "Empty",
+    "A",
+    "B",
+    "Recursive",
+    "SecretTstModel",
+    "SecretTstModelDumpable",
+    "MyStrEnum",
+    "MyIntEnum",
+    "HasEnums",
+    "UsesRefs",
+    "CustomRootListStr",
+    "CustomRootListObj",
+    "root",
+]
+
+from pathlib import Path
+
+import pydantic
+
+from .common import MyIntEnum, MyStrEnum
+
+root = Path(__file__).resolve().parent / "base_models"
+
+if pydantic.version.VERSION < "2":
+    from .v1.base_models import (
+        A,
+        B,
+        CustomRootListObj,
+        CustomRootListStr,
+        Empty,
+        HasEnums,
+        Recursive,
+        SecretTstModel,
+        SecretTstModelDumpable,
+        UsesRefs,
+    )
+else:
+    from .v2.base_models import (  # type: ignore
+        A,
+        B,
+        CustomRootListObj,
+        CustomRootListStr,
+        Empty,
+        HasEnums,
+        Recursive,
+        SecretTstModel,
+        SecretTstModelDumpable,
+        UsesRefs,
+    )
