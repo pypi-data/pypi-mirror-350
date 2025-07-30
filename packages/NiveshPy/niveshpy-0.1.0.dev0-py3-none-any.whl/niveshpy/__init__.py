e2973a424cdd316c5814306cf2e75d7a55a892a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:59 +0000

    Add package NiveshPy/0.1.0.dev0/niveshpy-0.1.0.dev0-py3-none-any.whl

---
+++ packages/NiveshPy/niveshpy-0.1.0.dev0-py3-none-any.whl/niveshpy/__init__.py
@@ -0,0 +1,8 @@
+"""A simple python library for all your investment needs."""
+
+from niveshpy.main import Nivesh
+from importlib.metadata import version
+
+__all__ = ["Nivesh"]
+
+__version__ = version(__name__)
