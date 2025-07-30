4b275e5ab4addc91d6e5d9a4acbb6c090a206363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:05:56 +0000

    Add package tigergraphx/0.2.8/tigergraphx-0.2.8-py3-none-any.whl

---
+++ packages/tigergraphx/tigergraphx-0.2.8-py3-none-any.whl/tigergraphx/core/tigergraph_api/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2025 TigerGraph Inc.
+# Licensed under the Apache License, Version 2.0.
+# See the LICENSE file or https://www.apache.org/licenses/LICENSE-2.0
+#
+# Permission is granted to use, copy, modify, and distribute this software
+# under the License. The software is provided "AS IS", without warranty.
+
+from .tigergraph_api import TigerGraphAPI
+from .endpoint_handler import EndpointRegistry
+from .api import (
+    TigerGraphAPIError,
+    AdminAPI,
+    GSQLAPI,
+    SchemaAPI,
+    QueryAPI,
+)
+
+__all__ = [
+    "EndpointRegistry",
+    "TigerGraphAPI",
+    "TigerGraphAPIError",
+    "AdminAPI",
+    "GSQLAPI",
+    "SchemaAPI",
+    "QueryAPI",
+]
