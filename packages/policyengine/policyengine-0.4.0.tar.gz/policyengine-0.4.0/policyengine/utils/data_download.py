ead703730d5a26f2269630d2d805d49cc729d552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:28 +0000

    Add package policyengine/0.4.0/policyengine-0.4.0.tar.gz

---
+++ packages/policyengine/policyengine-0.4.0.tar.gz/policyengine-0.4.0/policyengine/utils/data_download.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import logging
+import os
+from policyengine.utils.google_cloud_bucket import download_file_from_gcs
+from pydantic import BaseModel
+import json
+from typing import Tuple, Optional
+
+
+def download(
+    filepath: str,
+    gcs_bucket: str,
+    version: Optional[str] = None,
+) -> str:
+    logging.info("Using Google Cloud Storage for download.")
+    download_file_from_gcs(
+        bucket_name=gcs_bucket,
+        file_name=filepath,
+        destination_path=filepath,
+        version=version,
+    )
+    return filepath
