b1228c48f39cd19a97411c69483396c19e6e3121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:36 +0000

    Add package monero-usd-price/1.4.0/monero_usd_price-1.4.0.tar.gz

---
+++ packages/monero-usd-price/monero_usd_price-1.4.0.tar.gz/monero_usd_price-1.4.0/README.md
@@ -0,0 +1,56 @@
+# Monero-USD-Price
+![Version 1.2.0](https://img.shields.io/badge/Version-1.2.0-orange.svg)
+![Python 3.8+](https://img.shields.io/badge/Python-3.8+-3776ab.svg)
+
+Monero-USD-Price is an easy way to get the current median or average price of Monero in USD.
+
+Median is recommended over average, because it is less sensitive to outliers.
+
+
+# How To Use:
+* Install this package: `pip install monero_usd_price`
+* Import it at the top of your project: `import monero_usd_price`
+* To get the median XMR/USD price: `monero_usd_price.median_price()`
+* To get the average XMR/USD price: `monero_usd_price.average_price()`
+* To get the XMR/USD price from any of the supported exchanges: `monero_usd_price.coingecko()`, `monero_usd_price.coinmarketcap()`, `monero_usd_price.localmonero()`, etc.
+* To get the amount of Monero that is equivalent to a given amount of USD: `calculate_monero_from_usd(usd_amount)`
+* To get the amount of USD that is equivalent to a given amount of Monero: `calculate_usd_from_monero(monero_amount)`
+* To convert Monero to Atomic Units: `calculate_atomic_units_from_monero(monero_amount)`
+* To convert Atomic Units to Monero: `calculate_monero_from_atomic_units(atomic_units)`
+
+Additonally, you can add the argument `print_price_to_console=True` to any of these if you would like to see output.
+
+For example: `monero_usd_price.median_price(print_price_to_console=True)`, or `monero_usd_price.localmonero(print_price_to_console=True)`
+
+
+# Donate
+If you use this, send me some XMR
+- XMR: `4At3X5rvVypTofgmueN9s9QtrzdRe5BueFrskAZi17BoYbhzysozzoMFB6zWnTKdGC6AxEAbEE5czFR3hbEEJbsm4hCeX2S`
+- BTC: `1ACCQMwHYUkA1v449DvQ9t6dm3yv1enN87`
+- Cash App: `$LukeProfits`
+<a href="https://www.buymeacoffee.com/lukeprofits" target="_blank">
+  <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;">
+</a>
+
+
+# Supported Exchanges:
+* CoinGecko
+* Binance
+* CoinMarketCap
+* CryptoCompare
+* Kraken
+* Bitfinex
+* LocalMonero
+* Poloniex
+* Huobi
+* KuCoin
+* HitBTC
+
+
+## Requirements
+* [Python 3.8](https://www.python.org/downloads/) or above
+* [Requests](https://github.com/psf/requests)
+
+
+## License
+[MIT](https://github.com/Equim-chan/vanity-monero/blob/master/LICENSE)
---
+++ packages/monero-usd-price/monero_usd_price-1.4.0.tar.gz/monero_usd_price-1.4.0/monero_usd_price/monero_usd_price.py
@@ -0,0 +1,191 @@
+from decimal import Decimal
+import requests
+
+# FUNCTIONS ############################################################################################################    
+def price_request(url, func, pms={}):
+    try:
+        response = requests.get(url, params=pms)
+        if response.status_code == 200:
+            usd_rate = func(response.json())
+            return Decimal(usd_rate).quantize(Decimal('0.00'))
+        else:
+            return False
+
+    except Exception:
+        return False
+
+def coingecko():
+    def json_fetch(response):
+        return response['monero']['usd']
+    return price_request('https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd', json_fetch)
+
+def binance():
+    def json_fetch(response):
+        return response['price']
+    return price_request('https://api.binance.com/api/v3/ticker/price', json_fetch, {'symbol': 'XMRUSDT'})
+
+#Convert to actually using the API?
+# def coinmarketcap(print_price_to_console=False):
+#     try:
+#         response = requests.get('https://coinmarketcap.com/currencies/monero/')
+#         if response.status_code == 200:
+#             xmr_usd_rate = Decimal(str(response.content).split(
+#                 '"currency":"XMR","currentExchangeRate":{"@type":"UnitPriceSpecification","price":')[1].split(',"')[0]).quantize(Decimal('0.00'))
+#             print(f'CoinMarketCap: {xmr_usd_rate}') if print_price_to_console else None
+#             return xmr_usd_rate
+#         else:
+#             return None
+#     except:
+#         pass
+
+
+def cryptocompare():
+    def json_fetch(response):
+        return response['USD']
+    return price_request('https://min-api.cryptocompare.com/data/price', json_fetch, {'fsym': 'XMR', 'tsyms': 'USD'})
+
+def kraken():
+    def json_fetch(response):
+        return response['result']['XXMRZUSD']['c'][0]
+    return price_request('https://api.kraken.com/0/public/Ticker', json_fetch, {'pair': 'XMRUSD'})
+
+def bitfinex():
+    def json_fetch(response):
+        return response[6]
+    return price_request('https://api-pub.bitfinex.com/v2/ticker/tXMRUSD', json_fetch, {})
+
+def poloniex():
+    def json_fetch(response):
+        return response.get('USDT_XMR', {}).get('last')
+    return price_request('https://poloniex.com/public?command=returnTicker', json_fetch, {})
+
+def huobi():
+    def json_fetch(response):
+        return response.get('tick', {}).get('close')
+    return price_request('https://api.huobi.pro/market/detail/merged?symbol=xmrusdt', json_fetch, {})
+
+def kucoin():
+    def json_fetch(response):
+        return response.get('data', {}).get('price')
+    return price_request('https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=XMR-USDT', json_fetch, {})
+
+def hitbtc():
+    def json_fetch(response):
+        return response.get('last')
+    return price_request('https://api.hitbtc.com/api/3/public/ticker/XMRUSDT', json_fetch, {})
+
+def get_monero_price_from_all_exchanges_not_threaded():
+    prices = []
+
+    exchanges = [coingecko, binance, cryptocompare, kraken, bitfinex, poloniex, huobi, kucoin, hitbtc]
+    for exchange in exchanges:
+        price = exchange()
+        if price:
+            prices.append(Decimal(price))
+    if len(prices) >= 1:  # Make sure that getting at least 1 price was successful
+        sorted_prices = sorted(prices)  # low to high
+        return sorted_prices
+    else:
+        return None
+
+
+def get_monero_price_from_all_exchanges():
+    from concurrent.futures import ThreadPoolExecutor, as_completed
+    prices = []
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(func) for func in [coingecko, binance, cryptocompare, kraken, bitfinex, poloniex, huobi, kucoin, hitbtc]]
+
+        for future in as_completed(futures):
+            price = future.result()
+            if price:
+                prices.append(Decimal(price))
+
+    if len(prices) >= 1:  # Make sure that getting at least 1 price was successful
+        sorted_prices = sorted(prices)  # low to high
+        return sorted_prices
+    else:
+        return None
+
+
+def average_price():
+    # RECOMMENDED TO USE MEDIAN INSTEAD OF AVERAGE
+    sorted_prices = get_monero_price_from_all_exchanges()
+    if sorted_prices:
+        # Get average price
+        avg_price = sum(sorted_prices) / len(sorted_prices)
+        # round to 2 decimals
+        avg_price = round(avg_price, 2)
+        return avg_price
+    else:
+        return None
+
+
+def median_price():
+    sorted_prices = get_monero_price_from_all_exchanges()
+    if sorted_prices:
+        # Get median price (less sensitive to outliers than average)
+        if len(sorted_prices) % 2 == 1:
+            # If the length of the list is odd, return the middle element
+            median_price = sorted_prices[len(sorted_prices) // 2]
+        else:
+            # If the length of the list is even, return the average of the two middle elements
+            median_price = (sorted_prices[len(sorted_prices) // 2 - 1] + sorted_prices[len(sorted_prices) // 2]) / 2
+
+        median_price = round(median_price, 2)  # round to 2 decimals
+        return median_price
+    else:
+        return None
+
+
+def average_price_not_threaded():
+    # RECOMMENDED TO USE MEDIAN INSTEAD OF AVERAGE
+    sorted_prices = get_monero_price_from_all_exchanges_not_threaded()
+    if sorted_prices:
+        # Get average price
+        avg_price = sum(sorted_prices) / len(sorted_prices)
+        # round to 2 decimals
+        avg_price = round(avg_price, 2)
+        return avg_price
+    else:
+        return None
+
+
+def median_price_not_threaded():
+    sorted_prices = get_monero_price_from_all_exchanges_not_threaded()
+    if sorted_prices:
+        # Get median price (less sensitive to outliers than average)
+        if len(sorted_prices) % 2 == 1:
+            # If the length of the list is odd, return the middle element
+            median_price = sorted_prices[len(sorted_prices) // 2]
+        else:
+            # If the length of the list is even, return the average of the two middle elements
+            median_price = (sorted_prices[len(sorted_prices) // 2 - 1] + sorted_prices[len(sorted_prices) // 2]) / 2
+
+        median_price = round(median_price, 2)  # round to 2 decimals
+
+        return median_price
+    else:
+        return None
+
+
+def calculate_monero_from_usd(usd_amount, ):
+    monero_price = median_price()
+    monero_amount = round(Decimal(usd_amount) / Decimal(monero_price), 12)
+    return monero_amount
+
+
+def calculate_usd_from_monero(monero_amount, ):
+    monero_price = median_price()
+    usd_amount = round(Decimal(monero_amount) * Decimal(monero_price), 2)
+    return usd_amount
+
+
+def calculate_atomic_units_from_monero(monero_amount):
+    atomic_units = int(monero_amount * Decimal(1e12))
+    return atomic_units
+
+
+def calculate_monero_from_atomic_units(atomic_units):
+    monero = int(atomic_units) / Decimal(1e12)
+    return monero
