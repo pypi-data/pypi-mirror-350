43710ba086a352565e8f3bea0fb6775d5052616c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:36:07 +0000

    Add package mtrequests/1.8.0/mtrequests-1.8.0.tar.gz

---
+++ packages/mtrequests/mtrequests-1.8.0.tar.gz/mtrequests-1.8.0/mtrequests/__init__.py
@@ -0,0 +1,9 @@
+from .request import Request
+from .prepared_request import PreparedRequest
+from .session import Session
+from .pending_request import PendingRequest
+from .pending_response import PendingResponse
+from .pool import Pool
+from .pending_pool import PendingPool
+from .request_hook import RequestHook
+from .utils import get, options, head, post, put, patch, delete, make_request
