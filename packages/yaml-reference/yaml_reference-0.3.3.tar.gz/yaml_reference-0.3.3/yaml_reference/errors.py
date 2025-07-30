35635711f74c8062c4df926a2c3f3264d6567a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:09:07 +0000

    Add package yaml-reference/0.3.3/yaml_reference-0.3.3.tar.gz

---
+++ packages/yaml-reference/yaml_reference-0.3.3.tar.gz/yaml_reference-0.3.3/yaml_reference/errors.py
@@ -0,0 +1,10 @@
+class ConstructorException(Exception):
+    """Exception raised when a constructor fails to construct an object."""
+
+    pass
+
+
+class RepresenterException(Exception):
+    """Exception raised when a representer fails to represent an object."""
+
+    pass
