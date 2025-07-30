4fb76e29d2e25b46d117dd9ced049dd240599675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:06 +0000

    Add package ob-metaflow-stubs/6.0.3.172/ob-metaflow-stubs-6.0.3.172.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.172.tar.gz/ob-metaflow-stubs-6.0.3.172/metaflow-stubs/plugins/aws/secrets_manager/__init__.pyi
@@ -0,0 +1,11 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.14.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-05-27T03:43:23.578487                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import aws_secrets_manager_secrets_provider as aws_secrets_manager_secrets_provider
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.172.tar.gz/ob-metaflow-stubs-6.0.3.172/metaflow-stubs/plugins/cards/card_modules/renderer_tools.pyi
@@ -0,0 +1,18 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.14.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-05-27T03:43:23.565105                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from .basic import SerializationErrorComponent as SerializationErrorComponent
+
+def render_safely(func):
+    """
+    This is a decorator that can be added to any `MetaflowCardComponent.render`
+    The goal is to render subcomponents safely and ensure that they are JSON serializable.
+    """
+    ...
+
