7c5343c19104399b06a65f301d0687252d646d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:43 +0000

    Add package falyx/0.1.36/falyx-0.1.36-py3-none-any.whl

---
+++ packages/falyx/falyx-0.1.36-py3-none-any.whl/falyx/__init__.py
@@ -0,0 +1,29 @@
+"""
+Falyx CLI Framework
+
+Copyright (c) 2025 rtj.dev LLC.
+Licensed under the MIT License. See LICENSE file for details.
+"""
+
+import logging
+
+from .action.action import Action, ActionGroup, ChainedAction, ProcessAction
+from .command import Command
+from .context import ExecutionContext, SharedContext
+from .execution_registry import ExecutionRegistry
+from .falyx import Falyx
+
+logger = logging.getLogger("falyx")
+
+__all__ = [
+    "Action",
+    "ChainedAction",
+    "ActionGroup",
+    "ProcessAction",
+    "Falyx",
+    "Command",
+    "ExecutionContext",
+    "SharedContext",
+    "ExecutionRegistry",
+    "HookType",
+]
