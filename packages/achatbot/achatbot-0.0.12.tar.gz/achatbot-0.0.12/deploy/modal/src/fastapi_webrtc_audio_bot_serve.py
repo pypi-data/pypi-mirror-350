373e5f181cab148ffcce2bee794fd9af630216b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:06 +0000

    Add package achatbot/0.0.12/achatbot-0.0.12.tar.gz

---
+++ packages/achatbot/achatbot-0.0.12.tar.gz/achatbot-0.0.12/deploy/modal/src/fastapi_webrtc_audio_bot_serve.py
@@ -0,0 +1,94 @@
+import modal
+import os
+
+
+class ContainerRuntimeConfig:
+    images = {
+        "default": (
+            modal.Image.debian_slim(python_version="3.11")
+            .apt_install("git", "git-lfs", "ffmpeg")
+            .pip_install(
+                [
+                    "achatbot["
+                    "fastapi_bot_server,"
+                    "livekit,livekit-api,daily,agora,"
+                    "silero_vad_analyzer,daily_langchain_rag_bot,"
+                    "sense_voice_asr,deepgram_asr_processor,"
+                    "openai_llm_processor,google_llm_processor,litellm_processor,"
+                    "tts_edge,"
+                    "deep_translator,together_ai,"
+                    "queue"
+                    "]~=0.0.8.3",
+                    "huggingface_hub[hf_transfer]==0.24.7",
+                    "wget",
+                ],
+                extra_index_url="https://pypi.org/simple/",
+            )
+            .env(
+                {
+                    "HF_HUB_ENABLE_HF_TRANSFER": "1",
+                    "ACHATBOT_PKG": "1",
+                    "LOG_LEVEL": os.getenv("LOG_LEVEL", "info"),
+                    "IMAGE_NAME": os.getenv("IMAGE_NAME", "default"),
+                    # asr module engine TAG, default whisper_timestamped_asr
+                    "ASR_TAG": "sense_voice_asr",
+                    "ASR_LANG": "zn",
+                    "ASR_MODEL_NAME_OR_PATH": "/root/.achatbot/models/FunAudioLLM/SenseVoiceSmall",
+                    # llm processor model, default:google gemini_flash_latest
+                    "GOOGLE_LLM_MODEL": "gemini-1.5-flash-latest",
+                    # tts module engine TAG,default tts_edge
+                    "TTS_TAG": "tts_edge",
+                }
+            )
+        ),
+    }
+
+    @staticmethod
+    def get_img(image_name: str = None):
+        image_name = image_name or os.getenv("IMAGE_NAME", "default")
+        if image_name not in ContainerRuntimeConfig.images:
+            raise Exception(f"image name {image_name} not found")
+        print(f"use image:{image_name}")
+        return ContainerRuntimeConfig.images[image_name]
+
+    @staticmethod
+    def get_app_name(image_name: str = None):
+        image_name = image_name or os.getenv("IMAGE_NAME", "default")
+        if image_name != "default":
+            return f"{image_name}_fastapi_webrtc_audio_bot"
+        return "fastapi_webrtc_audio_bot"
+
+
+# ----------------------- app -------------------------------
+app = modal.App(ContainerRuntimeConfig.get_app_name())
+
+ASSETS_DIR = "/root/.achatbot/assets"
+assets_dir = modal.Volume.from_name("assets", create_if_missing=True)
+HF_MODEL_DIR = "/root/.achatbot/models"
+hf_model_vol = modal.Volume.from_name("models", create_if_missing=True)
+
+
+# 128 MiB of memory and 0.125 CPU cores by default container runtime
+@app.cls(
+    image=ContainerRuntimeConfig.get_img(),
+    secrets=[modal.Secret.from_name("achatbot")],
+    volumes={ASSETS_DIR: assets_dir, HF_MODEL_DIR: hf_model_vol},
+    cpu=2.0,
+    scaledown_window=300,
+    timeout=600,
+    allow_concurrent_inputs=100,
+)
+class Srv:
+    @modal.enter()
+    def enter(self):
+        print("start enter")
+
+    @modal.asgi_app()
+    def app(self):
+        from achatbot.cmd.http.server.fastapi_daily_bot_serve import app
+
+        return app
+
+
+if __name__ == "__main__":
+    pass
