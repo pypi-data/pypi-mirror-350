6c7231d92a8635a5175d0b9289e141064a63affc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:01 +0000

    Add package test-versioning-repo-25-d/1.2.0/test_versioning_repo_25_d-1.2.0-py3-none-any.whl

---
+++ packages/test-versioning-repo-25-d/test_versioning_repo_25_d-1.2.0-py3-none-any.whl/test_versioning_repo_25_d/example.py
@@ -0,0 +1,18 @@
+# src/test_versioning_repo_25_d/example.py
+
+"""Example."""
+
+
+async def iterations(num_iters: int) -> int:
+  """Sum one to zero num_iters times.
+
+  Args:
+      num_iters (int): Number of iterations.
+
+  Returns:
+      int: num_iters * 1
+  """
+  result: int = 0
+  for _ in range(num_iters):
+    result += 1
+  return result
