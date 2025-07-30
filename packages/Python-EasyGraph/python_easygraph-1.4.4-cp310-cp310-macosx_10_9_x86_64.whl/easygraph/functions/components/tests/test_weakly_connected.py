e7591893b1b5aa0e1c0ecd91bfcf7f311b5bff62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:48:30 +0000

    Add package Python-EasyGraph/1.4.4/python_easygraph-1.4.4-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/Python-EasyGraph/python_easygraph-1.4.4-cp310-cp310-macosx_10_9_x86_64.whl/easygraph/functions/components/tests/test_weakly_connected.py
@@ -0,0 +1,21 @@
+import unittest
+
+import easygraph as eg
+
+
+class Test_weakly_connected(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_number_weakly_connected_components(self):
+        pass
+
+    def test_weakly_connected_components(self):
+        pass
+
+    def test_is_weakly_connected(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
