e48fe7a95e01bcf09433846391720e869de4682f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:24 +0000

    Add package hfsynpy/0.1.0/hfsynpy-0.1.0-py3-none-any.whl

---
+++ packages/hfsynpy/hfsynpy-0.1.0-py3-none-any.whl/hfsynpy/__init__.py
@@ -0,0 +1,6 @@
+# This file marks the directory as a Python package.
+__version__ = "0.1.0"
+__author__ = "Dominik Mair"
+__description__ = "Synthesis and analysis tools for high frequency components (microstrip transmission lines)."
+
+from .hfsynpy import Microstrip
