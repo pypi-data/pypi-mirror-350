7fd0b176eb5855ce711b3d3dd7f7a079c27eeb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:20 +0000

    Add package aiokem/0.5.12/aiokem-0.5.12.tar.gz

---
+++ packages/aiokem/aiokem-0.5.12.tar.gz/aiokem-0.5.12/src/aiokem/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.5.12"
+
+from .exceptions import (
+    AioKemError,
+    AuthenticationCredentialsError,
+    AuthenticationError,
+    CommunicationError,
+    ServerError,
+)
+from .main import AioKem
+
+__all__ = (
+    "AioKem",
+    "AioKemError",
+    "AuthenticationCredentialsError",
+    "AuthenticationError",
+    "CommunicationError",
+    "ServerError",
+)
