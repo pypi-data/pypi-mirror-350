5dd604dd65468b44e5459a14008847fee8e7f78f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:15 +0000

    Add package open-api-framework/0.10.1/open_api_framework-0.10.1.tar.gz

---
+++ packages/open-api-framework/open_api_framework-0.10.1.tar.gz/open_api_framework-0.10.1/tests/test_settings.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.urls import reverse
+
+import requests_mock
+from django_webtest import WebTest
+
+
+def test_sentry_settings():
+    """
+    test that sentry settings are initialized
+    """
+
+    assert hasattr(settings, "SENTRY_CONFIG") is True
+    assert hasattr(settings, "SENTRY_DSN") is True
+
+
+@requests_mock.Mocker()
+class RosettaTests(WebTest):
+    def test_rosetta_redirect_fails_with_lazy_login_url(self, m):
+        response = self.app.get("/admin/rosetta/files/project/")
+
+        expected_login_url = (
+            reverse(settings.LOGIN_URL) + "?next=/admin/rosetta/files/project/"
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.location, expected_login_url)
