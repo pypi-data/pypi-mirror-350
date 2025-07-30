4592976fbcfc1302c093f58fe46b390d282cb235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:07:41 +0000

    Add package SylphietteIAMob/0.1.1/sylphietteiamob-0.1.1.tar.gz

---
+++ packages/SylphietteIAMob/sylphietteiamob-0.1.1.tar.gz/sylphietteiamob-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SylphietteIAMob",  # Nome único no PyPI!
+    version="0.1.1",
+    author="__token__",
+    author_email="seu@email.com",
+    description="Assistente IA estilo Sylphiette com fala, VTube Studio e Gemini.",
+    url="https://github.com/seuusuario/SylphietteIAMob",  # opcional
+    packages=find_packages(),
+    install_requires=[
+        "pyttsx3",
+        "google-generativeai",
+        "websockets",
+        "python-osc"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
