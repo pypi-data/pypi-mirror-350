33980cb303413096a318457a78cb48929546fdef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:43 +0000

    Add package squirrel-lang/0.2.1/squirrel_lang-0.2.1-cp38-cp38-win32.whl

---
+++ packages/squirrel-lang/squirrel_lang-0.2.1-cp38-cp38-win32.whl/squirrel/__init__.py
@@ -0,0 +1,15 @@
+from _squirrel import __version__, __author__, get_static_vm, StaticVM, SQUIRREL_VERSION
+from _squirrel import SQVM, compile, compile_bb
+from _squirrel.types import *  # noqa: F403
+
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "get_static_vm",
+    "StaticVM",
+    "SQVM",
+    "compile",
+    "compile_bb",
+    "SQUIRREL_VERSION",
+]
