821a619434fa76a3cc90a06592e79565287cc4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:49 +0000

    Add package NepTrainKit/2.1.1.dev37/neptrainkit-2.1.1.dev37-cp310-cp310-win32.whl

---
+++ packages/NepTrainKit/neptrainkit-2.1.1.dev37-cp310-cp310-win32.whl/NepTrainKit/core/custom_widget/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2024/11/28 22:45
+# @Author  : 兵
+# @email    : 1747193328@qq.com
+from .layout import FlowLayout
+from .label import ProcessLabel
+from .completer import CompleterModel, JoinDelegate, ConfigCompleter
+from .dialog import GetIntMessageBox, SparseMessageBox, ProgressDialog
+from .input import SpinBoxUnitInputFrame
+from .card_widget import (
+    CheckableHeaderCardWidget,
+    ShareCheckableHeaderCardWidget,
+    MakeDataCardWidget,
+)
+from .docker import MakeWorkflowArea
+from .search_widget import ConfigTypeSearchLineEdit
+from .settingscard import MyComboBoxSettingCard, DoubleSpinBoxSettingCard
+
+__all__ = [
+    "FlowLayout",
+    "ProcessLabel",
+    "CompleterModel",
+    "JoinDelegate",
+    "ConfigCompleter",
+    "GetIntMessageBox",
+    "SparseMessageBox",
+    "ProgressDialog",
+    "SpinBoxUnitInputFrame",
+    "CheckableHeaderCardWidget",
+    "ShareCheckableHeaderCardWidget",
+    "MakeDataCardWidget",
+    "MakeWorkflowArea",
+    "ConfigTypeSearchLineEdit",
+    "MyComboBoxSettingCard",
+    "DoubleSpinBoxSettingCard",
+]
