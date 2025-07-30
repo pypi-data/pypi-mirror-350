f5b8433d1665837a5b0cde09d718d99c722b68af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:36:20 +0000

    Add package spkrepo/0.2.8/spkrepo-0.2.8-py3-none-any.whl

---
+++ packages/spkrepo/spkrepo-0.2.8-py3-none-any.whl/spkrepo/config.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import os
+
+DEBUG = True
+TESTING = False
+SECRET_KEY = "secret-key"
+MAX_CONTENT_LENGTH = 170 * 1024 * 1024
+# Enable subdomain-based routing (False in local dev; True in prod)
+SUBDOMAIN_MATCHING = False
+
+# Application
+DATA_PATH = os.path.realpath("data")
+TEMPLATE_PATH = None
+GNUPG_TIMESTAMP_URL = "http://timestamp.synology.com/timestamp.php"
+GNUPG_PATH = None
+GNUPG_FINGERPRINT = "gnupg-fingerprint"
+
+# Security
+SECURITY_CONFIRMABLE = True
+SECURITY_REGISTERABLE = True
+SECURITY_RECOVERABLE = True
+SECURITY_CHANGEABLE = True
+SECURITY_PASSWORD_HASH = "sha512_crypt"
+SECURITY_PASSWORD_SALT = "password-salt"
+
+# SQLAlchemy
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "SPKREPO_SQLALCHEMY_DATABASE_URI",
+    "postgresql+psycopg2://spkrepo:spkrepo@localhost/spkrepo",
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Restful
+HTTP_BASIC_AUTH_REALM = "spkrepo"
+
+# Migrate
+MIGRATE_DIRECTORY = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "migrations")
+)
+
+# Cache
+CACHE_TYPE = "null"
+
+# Debug Toolbar
+DEBUG_TB_INTERCEPT_REDIRECTS = False
