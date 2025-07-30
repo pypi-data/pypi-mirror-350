5b6afbb337222dcf5bf587fbc2d1055d869390e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:36 +0000

    Add package swanlab/0.5.9/swanlab-0.5.9.tar.gz

---
+++ packages/swanlab/swanlab-0.5.9.tar.gz/swanlab-0.5.9/swanlab/__init__.py
@@ -0,0 +1,36 @@
+# 导出初始化函数和log函数，以及一些数据处理模块
+from .data import *
+from .env import SwanLabEnv
+from .package import get_package_version
+from .swanlab_settings import Settings
+from .sync import sync_wandb, sync_tensorboardX, sync_tensorboard_torch, sync_mlflow
+from .api.openapi import OpenApi
+
+# 设置默认环境变量
+SwanLabEnv.set_default()
+# 检查当前需要检查的环境变量
+SwanLabEnv.check()
+
+__version__ = get_package_version()
+
+__all__ = [
+    "login",
+    "Settings",
+    "merge_settings",
+    "init",
+    "log",
+    "register_callbacks",
+    "finish",
+    "Audio",
+    "Image",
+    "Object3D",
+    "Molecule",
+    "Text",
+    "Run",
+    "State",
+    "get_run",
+    "get_config",
+    "config",
+    "OpenApi",
+    "__version__",
+]
