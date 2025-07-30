a8074d48d7578c06cb908aff34d5d3c9988711e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:07:45 +0000

    Add package SylphietteIAMob/0.1.2/sylphietteiamob-0.1.2.tar.gz

---
+++ packages/SylphietteIAMob/sylphietteiamob-0.1.2.tar.gz/sylphietteiamob-0.1.2/Sylphiette/base.py
@@ -0,0 +1,54 @@
+from Sylphiette import Gemini
+from Sylphiette import Voices
+from Sylphiette import Terminal
+
+
+class AI:
+    def __init__(
+        self, name : str,
+        personalidade : str,
+        api_key : str, fala = True,
+        model='gemini-1.5-flash',
+        debug = False
+        ) -> None:
+        
+        Voices.conigureVoice()
+        self.name = name
+        self.personalidade = personalidade + f"Agora fale como {self.name} em tom de conversação curta sem *, minha fala: "
+        self.model = model
+        self.fala = fala
+        self.debug = debug
+        self.api = api_key
+        Gemini.gerarModelo(model, api_key)
+        self.terminal = Terminal.Terminal(self)
+            
+    def resposta(self, prompt : str) -> str:
+        prompt_completo = self.personalidade + prompt
+        if self.debug:
+            print(prompt_completo)
+        respost = Gemini.Gresponde(prompt_completo)
+        print(f"\033[1;33m{self.name}: \033[0m" + respost)
+        if self.fala:
+            Voices.falar(respost)
+        return respost
+    
+    def run(self) -> None:
+        print(f"""
+              \033[1;33mModelo: {self.name}\033[0m
+
+              """)
+        while True:
+            pgt = input("\033[1;32mUsuario: \033[0m")
+            if pgt == "SAIR CHAT":
+                break
+            print("")
+            self.resposta(pgt)
+            
+    def getName(self) -> str:
+        return self.name
+            
+    def runTerminal(self) -> str:
+        self.terminal.run()
+
+
+    
