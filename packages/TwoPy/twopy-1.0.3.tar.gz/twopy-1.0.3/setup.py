d28b0d1cfeb503734955cfc1d8ed2e0555393fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:07:43 +0000

    Add package TwoPy/1.0.3/twopy-1.0.3.tar.gz

---
+++ packages/TwoPy/twopy-1.0.3.tar.gz/twopy-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TwoPy",
+    version="1.0.3",
+    author="starfal8k",
+    include_package_data=True,
+    install_requires=[
+        'argparse',
+        'cryptography'
+    ],
+    description="Python project builder with encryption capability.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://example.com/twopy",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
