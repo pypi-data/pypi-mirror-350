22746f719a184aabcee73f404ca95709cb58495f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:32 +0000

    Add package mytrade/0.0.3/mytrade-0.0.3.tar.gz

---
+++ packages/mytrade/mytrade-0.0.3.tar.gz/mytrade-0.0.3/setup.py
@@ -0,0 +1,37 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+VERSION = '0.0.3'
+DESCRIPTION = ''
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="mytrade",
+    version=VERSION,
+    author="",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description="",
+    packages=find_packages(where='.', exclude=(), include=('*',)),
+    include_package_data=True,
+    install_requires=[
+
+    ],
+    keywords=['python', 'mytrade'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ],
+    package_data={
+        'mytrade': ['*'],
+    },
+)
