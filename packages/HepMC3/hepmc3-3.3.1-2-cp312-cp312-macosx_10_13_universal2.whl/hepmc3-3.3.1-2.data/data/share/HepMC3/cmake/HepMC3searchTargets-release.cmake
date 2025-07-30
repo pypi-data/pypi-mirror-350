610b00144f3aa156f0e405e6a0aa6a0dac0514a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:28 +0000

    Add package HepMC3/3.3.1/hepmc3-3.3.1-2-cp312-cp312-macosx_10_13_universal2.whl

---
+++ packages/HepMC3/hepmc3-3.3.1-2-cp312-cp312-macosx_10_13_universal2.whl/hepmc3-3.3.1-2.data/data/share/HepMC3/cmake/HepMC3searchTargets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RELEASE".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "HepMC3::search" for configuration "RELEASE"
+set_property(TARGET HepMC3::search APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(HepMC3::search PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libHepMC3search.5.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libHepMC3search.5.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets HepMC3::search )
+list(APPEND _cmake_import_check_files_for_HepMC3::search "${_IMPORT_PREFIX}/lib/libHepMC3search.5.dylib" )
+
+# Import target "HepMC3::search_static" for configuration "RELEASE"
+set_property(TARGET HepMC3::search_static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(HepMC3::search_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libHepMC3search-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets HepMC3::search_static )
+list(APPEND _cmake_import_check_files_for_HepMC3::search_static "${_IMPORT_PREFIX}/lib/libHepMC3search-static.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
