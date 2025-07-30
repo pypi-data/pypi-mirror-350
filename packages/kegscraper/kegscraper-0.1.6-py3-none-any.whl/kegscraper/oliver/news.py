985b055b6153025aec46e6429efd5a82b3840171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:09 +0000

    Add package kegscraper/0.1.6/kegscraper-0.1.6-py3-none-any.whl

---
+++ packages/kegscraper/kegscraper-0.1.6-py3-none-any.whl/kegscraper/oliver/news.py
@@ -0,0 +1,11 @@
+from . import utils
+
+
+def get_news() -> dict:
+    """
+    Fetch news from the api as JSON. Will be parsed later
+    """
+
+    data = utils.api_fetch("news")
+    # Parse this...
+    return data
