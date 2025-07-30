8b335abf645945ac2a9ff29d873ee964098332f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:58 +0000

    Add package wsidata/0.6.2/wsidata-0.6.2-py3-none-any.whl

---
+++ packages/wsidata/wsidata-0.6.2-py3-none-any.whl/wsidata/__init__.py
@@ -0,0 +1,31 @@
+"""Data structures and I/O functions for whole-slide images (WSIs)."""
+
+__version__ = "0.6.2"
+
+import wsidata.dataset as dataset
+from ._model import WSIData, TileSpec
+from .io import open_wsi, agg_wsi
+from .accessors import (
+    register_wsidata_accessor,
+    FetchAccessor,
+    IterAccessor,
+    DatasetAccessor,
+)
+from .reader import get_reader, SlideProperties
+from ._normalizer import ColorNormalizer
+
+
+__all__ = [
+    "open_wsi",
+    "agg_wsi",
+    "dataset",
+    "WSIData",
+    "TileSpec",
+    "register_wsidata_accessor",
+    "FetchAccessor",
+    "IterAccessor",
+    "DatasetAccessor",
+    "get_reader",
+    "SlideProperties",
+    "ColorNormalizer",
+]
