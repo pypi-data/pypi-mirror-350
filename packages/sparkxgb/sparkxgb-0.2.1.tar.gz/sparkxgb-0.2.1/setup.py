a10d03ae9ab5beb305dc08c08ff50e5c2a4f3731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:16 +0000

    Add package sparkxgb/0.2.1/sparkxgb-0.2.1.tar.gz

---
+++ packages/sparkxgb/sparkxgb-0.2.1.tar.gz/sparkxgb-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sparkxgb',
+    version='0.2.1',
+    description='sparkxgb module (fix)',
+    url='',
+    author='unk',
+    author_email='unk@gmail.com',
+    liscence='unk',
+    packages=['sparkxgb'],
+    zip_safe=False,
+    install_requires=[
+        'pyspark==3.5.4',
+    ]
+)
