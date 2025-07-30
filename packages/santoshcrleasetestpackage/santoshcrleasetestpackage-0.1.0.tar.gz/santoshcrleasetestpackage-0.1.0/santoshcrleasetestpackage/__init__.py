14c1133ead825a34dcd71b0f8d7f95e7152efd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:16 +0000

    Add package santoshcrleasetestpackage/0.1.0/santoshcrleasetestpackage-0.1.0.tar.gz

---
+++ packages/santoshcrleasetestpackage/santoshcrleasetestpackage-0.1.0.tar.gz/santoshcrleasetestpackage-0.1.0/santoshcrleasetestpackage/__init__.py
@@ -0,0 +1,2 @@
+def testfun():
+    return "This is just a test function"
---
+++ packages/santoshcrleasetestpackage/santoshcrleasetestpackage-0.1.0.tar.gz/santoshcrleasetestpackage-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="santoshcrleasetestpackage",
+    version="0.1.0",
+    author="Santosh Acharya",
+    description="This is a demo python package by santosh",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
