7d453f144fe5781b6a8f41fb75fe9a707a8c1386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:46 +0000

    Add package jitprofiler/0.1.0/jitprofiler-0.1.0.tar.gz

---
+++ packages/jitprofiler/jitprofiler-0.1.0.tar.gz/jitprofiler-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jitprofiler",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A package for generating automated data profiling reports.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "plotly",
+        "scipy",
+    ],
+    python_requires=">=3.6",
+)
