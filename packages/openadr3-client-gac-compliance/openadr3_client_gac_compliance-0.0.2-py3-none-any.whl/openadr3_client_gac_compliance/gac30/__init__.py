5d274d812a3114dd81e89539a6450fd3146f6483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:45 +0000

    Add package openadr3-client-gac-compliance/0.0.2/openadr3_client_gac_compliance-0.0.2-py3-none-any.whl

---
+++ packages/openadr3-client-gac-compliance/openadr3_client_gac_compliance-0.0.2-py3-none-any.whl/openadr3_client_gac_compliance/gac30/__init__.py
@@ -0,0 +1,4 @@
+# This module imports all the GAC 3.0 compliance validators.
+
+import openadr3_client_gac_compliance.gac30.program_gac_compliant  # noqa: F401
+import openadr3_client_gac_compliance.gac30.event_gac_compliant  # noqa: F401
---
+++ packages/openadr3-client-gac-compliance/openadr3_client_gac_compliance-0.0.2-py3-none-any.whl/openadr3_client_gac_compliance/gac30/program_gac_compliant.py
@@ -0,0 +1,38 @@
+"""Module which implements GAC compliance validators for the program OpenADR3 types."""
+
+from openadr3_client.models.program.program import Program
+from openadr3_client.models.model import ValidatorRegistry, Model as ValidatorModel
+
+import re
+
+
+@ValidatorRegistry.register(Program, ValidatorModel())
+def program_gac_compliant(self: Program) -> Program:
+    """Enforces that the program is GAC compliant.
+
+    GAC enforces the following constraints for programs:
+    - The program must have a retailer name
+    - The retailer name must be an EAN13 identifier.
+    - The program MUST have a programType.
+    - The programType MUST equal "DSO_CPO_INTERFACE-x.x.x, where x.x.x is the version as defined in the GAC specification.
+    - The program MUST have bindingEvents set to True.
+    are allowed there.
+    """
+    program_type_regex = r"^DSO_CPO_INTERFACE-(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+
+    if self.retailer_name is None:
+        raise ValueError("The program must have a retailer name.")
+    if not re.fullmatch(r"\d{13}", self.retailer_name):
+        raise ValueError("The retailer name must be an EAN13 identifier.")
+
+    if self.program_type is None:
+        raise ValueError("The program must have a program type.")
+    if not re.fullmatch(program_type_regex, self.program_type):
+        raise ValueError(
+            "The program type must follow the format DSO_CPO_INTERFACE-x.x.x."
+        )
+
+    if self.binding_events is False:
+        raise ValueError("The program must have bindingEvents set to True.")
+
+    return self
