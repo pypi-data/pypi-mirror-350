64dcad137a45663ada8d6d427ec9140760d3e6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:36:39 +0000

    Add package liblaf-melon/0.2.6/liblaf_melon-0.2.6.tar.gz

---
+++ packages/liblaf-melon/liblaf_melon-0.2.6.tar.gz/liblaf_melon-0.2.6/src/liblaf/melon/io/pyvista/unstructured_grid/convert/_convert.py
@@ -0,0 +1,5 @@
+import pyvista as pv
+
+from liblaf.melon.io.abc import ConverterDispatcher
+
+as_unstructured_grid = ConverterDispatcher(pv.UnstructuredGrid)
---
+++ packages/liblaf-melon/liblaf_melon-0.2.6.tar.gz/liblaf_melon-0.2.6/src/liblaf/melon/utils/_typing.py
@@ -0,0 +1,62 @@
+from collections.abc import Sequence
+from typing import Any
+
+
+def is_instance(obj: Any, prefix: str, name: str) -> bool:
+    return is_subclass(type(obj), prefix, name)
+
+
+def is_subclass(cls: type, prefix: str, name: str) -> bool:
+    return cls.__module__.startswith(prefix) and cls.__name__ == name
+
+
+def is_array_like(obj: Any) -> bool:
+    # TODO: Implement a more robust check for array-like objects.
+    return (
+        is_jax(obj)
+        or is_numpy(obj)
+        or is_torch(obj)
+        or is_warp(obj)
+        or isinstance(obj, Sequence)
+    )
+
+
+# ----------------------------------- numpy ---------------------------------- #
+
+
+def is_jax(obj: Any) -> bool:
+    return is_instance(obj, "jax", "Array")
+
+
+def is_numpy(obj: Any) -> bool:
+    return is_instance(obj, "numpy", "ndarray")
+
+
+def is_torch(obj: Any) -> bool:
+    return is_instance(obj, "torch", "Tensor")
+
+
+def is_warp(obj: Any) -> bool:
+    return is_instance(obj, "warp", "array")
+
+
+# ---------------------------------- pyvista --------------------------------- #
+def is_image_data(obj: Any) -> bool:
+    return is_instance(obj, "pyvista", "ImageData")
+
+
+def is_point_set(obj: Any) -> bool:
+    return is_instance(obj, "pyvista", "PointSet")
+
+
+def is_poly_data(obj: Any) -> bool:
+    return is_instance(obj, "pyvista", "PolyData")
+
+
+def is_unstructured_grid(obj: Any) -> bool:
+    return is_instance(obj, "pyvista", "UnstructuredGrid")
+
+
+# ---------------------------------- trimesh --------------------------------- #
+def is_trimesh(obj: Any) -> bool:
+    return is_instance(obj, "trimesh", "Trimesh")
