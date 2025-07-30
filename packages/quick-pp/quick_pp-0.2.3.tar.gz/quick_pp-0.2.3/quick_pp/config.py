4f972e6a82698191e535fe0d5939883f00d14e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:35 +0000

    Add package quick-pp/0.2.3/quick_pp-0.2.3.tar.gz

---
+++ packages/quick-pp/quick_pp-0.2.3.tar.gz/quick_pp-0.2.3/quick_pp/config.py
@@ -0,0 +1,94 @@
+class Config(object):
+
+    RESSUM_CUTOFFS = dict(
+        VSHALE=0.4,
+        PHIT=0.05,
+        SWT=0.95
+    )
+
+    VARS = dict(
+        GR=dict(name="Gamma Ray", unit="GAPI", min=0, max=200),
+        RT=dict(name="True Resistivity", unit="ohm.m", min=0, max=2000),
+        NPHI=dict(name="Neutron Porosity", unit="v/v", min=-.05, max=0.45),
+        RHOB=dict(name="Bulk Density", unit="g/cc", min=1.85, max=2.85),
+        VSAND=dict(name="Sand Volume", unit="v/v", min=0, max=1),
+        VSILT=dict(name="Silt Volume", unit="v/v", min=0, max=1),
+        VCLAY=dict(name="Clay Volume", unit="v/v", min=0, max=1),
+        VSHALE=dict(name="Shale Volume", unit="v/v", min=0, max=1),
+        PHIT=dict(name="Total Porosity", unit="v/v", min=0, max=0.5),
+        PHIE=dict(name="Effective Porosity", unit="v/v", min=0, max=0.5),
+        SWT=dict(vname="Total Water Saturation", unit="v/v", min=0, max=1),
+        SWE=dict(vname="Effective Water Saturation", unit="v/v", min=0, max=1),
+        PERM=dict(name="Permeability", unit="mD", min=0.01, max=100000)
+    )
+
+    SSC_ENDPOINTS = dict(
+        FLUID_POINT=(1.0, 1.0),
+        DRY_SAND_POINT=(-0.02, 2.65),
+        DRY_SILT_POINT=(None, 2.68),  # None means it will be calculated
+        DRY_CLAY_POINT=(None, 2.71),  # None means it will be calculated
+        WET_CLAY_POINT=(None, None),  # None means it will be calculated
+        SILT_LINE_ANGLE=119  # Deg angle from horizontal
+    )
+
+    CARB_NEU_DEN_ENDPOINTS = dict(
+        FLUID_POINT=(1.0, 1.0),
+        DRY_CALC_POINT=(0.0, 2.71),
+        DRY_DOLO_POINT=(0.01, 2.87),
+        DRY_CLAY_POINT=(0.24, 2.78),
+    )
+
+    CARB_DEN_PEF_ENDPOINTS = dict(
+        FLUID_POINT=(1.0, 1.0),
+        DRY_CALC_POINT=(5.08, 2.71),
+        DRY_DOLO_POINT=(3.14, 2.87),
+        DRY_CLAY_POINT=(2.2, 2.78),
+    )
+
+    MINERALS_LOG_VALUE = {
+        'GR_QUARTZ': 0.0,
+        'NPHI_QUARTZ': -0.02,
+        'RHOB_QUARTZ': 2.64,
+        'DTC_QUARTZ': 52.9,
+        'PEF_QUARTZ': 1.8,
+
+        'GR_CALCITE': 0.0,
+        'NPHI_CALCITE': 0.0,
+        'RHOB_CALCITE': 2.71,
+        'DTC_CALCITE': 49.7,
+        'PEF_CALCITE': 5.1,
+
+        'GR_DOLOMITE': 0.0,
+        'NPHI_DOLOMITE': 0.01,
+        'RHOB_DOLOMITE': 2.85,
+        'DTC_DOLOMITE': 43.5,
+        'PEF_DOLOMITE': 3.1,
+
+        'GR_SILT': 80.0,
+        'NPHI_SILT': 0.1,
+        'RHOB_SILT': 2.68,
+        'DTC_SILT': 100.0,
+        'PEF_SILT': 2.0,
+
+        # Kaolinite
+        'GR_SH': 130.0,
+        'NPHI_SH': 0.34,
+        'RHOB_SH': 2.41,
+        'DTC_SH': 143.0,
+        'PEF_SH': 1.8,
+
+        'GR_MUD': 0.0,
+        'NPHI_MUD': 1.0,
+        'RHOB_MUD': 1.3,
+        'DTC_MUD': 180.0,
+        'PEF_MUD': 0.0
+    }
+
+    @staticmethod
+    def vars_units(data):
+        """Return list of units for variables exist in data.columns.
+
+        Returns:
+            list: List of units for variables exist in data.columns.
+        """
+        return {k: v['unit'] for k, v in Config.VARS.items() if k in data.columns and 'unit' in v.keys()}
---
+++ packages/quick-pp/quick_pp-0.2.3.tar.gz/quick_pp-0.2.3/quick_pp/porosity.py
@@ -0,0 +1,341 @@
+import numpy as np
+
+from quick_pp.config import Config
+from quick_pp.utils import length_a_b, line_intersection
+
+
+def normalize_volumetric(phit, **volumetrics):
+    """Normalize lithology given total porosity.
+
+    Args:
+        phit (float): Total porosity in fraction (v/v).
+        **volumetrics: Keyword arguments representing volumetric fractions (v/v).
+
+    Returns:
+        dict: Normalized volumetric fractions.
+    """
+    # Normalize the volumetrics
+    vmatrix = 1 - phit
+    normalized_volumetrics = {key: value * vmatrix for key, value in volumetrics.items()}
+
+    return normalized_volumetrics
+
+
+def effective_porosity(phit, phi_shale, vshale):
+    """
+    Computes effective porosity from total porosity, total porosity of shale and shale volume.
+
+    Parameters
+    ----------
+    phit : float
+        Total porosity [fraction].
+    phi_vsh : float
+        Total porosity of shale [fraction].
+    vshale : float
+        Shale volume [fraction].
+
+    Returns
+    -------
+    porosity : float
+        Effective porosity [fraction].
+
+    """
+    return phit - (vshale * phi_shale)
+
+
+def clay_porosity(rho_clw: np.ndarray, rho_dry_clay: float = 2.72, rho_fluid: float = 1.0):
+    """Calculate clay porosity given bulk density of wet clay line.
+
+    Args:
+        rho_clw (float): Bulk density of wet clay line.
+        rho_dry_clay (float, optional): Bulk density of dry clay. Defaults to 2.72.
+        rho_fluid (float, optional): Bulk density of fluid. Defaults to 1.0.
+
+    Returns:
+        float: Clay porosity.
+    """
+    return (rho_dry_clay - rho_clw) / (rho_dry_clay - rho_fluid)
+
+
+def shale_porosity(vshale, phi_shale):
+    """
+    Computes shale porosity from shale volume and total porosity of shale.
+
+    Parameters
+    ----------
+    vshale : float
+        Shale volume [fraction].
+    phi_vsh : float
+        Total porosity of shale [fraction].
+
+    Returns
+    -------
+    porosity : float
+        Shale porosity [fraction].
+
+    """
+    return vshale * phi_shale
+
+
+def rho_matrix(vsand=0, vsilt=0, vclay=0, vcalc=0, vdolo=0, vheavy=0,
+               rho_sand: float = 0, rho_silt: float = 0, rho_clay: float = 0,
+               rho_calc: float = 0, rho_dolo: float = 0, rho_heavy: float = 0):
+    """Estimate average matrix density based on dry sand, dry silt, dry clay, dry calcite and
+    dry dolomite volume and density of each.
+
+    Args:
+        vsand (float): Volume of sand.
+        vsilt (float): Volume of silt.
+        vclay (float): Volume of clay.
+        vcalc (float): Volume of calcite.
+        vdolo (float): Volume of dolomite.
+        vheavy (float): Volume of heavy minerals.
+        rho_sand (float, optional): Density of sand in g/cc. Defaults to None.
+        rho_silt (float, optional): Density of silt in g/cc. Defaults to None.
+        rho_clay (float, optional): Density of clay in g/cc. Defaults to None.
+        rho_calc (float, optional): Density of calcite in g/cc. Defaults to None.
+        rho_dolo (float, optional): Density of dolomite in g/cc. Defaults to None.
+        rho_heavy (float, optional): Density of heavy minerals in g/cc. Defaults to 0.
+
+    Returns:
+        float: Matrix density in g/cc.
+    """
+    minerals_log_value = Config.MINERALS_LOG_VALUE
+    rho_sand = rho_sand or minerals_log_value['RHOB_QUARTZ']
+    rho_silt = rho_silt or minerals_log_value['RHOB_SILT']
+    rho_clay = rho_clay or minerals_log_value['RHOB_SH']
+    rho_calc = rho_calc or minerals_log_value['RHOB_CALCITE']
+    rho_dolo = rho_dolo or minerals_log_value['RHOB_DOLOMITE']
+    return vsand * rho_sand + vsilt * rho_silt + vclay * rho_clay + \
+        vcalc * rho_calc + vdolo * rho_dolo + vheavy * rho_heavy
+
+
+def density_porosity(rhob, rho_matrix, rho_fluid: float = 1.0):
+    """Computes density porosity from bulk, matrix and fluid densities
+
+    Args:
+        rhob (float): Bulk density log in g/cc.
+        rho_matrix (float): Matrix density in g/cc.
+        rho_fluid (float, optional): Density of fluid in g/cc. Defaults to 1.0 g/cc.
+
+    Returns:
+        float: Density porosity [fraction]
+    """
+    return (rho_matrix - rhob) / (rho_matrix - rho_fluid)
+
+
+def dt_matrix(vsand=0, vsilt=0, vclay=0, vcalc=0, vdolo=0, vheavy=0,
+              dt_sand: float = 0, dt_silt: float = 0, dt_clay: float = 0,
+              dt_calc: float = 0, dt_dolo: float = 0, dt_heavy: float = 0):
+    """Estimate average matrix sonic transit time based on dry sand, dry silt dry calcite and
+    dry dolomite volume and transit time of each.
+
+    Args:
+        vsand (float): Volume of sand.
+        vsilt (float): Volume of silt.
+        vclay (float): Volume of clay.
+        vcalc (float): Volume of calcite.
+        vdolo (float): Volume of dolomite.
+        vheavy (float): Volume of heavy minerals.
+        dt_sand (float, optional): Sonic transit time of sand in us/ft. Defaults to None.
+        dt_silt (float, optional): Sonic transit time of silt in us/ft. Defaults to None.
+        dt_clay (float, optional): Sonic transit time of clay in us/ft. Defaults to None.
+        dt_calc (float, optional): Sonic transit time of calcite in us/ft. Defaults to None.
+        dt_dolo (float, optional): Sonic transit time of dolomite in us/ft. Defaults to None.
+        dt_heavy (float, optional): Sonic transit time of heavy minerals in us/ft. Defaults to 0.
+
+    Returns:
+        float: Matrix sonic transit time in us/ft.
+    """
+    minerals_log_value = Config.MINERALS_LOG_VALUE
+    dt_sand = dt_sand or minerals_log_value['DTC_QUARTZ']
+    dt_silt = dt_silt or minerals_log_value['DTC_SILT']
+    dt_clay = dt_clay or minerals_log_value['DTC_SH']
+    dt_calc = dt_calc or minerals_log_value['DTC_CALCITE']
+    dt_dolo = dt_dolo or minerals_log_value['DTC_DOLOMITE']
+    return vsand * dt_sand + vsilt * dt_silt + vclay * dt_clay + vcalc * dt_calc + vdolo * dt_dolo + vheavy * dt_heavy
+
+
+def sonic_porosity_wyllie(dt, dt_matrix, dt_fluid):
+    """
+    Computes sonic porosity based on Wyllie's equation from interval, matrix, and fluid transit time.
+
+    Parameters
+    ----------
+    dt : float
+        Interval transit time [us/ft].
+    dt_matrix : float
+        Matrix transit time [us/ft]. Sandstone: 51-55, Limestone: 43-48, Dolomite: 43-39, Shale: 60-170.
+    dt_fluid : float
+        Fluid transit time [us/ft]. Water: 190, Oil: 240, Gas: 630.
+
+    Returns
+    -------
+    porosity : float
+        Sonic porosity [fraction].
+
+    """
+    return (dt - dt_matrix) / (dt_fluid - dt_matrix)
+
+
+def sonic_porosity_hunt_raymer(dt, dt_matrix, dt_fluid):
+    """
+    Computes sonic porosity based on Hunt-Raymer's equation from interval, matrix and transit time.
+
+    Parameters
+    ----------
+    dt : float
+        Interval transit time [us/ft].
+    dt_matrix : float
+        Matrix transit time [us/ft]. Sandstone: 51-55, Limestone: 43-48, Dolomite: 43-39, Shale: 60-170.
+    dt_fluid : float
+        Fluid transit time [us/ft]. Water: 190, Oil: 240, Gas: 630.
+
+    Returns
+    -------
+    porosity : float
+        Sonic porosity [fraction].
+
+    """
+    c = (dt_matrix / (2 * dt_fluid)) - 1
+    return - c - (c**2 + (dt_matrix / dt) - 1)**0.5
+
+
+def neu_den_xplot_poro_pt(
+        nphi: float, rhob: float, model: str = 'ssc',
+        dry_min1_point: tuple = (),
+        dry_silt_point: tuple = (),
+        dry_clay_point: tuple = (),
+        fluid_point: tuple = (1.0, 1.0)):
+    """Calculate porosity given a pair of neutron porosity and bulk density data point.
+
+    Args:
+        nphi (float): Neutron porosity log.
+        rhob (float): Bulk density log.
+        model (str, optional): Lithology model, either 'ssc' (Sand Silt Clay) or 'ss' (Sand Shale). Defaults to 'ssc'.
+        reservoir (bool, optional): Either in reservoir or non-reservoir section. Defaults to False.
+        dry_min1_point (tuple): Neutron porosity and bulk density of mineral 1 point.
+        dry_silt_point (tuple): Neutron porosity and bulk density of dry silt point.
+        dry_clay_point (tuple): Neutron porosity and bulk density of dry clay point.
+        fluid_point (tuple): Neutron porosity and bulk density of fluid point. Defaults to (1.0, 1.0).
+
+    Returns:
+        float: Total porosity.
+    """
+    assert model in ['ssc', 'ss', 'carb'], "Please specify either 'ssc', 'ss' or 'carb' model."
+
+    A = dry_min1_point
+    B = dry_silt_point
+    C = dry_clay_point
+    D = fluid_point
+
+    phit = []
+    if model == 'ssc':
+        # Check if the point is in the reservoir or non-reservoir section
+        thold_pt = line_intersection((A, C), (D, B))
+        thold_line = length_a_b(thold_pt, A)
+        proj_pt = line_intersection((A, C), (D, (nphi, rhob)))
+        proj_line = length_a_b(proj_pt, A)
+        if proj_line < thold_line:
+            m = (A[1] - B[1]) / (A[0] - B[0])
+        else:
+            m = (C[1] - B[1]) / (C[0] - B[0])
+
+        c = rhob - m * nphi
+        iso_poro_pt = line_intersection(((0, c), (nphi, rhob)), (D, B))
+        iso_poro_line = length_a_b(iso_poro_pt, B)
+        poro_line = length_a_b(D, B)
+        phit = iso_poro_line / poro_line
+    else:
+        m = (A[1] - C[1]) / (A[0] - C[0])
+        c = rhob - m * nphi
+        iso_poro_pt = line_intersection(((0, c), (nphi, rhob)), (D, A))
+        iso_poro_line = length_a_b(iso_poro_pt, A)
+        poro_line = length_a_b(D, A)
+
+    phit = iso_poro_line / poro_line
+    return phit
+
+
+def neu_den_xplot_poro(nphi, rhob, model: str = 'ssc',
+                       dry_min1_point: tuple = (),
+                       dry_silt_point: tuple = (),
+                       dry_clay_point: tuple = (),
+                       fluid_point: tuple = (1.0, 1.0)):
+    """Calculate porosity given neutron porosity and bulk density logs.
+
+    Args:
+        nphi (float): Neutron porosity log.
+        rhob (float): Bulk density log.
+        model (str, optional): Lithology model, either 'ssc' (Sand Silt Clay), 'ss' (Sand Shale) or 'carb' (Carbonate).
+                               Defaults to 'ssc'.
+        reservoir (bool, optional): Either in reservoir or non-reservoir section. Defaults to False.
+        dry_min1_point (tuple): Neutron porosity and bulk density of dry min1 point. Defaults to None.
+        dry_silt_point (tuple): Neutron porosity and bulk density of dry silt point. Defaults to None.
+        dry_clay_point (tuple): Neutron porosity and bulk density of dry clay point. Defaults to None.
+        fluid_point (tuple): Neutron porosity and bulk density of fluid point. Defaults to (1.0, 1.0).
+
+    Returns:
+        float: Total porosity.
+    """
+    assert model in ['ssc', 'ss', 'carb'], "Please specify either 'ssc', 'ss' or 'carb' model."
+
+    A = dry_min1_point
+    B = dry_silt_point
+    C = dry_clay_point
+    D = fluid_point
+    E = list(zip(nphi, rhob))
+
+    phit = np.empty(0)
+    for i, point in enumerate(E):
+        if model == 'ssc':
+            phit = np.append(phit, neu_den_xplot_poro_pt(point[0], point[1], 'ssc', A, B, C, D))
+        else:
+            phit = np.append(phit, neu_den_xplot_poro_pt(point[0], point[1], 'ss', A, (0, 0), C, D))
+
+    return phit
+
+
+def porosity_correction_averaging(nphi, rhob, rho_ma=2.65, method='weighted'):
+    """Correct porosity using averaging method.
+    Weighted average: (2 * dphi + nphi) / 3
+    Arithmetic average: (dphi + nphi) / 2
+    Gaymard average: sqrt((dphi**2 + nphi**2) / 2)
+    Gas average: sqrt((dphi**2 + nphi**2) / 2)
+
+    Args:
+        nphi (float): Neutron porosity.
+        dphi (float): Density porosity.
+        method (str, optional): Averaging method selection from 'weighted', 'arithmetic' or 'gaymard'.
+         Defaults to 'weighted'.
+
+    Returns:
+        float: Corrected porosity.
+    """
+    assert method in ['weighted', 'arithmetic', 'gaymard', 'gas'], "method must be either \
+        'weighted', 'arithmetic', 'gaymard' or 'gas"
+    dphi = density_porosity(rhob, rho_ma, 1.0)
+    if method == 'weighted':
+        return (2 * dphi + nphi) / 3
+    elif method == 'arithmetic':
+        return (dphi + nphi) / 2
+    elif method == 'gaymard':
+        return np.sqrt((dphi**2 + nphi**2) / 2)
+    elif method == 'gas':
+        return ((nphi**2 + dphi**2) / 2)**0.5
+
+
+def porosity_trend(tvdss, unit='ft'):
+    """Calculate porosity trend based on TVDSS (Schmoker, 1982)
+
+    Args:
+        tvdss (float): True Vertical Depth Subsea.
+
+    Returns:
+        float: Porosity trend.
+    """
+    assert unit in ['ft', 'm'], 'Please specify either ft or m as unit.'
+    if unit == 'ft':
+        return 41.73 * np.exp(-tvdss / 8197)
+    else:
+        return 41.73 * np.exp(-tvdss / 2498)
