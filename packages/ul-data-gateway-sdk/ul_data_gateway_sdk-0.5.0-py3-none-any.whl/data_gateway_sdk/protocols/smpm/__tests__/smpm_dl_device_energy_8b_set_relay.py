44950ead974cf38f4c8adfb404c9b7d739b623a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:38 +0000

    Add package ul-data-gateway-sdk/0.5.0/ul_data_gateway_sdk-0.5.0-py3-none-any.whl

---
+++ packages/ul-data-gateway-sdk/ul_data_gateway_sdk-0.5.0-py3-none-any.whl/data_gateway_sdk/protocols/smpm/__tests__/smpm_dl_device_energy_8b_set_relay.py
@@ -0,0 +1,11 @@
+from data_gateway_sdk.protocols.smpm.smpm_dl_device_energy_8b_set_relay import SmpmDlDeviceEnergy8BSetRelayData, SmpmDlDeviceEnergy8bSetRelayId
+from data_gateway_sdk.utils.buf_ref import BufRef
+
+
+def test_smpm_dl_device_energy_8b_set_relay() -> None:
+    case_serialized = bytes.fromhex("aa01000000000000")
+    assert SmpmDlDeviceEnergy8BSetRelayData(state=False, relay_id=SmpmDlDeviceEnergy8bSetRelayId.ALL) == SmpmDlDeviceEnergy8BSetRelayData.parse(BufRef(case_serialized))  # noqa: E501
+    assert case_serialized == SmpmDlDeviceEnergy8BSetRelayData.serialize(SmpmDlDeviceEnergy8BSetRelayData(state=False, relay_id=SmpmDlDeviceEnergy8bSetRelayId.ALL))  # noqa: E501
+    case_serialized = bytes.fromhex("aa71000000000000")  # overflow
+    assert SmpmDlDeviceEnergy8BSetRelayData(state=False, relay_id=SmpmDlDeviceEnergy8bSetRelayId.RELAY_ID_7) == SmpmDlDeviceEnergy8BSetRelayData.parse(BufRef(case_serialized))  # noqa: E501
+    assert case_serialized == SmpmDlDeviceEnergy8BSetRelayData.serialize(SmpmDlDeviceEnergy8BSetRelayData(relay_id=SmpmDlDeviceEnergy8bSetRelayId.RELAY_ID_7, state=False))  # noqa: E501
